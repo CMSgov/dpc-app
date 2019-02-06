9ff97e9355f999a480fb73e9601f477a31d4ceec
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-02-06 16:32:34 -0500

    DPC-74: Endpoint now returns a 204 status, along with a Content-Location header of the fake job.
    This still needs to be improved as it's using hardcoded values and allows for any parameters to be passed.
    We've also improved some of the testing for both the capabilities statement as well as the resources.
    
    The maven test integration is wonky, it's only running the Capabilities test, even though IntelliJ runs everything.

---
+++ src/main/java/gov/cms/dpc/resources/v1/BaseResource.java
@@ -0,0 +1,38 @@
+package gov.cms.dpc.resources.v1;
+
+import ca.uhn.fhir.parser.IParser;
+import gov.cms.dpc.core.Capabilities;
+import gov.cms.dpc.resources.AbstractBaseResource;
+import gov.cms.dpc.resources.AbstractGroupResource;
+
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+
+
+@Path("/v1")
+public class BaseResource extends AbstractBaseResource {
+
+    private final IParser parser;
+    private final AbstractGroupResource gr;
+
+    @Inject
+    public BaseResource(IParser jsonParser, GroupResource gr) {
+        this.parser = jsonParser;
+        this.gr = gr;
+    }
+
+    @Override
+    public String version() {
+        return "Version 1";
+    }
+
+    @Override
+    public String metadata() {
+        return parser.encodeResourceToString(Capabilities.buildCapabilities("http://localhost:3002", "/v1"));
+    }
+
+    @Override
+    public AbstractGroupResource groupOperations() {
+        return this.gr;
+    }
+}
--- src/test/java/gov/cms/dpc/core/CapabilitiesTest.java
+++ src/test/java/gov/cms/dpc/core/CapabilitiesTest.java
@@ -5,25 +5,37 @@ import ca.uhn.fhir.validation.FhirValidator;
 import ca.uhn.fhir.validation.SchemaBaseValidator;
 import ca.uhn.fhir.validation.ValidationResult;
 import ca.uhn.fhir.validation.schematron.SchematronBaseValidator;
+import org.hl7.fhir.r4.model.CapabilityStatement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CapabilitiesTest {
 
-    @Test
-    public void testCapabilities() {
+    private FhirValidator validator;
+
+    @BeforeEach
+    public void setupValidator() {
         final FhirContext ctx = FhirContext.forR4();
 
-        final FhirValidator validator = ctx.newValidator();
+        validator = ctx.newValidator();
 
         final SchemaBaseValidator val1 = new SchemaBaseValidator(ctx);
         final SchematronBaseValidator val2 = new SchematronBaseValidator(ctx);
 //        new FhirInstanceValidator();
         validator.registerValidatorModule(val1);
         validator.registerValidatorModule(val2);
+    }
 
-        final ValidationResult validationResult = validator.validateWithResult(Capabilities.buildCapabilities());
-        assertTrue(validationResult.isSuccessful(), "Should successfully validate");
+    @Test
+    public void capabilitiesIsValid() {
+        final CapabilityStatement capabilities = Capabilities.buildCapabilities("http://localhost:3002", "/v1");
+        final ValidationResult validationResult = validator.validateWithResult(capabilities);
+        assertTrue(validationResult.isSuccessful(), "Capabilities should be valid");
+
+        // Verify properties
+        assertAll(() -> assertEquals(1, capabilities.getRest().size(), "Should have a single server operation"),
+                () -> assertEquals(3, capabilities.getRest().get(0).getOperation().size(), "Should have three routes"));
     }
 }
