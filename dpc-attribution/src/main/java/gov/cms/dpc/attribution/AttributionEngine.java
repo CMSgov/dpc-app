f48e6694070a5bc61e61d1211f66ae588596a884
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-02-11 13:59:23 -0500

    DPC-82: AttributionEngine now runs in local mode.
    We can seed the attribution engine with some test data and verify that relationships are asserted correctly.

--- dpc-attribution/src/main/java/gov/cms/dpc/attribution/AttributionEngine.java
+++ dpc-attribution/src/main/java/gov/cms/dpc/attribution/AttributionEngine.java
@@ -1,8 +1,12 @@
 package gov.cms.dpc.attribution;
 
-import java.util.List;
+import java.util.Set;
 
 public interface AttributionEngine {
 
-    List<String> getAttributedPatients(String providerID);
+    Set<String> getAttributedBeneficiaries(String providerID);
+
+    void addAttributionRelationship(String providerID, String beneficiaryID);
+
+    void removeAttributionRelationship(String providerID, String beneficiaryID);
 }
---
+++ dpc-attribution/src/main/java/gov/cms/dpc/attribution/TestSeeder.java
@@ -0,0 +1,50 @@
+package gov.cms.dpc.attribution;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.MissingResourceException;
+
+public class TestSeeder implements AttributionSeeder {
+
+    private static final String CSV = "./test_associations.csv";
+    private final Logger logger = LoggerFactory.getLogger(TestSeeder.class);
+
+    private final AttributionEngine engine;
+
+    @Inject
+    TestSeeder(AttributionEngine engine) {
+        this.engine = engine;
+        this.seedAttribution();
+    }
+
+    @Override
+    public void seedAttribution() {
+        logger.info("Seeding attributions");
+        // Get the test seeds
+        final InputStream resource = TestSeeder.class.getClassLoader().getResourceAsStream(CSV);
+        if (resource == null) {
+            throw new MissingResourceException("Can not find seeds", this.getClass().getName(), CSV);
+        }
+
+        try {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource, StandardCharsets.UTF_8))) {
+                for (String line; (line = reader.readLine()) != null; ) {
+                    final String[] splits = line.split(",");
+                    logger.debug("Associating {} with {}.", splits[1], splits[0]);
+                    engine.addAttributionRelationship(splits[1], splits[0]);
+                }
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+
+        logger.debug("Finished loading seeds");
+    }
+}
