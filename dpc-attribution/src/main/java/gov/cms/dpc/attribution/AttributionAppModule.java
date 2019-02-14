54d9937b54eab23f7ee75fa63a4e0cb4919f2d4f
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-02-14 11:30:51 -0500

    DPC-92: SessionFactory is now injected correctly.
    Cleaned up some of the testing code and made the SessionFactory be explicitly injected by Guice.

--- dpc-attribution/src/main/java/gov/cms/dpc/attribution/AttributionAppModule.java
+++ dpc-attribution/src/main/java/gov/cms/dpc/attribution/AttributionAppModule.java
@@ -1,13 +1,28 @@
 package gov.cms.dpc.attribution;
 
 import com.google.inject.Binder;
+import com.google.inject.Provides;
 import com.hubspot.dropwizard.guicier.DropwizardAwareModule;
 import gov.cms.dpc.attribution.jdbi.AttributedPatientsDAO;
+import gov.cms.dpc.attribution.models.AttributionRelationship;
+import io.dropwizard.db.PooledDataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
+import org.hibernate.SessionFactory;
 
 class AttributionAppModule extends DropwizardAwareModule<DPCAttributionConfiguration> {
 
+    private final HibernateBundle<DPCAttributionConfiguration> bundle;
+
     AttributionAppModule() {
-        // Not used
+
+        bundle = new HibernateBundle<>(AttributionRelationship.class) {
+
+            @Override
+            public PooledDataSourceFactory getDataSourceFactory(DPCAttributionConfiguration configuration) {
+                return configuration.getDatabase();
+            }
+            // Not used
+        };
     }
 
     @Override
@@ -15,5 +30,10 @@ class AttributionAppModule extends DropwizardAwareModule<DPCAttributionConfigura
 
         binder.bind(AttributedPatientsDAO.class);
 //        binder.bind(SeedCommand.class);
+    }
+
+    @Provides
+    SessionFactory getSessionFactory() {
+        return bundle.getSessionFactory();
     }
 }
