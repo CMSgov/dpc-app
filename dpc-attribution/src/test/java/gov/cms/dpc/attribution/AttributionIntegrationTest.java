c9e261f9a4d18d6253b3f13cb824b6cdc8c37f57
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-02-14 11:30:51 -0500

    DPC-92: SessionFactory is now injected correctly.
    Cleaned up some of the testing code and made the SessionFactory be explicitly injected by Guice.

---
+++ dpc-attribution/src/test/java/gov/cms/dpc/attribution/AttributionIntegrationTest.java
@@ -0,0 +1,29 @@
+package gov.cms.dpc.attribution;
+
+import io.dropwizard.testing.ConfigOverride;
+import io.dropwizard.testing.DropwizardTestSupport;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AttributionIntegrationTest {
+    private static final DropwizardTestSupport<DPCAttributionConfiguration> APPLICATION = new DropwizardTestSupport<>(DPCAttributionService.class, null, ConfigOverride.config("server.applicationConnectors[0].port", "3727"));
+
+    @BeforeAll
+    public static void setup() {
+        APPLICATION.before();
+    }
+
+    @BeforeEach()
+    public void migrateAndSeed() throws Exception {
+        APPLICATION.getApplication().run("seed");
+    }
+
+    @Test
+    public void test() {
+        assertEquals(1, 1, "Yes");
+    }
+
+}
