1c187cda0b947490b0c3730b45b8f99fc0fa02e0
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-02-22 16:01:24 -0500

    DPC-109: Added shared Docker volume and improved documentation
    The Docker compose script now creates a shared volume which can be used to pass data between the aggregation engine and the web api.
    
    Added some Javadocs for the new annotations.
    Expanded in base README to include build and test instructions.

--- dpc-common/src/main/java/gov/cms/dpc/common/annotations/APIV1.java
+++ dpc-common/src/main/java/gov/cms/dpc/common/annotations/APIV1.java
@@ -7,6 +7,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation that indicates the API returns data from a V1 resource.
+ * This extends the {@link ServiceBaseURL} by appending /v1 to the end.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD})
 @BindingAnnotation
--- dpc-common/src/main/java/gov/cms/dpc/common/annotations/ServiceBaseURL.java
+++ dpc-common/src/main/java/gov/cms/dpc/common/annotations/ServiceBaseURL.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Root URL of the given Dropwizard service.
+ * This should be generated from the HTTP Request host/port combination.
+ * The injection can be extended by using the {@link APIV1} annotation to append the /v1 suffix to the end of the URL.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD})
 public @interface ServiceBaseURL {
