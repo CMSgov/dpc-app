b7b080ed968b9d67ab29b2b0f31601e9e0a1a8b4
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-02-22 16:01:24 -0500

    DPC-109: Added shared Docker volume and improved documentation
    The Docker compose script now creates a shared volume which can be used to pass data between the aggregation engine and the web api.
    
    Added some Javadocs for the new annotations.
    Expanded in base README to include build and test instructions.

--- dpc-common/src/main/java/gov/cms/dpc/common/annotations/ExportPath.java
+++ dpc-common/src/main/java/gov/cms/dpc/common/annotations/ExportPath.java
@@ -7,6 +7,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Injectable {@link String} annotation pointing to the root directory where the NDJSON exports from the Aggregation engine will be stored.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
 @BindingAnnotation
