be21c2b919688176a3e12d5125068456971167bd
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-02-25 13:11:56 -0500

    DPC-95: Merge common and fhir modules.
    These are now combined, no reason for them to be separate.

--- dpc-web/src/main/java/gov/cms/dpc/web/annotations/AttributionService.java
+++ dpc-web/src/main/java/gov/cms/dpc/web/annotations/AttributionService.java
@@ -1,6 +1,7 @@
 package gov.cms.dpc.web.annotations;
 
 import com.google.inject.BindingAnnotation;
+import gov.cms.dpc.common.interfaces.AttributionEngine;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -8,7 +9,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Binds a web target to the remote {@link gov.cms.dpc.common.interfaces.AttributionEngine}
+ * Binds a web target to the remote {@link AttributionEngine}
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD})
