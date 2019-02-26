8750e1416504bbfbae43c996c0c09437d0bbbd83
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-02-26 14:50:45 -0500

    DPC-103: Capabilities Statement is fully updated
    The CapabilitiesStatement now generates a correct STU3 output.

--- dpc-web/src/test/java/gov/cms/dpc/web/core/CapabilitiesTest.java
+++ dpc-web/src/test/java/gov/cms/dpc/web/core/CapabilitiesTest.java
@@ -3,11 +3,14 @@ package gov.cms.dpc.web.core;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.validation.FhirValidator;
 import ca.uhn.fhir.validation.SchemaBaseValidator;
+import ca.uhn.fhir.validation.SingleValidationMessage;
 import ca.uhn.fhir.validation.ValidationResult;
 import ca.uhn.fhir.validation.schematron.SchematronBaseValidator;
 import org.hl7.fhir.dstu3.model.CapabilityStatement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,10 +35,18 @@ public class CapabilitiesTest {
     public void capabilitiesIsValid() {
         final CapabilityStatement capabilities = Capabilities.buildCapabilities("http://localhost:3002", "/v1");
         final ValidationResult validationResult = validator.validateWithResult(capabilities);
-        assertTrue(validationResult.isSuccessful(), "Capabilities should be valid");
+        assertTrue(validationResult.isSuccessful(), validationResultsToString(validationResult));
 
         // Verify properties
         assertAll(() -> assertEquals(1, capabilities.getRest().size(), "Should have a single server operation"),
                 () -> assertEquals(3, capabilities.getRest().get(0).getOperation().size(), "Should have three routes"));
+    }
+
+    private static String validationResultsToString(ValidationResult result) {
+        return result
+                .getMessages()
+                .stream()
+                .map(SingleValidationMessage::getMessage)
+                .collect(Collectors.joining("\n"));
     }
 }
