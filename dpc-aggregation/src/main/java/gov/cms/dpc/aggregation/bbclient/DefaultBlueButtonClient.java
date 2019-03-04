307cd08c81f456f3971f170abc6b59e9da9b296e
Isaac Sears <isaac.sears@cms.hhs.gov>
2019-03-04 11:21:15 -0500

    DPC-106: Inject Hapi FHIR REST client too

--- dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/bbclient/DefaultBlueButtonClient.java
+++ dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/bbclient/DefaultBlueButtonClient.java
@@ -6,40 +6,29 @@ import org.hl7.fhir.dstu3.model.Patient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 public class DefaultBlueButtonClient implements BlueButtonClient {
 
     private static final Logger logger = LoggerFactory.getLogger(DefaultBlueButtonClient.class);
 
-    private URL serverBaseUrl;
     private IGenericClient client;
 
 
-    public DefaultBlueButtonClient(IGenericClient client, URL baseURL){
+    public DefaultBlueButtonClient(IGenericClient client){
         this.client = client;
-        this.serverBaseUrl = baseURL;
     }
 
     public Patient requestFHIRFromServer(String beneficiaryID) {
 
         try {
-            logger.info("Attempting to fetch data from baseURL: {}", serverBaseUrl);
+            logger.info("Attempting to fetch data from baseURL: {}", client.getServerBase());
             return client.read().resource(Patient.class).withUrl(buildSearchUrl(beneficiaryID)).execute();
-
-        } catch (MalformedURLException ex) {
-            throw new BlueButtonClientException(
-                    "There was an error building the URL from the patientID: " + beneficiaryID,
-                    ex
-            );
 
         } catch (ResourceNotFoundException ex) {
             throw new BlueButtonClientException("Could not find beneficiary with ID: " + beneficiaryID, ex);
         }
     }
 
-    private String buildSearchUrl(String beneficiaryID) throws MalformedURLException {
-        return new URL(serverBaseUrl, "Patient/" + beneficiaryID).toString();
+    private String buildSearchUrl(String beneficiaryID) {
+        return client.getServerBase() + "Patient/" + beneficiaryID;
     }
 }
