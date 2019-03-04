19c359c1c627588bf035c2db2d446e9b4ead8f8e
Isaac Sears <isaac.sears@cms.hhs.gov>
2019-03-04 14:31:34 -0500

    DPC-83: Create mvp standalone aggregation service

--- dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/AggregationModule.java
+++ dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/AggregationModule.java
@@ -1,11 +1,28 @@
 package gov.cms.dpc.aggregation;
 
-import com.google.inject.AbstractModule;
+import ca.uhn.fhir.context.FhirContext;
+import com.google.inject.Binder;
+import com.google.inject.Provides;
+import com.hubspot.dropwizard.guicier.DropwizardAwareModule;
+import com.typesafe.config.Config;
 
-public class AggregationModule extends AbstractModule {
+import javax.inject.Singleton;
+
+public class AggregationModule extends DropwizardAwareModule<DPCAggregationConfiguration> {
 
     @Override
-    protected void configure() {
+    public void configure(Binder binder) {
 
+    }
+
+    @Provides
+    public Config provideConfig() {
+        return getConfiguration().getConfig();
+    }
+
+    @Provides
+    @Singleton
+    public FhirContext provideSTU3Context() {
+        return FhirContext.forDstu3();
     }
 }
