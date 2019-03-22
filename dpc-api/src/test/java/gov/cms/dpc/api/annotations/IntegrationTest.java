03cc72a1436dba5bae27188dc19b2467e17c06ad
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-03-22 10:18:05 -0400

    Added missing API tests
    A bad merge caused these tests to be deleted. So we’re just adding them again.

--- dpc-api/src/test/java/gov/cms/dpc/api/annotations/IntegrationTest.java
+++ dpc-api/src/test/java/gov/cms/dpc/api/annotations/IntegrationTest.java
@@ -8,7 +8,7 @@ import java.lang.annotation.*;
  * Marks the test suite (or test method) as being an Integration test, which can only be executed against a fully functioning setup.
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Tag("integration")
+@Tag("Integration")
 @Target({ElementType.METHOD, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Inherited
 public @interface IntegrationTest {
