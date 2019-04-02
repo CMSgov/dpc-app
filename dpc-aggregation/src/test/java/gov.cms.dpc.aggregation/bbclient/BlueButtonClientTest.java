2c55a65ed28cb85bc0712e3cb9145d7b21997fcb
Isaac Sears <isaac.sears@cms.hhs.gov>
2019-04-02 15:43:50 -0400

    DPC-176: Make all tests work with mockserver

--- dpc-aggregation/src/test/java/gov.cms.dpc.aggregation/bbclient/BlueButtonClientTest.java
+++ dpc-aggregation/src/test/java/gov.cms.dpc.aggregation/bbclient/BlueButtonClientTest.java
@@ -3,28 +3,20 @@ package gov.cms.dpc.aggregation.bbclient;
 import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.hl7.fhir.dstu3.model.*;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.mockserver.client.server.MockServerClient;
 import org.mockserver.integration.ClientAndServer;
 import org.mockserver.matchers.Times;
 import org.mockserver.model.Header;
 import org.mockserver.model.HttpRequest;
+import org.mockserver.model.Parameter;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.sql.Date;
-import java.util.Base64;
-import java.util.List;
-import java.util.MissingResourceException;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,24 +26,17 @@ class BlueButtonClientTest {
     private static final String TEST_PATIENT_ID = "20140000008325";
     // A patient that only has a single EOB record in bluebutton
     private static final String TEST_SINGLE_EOB_PATIENT_ID = "20140000009893";
-    // A patient with very many (592) EOB records in bluebutton
-    private static final String TEST_LARGE_EOB_PATIENT_ID = "20140000001827";
     // A patient id that should not exist in bluebutton
     private static final String TEST_NONEXISTENT_PATIENT_ID = "31337";
 
-    private static final String CSV = "testBlueButtonRequests.csv";
+    // Paths to test resources
+    private static final String METADATA_PATH = "bb-test-data/meta.xml";
+    private static final String SAMPLE_EOB_PATH_PREFIX = "bb-test-data/eob/";
+    private static final String SAMPLE_PATIENT_PATH_PREFIX = "bb-test-data/patient/";
+    private static final String[] TEST_PATIENT_IDS = {"20140000008325", "20140000009893"};
 
     private static BlueButtonClient bbc;
-
-    //@InjectMocks
-    //private static BlueButtonClientModule blueButtonClientModule;
-
-    //@Mock
-    private static HttpClient mockHttpClient;
-
     private static ClientAndServer mockServer;
-
-
 
     @BeforeAll
     public static void setupMockHttpClient() throws IOException {
@@ -60,20 +45,24 @@ class BlueButtonClientTest {
 
     @BeforeAll
     public static void setupBlueButtonClient() throws IOException {
-        //blueButtonClientModule = new BlueButtonClientModule();
+        // TODO: throw missing resource exceptions if test data not found
         mockServer = ClientAndServer.startClientAndServer(1080); // TODO: verify this starts/stops only once during tests
+        createMockServerExpectation("/v1/fhir/metadata", 200, getRawXML(METADATA_PATH), List.of());
 
-        final InputStream resource = BlueButtonClientTest.class.getClassLoader().getResourceAsStream(CSV);
-        if (resource == null) {
-            throw new MissingResourceException("Can not find sample requests", BlueButtonClientTest.class.getName(), CSV);
-        }
+        for(String patientId : TEST_PATIENT_IDS) {
+            createMockServerExpectation(
+                    "/v1/fhir/Patient/" + patientId,
+                    200,
+                    getRawXML(SAMPLE_PATIENT_PATH_PREFIX + patientId + ".xml"),
+                    List.of()
+            );
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource, StandardCharsets.UTF_8))) {
-            for (String line; (line = reader.readLine()) != null; ) {
-                final String[] splits = line.split(",");
-                createMockServerExpectation(splits[0], Integer.parseInt(splits[1]), splits[2]);
-
-            }
+            createMockServerExpectation(
+                    "/v1/fhir/ExplanationOfBenefit",
+                    200,
+                    getRawXML(SAMPLE_EOB_PATH_PREFIX + patientId + ".xml"),
+                    Arrays.asList(Parameter.param("patient", patientId))
+            );
         }
 
         final Injector injector = Guice.createInjector(new TestModule(), new BlueButtonClientModule());
@@ -84,14 +73,6 @@ class BlueButtonClientTest {
     public static void tearDown() {
         mockServer.stop();
     }
-
-//    @Test
-//    void mockitoTest() throws IOException {
-//        HttpGet sampleGetRequest = new HttpGet("https://fhir.backend.bluebutton.hhsdevcloud.us/v1/fhir/Patient/20140000008325");
-//        HttpResponse resp = mockHttpClient.execute(sampleGetRequest);
-//
-//        assertEquals(resp.getStatusLine().getStatusCode(),888);
-//    }
 
     @Test
     void shouldGetFHIRFromPatientID() {
@@ -137,13 +118,6 @@ class BlueButtonClientTest {
     }
 
     @Test
-    void shouldHandlePatientsWithVeryManyEOBs() {
-        Bundle response = bbc.requestEOBBundleFromServer(TEST_LARGE_EOB_PATIENT_ID);
-
-        assertEquals(response.getTotal(), 592, "This demo patient should have exactly 592 EOBs");
-    }
-
-    @Test
     void shouldThrowExceptionWhenResourceNotFound() {
         assertThrows(
                 ResourceNotFoundException.class, () -> {
@@ -158,35 +132,16 @@ class BlueButtonClientTest {
                 },
                 "BlueButton client should throw exceptions when asked to retrieve EOBs for a non-existent patient"
         );
-
     }
 
-    private static void addMockBlueButtonInteraction(HttpClient mockHttpClient, String requestURI, int statusCode, String payload) throws IOException {
-        StatusLine statusLine = Mockito.mock(StatusLine.class);
-        Mockito.when(statusLine.getStatusCode())
-                .thenReturn(statusCode);
-
-        HttpEntity httpEntity = Mockito.mock(HttpEntity.class);
-        Mockito.when(httpEntity.getContent())
-                .thenReturn(new ByteArrayInputStream(payload.getBytes()));
-
-        HttpResponse httpResponse = Mockito.mock(HttpResponse.class);
-        Mockito.when(httpResponse.getStatusLine())
-                .thenReturn(statusLine);
-        Mockito.when(httpResponse.getEntity())
-                .thenReturn(httpEntity);
-
-        Mockito.when(mockHttpClient.execute(Mockito.argThat(new HttpGetMatcher(new HttpGet(requestURI)))))
-                .thenReturn(httpResponse);
-    }
-
-    private static void createMockServerExpectation(String path, int respCode, String payload){
+    private static void createMockServerExpectation(String path, int respCode, String payload, List<Parameter> qStringParams){
         new MockServerClient("localhost", 1080)
                 .when(
                         HttpRequest.request()
                         .withMethod("GET")
-                        .withPath(path),
-                        Times.exactly(1)
+                        .withPath(path)
+                        .withQueryStringParameters(qStringParams),
+                        Times.unlimited()
                 )
                 .respond(
                         org.mockserver.model.HttpResponse.response()
@@ -194,9 +149,18 @@ class BlueButtonClientTest {
                         .withHeader(
                             new Header("Content-Type", "application/fhir+xml;charset=UTF-8")
                         )
-                        .withBody(Base64.getDecoder().decode(payload))
+                        .withBody(payload)
                         .withDelay(TimeUnit.SECONDS, 1)
                 );
     }
 
+    private static String getRawXML(String path) throws IOException {
+        InputStream sampleData = BlueButtonClientTest.class.getClassLoader().getResourceAsStream(path);
+
+        if(sampleData == null) {
+            throw new MissingResourceException("Cannot find sample requests", BlueButtonClientTest.class.getName(), path);
+        }
+
+        return new String(sampleData.readAllBytes());
+    }
 }
