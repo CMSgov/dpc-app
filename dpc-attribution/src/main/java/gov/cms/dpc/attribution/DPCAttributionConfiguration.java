79b2c696c269cb6080928dd0818afa4ccecf834e
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-04-04 11:18:54 -0400

    DPC-22: Add task to automatically prune attribution relationships (#36)
    Added a cron job to automatically expire attribution relationships that were created before a given expiration threshold. The job is run and managed by Dropwizard using the Sundial job scheduler. It's currently configured to run everyday at midnight (based on the system clock).
    
    The testing for it is rather simplistic, we essentially seed the database with attributions and manually override the creation time to be before the expiration threshold. DPC-186 has been created to improve the testing process by manually creating attributions using the current date time and verifying that they are not removed.
    
    In the future, we may want to consider moving the JOOQ SQL into a stored procedure and potentially schedule it within the database itself, but we'll cross that bridge when we get to it.
    
    This PR also includes some changes to the SeedCommand, by using JOOQ we've made things significantly simpler and safer.

--- dpc-attribution/src/main/java/gov/cms/dpc/attribution/DPCAttributionConfiguration.java
+++ dpc-attribution/src/main/java/gov/cms/dpc/attribution/DPCAttributionConfiguration.java
@@ -3,18 +3,40 @@ package gov.cms.dpc.attribution;
 import ca.mestevens.java.configuration.TypesafeConfiguration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.db.DataSourceFactory;
+import org.knowm.dropwizard.sundial.SundialConfiguration;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.time.Duration;
 
 public class DPCAttributionConfiguration extends TypesafeConfiguration {
+
+    @Valid
+    private Duration expirationThreshold;
 
     @Valid
     @NotNull
     @JsonProperty("database")
     private DataSourceFactory database = new DataSourceFactory();
 
+    @Valid
+    @NotNull
+    @JsonProperty("sundial")
+    private SundialConfiguration sundial = new SundialConfiguration();
+
     public DataSourceFactory getDatabase() {
         return database;
+    }
+
+    public SundialConfiguration getSundial() {
+        return sundial;
+    }
+
+    public Duration getExpirationThreshold() {
+        return expirationThreshold;
+    }
+
+    public void setExpirationThreshold(int expirationThreshold) {
+        this.expirationThreshold = Duration.ofDays(expirationThreshold);
     }
 }
---
+++ dpc-attribution/src/main/java/gov/cms/dpc/attribution/jobs/ExpireAttributions.java
@@ -0,0 +1,58 @@
+package gov.cms.dpc.attribution.jobs;
+
+import com.google.inject.Injector;
+import gov.cms.dpc.attribution.DPCAttributionConfiguration;
+import gov.cms.dpc.attribution.dao.tables.Attributions;
+import org.jooq.DSLContext;
+import org.knowm.sundial.Job;
+import org.knowm.sundial.SundialJobScheduler;
+import org.knowm.sundial.annotations.CronTrigger;
+import org.knowm.sundial.exceptions.JobInterruptException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * This job runs every day at midnight to expire (remove) attribution relationships which are older than a certain threshold.
+ * The value is set in the config file ({@link DPCAttributionConfiguration#getExpirationThreshold()}) and defaults to 90 days.
+ */
+@CronTrigger(cron = "0 0 * * * ?")
+public class ExpireAttributions extends Job {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExpireAttributions.class);
+
+    @Inject
+    private DSLContext context;
+    @Inject
+    private Duration expirationThreshold;
+    private OffsetDateTime expirationTemporal;
+
+    public ExpireAttributions() {
+        // Manually load the Guice injector. Since the job loads at the beginning of the startup process, Guice is not automatically injected.
+        final Injector attribute = (Injector) SundialJobScheduler.getServletContext().getAttribute("com.google.inject.Injector");
+        attribute.injectMembers(this);
+        // Calculate the expiration date (e.g. all relationships created BEFORE this time will be removed
+        this.expirationTemporal = OffsetDateTime.now().minus(this.expirationThreshold);
+    }
+
+    @Override
+    public void doRun() throws JobInterruptException {
+        // Find all the jobs and remove them
+        logger.debug("Removing attribution relationships created before {}.", expirationTemporal.format(DateTimeFormatter.ISO_DATE_TIME));
+
+        try {
+            final int removed = context
+                    .delete(Attributions.ATTRIBUTIONS)
+                    .where(Attributions.ATTRIBUTIONS.CREATED_AT.le(Timestamp.from(this.expirationTemporal.toInstant())))
+                    .execute();
+            logger.debug("Expired {} attribution relationships.", removed);
+        } finally {
+            context.close();
+        }
+    }
+}
