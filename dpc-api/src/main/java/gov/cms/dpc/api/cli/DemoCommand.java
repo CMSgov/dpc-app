26b395458c357ca338f66d3387344b2922593ec7
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-04-29 12:12:07 -0400

    DPC-206: Add demo client and re-enable integration tests (#46)
    * DPC-206: Fix API Service dependencies and Queue config location
    
    The API service no longer depends on the aggregation service as a dependency.
    
    Fixed queue configuration inclusion, it actually works now.
    
    * DPC-163: Updated configuration files for integration tests
    
    The docker compose scripts and ports are now configured correctly.
    
    Integration test is back to passing.
    
    * DPC-206: Demo client is now working correctly.
    
    We can now start everything with docker-compose and run the demo against the fresh setup.
    
    * DPC-206: Refactored export test code into helper class
    
    We now have a class for sharing basic client functionality between the Demo command and the test code.
    
    This also allows us to remove the ExportResponse class, since it largely duplicates the JobCompletionModel class. This does require ensuring that the JavaTimeModule() is registered with Jackson, which may be annoying.
    
    * DPC-206: DemoCommand now uses the ClientUtils helpers
    
    Migrated the DemoCommand to use the shared client methods, rather than its own duplicate copies.
    
    * DPC-163: Add integration tests to Travis config
    
    Once the install and main test phases have run, we startup the Docker-compose cluster and run just the integration tests for dpc-api
    
    We also removed the Travis dependencies on redis and postgres, we simply use our own Docker containers.
    
    * DPC-163: Manually disable Travis postgres
    
    Prevent Travis from automatically starting Postgres, since we’re bringing our own instance.
    
    * DPC-162: Setup integration test maven profile
    
    Added a maven profile for only running the integration tests.
    
    * DPC-162: Persist code coverage reports between test runs
    
    Copy the jacoco reports out of the target dir, that way they can be summed up by code climate.
    
    * DPC-162: Added truncate command to attribution service
    
    Added an admin command to truncate the database so we can run our integration tests in a clean environment.
    
    * DPC-162: Updated integration tests
    
    Updated the integration tests to correctly pass, since it’s been a while since we last ran them.
    
    Remove the RosterResource test since it’s really basic and covered by the other tests.
    
    * DPC-162: Fix output path for test coverage generation
    
    Updated the sum-coverage command to hopefully fix the output path.
    
    * DPC-184: Update jacoco settings to skip classes
    
    Jacoco no longer generates code coverage data for JOOQ generated classes.
    
    * DPC-206: Address code climate warnings
    
    * DPC-206: Refactor demo client
    
    Split the main run method into a couple of helper functions, should be easier to understand.
    
    * DPC-206: Moved test resources into correct directory
    
    All the common files should be under src/main/resources, not split between multiple directories.
    
    * DPC-206: Updated README
    
    Updated the README to give some info about the demo client.

---
+++ dpc-api/src/main/java/gov/cms/dpc/api/cli/DemoCommand.java
@@ -0,0 +1,132 @@
+package gov.cms.dpc.api.cli;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.client.exceptions.NonFhirResponseException;
+import ca.uhn.fhir.rest.gclient.ICreateTyped;
+import ca.uhn.fhir.rest.gclient.IOperationUntypedWithInput;
+import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
+import gov.cms.dpc.api.client.ClientUtils;
+import gov.cms.dpc.api.models.JobCompletionModel;
+import io.dropwizard.cli.Command;
+import io.dropwizard.setup.Bootstrap;
+import net.sourceforge.argparse4j.inf.Namespace;
+import net.sourceforge.argparse4j.inf.Subparser;
+import org.eclipse.jetty.http.HttpStatus;
+import org.hl7.fhir.dstu3.model.Parameters;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+public class DemoCommand extends Command {
+    private final FhirContext ctx;
+
+    public DemoCommand() {
+        super("demo", "Demo client");
+        ctx = FhirContext.forDstu3();
+    }
+
+    @Override
+    public void configure(Subparser subparser) {
+        // Option for specifying seeds file
+        subparser
+                .addArgument("-f", "--file")
+                .dest("seed-file")
+                .type(String.class)
+                .setDefault("src/main/resources/test_associations.csv")
+                .help("Association file to use for demo purposes. Defaults to project root");
+
+        // Option for overriding provider id
+        subparser
+                .addArgument("-p", "--provider")
+                .dest("provider-id")
+                .type(String.class)
+                .setDefault(ClientUtils.PROVIDER_ID)
+                .help("Execute as a specific provider");
+    }
+
+    @Override
+    public void run(Bootstrap<?> bootstrap, Namespace namespace) throws Exception {
+        System.out.println("Running demo!");
+        final String baseURL = "http://localhost:3002/v1/";
+
+        // Make the initial export request
+        // If it's a 404, that's fine, for anything else, fail
+        final IOperationUntypedWithInput<Parameters> exportOperation = createExportOperation(namespace, baseURL);
+        try {
+            exportOperation.execute();
+        } catch (ResourceNotFoundException e) {
+            System.out.println("Provider is not registered with the system");
+        }
+
+        // Sleep for 2 seconds, for presentation reasons
+        Thread.sleep(2000);
+
+        this.uploadBundle(namespace, baseURL);
+
+        // Sleep for 2 seconds, for presentation reasons
+        Thread.sleep(2000);
+
+        // Retry the export request
+        final JobCompletionModel jobResponse = monitorExportRequest(exportOperation);
+
+        System.out.print("\n\nExport job completed successfully.%n%nAvailable files:%n");
+        jobResponse.getOutput().forEach(System.out::println);
+
+        System.exit(0);
+    }
+
+    private IOperationUntypedWithInput<Parameters> createExportOperation(Namespace namespace, String baseURL) {
+        // Submit an export request for a provider which is not known to the system.
+        final IGenericClient exportClient = ctx.newRestfulGenericClient(baseURL);
+
+        final String providerID = namespace.getString("provider-id");
+
+        return ClientUtils.createExportOperation(exportClient, providerID);
+    }
+
+    private void uploadBundle(Namespace namespace, String baseURL) throws IOException {
+        // Read the provider bundle from the given file
+        final String seedsFile = getSeedsFile(namespace);
+        try (InputStream resource = new FileInputStream(new File(seedsFile))) {
+            // Now, submit the bundle
+            System.out.println("Uploading Patient roster");
+            final IGenericClient rosterClient = ctx.newRestfulGenericClient(baseURL);
+            final ICreateTyped rosterSubmission = ClientUtils.createRosterSubmission(rosterClient, resource);
+            rosterSubmission.execute();
+        }
+    }
+
+    private JobCompletionModel monitorExportRequest(IOperationUntypedWithInput<Parameters> exportOperation) throws IOException, InterruptedException {
+        System.out.println("Retrying export request");
+        String exportURL = "";
+
+        try {
+            exportOperation.execute();
+        } catch (NonFhirResponseException e) {
+            final NonFhirResponseException e1 = e;
+            if (e1.getStatusCode() != HttpStatus.NO_CONTENT_204) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+
+            // Get the correct header
+            final Map<String, List<String>> headers = e1.getResponseHeaders();
+
+            // Get the headers and check the status
+            exportURL = headers.get("content-location").get(0);
+            System.out.printf("Export job started. Progress URL: %s%n", exportURL);
+        }
+
+        // Poll the job until it's done
+        return ClientUtils.awaitExportResponse(exportURL, "Checking job status");
+    }
+
+    private static String getSeedsFile(Namespace namespace) {
+        return namespace.getString("seed-file");
+    }
+}
--- dpc-attribution/src/test/java/gov/cms/dpc/attribution/AttributionFHIRTest.java
+++ dpc-attribution/src/test/java/gov/cms/dpc/attribution/AttributionFHIRTest.java
@@ -31,7 +31,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.MissingResourceException;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static gov.cms.dpc.attribution.SharedMethods.createAttributionBundle;
