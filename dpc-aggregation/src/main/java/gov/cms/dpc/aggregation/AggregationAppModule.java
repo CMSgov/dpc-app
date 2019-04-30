d63919abdcd56422b4065df46a1cd4ce8cbf6f93
Isaac Sears <isaac.sears@cms.hhs.gov>
2019-04-30 10:52:55 -0400

    DPC-203: Using encrypting aggregation engine when enabled in config

--- dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/AggregationAppModule.java
+++ dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/AggregationAppModule.java
@@ -6,6 +6,7 @@ import com.google.inject.Provides;
 import com.hubspot.dropwizard.guicier.DropwizardAwareModule;
 import com.typesafe.config.Config;
 import gov.cms.dpc.aggregation.engine.AggregationEngine;
+import gov.cms.dpc.aggregation.engine.EncryptingAggregationEngine;
 import gov.cms.dpc.common.annotations.AdditionalPaths;
 import gov.cms.dpc.common.hibernate.DPCHibernateBundle;
 
@@ -23,7 +24,16 @@ public class AggregationAppModule extends DropwizardAwareModule<DPCAggregationCo
     @Override
     public void configure(Binder binder) {
         binder.requestStaticInjection(DPCHibernateBundle.class);
-        binder.bind(AggregationEngine.class);
+
+        if(
+                getConfiguration().getConfig().hasPath("encryption.enabled") &&
+                getConfiguration().getConfig().getBoolean("encryption.enabled")
+        )  {
+            binder.bind(AggregationEngine.class).to(EncryptingAggregationEngine.class);
+        } else  {
+            binder.bind(AggregationEngine.class);
+        }
+
         binder.bind(Aggregation.class).asEagerSingleton();
     }
 
