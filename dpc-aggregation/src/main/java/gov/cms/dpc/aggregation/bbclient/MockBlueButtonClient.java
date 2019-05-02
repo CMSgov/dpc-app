8afd5e500e56cf409e806b9dfd66301062cb63fb
Richard Hawes <richard.hawes@cms.hhs.gov>
2019-05-01 21:55:25 -0400

    Added error for BB to AggregationEngine
    - Writes a record into an .error.ndjson file for each error from BlueButton
    - Doesn't create error if non exists
    - Stores a erringResource if an error occurs
    - Doesn't fail the job because of one particular BB error

--- dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/bbclient/MockBlueButtonClient.java
+++ dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/bbclient/MockBlueButtonClient.java
@@ -2,18 +2,22 @@ package gov.cms.dpc.aggregation.bbclient;
 
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.Patient;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.MissingResourceException;
 
 public class MockBlueButtonClient implements BlueButtonClient {
 
     private static final String SAMPLE_EOB_PATH_PREFIX = "bb-test-data/eob/";
     private static final String SAMPLE_PATIENT_PATH_PREFIX = "bb-test-data/patient/";
-    public static final String[] TEST_PATIENT_IDS = {"20140000008325", "20140000009893"};
+    public static final List<String> TEST_PATIENT_IDS = List.of("20140000008325", "20140000009893");
 
     public MockBlueButtonClient(){
 
@@ -21,12 +25,18 @@ public class MockBlueButtonClient implements BlueButtonClient {
 
     @Override
     public Patient requestPatientFromServer(String patientID) {
+        if (!TEST_PATIENT_IDS.contains(patientID)) {
+            throw new ResourceNotFoundException("No patient found with ID: " + patientID);
+        }
         final var path = SAMPLE_PATIENT_PATH_PREFIX + patientID + ".xml";
         return requestFromServer(Patient.class, path);
     }
 
     @Override
     public Bundle requestEOBBundleFromServer(String patientID) {
+        if (!TEST_PATIENT_IDS.contains(patientID)) {
+            throw new ResourceNotFoundException("No patient found with ID: " + patientID);
+        }
         final var path = SAMPLE_EOB_PATH_PREFIX + patientID + ".xml";
         return requestFromServer(Bundle.class, path);
     }
