b937cab428a668e92ea92283b096c1641fc9d202
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-05-07 08:40:00 -0700

    DPC-229: Added timeout and logging
    Added a timeout call to try and exit the failing tests early.
    
    Also added some more logging and dumped it to the Travis output.

--- dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/DPCAggregationService.java
+++ dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/DPCAggregationService.java
@@ -9,8 +9,12 @@ import gov.cms.dpc.queue.JobQueueModule;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DPCAggregationService extends Application<DPCAggregationConfiguration> {
+
+    private static final Logger logger = LoggerFactory.getLogger(DPCAggregationService.class);
 
     public static void main(final String[] args) throws Exception {
         new DPCAggregationService().run(args);
@@ -34,5 +38,13 @@ public class DPCAggregationService extends Application<DPCAggregationConfigurati
 
     @Override
     public void run(DPCAggregationConfiguration configuration, Environment environment) {
+        String envVar = "local";
+        try {
+            envVar = System.getenv("ENV");
+        } catch (NullPointerException e) {
+            // If ENV isn't set, just ignore it.
+        }
+
+        logger.info("Starting Aggregation Service in environment: {}", envVar);
     }
 }
