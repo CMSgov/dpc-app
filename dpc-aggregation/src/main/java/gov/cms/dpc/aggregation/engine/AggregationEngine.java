6d247601e43c8aadac15bceceaa034f65459467c
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-05-08 14:49:00 -0400

    DPC-146: Initial refactor of queue polling into reactive loop.
    Really terrible copy-pasta of using RxJava and Resilience4j in order to improve our handling of export requests.
    
    Right now, it runs, and it should automatically timeout and fail individual requests when something goes wrong.

--- dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/engine/AggregationEngine.java
+++ dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/engine/AggregationEngine.java
@@ -6,25 +6,28 @@ import com.typesafe.config.Config;
 import gov.cms.dpc.aggregation.bbclient.BlueButtonClient;
 import gov.cms.dpc.queue.JobQueue;
 import gov.cms.dpc.queue.JobStatus;
-import gov.cms.dpc.queue.Pair;
 import gov.cms.dpc.queue.exceptions.JobQueueFailure;
 import gov.cms.dpc.queue.models.JobModel;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
+import io.github.resilience4j.retry.transformer.RetryTransformer;
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import org.hl7.fhir.dstu3.model.Resource;
 import org.hl7.fhir.dstu3.model.ResourceType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 import javax.inject.Inject;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
-import java.security.interfaces.RSAPrivateKey;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class AggregationEngine implements Runnable {
 
@@ -37,12 +40,14 @@ public class AggregationEngine implements Runnable {
     private final FhirContext context;
     protected final String exportPath;
     private volatile boolean run = true;
+    private Disposable subscribe;
 
     /**
      * Create an engine
+     *
      * @param bbclient - the BlueButton client to use
-     * @param queue - the Job queue that will direct the work done
-     * @param config - the configuration for the engine
+     * @param queue    - the Job queue that will direct the work done
+     * @param config   - the configuration for the engine
      */
     @Inject
     public AggregationEngine(BlueButtonClient bbclient, JobQueue queue, Config config) {
@@ -59,27 +64,30 @@ public class AggregationEngine implements Runnable {
     public void run() {
         // Run loop
         logger.info("Starting aggregation engine with exportPath:\"{}\"", exportPath);
-        while (run) {
-            this.queue.workJob().ifPresentOrElse(pair -> {
-                completeJob(pair.getRight());
-            }, () -> {
-                try {
-                    logger.debug("No job, waiting {} milliseconds", WAIT_TIME);
-                    Thread.sleep(WAIT_TIME);
-                } catch (InterruptedException e) {
-                    logger.error("Interrupted. {}", e.getMessage());
-                    Thread.currentThread().interrupt();
-                }
-            });
-        }
-        logger.info("Shutting down aggregation engine");
+        this.pollQueue();
+//        while (run) {
+//            this.queue.workJob().ifPresentOrElse(pair -> {
+//                completeJob(pair.getRight());
+//            }, () -> {
+//                try {
+//                    logger.debug("No job, waiting {} milliseconds", WAIT_TIME);
+//                    Thread.sleep(WAIT_TIME);
+//                } catch (InterruptedException e) {
+//                    logger.error("Interrupted. {}", e.getMessage());
+//                    Thread.currentThread().interrupt();
+//                }
+//            });sa
+//        }
+
     }
 
     /**
      * Stop the engine
      */
     public void stop() {
+        logger.info("Shutting down aggregation engine");
         this.run = false;
+        this.subscribe.dispose();
     }
 
     /**
@@ -111,7 +119,7 @@ public class AggregationEngine implements Runnable {
 
         logger.debug("Has {} attributed beneficiaries", attributedBeneficiaries.size());
         try {
-            for (ResourceType resourceType: job.getResourceTypes()) {
+            for (ResourceType resourceType : job.getResourceTypes()) {
                 if (!JobModel.isValidResourceType(resourceType)) {
                     throw new JobQueueFailure(job.getJobID(), "Unexpected resource type: " + resourceType.toString());
                 }
@@ -132,34 +140,101 @@ public class AggregationEngine implements Runnable {
     /**
      * Write out a single provider NDJSON file for a single resource type
      *
-     * @param writer - the stream to write to
-     * @param job - the job to process
+     * @param writer       - the stream to write to
+     * @param job          - the job to process
      * @param resourceType - the FHIR resource type to write out
      */
     protected void workResource(OutputStream writer, JobModel job, ResourceType resourceType) {
         final IParser parser = context.newJsonParser();
 
-        job.getPatients()
-                .stream()
-                .map(patientId -> {
-                    switch (resourceType) {
-                        case Patient:
-                            return this.bbclient.requestPatientFromServer(patientId);
-                        case ExplanationOfBenefit:
-                            return this.bbclient.requestEOBBundleFromServer(patientId);
-                        default:
-                            throw new JobQueueFailure(job.getJobID(), "Unexpected resource type: " + resourceType.toString());
-                    }
-                })
-                .map(parser::encodeResourceToString)
-                .forEach(str -> {
+        Observable.fromIterable(job.getPatients())
+                .flatMap(patient -> this.handleResource(patient, resourceType, parser))
+                .subscribeOn(Schedulers.io())
+                .doOnError(e -> logger.error("Error: ", e))
+                .blockingSubscribe(str -> {
                     try {
-                        logger.debug("Writing {} to file", str);
+                        logger.debug("Writing {} to file on thread {}", str, Thread.currentThread().getName());
                         writer.write(str.getBytes(StandardCharsets.UTF_8));
                         writer.write(DELIM);
                     } catch (IOException e) {
                         throw new JobQueueFailure(job.getJobID(), e);
                     }
                 });
+
+//        job.getPatients()
+//                .stream()
+//                .map(patientId -> )
+////                .map(patientId -> {
+////                    switch (resourceType) {
+////                        case Patient:
+////                            return this.bbclient.requestPatientFromServer(patientId);
+////                        case ExplanationOfBenefit:
+////                            return this.bbclient.requestEOBBundleFromServer(patientId);
+////                        default:
+////                            throw new JobQueueFailure(job.getJobID(), "Unexpected resource type: " + resourceType.toString());
+////                    }
+////                })
+////                .map(parser::encodeResourceToString)
+//                .forEach(str -> {
+//                    try {
+//                        logger.debug("Writing {} to file", str);
+//                        writer.write(str.getBytes(StandardCharsets.UTF_8));
+//                        writer.write(DELIM);
+//                    } catch (IOException e) {
+//
+//                    }
+//                });
+    }
+
+    private void pollQueue() {
+        subscribe = Observable.fromCallable(this.queue::workJob)
+                .doOnNext(job -> logger.debug("Awaiting job on {}", Thread.currentThread().getName()))
+                .filter(Optional::isPresent)
+                .repeatWhen(completed -> {
+                    logger.debug("No job, retrying in 2 seconds");
+                    return completed.delay(2, TimeUnit.SECONDS);
+                })
+                .map(Optional::get)
+                .doOnError(e -> logger.error("Error", e))
+                .subscribe(workPair -> {
+                    final JobModel model = workPair.getRight();
+                    final UUID jobID = workPair.getLeft();
+                    logger.debug("Has job {}. Working.", jobID);
+                    List<String> attributedBeneficiaries = model.getPatients();
+
+                    if (!attributedBeneficiaries.isEmpty()) {
+                        logger.debug("Has {} attributed beneficiaries", attributedBeneficiaries.size());
+                        try {
+                            this.completeJob(model);
+                        } catch (Exception e) {
+                            logger.error("Cannot process job {}", jobID, e);
+                            this.queue.completeJob(jobID, JobStatus.FAILED);
+                        }
+                    } else {
+                        logger.error("Cannot execute Job {} with no beneficiaries", jobID);
+                        this.queue.completeJob(jobID, JobStatus.FAILED);
+                    }
+                });
+    }
+
+    private Observable<String> handleResource(String identifier, ResourceType resourceType, IParser parser) {
+        // Create retry handler
+        RetryConfig config = RetryConfig.ofDefaults();
+        Retry retry = Retry.of("testName", config);
+        RetryTransformer<Resource> retryTransformer = RetryTransformer.of(retry);
+
+        return Observable.fromCallable(() -> {
+            switch (resourceType) {
+                case Patient:
+                    return this.bbclient.requestPatientFromServer(identifier);
+                case ExplanationOfBenefit:
+                    return this.bbclient.requestEOBBundleFromServer(identifier);
+                default:
+                    throw new IllegalArgumentException("Unexpected resource type: " + resourceType.toString());
+            }
+        })
+                .compose(retryTransformer)
+                .doOnNext(p -> logger.debug("Fetching {} on {}", p, Thread.currentThread().getName()))
+                .map(parser::encodeResourceToString);
     }
 }
