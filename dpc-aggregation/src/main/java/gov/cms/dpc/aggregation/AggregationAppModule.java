2bb64d550f940cddbb24a470e620705533c74bdc
Richard Hawes <richard.hawes@cms.hhs.gov>
2019-06-11 20:56:08 -0400

    Refactored
    - Removed the concept of error files. It is just OperationOutcome files.
    - JobResults always contains OperationalOutcome resource types
    - removed error count from JobResults
    - removed error filename functions
    - Updated tests

--- dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/AggregationAppModule.java
+++ dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/AggregationAppModule.java
@@ -6,7 +6,6 @@ import com.google.inject.Provides;
 import com.hubspot.dropwizard.guicier.DropwizardAwareModule;
 import com.typesafe.config.Config;
 import gov.cms.dpc.aggregation.engine.AggregationEngine;
-import gov.cms.dpc.aggregation.engine.EncryptingAggregationEngine;
 import gov.cms.dpc.common.annotations.AdditionalPaths;
 import gov.cms.dpc.common.annotations.ExportPath;
 import gov.cms.dpc.common.hibernate.DPCHibernateBundle;
@@ -24,16 +23,7 @@ public class AggregationAppModule extends DropwizardAwareModule<DPCAggregationCo
     @Override
     public void configure(Binder binder) {
         binder.requestStaticInjection(DPCHibernateBundle.class);
-
-        if (
-                getConfiguration().getConfig().hasPath("encryption.enabled") &&
-                        getConfiguration().getConfig().getBoolean("encryption.enabled")
-        ) {
-            binder.bind(AggregationEngine.class).to(EncryptingAggregationEngine.class);
-        } else {
-            binder.bind(AggregationEngine.class);
-        }
-
+        binder.bind(AggregationEngine.class);
         binder.bind(AggregationManager.class).asEagerSingleton();
     }
 
