14e93a2d0dab6e81b8e517cdda563dcfcdfc9b68
Jared C Cunha <jcunha@omb.eop.gov>
2019-06-21 16:53:51 -0400

    use cms design system (#68)

--- dpc-web/config/environments/development.rb
+++ dpc-web/config/environments/development.rb
@@ -58,4 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Inline source maps
+  config.sass.inline_source_maps = true
 end
