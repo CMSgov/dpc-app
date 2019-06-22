640dea69156843a528bbe9781743f3308b000e19
Richard Hawes <richard.hawes@cms.hhs.gov>
2019-06-22 11:12:33 -0400

    Fixed an error in a test

--- dpc-queue/src/test/java/gov/cms/dpc/queue/models/JobModelTest.java
+++ dpc-queue/src/test/java/gov/cms/dpc/queue/models/JobModelTest.java
@@ -37,7 +37,8 @@ class JobModelTest {
         final var a = new JobModel(jobID, List.of(ResourceType.Patient), "1", List.of("1"));
         final var b = a.clone();
         final var completedA = a.makeRunningJob().makeFinishedJob(JobStatus.COMPLETED, List.of(new JobResult(jobID, ResourceType.Patient, 0, 1)));
-        final var completedB = b.makeRunningJob().makeFinishedJob(JobStatus.COMPLETED, List.of(new JobResult(jobID, ResourceType.Patient, 0, 1)));
+        final var completedB = completedA.clone();
         assertEquals(a.hashCode(), b.hashCode(), "expected a to equal b");
+        assertEquals(completedA.hashCode(), completedB.hashCode(), "expected a to equal b");
     }
 }
