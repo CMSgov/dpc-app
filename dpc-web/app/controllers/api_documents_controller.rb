b8d3e4a246f93b3fde930cc34f7875a913d3286a
Jared Cunha <jcunha@omb.eop.gov>
2019-07-02 13:24:51 -0400

    convert load_markdown to private method

--- dpc-web/app/controllers/api_documents_controller.rb
+++ dpc-web/app/controllers/api_documents_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class ApiDocumentsController < ApplicationController
-  def home
-    # Assume markdown is named the same as the action.
-    file_path = lookup_context.find_template("#{controller_path}/#{action_name}")
-                               .identifier.sub(".html.erb", ".md")
+  before_action :load_markdown
 
+  private
+
+  def load_markdown
+    # Assume markdown is named the same as the action.
+    file_path = lookup_context.find_template("#{controller_path}/#{action_name}").identifier.sub('.html.erb', '.md')
     md_content = File.read(file_path)
     @html_content = Kramdown::Document.new(md_content).to_html
   end
