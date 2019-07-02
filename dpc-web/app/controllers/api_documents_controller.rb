ae0b43a0300ea6c711b5c3edca9ba9a138a687d5
Jared Cunha <jcunha@omb.eop.gov>
2019-07-02 12:13:45 -0400

    add redcarpet to controller

--- dpc-web/app/controllers/api_documents_controller.rb
+++ dpc-web/app/controllers/api_documents_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApiDocumentsController < ApplicationController
+  require 'redcarpet'
+
+  def home
+    renderer = Redcarpet::Render::HTML.new()
+    @markdown = Redcarpet::Markdown.new(renderer)
+  end
 end
