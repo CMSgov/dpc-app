a6a2a4b8387c115f744dffcde7994be903ca85f4
Jared C Cunha <jcunha@omb.eop.gov>
2019-07-08 08:48:42 -0400

    Markdown support and syntax highlighting (#84)
    * install redcarpet gem
    
    * add redcarpet to controller
    
    * MD added
    
    * convert load_markdown to private method
    
    * syntax highlights

--- dpc-web/app/controllers/api_documents_controller.rb
+++ dpc-web/app/controllers/api_documents_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApiDocumentsController < ApplicationController
+  before_action :load_markdown
+
+  private
+
+  def load_markdown
+    # Assume markdown is named the same as the action.
+    file_path = lookup_context.find_template("#{controller_path}/#{action_name}").identifier.sub('.html.erb', '.md')
+    md_content = File.read(file_path)
+    @html_content = Kramdown::Document.new(md_content).to_html
+  end
 end
--- dpc-web/app/helpers/application_helper.rb
+++ dpc-web/app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+
+  def syntax_highlight(text)
+    # Initialized in config/initializers/rouge_highlighter.rb
+    html = HighlightSource.render(text)
+    html.html_safe
+  end
 end
