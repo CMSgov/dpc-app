22304acf5b31b229a98b0ad2230f0468edcb779e
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-07-13 22:00:26 -0400

    DPC-93: Config overrides are now working.
    We need to pass in the config prefix of each application, otherwise the config overrides (which are system properties) are not overridden correctly.

--- dpc-api/src/test/java/gov/cms/dpc/api/AbstractApplicationTest.java
+++ dpc-api/src/test/java/gov/cms/dpc/api/AbstractApplicationTest.java
@@ -22,8 +22,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @IntegrationTest
 public class AbstractApplicationTest {
 
-    protected static final DropwizardTestSupport<DPCAPIConfiguration> APPLICATION = new DropwizardTestSupport<>(DPCAPIService.class, null, ConfigOverride.config("server.applicationConnectors[0].port", "7777"));
-    public static final String ATTRIBUTION_TRUNCATE_TASK = "http://localhost:9902/tasks/truncate";
+    // Application prefix, which we need in order to correctly override config values.
+    private static final String KEY_PREFIX = "dpc.api";
+
+    protected static final DropwizardTestSupport<DPCAPIConfiguration> APPLICATION = new DropwizardTestSupport<>(DPCAPIService.class, null,
+            ConfigOverride.config(KEY_PREFIX, "authenticationDisabled", "true"));
+    static final String ATTRIBUTION_TRUNCATE_TASK = "http://localhost:9902/tasks/truncate";
     protected FhirContext ctx;
 
     protected AbstractApplicationTest() {
