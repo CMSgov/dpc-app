95e9b60f55bf2b97ee60f26c0b9e7ab8e825d7d3
Jared C Cunha <jcunha@omb.eop.gov>
2019-07-22 12:56:25 -0400

    Implement landing page design (#109)
    * update landing page design
    
    more work on landing page
    
    accordions
    
    Use Roboto
    
    css fixes for IE
    
    navbar
    
    top nav
    
    mobile fixes
    
    mobile friendly process list
    
    benefit prop mobile friendly
    
    hero and benefit prop ie fixes
    
    move accordion js to vendor assets
    
    IE11 fix
    
    * fix a11y errors

--- dpc-web/config/application.rb
+++ dpc-web/config/application.rb
@@ -22,6 +22,9 @@ module DpcWebsite
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Add fonts to asset pipeline
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
