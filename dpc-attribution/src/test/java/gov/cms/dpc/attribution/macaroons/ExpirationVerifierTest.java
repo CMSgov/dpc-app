fe316025c08cfbb5349c49acb77b6fd36c494bcd
Ron Heft <52176593+ronaldheft-usds@users.noreply.github.com>
2019-07-24 13:49:56 -0400

    DPC-312: Fix token expiration logic (#121)
    DPC-312: Fix token expiration logic

--- dpc-attribution/src/test/java/gov/cms/dpc/attribution/macaroons/ExpirationVerifierTest.java
+++ dpc-attribution/src/test/java/gov/cms/dpc/attribution/macaroons/ExpirationVerifierTest.java
@@ -1,37 +1,36 @@
-//package gov.cms.dpc.attribution.macaroons;
-//
-//import gov.cms.dpc.macaroons.MacaroonCaveat;
-//import org.junit.jupiter.api.Disabled;
-//
-//import java.time.LocalDate;
-//import java.time.OffsetDateTime;
-//import java.time.ZoneOffset;
-//import java.time.temporal.ChronoUnit;
-//
-//@Disabled // TODO: Disabled until DPC-312 is merged
-//class ExpirationVerifierTest extends AbstractVerifierTest<ExpirationCaveatVerifier> {
-//
-//    ExpirationVerifierTest() {
-//        super(new ExpirationCaveatVerifier(getTokenPolicy()));
-//    }
-//
-//    @Override
-//    MacaroonCaveat getNonMatchingCaveat() {
-//        return new MacaroonCaveat("nothing", MacaroonCaveat.Operator.EQ, "nothing");
-//    }
-//
-//    @Override
-//    MacaroonCaveat getWrongCaveat() {
-//        return new MacaroonCaveat("expires", MacaroonCaveat.Operator.EQ, LocalDate.of(1990, 1, 1).atStartOfDay().atOffset(ZoneOffset.UTC).toString());
-//    }
-//
-//    @Override
-//    MacaroonCaveat getCorrectCaveat() {
-//        return new MacaroonCaveat("expires", MacaroonCaveat.Operator.EQ, OffsetDateTime.now().plus(2, ChronoUnit.YEARS).toString());
-//    }
-//
-//    @Override
-//    String provideFailureMessage() {
-//        return ExpirationCaveatVerifier.CAVEAT_INVALID;
-//    }
-//}
+package gov.cms.dpc.attribution.macaroons;
+
+import gov.cms.dpc.macaroons.MacaroonCaveat;
+import org.junit.jupiter.api.Disabled;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+
+class ExpirationVerifierTest extends AbstractVerifierTest<ExpirationCaveatVerifier> {
+
+    ExpirationVerifierTest() {
+        super(new ExpirationCaveatVerifier(getTokenPolicy()));
+    }
+
+    @Override
+    MacaroonCaveat getNonMatchingCaveat() {
+        return new MacaroonCaveat("nothing", MacaroonCaveat.Operator.EQ, "nothing");
+    }
+
+    @Override
+    MacaroonCaveat getWrongCaveat() {
+        return new MacaroonCaveat("expires", MacaroonCaveat.Operator.EQ, LocalDate.of(1990, 1, 1).atStartOfDay().atOffset(ZoneOffset.UTC).toString());
+    }
+
+    @Override
+    MacaroonCaveat getCorrectCaveat() {
+        return new MacaroonCaveat("expires", MacaroonCaveat.Operator.EQ, OffsetDateTime.now().plus(2, ChronoUnit.YEARS).toString());
+    }
+
+    @Override
+    String provideFailureMessage() {
+        return ExpirationCaveatVerifier.CAVEAT_INVALID;
+    }
+}
