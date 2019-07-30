5de60f4803c3c873d284b52fd12f704be0c50751
H3DU <marc.p.harbatkin@omb.dop.gov>
2019-07-29 22:28:44 -0400

    Fixed up devise helper

--- dpc-web/app/helpers/devise_helper.rb
+++ dpc-web/app/helpers/devise_helper.rb
@@ -2,29 +2,34 @@
 
 module DeviseHelper
   def devise_error_messages!
-    flash_alerts = []
-    error_key = 'errors.messages.not_saved'
+    return if flash.notice.blank? && flash.alert.blank? || flash.notice =~ /signed out/i
+    return notice_msg.html_safe if flash.notice
 
-    unless flash.empty?
-      flash_alerts.push(flash[:error]) if flash[:error]
-      flash_alerts.push(flash[:alert]) if flash[:alert]
-      flash_alerts.push(flash[:notice]) if flash[:notice]
-      error_key = 'devise.failure.invalid'
-    end
+    incorrect_email_or_password_msg.html_safe
+  end
 
-    return '' if resource.errors.empty? && flash_alerts.empty?
+  def notice_msg
+    <<-HTML
+      <div class="ds-c-alert ds-u-margin-bottom--5">
+        <div class="ds-c-alert__body">
+          <h3 class="ds-c-alert__heading">#{flash.notice}</h3>
+        </div>
+      </div>
+    HTML
+  end
 
-    errors = resource.errors.empty? ? flash_alerts : resource.errors.full_messages
-
-    html = <<-HTML
+  def incorrect_email_or_password_msg
+    <<-HTML
     <div class="ds-c-alert ds-c-alert--error">
       <div class="ds-c-alert__body">
         <h3 class="ds-c-alert__heading">Your email or password is incorrect.</h3>
-        <p class="ds-c-alert__text">If you forgot your password, please email <a href="mailto:dpcinfo@cms.hhs.gov?subject=Forgot password">dpcinfo@cms.hhs.gov</a> to request a reset.</p>
+        <p class="ds-c-alert__text">
+          If you forgot your password, please email
+          <a href="mailto:dpcinfo@cms.hhs.gov?subject=Forgot password">dpcinfo@cms.hhs.gov</a>
+          to request a reset.
+        </p>
       </div>
     </div>
     HTML
-
-    html.html_safe
   end
 end
