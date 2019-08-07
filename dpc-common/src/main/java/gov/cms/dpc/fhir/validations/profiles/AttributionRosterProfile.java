ec0682137a1aee19ebf56b030185f5bba3d3d5eb
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-08-07 11:08:12 -0400

    DPC-293: Attribution Roster validations are passing.

---
+++ dpc-common/src/main/java/gov/cms/dpc/fhir/validations/profiles/AttributionRosterProfile.java
@@ -0,0 +1,16 @@
+package gov.cms.dpc.fhir.validations.profiles;
+
+public class AttributionRosterProfile implements IProfileLoader {
+
+    public static final String PROFILE_URI = "https://dpc.cms.gov/fhir/v1/StructureDefinition/dpc-profile-attribution-roster";
+
+    @Override
+    public String getPath() {
+        return "validations/DPCAttributionRoster.json";
+    }
+
+    @Override
+    public String getURI() {
+        return PROFILE_URI;
+    }
+}
--- dpc-common/src/test/java/gov/cms/dpc/fhir/validations/ProfileValidatorTests.java
+++ dpc-common/src/test/java/gov/cms/dpc/fhir/validations/ProfileValidatorTests.java
@@ -18,6 +18,6 @@ class ProfileValidatorTests {
 
         final List<StructureDefinition> definitions = support.fetchAllStructureDefinitions(ctx);
 
-        assertEquals(5, definitions.size(), "Should not have malformed or invalid resources");
+        assertEquals(6, definitions.size(), "Should not have malformed or invalid resources");
     }
 }
