52a3fd227b8829231e6dac68092d05d42c92ab0a
Ron Heft <52176593+ronaldheft-usds@users.noreply.github.com>
2019-08-21 12:21:57 -0400

    DPC-558: Jenkins Integration Tests (#199)
    * DPC-558: Fix integration tests running when disabled
    
    * DPC-558: Update dpc-test script to handle installing dependencies
    
    * DPC-558: Exclude more files from the Docker build for faster builds
    
    * DPC-558: Speed up dpc-web Docker builds
    
    * DPC-558: Run integration tests during dpc-web build
    
    * DPC-558: Remove sleep in favor of blocking test script
    
    * DPC-558: Fix docker-compose start order

--- dpc-web-test.sh
+++ dpc-web-test.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
+set -e
 
-if [ -n "$TRAVIS_SECURE_ENV_VARS" ] && [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]; then
-   echo "┌──────────────────────────────────────────┐"
-   echo "│                                          │"
-   echo "│        Running Website Tests...          │"
-   echo "│                                          │"
-   echo "└──────────────────────────────────────────┘"
+echo "┌──────────────────────────────────────────┐"
+echo "│                                          │"
+echo "│        Running Website Tests...          │"
+echo "│                                          │"
+echo "└──────────────────────────────────────────┘"
 
-   docker-compose down
-   docker-compose -f dpc-web/docker-compose.yml build
-   docker-compose -f dpc-web/docker-compose.yml run web rails db:migrate db:seed
+# Build the container
+make website
 
-   echo "┌──────────────────────────────────────────┐"
-   echo "│                                          │"
-   echo "│        All Website Tests Complete        │"
-   echo "│                                          │"
-   echo "└──────────────────────────────────────────┘"
-else
-    echo "┌──────────────────────────────────────────┐"
-    echo "│                                          │"
-    echo "│          Skipping Website Tests          │"
-    echo "│          (No Secure Environment)         │"
-    echo "│                                          │"
-    echo "└──────────────────────────────────────────┘"
-fi
+# Run the tests
+docker-compose -f dpc-web/docker-compose.yml run web rails db:create db:migrate db:seed
+docker-compose -f dpc-web/docker-compose.yml run web rails spec
+docker-compose -f dpc-web/docker-compose.yml down
+
+echo "┌──────────────────────────────────────────┐"
+echo "│                                          │"
+echo "│        All Website Tests Complete        │"
+echo "│                                          │"
+echo "└──────────────────────────────────────────┘"
