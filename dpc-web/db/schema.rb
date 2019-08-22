a70323dc684758b8df3622b2c40a998ee97b7a42
switzersc-usds <shelby.switzer@cms.hhs.gov>
2019-08-22 13:51:33 -0400

    Add reset_password_token index to users table
    Devise generators automatically set an index on the user table for
    reset_password_token, so let's do it to because this column is used
    to lookup users.

--- dpc-web/db/schema.rb
+++ dpc-web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_22_164911) do
+ActiveRecord::Schema.define(version: 2019_08_22_174930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2019_08_22_164911) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["last_name", "first_name"], name: "index_users_on_last_name_and_first_name"
     t.index ["organization"], name: "index_users_on_organization"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
