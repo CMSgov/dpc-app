9e32576153e3629afd8ee6c6505599b9ecaa8136
Jared Cunha <jcunha@omb.eop.gov>
2019-08-23 16:18:15 -0400

    fix image urls in mailers

--- dpc-web/config/environments/development.rb
+++ dpc-web/config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Devise requires mailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.asset_host = "http://localhost:3000"
 end
--- dpc-web/config/environments/production.rb
+++ dpc-web/config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "dpc-website_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.asset_host = "https://dpc.cms.gov"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
