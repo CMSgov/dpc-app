312d7d58226d531984e3c0c4e4451ec4e97ce0e1
switzersc-usds <shelby.switzer@cms.hhs.gov>
2019-08-23 14:36:27 -0400

    Update password reset feature spec
    To involve a successful reset after failed attempts, rather than just
    testing failed attempts. Decided to add this because I saw weird
    behavior dev where, after a failed reset attempt with an invalid
    password, I then got an error that the password reset token was
    invalid and I couldn't proceed even with a valid password.

--- dpc-web/spec/features/user_resets_password_spec.rb
+++ dpc-web/spec/features/user_resets_password_spec.rb
@@ -26,8 +26,8 @@ RSpec.feature 'user resets password' do
     end
   end
 
-  context 'when unsuccessful' do
-    before(:each) do
+  context 'with unsuccessful attempts' do
+    scenario 'user inputs invalid passwords before successful reset' do
       visit new_user_password_path
 
       fill_in 'user_email', with: user.email
@@ -40,25 +40,26 @@ RSpec.feature 'user resets password' do
       reset_link = last_delivery.body.raw_source.match(/href="http:\/\/localhost:3000(?<path>.+?)">/)[:path]
 
       visit reset_link
-    end
 
-    scenario 'user does not input matching password and password_confirmation' do
       fill_in 'user_password', with: "CrabW0rd$"
       fill_in 'user_password_confirmation', with: "Idon'tMatch"
       find('input[data-test="submit"]').click
 
       expect(page.body).to include("1 error prohibited this user from being saved:")
       expect(page.body).to include("Password confirmation doesn&#39;t match Password")
-    end
 
-
-    scenario 'user does not input valid password' do
       fill_in 'user_password', with: "crab"
       fill_in 'user_password_confirmation', with: "crab"
       find('input[data-test="submit"]').click
 
       expect(page.body).to include("1 error prohibited this user from being saved:")
       expect(page.body).to include("Password is too short")
+
+      fill_in 'user_password', with: "CrabW0rd$"
+      fill_in 'user_password_confirmation', with: "CrabW0rd$"
+      find('input[data-test="submit"]').click
+
+      expect(page.body).to include("Your password has been changed successfully")
     end
   end
 end
