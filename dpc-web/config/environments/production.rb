c310b6937a908bd8b78366e501e3f464d4e045ca
switzersc-usds <shelby.switzer@cms.hhs.gov>
2019-08-28 12:22:02 -0400

    WIP SMTP

--- dpc-web/config/environments/production.rb
+++ dpc-web/config/environments/production.rb
@@ -67,6 +67,16 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.asset_host = "https://dpc.cms.gov"
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV.fetch('SMTP_ADDRESS'),
+    port:                 ENV.fetch('SMTP_PORT'),
+    domain:               'dpc.cms.gov',
+    user_name:            ENV.fetch('SMTP_USER_NAME'),
+    password:             ENV.fetch('SMTP_PASSWORD'),
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
