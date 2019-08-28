a4ae5b8b938f8198adc2d51da32f6d6b4dc5bb57
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-08-28 15:32:05 -0400

    DPC-14: Organization removal and token management (#205)
    * DPC-14: Organization Listing now works
    
    We can look at which orgs are registered.
    
    * DPC-14: Organization resource now supports deletion.
    
    The deletion cascades to all associated resources, that's not great from a performance perspective, but it's good enough for now.
    
    * DPC-14: Deletion command is working
    
    * DPC-18: Token listing is wired in.
    
    Basic framework for listing tokens
    
    * DPC-18: Token listing is working correctly
    
    Organization endpoint now returns a token response, which gives more information about the type and expiration time.
    
    * DPC-18: Refactored Command hierarchy
    
    Commands now inherit from abstract classes, which makes things a bit easier to user and reason about.
    
    * DPC-18: Token deletion is working.
    
    We can now remove tokens from an organization, it's a little awkward, but should be improved as part of DPC-545.
    
    * DPC-14: Renamed Organization commands
    
    Org commands have been cleaned up and renamed.
    
    * DPC-18: Token creation and deletion is working.
    
    Cleaned up the way we're handling TokenEntity relationships in Hibernate, we can now add and remove tokens with no issue.
    
    * DPC-18: Simple cli tests
    
    Added a really basic test for creating and deleting organization tokens.
    
    * DPC-18: Error prone and Swagger fixes
    
    Make error prone happy and add a couple of new Swagger annotations.
    
    * DPC-14: Fix path handling for org command
    
    The Organization registration command now builds an entire path from the relative file link, which makes debugging easier and path handling more robust.
    
    * DPC-14: CodeClimate cleanups
    
    Make CodeClimate happy.

---
+++ dpc-api/src/test/java/gov/cms/dpc/api/cli/TokenTests.java
@@ -0,0 +1,117 @@
+package gov.cms.dpc.api.cli;
+
+import gov.cms.dpc.api.AbstractApplicationTest;
+import gov.cms.dpc.api.DPCAPIConfiguration;
+import gov.cms.dpc.api.DPCAPIService;
+import gov.cms.dpc.api.cli.tokens.TokenDelete;
+import gov.cms.dpc.api.cli.tokens.TokenList;
+import gov.cms.dpc.fhir.FHIRExtractors;
+import io.dropwizard.cli.Cli;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.util.JarLocation;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TokenTests extends AbstractApplicationTest {
+
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+    private final InputStream originalIn = System.in;
+
+    private final ByteArrayOutputStream stdOut = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream stdErr = new ByteArrayOutputStream();
+    private Cli cli;
+
+    TokenTests() {
+
+    }
+
+    @BeforeEach
+    void cliSetup() {
+        // Setup necessary mock
+        final JarLocation location = mock(JarLocation.class);
+        when(location.getVersion()).thenReturn(Optional.of("1.0.0"));
+
+        // Add commands you want to test
+        final Bootstrap<DPCAPIConfiguration> bootstrap = new Bootstrap<>(new DPCAPIService());
+        bootstrap.addCommand(new OrgRegistrationCommand());
+        bootstrap.addCommand(new TokenList());
+        bootstrap.addCommand(new TokenDelete());
+
+        // Redirect stdout and stderr to our byte streams
+        System.setOut(new PrintStream(stdOut));
+        System.setErr(new PrintStream(stdErr));
+
+        // Build what'll run the command and interpret arguments
+        cli = new Cli(location, bootstrap, stdOut, stdErr);
+    }
+
+    @AfterEach
+    void teardown() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+        System.setIn(originalIn);
+    }
+
+    @Test
+    void testTokenDeletion() throws Exception {
+        // Create the organization
+        final boolean success = cli.run("register", "-f", "../src/main/resources/organization.tmpl.json");
+
+        assertAll(() -> assertTrue(success, "Should have succeeded"),
+                () -> assertEquals("", stdErr.toString(), "Should not have errors"),
+                () -> assertTrue(stdOut.toString().contains("eyJ2IjoyLCJsIjoiaHR0"), "Should have token"));
+
+        // List the tokens
+        final String organizationID = "0c527d2e-2e8a-4808-b11d-0fa06baf8254";
+        final List<UUID> matchedTokenIDs = getTokenIDs(organizationID);
+        assertEquals(1, matchedTokenIDs.size(), "Should have a single token id");
+
+        // Try to remove it
+        stdOut.reset();
+        stdErr.reset();
+        final boolean s3 = cli.run("delete", "-o", organizationID, matchedTokenIDs.get(0).toString());
+
+        assertTrue(s3, "Should have succeeded");
+
+        final List<UUID> tokenIDs = getTokenIDs(organizationID);
+        assertTrue(tokenIDs.isEmpty(), "Should not have any tokens");
+    }
+
+    List<UUID> getTokenIDs(String organizationID) throws Exception {
+        stdOut.reset();
+        stdErr.reset();
+
+        final boolean s2 = cli.run("list", organizationID);
+
+        // Find all of the token IDs
+        final List<UUID> matchedTokenIDs = Pattern.compile("║\\s([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})")
+                .matcher(stdOut.toString())
+                .results()
+                .map(MatchResult::group)
+                .map(match -> match.replace("║ ", ""))
+                .map(UUID::fromString)
+                .collect(Collectors.toList());
+
+        assertAll(() -> assertTrue(s2, "Should have succeeded"),
+                () -> assertTrue(stdErr.toString().isEmpty(), "Should be empty"));
+
+        return matchedTokenIDs;
+    }
+
+}
