5ecc34b3057b0e798a8059bbb553479bf411f888
Shelby Switzer <54290681+switzersc-usds@users.noreply.github.com>
2019-08-28 09:11:30 -0400

    Enable ActiveJob with DelayedJob backend (#208)
    * Enable ActiveJob with DelayedJob backend
    
    Use DelayedJob as the backend for async job processing so that we can
    use Postgres for persistent job store rather than adding a new
    dependency. This also starts the DelayedJob process within the Rails
    container from docker/entrypoint.sh so that we don't need to have a new
    ECS task.
    
    * Update README with DelayedJob info

--- dpc-web/db/schema.rb
+++ dpc-web/db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_01_183311) do
+ActiveRecord::Schema.define(version: 2019_08_27_134448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+  end
 
   create_table "dpc_registrations", force: :cascade do |t|
     t.bigint "user_id", null: false
