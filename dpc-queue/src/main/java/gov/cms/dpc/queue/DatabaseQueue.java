c7dfe6ec754806f794d3e7d47c9d742a58fedc45
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2019-08-29 17:00:29 -0400

    DPC-499: Start setting up the new database queue

---
+++ dpc-queue/src/main/java/gov/cms/dpc/queue/DatabaseQueue.java
@@ -0,0 +1,186 @@
+package gov.cms.dpc.queue;
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import gov.cms.dpc.common.hibernate.DPCManagedSessionFactory;
+import gov.cms.dpc.common.utils.MetricMaker;
+import gov.cms.dpc.queue.annotations.HealthCheckQuery;
+import gov.cms.dpc.queue.exceptions.JobQueueFailure;
+import gov.cms.dpc.queue.exceptions.JobQueueUnhealthy;
+import gov.cms.dpc.queue.models.JobQueueBatch;
+import gov.cms.dpc.queue.models.JobResult;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Implements a distributed {@link gov.cms.dpc.queue.models.JobQueueBatch} using a Postgres database
+ */
+public class DatabaseQueue implements JobQueueInterface {
+
+    // Statics
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseQueue.class);
+    static final String DB_UNHEALTHY = "Database cluster is not responding";
+
+    // Object variables
+    private final SessionFactory factory;
+    private final String healthQuery;
+
+    // Metrics
+    private final Timer waitTimer; // The wait time for a job to start
+    private final Timer successTimer; // The work time a successful job takes
+    private final Timer failureTimer; // The work time a failed job takes
+
+
+    @Inject
+    public DatabaseQueue(
+            DPCManagedSessionFactory factory,
+            @HealthCheckQuery String healthQuery,
+            MetricRegistry metricRegistry
+    ) {
+        this.factory = factory.getSessionFactory();
+        this.healthQuery = healthQuery;
+
+        // Metrics
+        final var metricBuilder = new MetricMaker(metricRegistry, DatabaseQueue.class);
+        this.waitTimer = metricBuilder.registerTimer("waitTime");
+        this.successTimer = metricBuilder.registerTimer("successTime");
+        this.failureTimer = metricBuilder.registerTimer("failureTime");
+        metricBuilder.registerCachedGauge("queueLength", this::queueSize);
+    }
+
+    @Override
+    public void submitJob(JobQueueBatch job) {
+        assert (job.getStatus() == JobStatus.QUEUED);
+        logger.debug("Adding jobID {} batchID {} to the queue at {} with for organization {}.",
+                job.getJobID(),
+                job.getBatchID(),
+                job.getSubmitTime().orElseThrow().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME),
+                job.getOrgID());
+
+        // Persist the job in postgres
+        try (final Session session = this.factory.openSession()) {
+            final Transaction tx = session.beginTransaction();
+            try {
+                session.persist(job);
+                tx.commit();
+            } catch (Exception e) {
+                logger.error("Cannot add job to database", e);
+                tx.rollback();
+                throw new JobQueueFailure(job.getJobID(), job.getBatchID(), e);
+            }
+        }
+    }
+
+    @Override
+    public Optional<JobQueueBatch> getJobBatch(UUID jobID, UUID batchID) {
+        // Get from Postgres
+        try (final Session session = this.factory.openSession()) {
+
+            final Transaction tx = session.beginTransaction();
+            try {
+                final CriteriaBuilder builder = session.getCriteriaBuilder();
+                final CriteriaQuery<JobQueueBatch> query = builder.createQuery(JobQueueBatch.class);
+                final Root<JobQueueBatch> root = query.from(JobQueueBatch.class);
+
+                query.select(root);
+                query.where(
+                        builder.equal(root.get("jobID"), jobID),
+                        builder.equal(root.get("batchId"), batchID)
+                );
+
+                final JobQueueBatch job = session.createQuery(query).uniqueResult();
+                if (job == null) {
+                    return Optional.empty();
+                }
+                session.refresh(job);
+                return Optional.of(job);
+            } finally {
+                tx.commit();
+            }
+        }
+    }
+
+    @Override
+    public List<JobQueueBatch> getJobBatches(UUID jobID) {
+        // Get from Postgres
+        try (final Session session = this.factory.openSession()) {
+
+            final Transaction tx = session.beginTransaction();
+            try {
+                final CriteriaBuilder builder = session.getCriteriaBuilder();
+                final CriteriaQuery<JobQueueBatch> query = builder.createQuery(JobQueueBatch.class);
+                final Root<JobQueueBatch> root = query.from(JobQueueBatch.class);
+
+                query.select(root);
+                query.where(
+                        builder.equal(root.get("jobID"), jobID)
+                );
+
+                final List<JobQueueBatch> jobList = session.createQuery(query).getResultList();
+                return jobList;
+            } finally {
+                tx.commit();
+            }
+        }
+    }
+
+    @Override
+    public Optional<JobQueueBatch> workJob() {
+        return Optional.empty();
+    }
+
+    @Override
+    public void completeJob(JobQueueBatch job, List<JobResult> results) {
+
+    }
+
+    @Override
+    public long queueSize() {
+        try (final Session session = this.factory.openSession()) {
+            try {
+                final CriteriaBuilder builder = session.getCriteriaBuilder();
+                final CriteriaQuery<Long> query = builder.createQuery(Long.class);
+                final Root<JobQueueBatch> root = query.from(JobQueueBatch.class);
+
+                query.select(builder.count(root));
+                query.where(
+                        builder.equal(root.get("status"), JobStatus.QUEUED)
+                );
+
+                return session.createQuery(query).getSingleResult();
+            } catch ( Exception e ) {
+                return 0;
+            }
+        }
+    }
+
+    @Override
+    public String queueType() {
+        return "Database Queue";
+    }
+
+    @Override
+    public void assertHealthy() {
+        try (final Session session = this.factory.openSession()) {
+            try {
+                @SuppressWarnings("rawtypes") final Query healthCheck = session.createSQLQuery(healthQuery);
+                healthCheck.getFirstResult();
+            } catch (Exception e) {
+                throw new JobQueueUnhealthy(DB_UNHEALTHY, e);
+            }
+        }
+    }
+}
---
+++ dpc-queue/src/main/java/gov/cms/dpc/queue/JobQueueInterface.java
@@ -0,0 +1,77 @@
+package gov.cms.dpc.queue;
+
+import gov.cms.dpc.queue.models.JobQueueBatch;
+import gov.cms.dpc.queue.models.JobResult;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Interface for submitting/retrieving export jobs to a backing queue.
+ */
+public interface JobQueueInterface {
+    /**
+     * Submit a job into the queue. The job's {@link JobStatus} will be set to `QUEUED`, meaning
+     * that the job is ready to run.
+     *
+     * @param job - the job model that describes the job
+     */
+    void submitJob(JobQueueBatch job);
+
+    /**
+     * Find a job in the queue, regardless of job status. Does not alter the job.
+     *
+     * @param jobID - the id of the job to search
+     * @param batchID = the id of the batch to search
+     * @return Optional that contains the found job if present
+     */
+    Optional<JobQueueBatch> getJobBatch(UUID jobID, UUID batchID);
+
+    /**
+     * Find all the batches of a given job, regardless of job status. Does not alter the job.
+     *
+     * @param jobID
+     * @return a list of batches part of the job if present
+     */
+    List<JobQueueBatch> getJobBatches(UUID jobID);
+
+    /**
+     * Find the next job that is ready to run. Alter the job's {@link JobStatus} to `RUNNING`.
+     *
+     * @return The job to work, if present.
+     */
+    Optional<JobQueueBatch> workJob();
+
+    /**
+     * Alter the job's {@link JobStatus} to passed status. Called when the job batch is finished.
+     *
+     * @param job - the job
+     * @param results - The new counts for each job resource type.
+     */
+    void completeJob(JobQueueBatch job, List<JobResult> results);
+
+    /**
+     * Number of items in the queue.
+     * For the {@link MemoryQueue}, this returns the number of jobs with the {@link JobStatus#QUEUED}.
+     *
+     * @return - {@link long} number of jobs waiting to be run
+     */
+    long queueSize();
+
+    /**
+     * Returns the name of the type of underlying queue
+     *
+     * @return - {@link String} queue type
+     */
+    String queueType();
+
+    /**
+     * Determines if the underlying queue is healthy or not.
+     * This is accomplished by
+     * throws if not healthy
+     */
+    void assertHealthy();
+}
+
+
