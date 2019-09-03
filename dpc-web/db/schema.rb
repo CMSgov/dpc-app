dcb44e802735485f8dc97e0dab22f316f4cd0d50
Shelby Switzer <shelby.switzer@cms.hhs.gov>
2019-09-03 16:08:20 -0400

    Introduce InternalUser model and functionality (#214)
    * Start work on adding InternalUsers
    
    Internal users are users internal to CMS. They are a separate model and
    table because functionality will be much different for them and there
    is no expected overlap between users and internal users in terms of the
    people who will have these accounts and the types of tasks they will be
    performing.
    
    I followed this guide in Devise's wiki for creating a second model with
    Devise functionality: https://github.com/plataformatec/devise/wiki/How-to-Setup-Multiple-Devise-User-Models
    
    I left the omniauth_callbacks_controller for internal users because we
    talked about github auth for these users, so gonna leave this here til
    we make a decision on that.
    
    * Flesh out internal user functionality
    
    * Introduce 'internal' namespace for routes and files
    * Ensure internal users and users can't access the other's sign in page
    once they're logged in (prevent double login)
    * Route internal users to users index once logged in (and this page just
    says "Pending" right now)
    
    * Remove @users assignment from controller, not yet needed
    
    * Update for robucop style
    
    * Remove unnecessary files and comment
    
    * Rename internal/devise to internal/auth
    
    We saw a circular dependency warning in CI about using "Devise" as a
    module name, since the Devise gem already uses that module name in
    similar contexts.
    
    Also rewords some feature spec docstrings to make the language sound
    more natural in test output.
    
    * Remove unnecessary lines

--- dpc-web/db/schema.rb
+++ dpc-web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_22_174930) do
+ActiveRecord::Schema.define(version: 2019_08_27_164559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,25 @@ ActiveRecord::Schema.define(version: 2019_08_22_174930) do
     t.datetime "updated_at", null: false
     t.index ["access_level"], name: "index_dpc_registrations_on_access_level"
     t.index ["user_id"], name: "index_dpc_registrations_on_user_id"
+  end
+
+  create_table "internal_users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_internal_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_internal_users_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
