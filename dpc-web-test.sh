ee71d431910a9b91cfbc0ce906f9ea503d63a752
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-09-05 14:30:30 -0400

    Modified test script to start and wait for DB before proceeding
    Addressed comments in PR, hopefully this works to block until the DB is ready.

--- dpc-web-test.sh
+++ dpc-web-test.sh
@@ -11,6 +11,7 @@ echo "└───────────────────────�
 make website
 
 # Run the tests
+docker-compose -f dpc-web/docker-compose.yml up start_core_dependencies
 docker-compose -f dpc-web/docker-compose.yml run web rails db:create db:migrate db:seed
 docker-compose -f dpc-web/docker-compose.yml run web rails spec
 docker-compose -f dpc-web/docker-compose.yml down
