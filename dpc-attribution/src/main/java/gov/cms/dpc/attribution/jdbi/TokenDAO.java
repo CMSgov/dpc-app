21da40ec3cfdc7e9c21d0b81c7626eeefbb0fadc
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-09-06 16:22:36 -0400

    DPC-545: Refactor token endpoints (#220)
    * DPC-545: Initial token endpoint refactor
    
    Split off token operations into their own endpoints.
    
    * DPC-545: Attribution Tests are passing
    
    * merge master
    
    * DPC-545: API tests are passing
    
    * DPC-413: Organization creation now checks to see if the org exists, before creating
    
    Cleaned up the Organization resource tests to try and create the org on each test invocation, which makes them order independent.
    
    Also improved the actual creation process by checking to see if the org exists before creating it. This required fixed a bug in our NPI search capabilities. So that's good news.
    
    * DPC-545: Updated API Secure Tests
    
    API Authenticated tests are now passing and rocking. Forgot to update one of the test HTTP paths.
    
    * DPC-545: Codeclimate cleanups
    
    Removed superfluous and commented out methods.
    
    * DPC-545: Remove token expiration time from output
    
    Removing this until DPC-617 gets merged in, which will re-add the token expiration timestamp.

---
+++ dpc-attribution/src/main/java/gov/cms/dpc/attribution/jdbi/TokenDAO.java
@@ -0,0 +1,64 @@
+package gov.cms.dpc.attribution.jdbi;
+
+import gov.cms.dpc.common.entities.OrganizationEntity;
+import gov.cms.dpc.common.entities.OrganizationEntity_;
+import gov.cms.dpc.common.entities.TokenEntity;
+import gov.cms.dpc.common.entities.TokenEntity_;
+import gov.cms.dpc.common.hibernate.DPCManagedSessionFactory;
+import io.dropwizard.hibernate.AbstractDAO;
+import org.hibernate.query.Query;
+
+import javax.inject.Inject;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+import java.util.UUID;
+
+public class TokenDAO extends AbstractDAO<TokenEntity> {
+
+    @Inject
+    TokenDAO(DPCManagedSessionFactory factory) {
+        super(factory.getSessionFactory());
+    }
+
+    public TokenEntity persistToken(TokenEntity entity) {
+        // Check to ensure that the organization exists
+        final CriteriaBuilder builder = currentSession().getCriteriaBuilder();
+        final CriteriaQuery<Boolean> query = builder.createQuery(Boolean.class);
+        final Root<OrganizationEntity> root = query.from(OrganizationEntity.class);
+
+        query.select(builder.literal(true))
+                .where(builder.equal(root.get(OrganizationEntity_.id), entity.getOrganization().getId()));
+        final Query<Boolean> orgExists = this.currentSession().createQuery(query);
+        // If the org is not present, an exception will be thrown, which we can catch in the caller
+        orgExists.getSingleResult();
+
+        return persist(entity);
+    }
+
+    public List<TokenEntity> fetchTokens(UUID organizationID) {
+        final CriteriaBuilder builder = currentSession().getCriteriaBuilder();
+        final CriteriaQuery<TokenEntity> query = builder.createQuery(TokenEntity.class);
+        final Root<TokenEntity> root = query.from(TokenEntity.class);
+
+        query.where(builder.equal(root.get(TokenEntity_.organization).get(OrganizationEntity_.ID), organizationID));
+        return this.list(query);
+    }
+
+    public List<TokenEntity> findTokenByOrgAndID(UUID organizationID, UUID tokenID) {
+        final CriteriaBuilder builder = currentSession().getCriteriaBuilder();
+        final CriteriaQuery<TokenEntity> query = builder.createQuery(TokenEntity.class);
+        final Root<TokenEntity> root = query.from(TokenEntity.class);
+
+        query.where(builder.and(
+                builder.equal(root.get(TokenEntity_.id), tokenID.toString()),
+                builder.equal(root.get(TokenEntity_.organization).get(OrganizationEntity_.id), organizationID)));
+
+        return this.list(query);
+    }
+
+    public void deleteToken(TokenEntity entity) {
+        this.currentSession().delete(entity);
+    }
+}
