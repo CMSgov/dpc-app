a7bbe870227816de46234cb8bb071a49513fd4e2
switzersc-usds <shelby.switzer@cms.hhs.gov>
2019-09-06 11:11:04 -0400

    Change wrong password message now that we have PW rest

---
+++ dpc-web/spec/controllers/internal/users_controller_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Internal::UsersController, type: :controller do
+  describe '#index' do
+    context 'authenticated internal user' do
+      before(:each) do
+        internal_user = create(:internal_user)
+        sign_in internal_user, scope: :internal_user
+      end
+
+      it 'assigns @users to all users' do
+        users = create_list(:user, 2)
+        get :index
+        expect(assigns(:users)).to eq(users)
+      end
+
+      it 'renders the index template' do
+        get :index
+        expect(response).to render_template('index')
+      end
+    end
+
+    # TODO write shared examples for user and no user or internal user
+    # context 'invalud authentication'
+  end
+end
