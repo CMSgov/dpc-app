193d995e0728812e5cb7b60b97c39d172fe33122
Shelby Switzer <shelby.switzer@cms.hhs.gov>
2019-09-12 09:32:05 -0400

    As a CMS user I want to view users who have requested access. (#228)
    * WIP Users Index for internal users
    
    * Add controller test helpers for Devise
    * Add rails-controller-testing gem in order to use `assigns`. I want to
      use this for now to test that we're returning the users we want to
      from the controller. This may become unnecessary depending how this
      filter/search index evolves.
    
    * Change wrong password message now that we have PW rest
    
    * Work on users mgmt UI
    
    * Ignore .env for storing envs in local dev
    
    * More work on users views
    
    More work on users index page
    
    * MVPs of internal/users index and show
    
    * Functional on mobile and close enough to mockups for now. I want to
      start working on filter functionality at this point.
    * Update specs to look for email when internal user signs in. This seems
      like the most specific information for a user; we use their name if
      the email isn't available.
    
    * Add pagination with Kaminari
    
    * https://github.com/kaminari/kaminari
    * Default to 50 per page (my personal prefernece but we can change)
    * Add pagination links and info to users index
    * Also sort users by created_at DESC for now until we add sorting
    
    * Updates to appease rubocop
    
    * Add created_at index to users

--- dpc-web/app/controllers/internal/users_controller.rb
+++ dpc-web/app/controllers/internal/users_controller.rb
@@ -3,6 +3,13 @@
 module Internal
   class UsersController < ApplicationController
     before_action :authenticate_internal_user!
-    def index; end
+
+    def index
+      @users = User.order('created_at DESC').page params[:page]
+    end
+
+    def show
+      @user = User.find(params[:id])
+    end
   end
 end
