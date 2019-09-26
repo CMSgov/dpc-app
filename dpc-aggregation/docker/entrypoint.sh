aa674e56b51f89a841462131a1b8c8e427d2a96f
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-09-26 14:37:18 -0400

    DPC-562: Update docker entrypoint to download S3 config files
    Updated the Docker entrypoint to install the AWSCLI and sync the S3 config bucket on startup. This is behind the BOOTSTRAP environment variable, so it should only run when running in AWS.

--- dpc-aggregation/docker/entrypoint.sh
+++ dpc-aggregation/docker/entrypoint.sh
@@ -2,10 +2,27 @@
 
 set -e
 
+bootstrap_config() {
+  # Install the AWS CLI
+  apt-get update
+  apt-get -y install awscli
+
+  # Create the config directory
+  mkdir -p /config
+
+  # Sync the aws bucket
+  aws s3 sync s3://dpc-${ENV}-app-config/ config/
+}
+
 if [ -n "$JACOCO" ]; then
   JACOCO="-javaagent:/org.jacoco.agent-runtime.jar=destfile=/jacoco-report/jacoco-it.exec"
 else
   JACOCO=""
+fi
+
+if [ -n "$BOOTSTRAP" ]; then
+  echo "hello"
+  bootstrap_config
 fi
 
 CMDLINE="java ${JACOCO} -cp /app/resources:/app/classes:/app/libs/* gov.cms.dpc.aggregation.DPCAggregationService"
