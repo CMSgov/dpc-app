0657056afc03f871f7d3d5af09a8146d2d707bf0
Ron Heft <52176593+ronaldheft-usds@users.noreply.github.com>
2019-10-02 13:12:42 -0400

    DPC-642: Fix FHIR Content-Type headers (#248)
    * Add test to verify all API methods are metered
    
    * DPC-642: Fix missing FHIR Content-Type
    
    * DPC-642: Update documentation to indicate Content-Type header is required
    
    * DPC-642: Assert header Content-Type on E2E tests
    
    * DPC-642: Job status endpoint should be standard JSON
    
    * DPC-642: Fix failing test

--- dpc-common/src/main/java/gov/cms/dpc/fhir/annotations/FHIR.java
+++ dpc-common/src/main/java/gov/cms/dpc/fhir/annotations/FHIR.java
@@ -1,7 +1,5 @@
 package gov.cms.dpc.fhir.annotations;
 
-import gov.cms.dpc.fhir.FHIRMediaTypes;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import java.lang.annotation.*;
@@ -12,9 +10,7 @@ import java.lang.annotation.*;
  * Automatically adds the {@link Consumes}, and {@link Produces} annotations to restrict the possible inputs/outputs to the API
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Consumes({FHIRMediaTypes.FHIR_JSON})
 @Target({ElementType.TYPE, ElementType.METHOD})
-@Produces({FHIRMediaTypes.FHIR_JSON})
 @Inherited
 public @interface FHIR {
 }
