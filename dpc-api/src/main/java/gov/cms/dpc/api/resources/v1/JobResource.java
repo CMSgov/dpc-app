d9537a016879b956c2f10cf45b815f02f22de0af
Emily Hart <1923441+embh@users.noreply.github.com>
2019-10-09 16:50:39 -0400

    DPC-658: Improve export job response with data integrity elements (#287)

--- dpc-api/src/main/java/gov/cms/dpc/api/resources/v1/JobResource.java
+++ dpc-api/src/main/java/gov/cms/dpc/api/resources/v1/JobResource.java
@@ -6,6 +6,7 @@ import gov.cms.dpc.api.auth.OrganizationPrincipal;
 import gov.cms.dpc.api.models.JobCompletionModel;
 import gov.cms.dpc.api.resources.AbstractJobResource;
 import gov.cms.dpc.common.annotations.APIV1;
+import gov.cms.dpc.common.annotations.ExportPath;
 import gov.cms.dpc.fhir.FHIRExtractors;
 import gov.cms.dpc.queue.IJobQueue;
 import gov.cms.dpc.queue.JobStatus;
@@ -17,6 +18,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.bouncycastle.jcajce.provider.digest.SHA256;
+import org.bouncycastle.util.encoders.Hex;
 import org.eclipse.jetty.http.HttpStatus;
 import org.hl7.fhir.dstu3.model.ResourceType;
 import org.slf4j.Logger;
@@ -27,8 +30,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.FileInputStream;
 import java.time.OffsetDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
@@ -45,11 +52,13 @@ public class JobResource extends AbstractJobResource {
 
     private final IJobQueue queue;
     private final String baseURL;
+    private final String fileLocation;
 
     @Inject
-    public JobResource(IJobQueue queue, @APIV1 String baseURL) {
+    public JobResource(IJobQueue queue, @APIV1 String baseURL, @ExportPath String exportPath) {
         this.queue = queue;
         this.baseURL = baseURL;
+        this.fileLocation = exportPath;
     }
 
     @Override
@@ -173,10 +182,31 @@ public class JobResource extends AbstractJobResource {
                 .map(result -> new JobCompletionModel.OutputEntry(
                         result.getResourceType(),
                         String.format("%s/Data/%s.ndjson", this.baseURL, JobQueueBatchFile.formOutputFileName(result.getBatchID(), result.getResourceType(), result.getSequence())),
-                        result.getCount()))
+                        result.getCount(), buildExtension(result)))
                 .filter(entry -> (entry.getType() == ResourceType.OperationOutcome ^ !forOperationalOutcomes)
                         && entry.getCount() > 0)
                 .collect(Collectors.toList());
     }
 
+    public JobCompletionModel.OutputEntryExtension buildExtension(JobQueueBatchFile batchFile) {
+        String filePath = String.format("%s/%s.ndjson", fileLocation, JobQueueBatchFile.formOutputFileName(batchFile.getBatchID(), batchFile.getResourceType(), batchFile.getSequence()));
+        File file = new File(filePath);
+        String checksum = generateChecksum(file);
+        Long length = generateFileLength(file);
+        return new JobCompletionModel.OutputEntryExtension(checksum, length);
+    }
+
+    private String generateChecksum(File file) {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            byte[] digest = new SHA256.Digest().digest(fileInputStream.readAllBytes());
+            return String.format("%s:%s", "sha256", Hex.toHexString(digest));
+        } catch (Exception e) {
+            logger.error("Failed to generate checksum", e);
+            return "";
+        }
+    }
+
+    private Long generateFileLength(File file) {
+        return file != null ? file.length() : 0;
+    }
 }
