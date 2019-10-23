d2425154c2666f79e13dd2db2e5e9fdc0e2af913
Shelby Switzer <shelby.switzer@cms.hhs.gov>
2019-10-23 14:48:46 -0400

    DPC-684 - As an internal user, I want to assign users to an organization. (#260)
    * User belongs_to Organization
    
    * Add association between user and organization. Make this optional
      because new users won't have an organization, only a requested one
    * Refactor existing User column names (organization, organization_type,
      num_providers) to have `requested_` prefix, because those are user-
      input values and are technically requests, not fact.
    
    * Assign user to an organization
    
    * User belongs_to Organization
    
    * Add association between user and organization. Make this optional
      because new users won't have an organization, only a requested one
    * Refactor existing User column names (organization, organization_type,
      num_providers) to have `requested_` prefix, because those are user-
      input values and are technically requests, not fact.
    
    * Add functionality to user nav items
    
    I'm not a big fan of how I'm setting the "current" class for the
    assigned/unassigned sub-nav items, but it works for now.
    
    * style assigned org box on users#show
    
    * WIP user search refactor
    
    * style assigned users in index
    
    * improve assigning orgs hint and correct label for attribute
    
    * left nav css fix
    
    * WIP User org association refactoring
    
    * Resolve ambiguous column error
    
    * Abide by the rubocop
    
    * Fix rebase schema
    
    * Refactor keyword search to be a User scope
    
    * These should be scopes anyway per convention
    * Adds tests for the scope
    * Fixes too-long-of-line error from rubocop

--- dpc-web/app/models/user.rb
+++ dpc-web/app/models/user.rb
@@ -7,8 +7,10 @@ class User < ApplicationRecord
   has_one :dpc_registration, inverse_of: :user
   has_many :taggings, as: :taggable
   has_many :tags, through: :taggings
+  has_many :organization_user_assignments
+  has_many :organizations, through: :organization_user_assignments
 
-  before_save :num_providers_to_zero_if_blank
+  before_save :requested_num_providers_to_zero_if_blank
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable,
@@ -17,9 +19,12 @@ class User < ApplicationRecord
          :validatable, :trackable, :registerable,
          :timeoutable, :recoverable
 
+  enum requested_organization_type: ORGANIZATION_TYPES
+
+  validates :requested_organization_type, inclusion: { in: ORGANIZATION_TYPES.keys }
   validates :last_name, :first_name, presence: true
-  validates :organization, presence: true
-  validates :num_providers, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+  validates :requested_organization, presence: true
+  validates :requested_num_providers, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
   validates :address_1, presence: true
   validates :city, presence: true
   validates :state, inclusion: { in: Address::STATES.keys.map(&:to_s) }
@@ -28,9 +33,35 @@ class User < ApplicationRecord
     in: [true], message: 'you must agree to the terms of service to create an account'
   }
 
+  scope :assigned, -> do
+    left_joins(:organization_user_assignments).where('organization_user_assignments.id IS NOT NULL')
+  end
+
+  scope :unassigned, -> do
+    left_joins(:organization_user_assignments).where('organization_user_assignments.id IS NULL')
+  end
+
+  scope :assigned_non_vendor, -> do
+    joins(:organizations)
+      .where('organizations.organization_type <> :vendor', vendor: ORGANIZATION_TYPES['health_it_vendor'])
+  end
+
+  scope :assigned_vendor, -> do
+    joins(:organizations)
+      .where('organizations.organization_type = :vendor', vendor: ORGANIZATION_TYPES['health_it_vendor'])
+  end
+
+  scope :by_keyword, ->(keyword) do
+    where(
+      'LOWER(users.first_name) LIKE :keyword OR LOWER(users.last_name) LIKE :keyword OR
+      LOWER(users.email) LIKE :keyword',
+      keyword: "%#{keyword.downcase}%"
+    )
+  end
+
   def self.to_csv
-    attrs = %w[id first_name last_name email organization organization_type address_1 address_2
-               city state zip agree_to_terms num_providers created_at updated_at]
+    attrs = %w[id first_name last_name email requested_organization requested_organization_type
+               address_1 address_2 city state zip agree_to_terms requested_num_providers created_at updated_at]
 
     CSV.generate(headers: true) do |csv|
       csv << attrs
@@ -44,9 +75,13 @@ class User < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  def primary_organization
+    organizations.first
+  end
+
   private
 
-  def num_providers_to_zero_if_blank
-    self.num_providers = 0 if num_providers.blank?
+  def requested_num_providers_to_zero_if_blank
+    self.requested_num_providers = 0 if requested_num_providers.blank?
   end
 end
--- dpc-web/spec/factories/users.rb
+++ dpc-web/spec/factories/users.rb
@@ -6,8 +6,8 @@ FactoryBot.define do
     sequence(:last_name) { |n| "last_name_#{n}" }
     sequence(:first_name) { |n| "first_name_#{n}" }
 
-    organization { 'Amalgamated Lint' }
-    organization_type { 'primary_care_clinic' }
+    requested_organization { 'Amalgamated Lint' }
+    requested_organization_type { 'primary_care_clinic' }
 
     address_1 { '1234 Shut the Door Ave.' }
     city { 'Pecoima' }
@@ -20,6 +20,20 @@ FactoryBot.define do
 
     trait :zip_plus_4 do
       zip { '12345-6789' }
+    end
+
+    trait :vendor do
+      after(:create) do |user, evaluator|
+        vendor = create(:organization, organization_type: 'health_it_vendor')
+        user.organizations << vendor
+      end
+    end
+
+    trait :assigned do
+      after(:create) do |user, evaluator|
+        organization = create(:organization, organization_type: 'urgent_care')
+        user.organizations << organization
+      end
     end
   end
 end
