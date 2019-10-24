d89a38e46ea76121998acf595614e306010f2eee
Emily Hart <1923441+embh@users.noreply.github.com>
2019-10-24 12:25:51 -0400

    1-800-MEDICARE file ETL setup

--- dpc-consent/src/main/java/gov/cms/dpc/consent/DPCConsentService.java
+++ dpc-consent/src/main/java/gov/cms/dpc/consent/DPCConsentService.java
@@ -4,6 +4,7 @@ import ca.mestevens.java.configuration.bundle.TypesafeConfigurationBundle;
 import com.codahale.metrics.jersey2.InstrumentedResourceMethodApplicationListener;
 import com.hubspot.dropwizard.guicier.GuiceBundle;
 import com.squarespace.jersey2.guice.JerseyGuiceUtils;
+import gov.cms.dpc.common.hibernate.attribution.DPCHibernateBundle;
 import gov.cms.dpc.common.utils.EnvironmentParser;
 import io.dropwizard.Application;
 import io.dropwizard.db.PooledDataSourceFactory;
@@ -20,6 +21,7 @@ import java.sql.SQLException;
 public class DPCConsentService extends Application<DPCConsentConfiguration> {
 
     private static final Logger logger = LoggerFactory.getLogger(DPCConsentService.class);
+    private final DPCHibernateBundle<DPCConsentConfiguration> hibernateBundle = new DPCHibernateBundle<>();
 
     public static void main(final String[] args) throws Exception {
         new DPCConsentService().run(args);
@@ -34,6 +36,12 @@ public class DPCConsentService extends Application<DPCConsentConfiguration> {
     public void initialize(Bootstrap<DPCConsentConfiguration> bootstrap) {
         JerseyGuiceUtils.reset();
 
+        GuiceBundle<DPCConsentConfiguration> guiceBundle = GuiceBundle.defaultBuilder(DPCConsentConfiguration.class)
+                .modules(new ConsentAppModule())
+                .build();
+
+        bootstrap.addBundle(hibernateBundle);
+        bootstrap.addBundle(guiceBundle);
         bootstrap.addBundle(new TypesafeConfigurationBundle("dpc.consent"));
         bootstrap.addBundle(new MigrationsBundle<DPCConsentConfiguration>() {
             @Override
--- dpc-consent/src/main/java/gov/cms/dpc/consent/jdbi/ConsentDAO.java
+++ dpc-consent/src/main/java/gov/cms/dpc/consent/jdbi/ConsentDAO.java
@@ -1,15 +1,39 @@
 package gov.cms.dpc.consent.jdbi;
 
 import gov.cms.dpc.common.entities.ConsentEntity;
+import gov.cms.dpc.common.entities.OrganizationEntity;
 import gov.cms.dpc.common.hibernate.attribution.DPCManagedSessionFactory;
 import io.dropwizard.hibernate.AbstractDAO;
 
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public class ConsentDAO extends AbstractDAO<ConsentEntity> {
 
     @Inject
-    ConsentDAO(DPCManagedSessionFactory factory) {
+    public ConsentDAO(DPCManagedSessionFactory factory) {
         super(factory.getSessionFactory());
+    }
+
+    public ConsentEntity persistConsent(ConsentEntity consent) {
+        return this.persist(consent);
+    }
+
+    public Optional<ConsentEntity> getConsent(UUID consentID) {
+        return Optional.ofNullable(get(consentID));
+    }
+
+    public List<ConsentEntity> getConsentsByHICN(String hicn) {
+        EntityManager entityManager = currentSession().getEntityManagerFactory().createEntityManager();
+        return entityManager
+                .createQuery("select c from Consent c where c.hicn = :hicn", ConsentEntity.class)
+                .getResultList();
     }
 }
