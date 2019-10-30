0203bbfcadb02c1161dfcfa42953135cf24a1011
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-10-30 13:35:25 -0400

    DPC-742: Update attribution info on documentation (#341)
    * DPC-741: PUTs are now POSTs, like they always should have been.
    
    * DPC-743: Update documentation with correct formats and query types
    
    Updated the documentation to be correct regarding searching for provider groups.
    
    Fixed the characteristic values to correctly show that they're arrays, rather than just objects.
    
    * DPC-742: Add expiration details to attribution section
    
    The attribution docs now actually mention attribution and have updated resources which show it.
    
    * DPC-734: Added Produces and Consumes annotations to FHIR annotations
    
    Re-added the media type annotations to the FHIR annotation. We need them in order to convey to Swagger that we have a FHIR media type, otherwise it defaults to json.

--- dpc-common/src/main/java/gov/cms/dpc/fhir/annotations/FHIR.java
+++ dpc-common/src/main/java/gov/cms/dpc/fhir/annotations/FHIR.java
@@ -1,5 +1,7 @@
 package gov.cms.dpc.fhir.annotations;
 
+import gov.cms.dpc.fhir.FHIRMediaTypes;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import java.lang.annotation.*;
@@ -7,10 +9,12 @@ import java.lang.annotation.*;
 
 /**
  * Marks the endpoint as complying with the FHIR specification.
- * Automatically adds the {@link Consumes}, and {@link Produces} annotations to restrict the possible inputs/outputs to the API
+ * Automatically adds the {@link Consumes}, and {@link Produces} annotations, in order to give Swagger some hints as to what's going on.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
+@Consumes({FHIRMediaTypes.FHIR_JSON})
+@Produces({FHIRMediaTypes.FHIR_JSON})
 @Inherited
 public @interface FHIR {
 }
