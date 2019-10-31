4962fca897cc5fc737ea81b8c25899e52f83e83f
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-10-31 09:14:24 -0400

    DPC-605: Persist ECC keys between application restarts. (#333)
    * DPC-605: Added admin task for generating Bakery Keypairs
    
    We can now generate a keypair using an admin task. Which can then be persisted for future use.
    
    * DPC-605: Moved BakeryKeyPair class into dpc-macaroons module
    
    Refactored the keypair classes and updated all the signatures to take BakeryKeyPair values, rather than Curve25519 pairs directly.
    
    * DPC-605: Added keypair to Docker containers
    
    Add the keypair into the build process for the Docker containers.
    
    Also cleaned up some ErrorProne warnings.
    
    * DPC-605: Fix default keypair location
    
    Is now relative when running local tests, and absolute for all the Docker deployments.
    
    Follows the patterns establishing in the BBClient keyStore settings.
    
    * DPC-605: Added S3 bootstrapping to Docker entrypoint
    
    Moved logging statement in KeyPair task to allow outputting the user ID creating the key.
    
    * DPC-605: Add environment checking to keypairs
    
    Added an `environment` field to the keypair files. During application startup, we now check to see if the keypair matches the environment. If not, we reject it.
    
    This way we can't accidentally use our public testing keypair for any of our real environments.

---
+++ dpc-api/src/main/java/gov/cms/dpc/api/models/KeyPairResponse.java
@@ -0,0 +1,82 @@
+package gov.cms.dpc.api.models;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import gov.cms.dpc.api.converters.BakeryKeyPairDeserializer;
+import gov.cms.dpc.api.converters.BakeryKeyPairSerializer;
+import gov.cms.dpc.common.converters.jackson.OffsetDateTimeToStringConverter;
+import gov.cms.dpc.common.converters.jackson.StringToOffsetDateTimeConverter;
+import gov.cms.dpc.macaroons.thirdparty.BakeryKeyPair;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+import java.time.OffsetDateTime;
+
+/**
+ * Response containing the
+ */
+public class KeyPairResponse {
+
+    @NotEmpty
+    private String algorithm;
+
+    @NotEmpty
+    private String environment;
+
+    @NotNull
+    @JsonSerialize(using = BakeryKeyPairSerializer.class)
+    @JsonDeserialize(using = BakeryKeyPairDeserializer.class)
+    private BakeryKeyPair keyPair;
+
+    @NotNull
+    @JsonDeserialize(converter = StringToOffsetDateTimeConverter.class)
+    @JsonSerialize(converter = OffsetDateTimeToStringConverter.class)
+    private OffsetDateTime createdOn;
+
+    @NotEmpty
+    private String createdBy;
+
+    public KeyPairResponse() {
+        // Jackson required
+    }
+
+    public String getAlgorithm() {
+        return algorithm;
+    }
+
+    public void setAlgorithm(String algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public BakeryKeyPair getKeyPair() {
+        return keyPair;
+    }
+
+    public void setKeyPair(BakeryKeyPair keyPair) {
+        this.keyPair = keyPair;
+    }
+
+    public OffsetDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(OffsetDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+}
--- dpc-common/src/main/java/gov/cms/dpc/common/utils/EnvironmentParser.java
+++ dpc-common/src/main/java/gov/cms/dpc/common/utils/EnvironmentParser.java
@@ -3,6 +3,8 @@ package gov.cms.dpc.common.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
+
 public class EnvironmentParser {
 
     private static final Logger logger = LoggerFactory.getLogger(EnvironmentParser.class);
@@ -15,14 +17,24 @@ public class EnvironmentParser {
      * @return - {@link String} Currently executing environment, local by default
      */
     public static String getEnvironment(String applicationName) {
-        String envVar = "local";
-        try {
-            envVar = System.getenv("ENV");
-        } catch (NullPointerException e) {
-            // If ENV isn't set, just ignore it.
-        }
+        return getEnvironment(applicationName, true);
+    }
 
-        logger.info("Starting {} Service in environment: {}", applicationName, envVar);
+    /**
+     * Gets the currently executing environment from the ENV environment variable
+     * Returns 'local' if no ENV is set.
+     *
+     * @param applicationName - {@link String} application name for logging purposes
+     * @param logEnv          - {@code true} log environment. {@code false} skip environment logging
+     * @return - {@link String} Currently executing environment, local by default
+     */
+    public static String getEnvironment(String applicationName, boolean logEnv) {
+        final String envVar = Optional.ofNullable(System.getenv("ENV")).orElse("local");
+
+
+        if (logEnv)
+            logger.info("Starting {} Service in environment: {}", applicationName, envVar);
+
         return envVar;
     }
 }
