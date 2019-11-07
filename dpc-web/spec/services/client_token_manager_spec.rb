e6bf35dc2b636250f90a38cd0dbf030220f5ce33
switzersc-usds <shelby.switzer@cms.hhs.gov>
2019-11-07 13:15:53 -0500

    Flesh out tests for client token creation
    Also remove logic from ClientTokenManager that creates a registered org
    if one doesn't exist yet when creating a client token. This shouldn't
    ever happen and we probably want to know when it does.

---
+++ dpc-web/spec/services/client_token_manager_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ClientTokenManager do
+  describe '#create_client_token' do
+    context 'successful API request' do
+      it 'responds true with @client_token set' do
+        org = create(:organization)
+        registered_org = create(:registered_organization, api_env: 0, organization: org)
+        token = { 'token' => 'exampleToken' }
+        api_client = instance_double(APIClient)
+        allow(APIClient).to receive(:new).with('sandbox').and_return(api_client)
+        allow(api_client).to receive(:create_client_token)
+          .with(registered_org.api_id, params: { label: 'Test Token 1' })
+          .and_return(api_client)
+        allow(api_client).to receive(:response_successful?).and_return(true)
+        allow(api_client).to receive(:response_body).and_return(token)
+
+        manager = ClientTokenManager.new(api_env: 'sandbox', organization: registered_org.organization)
+        expect(manager.create_client_token(label: 'Test Token 1')).to eq(true)
+        expect(manager.client_token).to eq(token)
+      end
+    end
+
+    context 'failed API request' do
+      it 'responds false' do
+        org = create(:organization)
+        registered_org = create(:registered_organization, api_env: 0, organization: org)
+        api_client = instance_double(APIClient)
+        allow(APIClient).to receive(:new).with('sandbox').and_return(api_client)
+        allow(api_client).to receive(:create_client_token)
+          .with(registered_org.api_id, params: { label: 'Test Token 1' })
+          .and_return(api_client)
+        allow(api_client).to receive(:response_successful?).and_return(false)
+        allow(api_client).to receive(:response_body).and_return(nil)
+
+        manager = ClientTokenManager.new(api_env: 'sandbox', organization: registered_org.organization)
+        expect(manager.create_client_token(label: 'Test Token 1')).to eq(false)
+        expect(manager.client_token).to eq(nil)
+      end
+    end
+  end
+end
