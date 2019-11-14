177fd6c241fa3e96ffd451a621e1caa4227aefc5
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-11-13 20:37:36 -0500

    Fix failing smoke tests in dev. (#382)
    * Fixes a number of issues related to failing smoketests
    
    Disables strict SSL checking, so we can run our tests against the internal ALBs and in Dev, which doesn't have a DNS record yet.
    
    Fixed bad serialization of Bakery Keypairs, they were double base64 encoded.
    
    Likewise, fixed double encoding of V1 binary macaroons, which are automatically base64 encoded.
    
    * Remove enabled authentication for local.

--- dpc-api/src/main/java/gov/cms/dpc/api/converters/BakeryKeyPairSerializer.java
+++ dpc-api/src/main/java/gov/cms/dpc/api/converters/BakeryKeyPairSerializer.java
@@ -6,13 +6,10 @@ import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import gov.cms.dpc.macaroons.thirdparty.BakeryKeyPair;
 
 import java.io.IOException;
-import java.util.Base64;
 
 public class BakeryKeyPairSerializer extends StdSerializer<BakeryKeyPair> {
 
     public static final long serialVersionUID = 42L;
-
-    private final Base64.Encoder encoder;
 
     BakeryKeyPairSerializer() {
         this(null);
@@ -20,16 +17,13 @@ public class BakeryKeyPairSerializer extends StdSerializer<BakeryKeyPair> {
 
     private BakeryKeyPairSerializer(Class<BakeryKeyPair> t) {
         super(t);
-        this.encoder = Base64.getMimeEncoder();
     }
 
     @Override
     public void serialize(BakeryKeyPair value, JsonGenerator gen, SerializerProvider provider) throws IOException {
-        final byte[] publicKey = this.encoder.encode(value.getPublicKey());
-        final byte[] privateKey = this.encoder.encode(value.getPrivateKey());
         gen.writeStartObject();
-        gen.writeBinaryField("public_key", publicKey);
-        gen.writeBinaryField("private_key", privateKey);
+        gen.writeBinaryField("public_key", value.getPublicKey());
+        gen.writeBinaryField("private_key", value.getPrivateKey());
         gen.writeEndObject();
     }
 }
