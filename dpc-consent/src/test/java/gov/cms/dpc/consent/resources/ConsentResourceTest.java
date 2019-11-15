90396a233e7d7ddbf71094224f092a66b4a12cc7
DeirdreHolub <deirdre@adhocteam.us>
2019-11-15 14:39:15 -0700

    DPC-690: API for Consent Service (#359)

---
+++ dpc-consent/src/test/java/gov/cms/dpc/consent/resources/ConsentResourceTest.java
@@ -0,0 +1,135 @@
+package gov.cms.dpc.consent.resources;
+
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.gclient.IQuery;
+import ca.uhn.fhir.rest.gclient.IReadExecutable;
+import ca.uhn.fhir.rest.gclient.StringClientParam;
+import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
+import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
+import gov.cms.dpc.common.consent.entities.ConsentEntity;
+import gov.cms.dpc.consent.AbstractConsentTest;
+import gov.cms.dpc.fhir.DPCIdentifierSystem;
+import gov.cms.dpc.fhir.converters.entities.ConsentEntityConverter;
+import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.Consent;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Tests ConsentResource using a live database. These tests will fail if the database is not running or
+ * seed data is not loaded.
+ */
+class ConsentResourceTest extends AbstractConsentTest {
+
+    private static final String TEST_CONSENT_UUID = "3a51c388-a9b0-42e2-afe4-688a2b6cef95";
+    private static final String TEST_CONSENT_REF = String.format("Consent/%s", TEST_CONSENT_UUID);
+
+    private ConsentResourceTest() {
+    }
+
+    @Test
+    final void getConsentResource_fails_withNonExtantId() {
+        final IGenericClient client = createFHIRClient(ctx, getServerURL());
+
+        final IReadExecutable<Consent> sut = client
+                .read()
+                .resource(Consent.class)
+                .withId("1")
+                .encodedJson();
+
+        assertThrows(ResourceNotFoundException.class, sut::execute, "should not find resource");
+    }
+
+    @Test
+    final void getConsentResource_finds_validId() {
+        final IGenericClient client = createFHIRClient(ctx, getServerURL());
+
+        final Consent sut = client
+                .read()
+                .resource(Consent.class)
+                .withId(TEST_CONSENT_UUID)
+                .encodedJson()
+                .execute();
+
+        assertEquals(ConsentEntityConverter.OPT_IN_MAGIC, sut.getPolicyRule());
+        assertEquals(TEST_CONSENT_REF, sut.getId());
+    }
+
+    @Test
+    final void searchConsentResource_fails_withNoParams() {
+        // an attempt to the resource with an empty value is routed to the search endpoint
+
+        final IGenericClient client = createFHIRClient(ctx, getServerURL());
+
+        final IQuery sut = client
+                .search()
+                .forResource(Consent.class)
+                .encodedJson();
+
+        assertThrows(InvalidRequestException.class, sut::execute, "should fail with no search params");
+    }
+
+    @Test
+    final void searchConsentResource_finds_validIdParam() {
+
+        final IGenericClient client = createFHIRClient(ctx, getServerURL());
+
+        final Bundle sut = client
+                .search()
+                .forResource(Consent.class)
+                .where(Consent.RES_ID.exactly().identifier(TEST_CONSENT_UUID))
+                .encodedJson()
+                .returnBundle(Bundle.class)
+                .execute();
+
+        final Consent found = (Consent) sut.getEntryFirstRep().getResource();
+
+        assertEquals(ConsentEntityConverter.OPT_IN_MAGIC, found.getPolicyRule());
+        assertEquals(TEST_CONSENT_REF, found.getId());
+    }
+
+    @Test
+    final void searchConsentResource_finds_validIdentifierParam() {
+
+        final IGenericClient client = createFHIRClient(ctx, getServerURL());
+
+        final Bundle sut = client
+                .search()
+                .forResource(Consent.class)
+                .where(Consent.IDENTIFIER.exactly().identifier(TEST_CONSENT_UUID))
+                .encodedJson()
+                .returnBundle(Bundle.class)
+                .execute();
+
+        final Consent found = (Consent) sut.getEntryFirstRep().getResource();
+
+        assertEquals(ConsentEntityConverter.OPT_IN_MAGIC, found.getPolicyRule());
+        assertEquals(TEST_CONSENT_REF, found.getId());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"MBI,mbi_1", "HICN,hicn_1"})
+    final void searchConsentResource_finds_validPatientParam(String system, String patientId) {
+
+        final IGenericClient client = createFHIRClient(ctx, getServerURL());
+        final String patientValue = String.format("%s|%s", DPCIdentifierSystem.valueOf(system).getSystem(), patientId);
+
+        final Bundle sut = client
+                .search()
+                .forResource(Consent.class)
+                .where(new StringClientParam("patient").matches().value(patientValue))
+                .encodedJson()
+                .returnBundle(Bundle.class)
+                .execute();
+
+        final Consent found = (Consent) sut.getEntryFirstRep().getResource();
+        System.out.println(ctx.forDstu3().newJsonParser().setPrettyPrint(true).encodeResourceToString(found));
+
+        assertEquals(ConsentEntityConverter.OPT_IN_MAGIC, found.getPolicyRule());
+        assertEquals(TEST_CONSENT_REF, found.getId());
+    }
+}
---
+++ dpc-consent/src/test/java/gov/cms/dpc/consent/resources/ConsentResourceUnitTest.java
@@ -0,0 +1,108 @@
+package gov.cms.dpc.consent.resources;
+
+import ca.uhn.fhir.context.FhirContext;
+import com.squarespace.jersey2.guice.JerseyGuiceUtils;
+import gov.cms.dpc.common.consent.entities.ConsentEntity;
+import gov.cms.dpc.consent.jdbi.ConsentDAO;
+import gov.cms.dpc.fhir.converters.entities.ConsentEntityConverter;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import io.dropwizard.testing.junit5.ResourceExtension;
+import org.eclipse.jetty.http.HttpStatus;
+import org.glassfish.jersey.test.grizzly.GrizzlyWebTestContainerFactory;
+import org.hl7.fhir.dstu3.model.Consent;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mockito;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static gov.cms.dpc.fhir.FHIRMediaTypes.FHIR_JSON;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests ConsentResource using a mocked DAO, making it possible to run these tests with a database running.
+ * The intent for these tests is to be able to test corner and edge cases in an easily controlled way.
+ */
+@Disabled
+@ExtendWith(DropwizardExtensionsSupport.class)
+public class ConsentResourceUnitTest {
+
+    static {
+        // otherwise our testContainer doesn't get assembled properly
+        JerseyGuiceUtils.reset();
+    }
+
+    private static final String TEST_HICN = "this_is_a_placeholder_hicn";
+    private static final String TEST_MBI = "this_is_a_placeholder_mbi";
+    private static final UUID TEST_ID = UUID.randomUUID();
+
+    private static final ConsentDAO mockedDAO = mock(ConsentDAO.class);
+    private static final GrizzlyWebTestContainerFactory testContainer = new GrizzlyWebTestContainerFactory();
+
+    public static final ResourceExtension resource = ResourceExtension.builder()
+            .addResource(new ConsentResource(mockedDAO, "http://test-url", "http://test-org-url"))
+            .setTestContainerFactory(testContainer)
+            .build();
+
+    private ConsentResourceUnitTest() {
+    }
+
+    @BeforeAll
+    static void initMock() {
+        // Broke when introducing findBy
+        ConsentEntity goodRecord = ConsentEntity.defaultConsentEntity(Optional.of(TEST_ID), Optional.of(TEST_HICN), Optional.of(TEST_MBI));
+        List<ConsentEntity> goodRecordList = List.of(goodRecord);
+        when(mockedDAO.getConsent(null)).thenThrow(new IllegalArgumentException("empty"));
+        when(mockedDAO.getConsent(TEST_ID)).thenReturn(Optional.of(goodRecord));
+        when(mockedDAO.findBy("mbi", TEST_MBI)).thenReturn(goodRecordList);
+        when(mockedDAO.findBy("hicn", TEST_HICN)).thenReturn(goodRecordList);
+    }
+
+    @Test
+    final void getResource_withValidId_returnsConsentResource() {
+        try (Response response = resource.target("/Consent/" + TEST_ID)
+                .request()
+                .accept(FHIR_JSON)
+                .get()) {
+
+            assertEquals(HttpStatus.OK_200, response.getStatus(), "should find record for test id");
+        }
+        // can't parse the result here; assuming this is a configuration issue as it throws a jackson mapping error
+        // final Consent resource = FhirContext.forDstu3().newJsonParser().parseResource(Consent.class, response.readEntity(InputStream.class));
+        // assertDoesNotThrow(() -> FhirContext.forDstu3().newJsonParser().encodeResourceToString(resource));
+    }
+
+    @Test
+    final void search_withEmptyString_isInvalid() {
+        try (Response response = resource.target("/Consent/")
+                .request()
+                .accept(FHIR_JSON)
+                .get()) {
+
+            assertEquals(HttpStatus.BAD_REQUEST_400, response.getStatus(), "should reject request as invalid");
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"/Consent?", "/Consent?_id=,patient=mbi_1", "/Consent?identifier=", "/Consent?patient=", "/Consent?owieurijefj"})
+    final void searchConsentResource_fails_withInvalidSearchParameters(String path) {
+        try (Response response = resource.target(path)
+                .request()
+                .accept(FHIR_JSON)
+                .get()) {
+
+            assertEquals(HttpStatus.NOT_FOUND_404, response.getStatus(), "invalid paths should not be found");
+        }
+    }
+}
