c374762b3789259e45107326023888b1053e666b
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-11-15 12:44:46 -0500

    DPC-540: Update exception handlers to improve logging
    ExceptionHandlers now log their exceptions and return the unique ID as part of their response.
    
    For FHIR errors, we just set the ID of the OperationOutcome to be the returned value.
    
    For everything else, we add it to the response body.

--- dpc-common/src/main/java/gov/cms/dpc/fhir/dropwizard/handlers/exceptions/PersistenceExceptionHandler.java
+++ dpc-common/src/main/java/gov/cms/dpc/fhir/dropwizard/handlers/exceptions/PersistenceExceptionHandler.java
@@ -42,15 +42,15 @@ public class PersistenceExceptionHandler extends AbstractFHIRExceptionHandler<Pe
 
     @Override
     Response handleFHIRException(PersistenceException exception) {
-
         final Pair<Response.Status, String> statusStringPair = handleResponseGeneration(exception);
+        final long exceptionID = this.logException(exception);
 
         final OperationOutcome outcome = new OperationOutcome();
+        outcome.setId(Long.toString(exceptionID));
         outcome.addIssue()
                 .setSeverity(OperationOutcome.IssueSeverity.FATAL)
                 .setDetails(new CodeableConcept().setText(statusStringPair.getRight()));
 
-        // TODO: Need to log and correlate this exception (DPC-540)
         return Response
                 .status(statusStringPair.getLeft())
                 .type(FHIRMediaTypes.FHIR_JSON)
@@ -60,8 +60,8 @@ public class PersistenceExceptionHandler extends AbstractFHIRExceptionHandler<Pe
 
     @Override
     Response handleNonFHIRException(PersistenceException exception) {
+        final long exceptionID = super.logException(exception);
         final Pair<Response.Status, String> statusStringPair = handleResponseGeneration(exception);
-        // TODO: Need to log and correlate this exception (DPC-540)
         return Response
                 .status(statusStringPair.getLeft())
                 .entity(statusStringPair.getRight())
