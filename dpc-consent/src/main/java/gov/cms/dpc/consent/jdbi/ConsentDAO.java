a3bd63480d299b4a019b0b017707bb8f467741da
Emily Hart <1923441+embh@users.noreply.github.com>
2019-11-15 09:33:10 -0500

    DPC-693: 1-800-MEDICARE ETL (#339)
    * 1-800-MEDICARE file ETL setup
    
    * Consent table migration, job fixes, tests
    
    * More ETL testing and cleanup
    
    * Synthetic data and test
    
    * Shared startJob/stopJob in test utils
    
    * Cleanup from Code Climate
    
    * Un-break failure from Code Climate change
    
    * Add .gitignore file so directory can be added to repo
    
    * Error handling; moving ConsentEntity
    
    * Hibernate classes for consent database
    
    * Use managed session factory; compiled pattern for 1-800 filename
    
    * Add Sundial dependency to parent POM
    
    * Consent db
    
    * Serial ID for InvalidSuppressionRecordException; more consent db changes
    
    * Transaction try/catch; renaming
    
    * Refactoring; file name and line number in exception; entity annotation change
    
    * Rename migrations file
    
    * Try with resources on buffered reader
    
    * Clean up log message and add exception
    
    * Re-add suppression file dir provider
    
    * Disable SuppressionFileImport job trigger

--- dpc-consent/src/main/java/gov/cms/dpc/consent/jdbi/ConsentDAO.java
+++ dpc-consent/src/main/java/gov/cms/dpc/consent/jdbi/ConsentDAO.java
@@ -1,15 +1,35 @@
 package gov.cms.dpc.consent.jdbi;
 
-import gov.cms.dpc.common.entities.ConsentEntity;
-import gov.cms.dpc.common.hibernate.attribution.DPCManagedSessionFactory;
+import gov.cms.dpc.common.hibernate.consent.DPCConsentManagedSessionFactory;
+import gov.cms.dpc.consent.entities.ConsentEntity;
 import io.dropwizard.hibernate.AbstractDAO;
 
 import javax.inject.Inject;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public class ConsentDAO extends AbstractDAO<ConsentEntity> {
 
     @Inject
-    ConsentDAO(DPCManagedSessionFactory factory) {
+    public ConsentDAO(DPCConsentManagedSessionFactory factory) {
         super(factory.getSessionFactory());
+    }
+
+    public ConsentEntity persistConsent(ConsentEntity consentEntity) {
+        return this.persist(consentEntity);
+    }
+
+    public Optional<ConsentEntity> getConsent(UUID consentID) {
+        return Optional.ofNullable(get(consentID));
+    }
+
+    public List<ConsentEntity> list() {
+        final CriteriaBuilder builder = currentSession().getCriteriaBuilder();
+        final CriteriaQuery<ConsentEntity> query = builder.createQuery(ConsentEntity.class);
+        query.from(ConsentEntity.class);
+        return list(query);
     }
 }
