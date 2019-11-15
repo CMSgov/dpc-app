ff8d9b7778dbd2b6b6fb81557a310680dfdac53c
Shelby Switzer <shelby.switzer@cms.hhs.gov>
2019-11-15 08:32:47 -0500

    DPC-593: As a user, I want to create client tokens in the dashboard. (#379)
    * Remove DpcRegistration model
    
    This wasn't really being used. I think it had been originally intended
    as the model where API access and account info would be stored, but
    now that logic is tied to organization rather than user so it doesn't
    make sense to continue with this model which is tied to user.
    
    This commit also adds dependent: :destroy to
    organization_user_assignments in both Organization and User, as that
    was an oversight previously. We want to make sure the assignment records
    are deleted when the user or org is deleted.
    
    * Move old profile to be registrations#show
    
    * Work on client tokens management
    
    * Add ClientTokenManager service object to handle client token creation login
    
    * WIP Client token functionality
    
    * Add macaroons gem
    
    * Client token creation and view workflow
    
    * With feature tests
    * Remove client token destroy stuff until we're ready to finish it
    * Only show client token if api request was successful; figure out
      appropriate error message to show user later
    
    * Flesh out tests for client token creation
    
    Also remove logic from ClientTokenManager that creates a registered org
    if one doesn't exist yet when creating a client token. This shouldn't
    ever happen and we probably want to know when it does.
    
    * Add unit test for getting client tokens from api
    
    * Add test token for development view purposes, commented
    
    * fix unclosed section
    
    * dashboard grid fix
    
    * dashboard headings
    
    * unassigned user design
    
    * change button type
    
    * styles for token front-end
    
    * Format datetime strings from API in a helper
    
    * DRYs up the view code
    * Use strptime to get the hour/minutes
    * Tell user that this is UTC (until we add timezone support)
    
    * Do not break app for users if API Token GET fails
    
    Rescue error and return empty array. Log that there was an error.
    
    * WIP
    
    * WIP with new API functionality
    
    * More work on getting API integration working
    
    * Get auth with API working!
    
    * Abide by the cop
    
    * Add error handling for API connection issue
    
    * Remove temp fix for API auth
    
    * Add maxlength for client token label

--- dpc-web/app/models/dpc_registration.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class DpcRegistration < ApplicationRecord
-  belongs_to :user, inverse_of: :dpc_registration
-
-  enum access_level: { no_access: 0, synthesized: 1, production: 2 }
-
-  validates :access_level, inclusion: { in: access_levels.keys }
-end
