b738e2e4d4305fecb7b07d22773dd91fc03f208d
switzersc-usds <shelby.switzer@cms.hhs.gov>
2019-11-15 12:43:15 -0500

    Set Accept header for Key-related API requests
    Necessary to tell the API to respond with JSON.

--- dpc-web/app/services/api_client.rb
+++ dpc-web/app/services/api_client.rb
@@ -98,7 +98,7 @@ class APIClient
   def post_text_request(uri_string, text, query_params, token)
     uri = URI.parse uri_string
     uri.query = URI.encode_www_form(query_params)
-    text_headers = { 'Content-Type': 'text/plain' }.merge(auth_header(token))
+    text_headers = { 'Content-Type': 'text/plain', 'Accept': 'application/json' }.merge(auth_header(token))
 
     request = Net::HTTP::Post.new(uri.request_uri, text_headers)
     request.body = text
@@ -119,6 +119,6 @@ class APIClient
   end
 
   def headers(token)
-    { 'Content-Type': 'application/json' }.merge(auth_header(token))
+    { 'Content-Type': 'application/json', 'Accept': 'application/json' }.merge(auth_header(token))
   end
 end
