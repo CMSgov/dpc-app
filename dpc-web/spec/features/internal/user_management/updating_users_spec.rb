464e198f15208d9c792ce7d35f6f826a04b458f1
Shelby Switzer <shelby.switzer@cms.hhs.gov>
2019-11-18 09:59:50 -0500

    DPC-721: Enable internal user to create org from user page (#397)
    * Enable internal user to create org from user page
    
    If a user requests an org and that org doesn't exist, the internal user
    managing that user can create an organization directly from the user's
    edit page. The internal user clicks a link and then the new organization
    form is populated on the organziations#new page with the user's
    requested org info, so the internal user can modify or add anything as
    needed. Then the internal user saves the org and gets redirected back to
    the user's edit page and they can carry on from there.
    
    * add interaction and design to reqorg converter
    
    * Fix select expectation in test

--- dpc-web/spec/features/internal/user_management/updating_users_spec.rb
+++ dpc-web/spec/features/internal/user_management/updating_users_spec.rb
@@ -81,4 +81,59 @@ RSpec.feature 'updating users' do
       expect(page).to have_content('Yellow')
     end
   end
+
+  scenario 'assigning new organization from a user\'s requested organization' do
+    crabby = create(:user, first_name: 'Crab', last_name: 'Olsen', email: 'co@beach.com',
+                           requested_organization: 'Happy Health', requested_organization_type: 3,
+                           address_1: '55 Euphoria Dr', address_2: '#1', city: 'Blytheville',
+                           state: 'CO', zip: '80313', requested_num_providers: 999)
+
+    visit internal_user_path(crabby)
+
+    find('[data-test="assign-org-link"]').click
+
+    # On edit user page
+    expect(page.body).to have_content('Happy Health')
+    expect(page.body).to have_content('Inpatient Facility')
+
+    find('[data-test="convert-org-link"]').click
+
+    # On new org page
+    expect(page).to have_css('[data-test="new-org-page"]')
+    name_field = find(:css, 'input#organization_name').value
+    expect(name_field).to eq('Happy Health')
+    type_field = find(:css, 'select#organization_organization_type').value
+    expect(type_field).to eq('inpatient_facility')
+    num_field = find(:css, 'input#organization_num_providers').value
+    expect(num_field).to eq('999')
+    address_1_field = find(:css, 'input#organization_address_attributes_street').value
+    expect(address_1_field).to have_content('55 Euphoria Dr')
+    address_2_field = find(:css, 'input#organization_address_attributes_street_2').value
+    expect(address_2_field).to have_content('#1')
+    city_field = find(:css, 'input#organization_address_attributes_city').value
+    expect(city_field).to have_content('Blytheville')
+    state_field = find(:css, 'select#organization_address_attributes_state').value
+    expect(state_field).to have_content('CO')
+    zip_field = find(:css, 'input#organization_address_attributes_zip').value
+    expect(zip_field).to have_content('80313')
+    find('[data-test="form-submit"]').click
+
+    # Back on user edit page
+    expect(page).to have_select('user_organization_ids', selected: 'Happy Health')
+    find('[data-test="user-form-submit"]').click
+
+    expect(page).not_to have_css('[data-test="user-form-submit"]')
+    expect(page).not_to have_css('[data-test="assign-org-link"]')
+    expect(page.body).to have_content('Happy Health')
+
+    click_link 'Happy Health'
+
+    expect(page).to have_content('Inpatient Facility')
+    expect(page).to have_content('55 Euphoria Dr')
+    expect(page).to have_content('#1')
+    expect(page).to have_content('Blytheville')
+    expect(page).to have_content('CO')
+    expect(page).to have_content('80313')
+    expect(page).to have_content('999')
+  end
 end
