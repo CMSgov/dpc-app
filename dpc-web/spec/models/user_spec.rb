3e95044e77fdcf7928c47d1d40ca491d994c809e
switzersc-usds <shelby.switzer@cms.hhs.gov>
2019-11-22 16:37:28 -0500

    Make sure users give a min of 1 for num providers
    Also only validate requested_num_providers on create: this is pretty
    much the only time a user sees this field anyway, unless they edit their
    profile. This field also becomes pretty meaningless after an org is
    created and a user is added to an org, because the org's num_providers
    is what will continue to be seen/updated.
    
    This commit fixes the bug recently introduced causing internal users to
    be unable to assign a user to an org if the user's
    requested_num_providers is 0 (from before we added the validation).

--- dpc-web/spec/models/user_spec.rb
+++ dpc-web/spec/models/user_spec.rb
@@ -64,40 +64,52 @@ RSpec.describe User, type: :model do
   end
 
   describe '#requested_num_providers' do
-    context 'health_it_vendor' do
-      before(:each) do
-        subject.requested_organization_type = 'health_it_vendor'
-      end
-
-      it 'may be 0' do
+    context 'on update' do
+      it 'does not validate num_providers is greater than 0' do
+        subject.requested_organization_type = 'primary_care_clinic'
         subject.requested_num_providers = 0
         expect(subject).to be_valid
       end
-
-      it 'must be greater than or equal to 0' do
-        subject.requested_num_providers = -1
-        expect(subject).to_not be_valid
-      end
-
-      it 'must be an integer' do
-        subject.requested_num_providers = 1.23
-        expect(subject).to_not be_valid
-      end
     end
 
-    context 'not health_it_vendor' do
-      before(:each) do
-        subject.requested_organization_type = 'primary_care_clinic'
+    context 'on create' do
+      subject { build(:user) }
+
+      context 'health_it_vendor' do
+        before(:each) do
+          subject.requested_organization_type = 'health_it_vendor'
+        end
+
+        it 'may be 0' do
+          subject.requested_num_providers = 0
+          expect(subject).to be_valid
+        end
+
+        it 'must be greater than or equal to 0' do
+          subject.requested_num_providers = -1
+          expect(subject).to_not be_valid
+        end
+
+        it 'must be an integer' do
+          subject.requested_num_providers = 1.23
+          expect(subject).to_not be_valid
+        end
       end
 
-      it 'must be greater than 0' do
-        subject.requested_num_providers = 0
-        expect(subject).to_not be_valid
-      end
+      context 'not health_it_vendor' do
+        before(:each) do
+          subject.requested_organization_type = 'primary_care_clinic'
+        end
 
-      it 'must be an integer' do
-        subject.requested_num_providers = 1.23
-        expect(subject).to_not be_valid
+        it 'must be greater than 0' do
+          subject.requested_num_providers = 0
+          expect(subject).to_not be_valid
+        end
+
+        it 'must be an integer' do
+          subject.requested_num_providers = 1.23
+          expect(subject).to_not be_valid
+        end
       end
     end
   end
