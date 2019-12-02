af3d5e8c7d2bc6de4090bbbab265fd571841e48c
switzersc-usds <shelby.switzer@cms.hhs.gov>
2019-12-02 12:06:21 -0500

    Remove bundle exec from audit step

--- dpc-web-test.sh
+++ dpc-web-test.sh
@@ -16,8 +16,7 @@ docker-compose -f dpc-web/docker-compose.yml run web rails db:create db:migrate 
 docker-compose -f dpc-web/docker-compose.yml run web rails spec
 
 # Run bundler audit
-docker-compose -f dpc-web/docker-compose.yml run web bundle exec bundle audit update && bundle exec bundle audit check --ignore CVE-2015-9284
-docker-compose -f dpc-web/docker-compose.yml down
+docker-compose -f dpc-web/docker-compose.yml run web bundle audit update && bundle audit check --ignore CVE-2015-9284
 
 echo "┌──────────────────────────────────────────┐"
 echo "│                                          │"
