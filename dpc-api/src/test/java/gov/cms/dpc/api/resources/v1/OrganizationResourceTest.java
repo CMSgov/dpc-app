9a5cdfd023d80644d53afc26d1eee45819aa65cf
Emily Hart <1923441+embh@users.noreply.github.com>
2019-12-02 14:07:06 -0500

    DPC-307: Add update capability to Organization resources (#423)
    * Update Organization

--- dpc-api/src/test/java/gov/cms/dpc/api/resources/v1/OrganizationResourceTest.java
+++ dpc-api/src/test/java/gov/cms/dpc/api/resources/v1/OrganizationResourceTest.java
@@ -1,22 +1,27 @@
 package gov.cms.dpc.api.resources.v1;
 
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.gclient.IOperationUntypedWithInput;
+import ca.uhn.fhir.rest.gclient.IUpdateTyped;
 import ca.uhn.fhir.rest.server.exceptions.AuthenticationException;
 import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
 import gov.cms.dpc.api.APITestHelpers;
 import gov.cms.dpc.api.AbstractSecureApplicationTest;
+import gov.cms.dpc.fhir.helpers.FHIRHelpers;
 import gov.cms.dpc.testing.APIAuthHelpers;
 import gov.cms.dpc.testing.factories.OrganizationFactory;
 import gov.cms.dpc.testing.OrganizationHelpers;
-import org.hl7.fhir.dstu3.model.Bundle;
-import org.hl7.fhir.dstu3.model.Endpoint;
-import org.hl7.fhir.dstu3.model.Organization;
-import org.hl7.fhir.dstu3.model.Parameters;
+import org.apache.commons.lang3.tuple.Pair;
+import org.assertj.core.util.Lists;
+import org.hl7.fhir.dstu3.model.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.util.UUID;
 
 import static gov.cms.dpc.api.APITestHelpers.ORGANIZATION_ID;
@@ -129,5 +134,47 @@ class OrganizationResourceTest extends AbstractSecureApplicationTest {
                 .encodedJson();
 
         assertThrows(InvalidRequestException.class, operation::execute, "Should be unprocessable");
+    }
+
+    @Test
+    void testUpdateOrganization() throws IOException, URISyntaxException, NoSuchAlgorithmException {
+        final String orgID = UUID.randomUUID().toString();
+        final IParser parser = ctx.newJsonParser();
+        final IGenericClient attrClient = APITestHelpers.buildAttributionClient(ctx);
+        final String macaroon = FHIRHelpers.registerOrganization(attrClient, parser, orgID, getAdminURL());
+        final Pair<UUID, PrivateKey> uuidPrivateKeyPair = APIAuthHelpers.generateAndUploadKey("org-update-key", orgID, GOLDEN_MACAROON, getBaseURL());
+        final IGenericClient client = APIAuthHelpers.buildAuthenticatedClient(ctx, getBaseURL(), macaroon, uuidPrivateKeyPair.getLeft(), uuidPrivateKeyPair.getRight());
+
+        Organization organization = client
+                .read()
+                .resource(Organization.class)
+                .withId(orgID)
+                .encodedJson()
+                .execute();
+
+        assertNotNull(organization);
+
+        organization.setName("New Org Name");
+        organization.setContact(Lists.emptyList());
+
+        MethodOutcome outcome = client
+                .update()
+                .resource(organization)
+                .execute();
+
+        Organization result = (Organization) outcome.getResource();
+        assertEquals(orgID, result.getIdentifierFirstRep().getValue());
+        assertEquals(organization.getName(), result.getName(), "Name should be updated");
+        assertTrue(organization.getContact().isEmpty(), "Contact list should be updated");
+        assertEquals(1, result.getEndpoint().size(), "Endpoint list should be unchanged");
+
+        // Try to update when authenticated as different organization
+        final String org2ID = UUID.randomUUID().toString();
+        final String org2Macaroon = FHIRHelpers.registerOrganization(attrClient, parser, org2ID, getAdminURL());
+        final Pair<UUID, PrivateKey> org2UUIDPrivateKeyPair = APIAuthHelpers.generateAndUploadKey("org2-update-key", org2ID, GOLDEN_MACAROON, getBaseURL());
+        final IGenericClient org2Client = APIAuthHelpers.buildAuthenticatedClient(ctx, getBaseURL(), org2Macaroon, org2UUIDPrivateKeyPair.getLeft(), org2UUIDPrivateKeyPair.getRight());
+
+        IUpdateTyped update = org2Client.update().resource(organization);
+        assertThrows(AuthenticationException.class, update::execute);
     }
 }
