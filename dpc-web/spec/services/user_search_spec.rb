a6f6aee3b845c21594ce090e53f2182d6898bcde
switzersc-usds <shelby.switzer@cms.hhs.gov>
2019-12-06 20:07:46 -0500

    Flesh out internal pages for vendor users and orgs
    * Re-do internal side nav to organize by users and orgs, not providers
      and vendors, based on product convos
    * Rename user scopes to .vendor and .provider
    * Refactor specs a bit

--- dpc-web/spec/services/user_search_spec.rb
+++ dpc-web/spec/services/user_search_spec.rb
@@ -3,21 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe UserSearch do
-  describe 'allowed scopes' do
-    it 'raises error when scope not allowed' do
-      expect { UserSearch.new(params: {}, scope: 'bad') }.to raise_error(ArgumentError)
+  describe '#initial_scope' do
+    it 'sets initial scope to all no scope provided' do
+      expect(UserSearch.new(params: {}).initial_scope).to eq('all')
+    end
+
+    it 'sets initial scope to scope passed' do
+      expect(UserSearch.new(params: {}, scope: 'vendor').initial_scope).to eq('vendor')
     end
   end
 
   describe '#results' do
     let!(:user) { create(:user, :assigned, created_at: 5.days.ago) }
     let!(:unassigned_user) { create(:user, created_at: 5.days.ago) }
+    let!(:old_unassigned_user) { create(:user, created_at: 10.days.ago) }
     let!(:new_user) { create(:user, :assigned, created_at: Time.now) }
     let!(:vendor_user) { create(:user, :vendor, created_at: 5.days.ago) }
     let!(:old_vendor_user) { create(:user, :vendor, created_at: 1.month.ago) }
+    let!(:unassigned_vendor_user) { create(:user, created_at: 1.day.ago, requested_organization_type: 'health_it_vendor') }
 
-    describe 'no scope' do
-      it 'applies params to search all users' do
+    context 'no scope' do
+      it 'returns all users matching params' do
         params = {
           org_status: 'assigned',
           created_after: 7.days.ago,
@@ -28,25 +34,51 @@ RSpec.describe UserSearch do
       end
     end
 
-    describe 'assigned_non_vendor scope' do
-      it 'applies params to search only non_vendor users' do
-        params = {
-          org_status: 'assigned',
-          created_after: 7.days.ago,
-        }
+    context 'provider scope' do
+      context 'assigned with created_after' do
+        it 'returns assigned users created after a date' do
+          params = {
+            org_status: 'assigned',
+            created_after: 7.days.ago,
+          }
 
-        expect(UserSearch.new(params: params, scope: :assigned_non_vendor).results).to match_array([user, new_user])
+          expect(UserSearch.new(params: params, scope: 'provider').results).to match_array([user, new_user])
+        end
+      end
+
+      context 'unassigned with created_before' do
+        it 'returns unassigned users created before a date' do
+          params = {
+            org_status: 'unassigned',
+            created_before: 7.days.ago,
+          }
+
+          expect(UserSearch.new(params: params, scope: 'provider').results).to match_array([old_unassigned_user])
+        end
       end
     end
 
-    describe 'assigned_vendor scope' do
-      it 'applies params to search only vendor users' do
-        params = {
-          org_status: 'assigned',
-          created_after: 7.days.ago,
-        }
+    context 'vendor scope' do
+      context 'assigned with created_before' do
+        it 'returns assigned users created before a date' do
+          params = {
+            org_status: 'assigned',
+            created_before: 7.days.ago,
+          }
 
-        expect(UserSearch.new(params: params, scope: :assigned_vendor).results).to match_array([vendor_user])
+          expect(UserSearch.new(params: params, scope: 'vendor').results).to match_array([old_vendor_user])
+        end
+      end
+
+      context 'unassigned with created_after' do
+        it 'returns unassigned users created after a date' do
+          params = {
+            org_status: 'unassigned',
+            created_after: 7.days.ago,
+          }
+
+          expect(UserSearch.new(params: params, scope: 'vendor').results).to match_array([unassigned_vendor_user])
+        end
       end
     end
   end
