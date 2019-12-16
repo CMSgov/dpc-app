c7b529bd7a60e7623641f40a25d8dd3081e937b2
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-12-16 11:45:40 -0500

    Moved bundler audit into the Docker build process.
    This avoids having to pull in git as a dependency in the production docker container, as well as surfaces security warnings earlier in the CI process.

--- dpc-web-test.sh
+++ dpc-web-test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "┌──────────────────────────────────────────┐"
-echo "│                                          │"
-echo "│  Running Website Tests & Bundler Audit   │"
-echo "│                                          │"
-echo "└──────────────────────────────────────────┘"
+echo "┌─────────────────────────┐"
+echo "│                         │"
+echo "│  Running Website Tests  │"
+echo "│                         │"
+echo "└─────────────────────────┘"
 
 # Build the container
 make website
@@ -15,11 +15,8 @@ docker-compose -f dpc-web/docker-compose.yml up start_core_dependencies
 docker-compose -f dpc-web/docker-compose.yml run web bundle exec rails db:create db:migrate db:seed
 docker-compose -f dpc-web/docker-compose.yml run web bundle exec rails spec
 
-# Run bundler audit (wrap all of this up as a shell command, otherwise we get weird PATH issues
-docker-compose -f dpc-web/docker-compose.yml run web sh -c "cd /dpc-web && bundle exec bundle audit update && bundle exec bundle audit check --ignore CVE-2015-9284"
-
-echo "┌──────────────────────────────────────────┐"
-echo "│                                          │"
-echo "│    All Website Tests & Audit Complete    │"
-echo "│                                          │"
-echo "└──────────────────────────────────────────┘"
+echo "┌──────────────────────────────────┐"
+echo "│                                  │"
+echo "│    All Website Tests Complete    │"
+echo "│                                  │"
+echo "└──────────────────────────────────┘"
