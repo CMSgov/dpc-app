f1465c1d15ff1955ca32fcad9407d9fe8f33afd3
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-12-17 09:16:16 -0500

    DPC-837: Add support for provenance resources (#407)
    * DPC-837: Added custom parameter for injecting provenance resource into method.
    
    We can pull the X-Provenance header from the request and translate it into a valid provenance resource.
    The validation is currently failing, because we don't have a profile for it, but it is giving some useful error messages.
    
    * DPC-837: Unit tests for provenance factories
    
    Added unit tests for ensuring provenance factories are working correctly.
    Test coverage will be expanded once we actually start using the factories, but this is good enough for now.
    
    * DPC-837: Added unit tests for FHIR parameter factories
    
    Added some additional unit tests to improve code coverage of already existing resources.
    
    * DPC-837: A few improvements to the test suite
    
    Updated some error messaging to be more useful and consistent
    
    * DPC-837: Actually commit new unit tests
    
    Unit tests were missing. They're not anymore.
    
    * DPC-837: Refactor provides method
    
    Make code climate happy to refactoring one of our bigger methods
    
    * DPC-837: Address code review comments
    
    Rename factory classes to be more consistent and clear as to what each class actually does/provides.
    
    * DPC-837: Remove unused import
    
    * DPC-837: Fix incorrect test assertion
    
    Assertion message now actually reflects what's going on.

---
+++ dpc-common/src/test/java/gov/cms/dpc/fhir/parameters/FHIRParamValueFactoryTest.java
@@ -0,0 +1,63 @@
+package gov.cms.dpc.fhir.parameters;
+
+import ca.uhn.fhir.context.FhirContext;
+import com.google.inject.Injector;
+import gov.cms.dpc.fhir.annotations.FHIRParameter;
+import gov.cms.dpc.testing.BufferedLoggerHandler;
+import org.glassfish.hk2.api.Factory;
+import org.glassfish.jersey.server.model.Parameter;
+import org.hl7.fhir.dstu3.model.Patient;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(BufferedLoggerHandler.class)
+class FHIRParamValueFactoryTest {
+
+    private static Injector injector = Mockito.mock(Injector.class);
+    private static FhirContext ctx = Mockito.mock(FhirContext.class);
+    private static FHIRParamValueFactory factory;
+
+    FHIRParamValueFactoryTest() {
+        // Not used
+    }
+
+    @BeforeAll
+    static void setup() {
+        factory = new FHIRParamValueFactory(injector, ctx);
+    }
+
+    @Test
+    void testCorrectFactory() {
+        final Parameter parameter = Mockito.mock(Parameter.class);
+        final FHIRParameter mockAnnotation = Mockito.mock(FHIRParameter.class);
+        Mockito.when(parameter.getDeclaredAnnotation(FHIRParameter.class)).thenReturn(mockAnnotation);
+        Mockito.when(parameter.getRawType()).thenAnswer(answer -> Patient.class);
+
+        final Factory<?> valueFactory = factory.getValueFactory(parameter);
+        assertAll(() -> assertNotNull(valueFactory, "Should have factory"),
+                () -> assertEquals(ParamResourceFactory.class, valueFactory.getClass(), "Should have provenance factory"));
+    }
+
+    @Test
+    void testMissingAnnotation() {
+        final Parameter parameter = Mockito.mock(Parameter.class);
+        Mockito.when(parameter.getDeclaredAnnotation(FHIRParameter.class)).thenReturn(null);
+        Mockito.when(parameter.getRawType()).thenAnswer(answer -> Patient.class);
+
+        assertNull(factory.getValueFactory(parameter), "Should not have factory");
+    }
+
+    @Test
+    void testIncorrectParameter() {
+        final Parameter parameter = Mockito.mock(Parameter.class);
+        final FHIRParameter mockAnnotation = Mockito.mock(FHIRParameter.class);
+        Mockito.when(parameter.getDeclaredAnnotation(FHIRParameter.class)).thenReturn(mockAnnotation);
+        Mockito.when(parameter.getRawType()).thenAnswer(answer -> Mockito.class);
+
+        assertNull(factory.getValueFactory(parameter), "Should not have factory");
+    }
+}
