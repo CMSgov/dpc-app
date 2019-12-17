facb3bdba07076b3f7a0e61dc599fc4ad1999880
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-12-17 09:16:16 -0500

    DPC-837: Add support for provenance resources (#407)
    * DPC-837: Added custom parameter for injecting provenance resource into method.
    
    We can pull the X-Provenance header from the request and translate it into a valid provenance resource.
    The validation is currently failing, because we don't have a profile for it, but it is giving some useful error messages.
    
    * DPC-837: Unit tests for provenance factories
    
    Added unit tests for ensuring provenance factories are working correctly.
    Test coverage will be expanded once we actually start using the factories, but this is good enough for now.
    
    * DPC-837: Added unit tests for FHIR parameter factories
    
    Added some additional unit tests to improve code coverage of already existing resources.
    
    * DPC-837: A few improvements to the test suite
    
    Updated some error messaging to be more useful and consistent
    
    * DPC-837: Actually commit new unit tests
    
    Unit tests were missing. They're not anymore.
    
    * DPC-837: Refactor provides method
    
    Make code climate happy to refactoring one of our bigger methods
    
    * DPC-837: Address code review comments
    
    Rename factory classes to be more consistent and clear as to what each class actually does/provides.
    
    * DPC-837: Remove unused import
    
    * DPC-837: Fix incorrect test assertion
    
    Assertion message now actually reflects what's going on.

---
+++ dpc-common/src/main/java/gov/cms/dpc/fhir/annotations/ProvenanceHeader.java
@@ -0,0 +1,14 @@
+package gov.cms.dpc.fhir.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation which specifies that the necessary {@link org.hl7.fhir.dstu3.model.Provenance} resource should be found in the 'X-PROVENANCE' header of the request
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface ProvenanceHeader {
+}
