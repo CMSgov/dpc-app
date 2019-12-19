099f1f60f3a64286374f983af8f0a0ba73a82794
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-12-19 14:22:46 -0500

    DPC-921: Updated FHIR request filter to check both Accepts and Content-Type headers
    We're now checking that both values are correct, improved the check logic to be more generic, may be useful in the future.
    
    Added unit tests to ensure the headers are parsed correctly.

--- dpc-common/src/main/java/gov/cms/dpc/fhir/dropwizard/filters/FHIRRequestFilter.java
+++ dpc-common/src/main/java/gov/cms/dpc/fhir/dropwizard/filters/FHIRRequestFilter.java
@@ -6,26 +6,50 @@ import org.eclipse.jetty.server.Response;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class FHIRRequestFilter implements ContainerRequestFilter {
 
+    FHIRRequestFilter() {
+        // Not used
+    }
+
     @Override
     public void filter(ContainerRequestContext requestContext) {
+        // Ensure the Accepts header is set to a FHIR media type
+        checkAccepts(requestContext);
+        // Ensure the Content-Type header is set to a FHIR media type
+        checkContentType(requestContext);
+    }
+
+    private void checkAccepts(ContainerRequestContext requestContext) {
         final List<MediaType> contentHeader = requestContext.getAcceptableMediaTypes();
 
-        boolean valid = false;
+        if (!shortCircuitBooleanCheck(contentHeader, FHIRMediaTypes::isFHIRContent)) {
+            throw new WebApplicationException("`Accept:` header must specify valid FHIR content type", Response.SC_UNSUPPORTED_MEDIA_TYPE);
+        }
+    }
 
-        for (MediaType header : contentHeader) {
-            valid = FHIRMediaTypes.isFHIRContent(header);
+    private void checkContentType(ContainerRequestContext requestContext) {
+        final List<String> typeHeaders = requestContext.getHeaders().get(HttpHeaders.CONTENT_TYPE);
+
+        if (!shortCircuitBooleanCheck(typeHeaders, (typeHeader) -> FHIRMediaTypes.isFHIRContent(MediaType.valueOf(typeHeader)))) {
+            throw new WebApplicationException("`Accept:` header must specify valid FHIR content type", Response.SC_UNSUPPORTED_MEDIA_TYPE);
+        }
+    }
+
+    private static <T> boolean shortCircuitBooleanCheck(Collection<T> collection, Predicate<T> checker) {
+        boolean valid = false;
+        for (T element : collection) {
+            valid = checker.test(element);
             if (valid)
                 break;
         }
 
-        if (!valid) {
-            final IllegalArgumentException cause = new IllegalArgumentException("Must specify valid FHIR content type");
-            throw new WebApplicationException(cause, Response.SC_UNSUPPORTED_MEDIA_TYPE);
-        }
+        return valid;
     }
 }
---
+++ dpc-common/src/test/java/gov/cms/dpc/fhir/dropwizard/filters/FHIRRequestFilterTest.java
@@ -0,0 +1,78 @@
+package gov.cms.dpc.fhir.dropwizard.filters;
+
+import gov.cms.dpc.fhir.FHIRMediaTypes;
+import org.eclipse.jetty.server.Response;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class FHIRRequestFilterTest {
+
+    private static final FHIRRequestFilter filter = new FHIRRequestFilter();
+
+    @Test
+    void testSuccess() {
+        final MultivaluedMap headerMap = Mockito.mock(MultivaluedMap.class);
+        Mockito.when(headerMap.get(HttpHeaders.CONTENT_TYPE)).thenReturn(Collections.singletonList(FHIRMediaTypes.FHIR_JSON));
+        final ContainerRequestContext request = Mockito.mock(ContainerRequestContext.class);
+        Mockito.when(request.getAcceptableMediaTypes()).thenReturn(Collections.singletonList(MediaType.valueOf(FHIRMediaTypes.FHIR_JSON)));
+        Mockito.when(request.getHeaders()).thenReturn(headerMap);
+
+        filter.filter(request);
+    }
+
+    @Test
+    void testMissingAcceptsHeader() {
+        final ContainerRequestContext request = Mockito.mock(ContainerRequestContext.class);
+        Mockito.when(request.getAcceptableMediaTypes()).thenReturn(Collections.singletonList(MediaType.APPLICATION_JSON_TYPE));
+
+        final WebApplicationException exception = assertThrows(WebApplicationException.class, () -> filter.filter(request));
+        assertEquals(Response.SC_UNSUPPORTED_MEDIA_TYPE, exception.getResponse().getStatus(), "Should have 415 error");
+    }
+
+    @Test
+    void testNestedAcceptsHeader() {
+        final MultivaluedMap headerMap = Mockito.mock(MultivaluedMap.class);
+        Mockito.when(headerMap.get(HttpHeaders.CONTENT_TYPE)).thenReturn(Collections.singletonList(FHIRMediaTypes.FHIR_JSON));
+        final ContainerRequestContext request = Mockito.mock(ContainerRequestContext.class);
+        Mockito.when(request.getAcceptableMediaTypes()).thenReturn(List.of(MediaType.APPLICATION_JSON_TYPE, MediaType.valueOf(FHIRMediaTypes.FHIR_JSON)));
+        Mockito.when(request.getHeaders()).thenReturn(headerMap);
+
+        filter.filter(request);
+    }
+
+    @Test
+    void testMissingContentHeader() {
+        final MultivaluedMap headerMap = Mockito.mock(MultivaluedMap.class);
+        Mockito.when(headerMap.get(HttpHeaders.CONTENT_TYPE)).thenReturn(Collections.singletonList("application/fire+json"));
+        final ContainerRequestContext request = Mockito.mock(ContainerRequestContext.class);
+        Mockito.when(request.getAcceptableMediaTypes()).thenReturn(Collections.singletonList(MediaType.valueOf(FHIRMediaTypes.FHIR_JSON)));
+        Mockito.when(request.getHeaders()).thenReturn(headerMap);
+
+        final WebApplicationException exception = assertThrows(WebApplicationException.class, () -> filter.filter(request));
+        assertEquals(Response.SC_UNSUPPORTED_MEDIA_TYPE, exception.getResponse().getStatus(), "Should have 415 error");
+    }
+
+    @Test
+    void testTestedContentHeader() {
+        final MultivaluedMap headerMap = Mockito.mock(MultivaluedMap.class);
+        Mockito.when(headerMap.get(HttpHeaders.CONTENT_TYPE)).thenReturn(List.of("application/fire+json", FHIRMediaTypes.FHIR_JSON));
+        final ContainerRequestContext request = Mockito.mock(ContainerRequestContext.class);
+        Mockito.when(request.getAcceptableMediaTypes()).thenReturn(Collections.singletonList(MediaType.valueOf(FHIRMediaTypes.FHIR_JSON)));
+        Mockito.when(request.getHeaders()).thenReturn(headerMap);
+
+        filter.filter(request);
+
+    }
+}
