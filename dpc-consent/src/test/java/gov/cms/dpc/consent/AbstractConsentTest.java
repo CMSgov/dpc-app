60409d1a30e28995215e2d0a048b30836615e73e
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2020-01-29 09:46:58 -0500

    DPC-966: Attempt to configure logging output from config file

--- dpc-consent/src/test/java/gov/cms/dpc/consent/AbstractConsentTest.java
+++ dpc-consent/src/test/java/gov/cms/dpc/consent/AbstractConsentTest.java
@@ -3,23 +3,20 @@ package gov.cms.dpc.consent;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.api.ServerValidationModeEnum;
-import io.dropwizard.testing.ConfigOverride;
 import io.dropwizard.testing.DropwizardTestSupport;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 public abstract class AbstractConsentTest {
-    private static final String KEY_PREFIX = "dpc.consent";
-    protected static final DropwizardTestSupport<DPCConsentConfiguration> APPLICATION = new DropwizardTestSupport<>(DPCConsentService.class, null, ConfigOverride.config(KEY_PREFIX, "", ""),
-            ConfigOverride.config(KEY_PREFIX, "logging.level", "ERROR"));
+    protected static final DropwizardTestSupport<DPCConsentConfiguration> APPLICATION = new DropwizardTestSupport<>(DPCConsentService.class, "ci.application.conf");
 
     protected FhirContext ctx = FhirContext.forDstu3();
 
     @BeforeAll
     public static void initDB() throws Exception {
         APPLICATION.before();
-        APPLICATION.getApplication().run("db", "migrate");
-        APPLICATION.getApplication().run("seed");
+        APPLICATION.getApplication().run("db", "migrate", "ci.application.conf");
+        APPLICATION.getApplication().run("seed", "ci.application.conf");
     }
 
     @AfterAll
