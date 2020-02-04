a86c36ce3eb8bbc5871319fc89a2580776d25e35
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2020-02-04 16:43:17 -0500

    DPC-1010 Vendor Id Column (#585)
    * added vendor id ('vendor_id') column
    
    * fixed DPC-API's README docker-compose directions
    
    * update dpc-web-test.sh for travis

--- dpc-web/db/schema.rb
+++ dpc-web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_135210) do
+ActiveRecord::Schema.define(version: 2020_02_04_152555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_135210) do
     t.datetime "updated_at", null: false
     t.string "npi"
     t.string "vendor"
+    t.string "vendor_id"
   end
 
   create_table "registered_organizations", force: :cascade do |t|
