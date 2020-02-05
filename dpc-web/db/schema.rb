2dfaa1abdc8e0a015a5d6ff00b7d130747ea40e6
Sun-Mountain <nzonnenberg@fearless.tech>
2020-02-05 13:10:19 -0500

    reran migrations, updated version number

--- dpc-web/db/schema.rb
+++ dpc-web/db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2020_02_04_135210) do
+=======
+ActiveRecord::Schema.define(version: 2020_02_04_190846) do
+>>>>>>> Stashed changes
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
