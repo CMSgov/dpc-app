5f5ddef3d8052391dd4b87e97595325c7be8151f
Sun-Mountain <nzonnenberg@fearless.tech>
2020-02-14 15:28:03 -0500

    on org update in api, if org type is 'health_it_vendor' will use vendor id instead of npi

--- dpc-web/app/services/fhir_resource_builder.rb
+++ dpc-web/app/services/fhir_resource_builder.rb
@@ -6,7 +6,12 @@ class FhirResourceBuilder
     fhir_org = FHIR::Organization.new(
       id: reg_org.api_id,
       name: org.name,
-      identifier: [{ system: 'http://hl7.org/fhir/sid/us-npi', value: org.npi }]
+      identifier: [
+        {
+          system: 'http://hl7.org/fhir/sid/us-npi',
+          value: org.organization_type == 'health_it_vendor' ? org.vendor_id : org.npi
+        }
+      ]
     )
     fhir_org.endpoint = { reference: reg_org.api_endpoint_ref }
 
