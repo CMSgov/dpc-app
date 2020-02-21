130942d3ceed0889edff73b5b6e6a560d2f27c13
Sun-Mountain <nzonnenberg@fearless.tech>
2020-02-21 15:43:48 -0500

    more efficient code for serializers

--- dpc-web/app/serializers/organization_submit_serializer.rb
+++ dpc-web/app/serializers/organization_submit_serializer.rb
@@ -26,7 +26,7 @@ class OrganizationSubmitSerializer < ActiveModel::Serializer
         identifier: [
           {
             system: 'http://hl7.org/fhir/sid/us-npi',
-            value: object.health_it_vendor? ? object.vendor_id : object.npi
+            value: object.external_identifier
           }
         ],
         name: object.name,
