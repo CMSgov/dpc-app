a6dcaf2eef41f6d38dae1d3b9bf7f7209b0c77de
Nick Robison <nicholas.a.robison@omb.eop.gov>
2020-03-03 12:30:00 -0500

    DPC-1133: Initial support for converting FHIR resource to ConsentEntity
    Very, very crappy conversion logic.
    
    It's difficult to work with logical ids, we probably need to migrate to use actual resource IDs for the given resource in the attribution service.

--- dpc-common/src/test/java/gov/cms/dpc/fhir/converters/entities/ConsentEntityConverterTest.java
+++ dpc-common/src/test/java/gov/cms/dpc/fhir/converters/entities/ConsentEntityConverterTest.java
@@ -22,7 +22,7 @@ public class ConsentEntityConverterTest {
     @Test
     final void convert_correctlyConverts_fromDefaultEntity() {
         ConsentEntity ce = ConsentEntity.defaultConsentEntity(Optional.empty(), Optional.of(TEST_HICN), Optional.of(TEST_MBI));
-        final Consent result = ConsentEntityConverter.convert(ce, TEST_DPC_URL, TEST_FHIR_URL);
+        final Consent result = ConsentEntityConverter.toFHIR(ce, TEST_DPC_URL, TEST_FHIR_URL);
 
         assertNotNull(result);
         assertEquals(Consent.ConsentState.ACTIVE, result.getStatus());
@@ -39,7 +39,7 @@ public class ConsentEntityConverterTest {
     final void convert_correctlyConverts_fromOptOutEntity() {
         ConsentEntity ce = ConsentEntity.defaultConsentEntity(Optional.empty(), Optional.of(TEST_HICN), Optional.of(TEST_MBI));
         ce.setPolicyCode(ConsentEntity.OPT_OUT);
-        final Consent result = ConsentEntityConverter.convert(ce, TEST_DPC_URL, TEST_FHIR_URL);
+        final Consent result = ConsentEntityConverter.toFHIR(ce, TEST_DPC_URL, TEST_FHIR_URL);
 
         assertEquals(ConsentEntityConverter.OPT_OUT_MAGIC, result.getPolicyRule());
         assertDoesNotThrow(() -> {
@@ -52,7 +52,7 @@ public class ConsentEntityConverterTest {
         ConsentEntity ce = ConsentEntity.defaultConsentEntity(Optional.empty(), Optional.of(TEST_HICN), Optional.of(TEST_MBI));
         UUID uuid = UUID.randomUUID();
         ce.setCustodian(uuid);
-        final Consent result = ConsentEntityConverter.convert(ce, TEST_DPC_URL, TEST_FHIR_URL);
+        final Consent result = ConsentEntityConverter.toFHIR(ce, TEST_DPC_URL, TEST_FHIR_URL);
 
         assertEquals("Organization/" + uuid.toString(), result.getOrganizationFirstRep().getReference());
         assertDoesNotThrow(() -> {
@@ -65,7 +65,7 @@ public class ConsentEntityConverterTest {
         ConsentEntity ce = ConsentEntity.defaultConsentEntity(Optional.empty(), Optional.of(TEST_HICN), Optional.of(TEST_MBI));
         ce.setMbi(null);
 
-        assertThrows(IllegalArgumentException.class, () -> ConsentEntityConverter.convert(ce, TEST_DPC_URL, TEST_FHIR_URL), "should throw an error with invalid data");
+        assertThrows(IllegalArgumentException.class, () -> ConsentEntityConverter.toFHIR(ce, TEST_DPC_URL, TEST_FHIR_URL), "should throw an error with invalid data");
     }
 
     @Test
@@ -73,6 +73,16 @@ public class ConsentEntityConverterTest {
         ConsentEntity ce = ConsentEntity.defaultConsentEntity(Optional.empty(), Optional.of(TEST_HICN), Optional.of(TEST_MBI));
         ce.setPolicyCode("BANANA");
 
-        assertThrows(IllegalArgumentException.class, () -> ConsentEntityConverter.convert(ce, TEST_DPC_URL, TEST_FHIR_URL), "should throw an error with invalid data");
+        assertThrows(IllegalArgumentException.class, () -> ConsentEntityConverter.toFHIR(ce, TEST_DPC_URL, TEST_FHIR_URL), "should throw an error with invalid data");
+    }
+
+    @Test
+    void testRoundTrip() {
+        final ConsentEntity ce = ConsentEntity.defaultConsentEntity(Optional.empty(), Optional.of(TEST_HICN), Optional.of(TEST_MBI));
+        final Consent consent = ConsentEntityConverter.toFHIR(ce, TEST_DPC_URL, TEST_FHIR_URL);
+        final ConsentEntity ce2 = ConsentEntityConverter.fromFHIR(consent);
+        assertAll(() -> assertEquals(ce.getId(), ce2.getId(), "Should have ID"),
+                () -> assertEquals(ce.getMbi(), ce2.getMbi(), "Should have MBI"),
+                () -> assertEquals(ce.getEffectiveDate(), ce2.getEffectiveDate(), "Should have correct date"));
     }
 }
