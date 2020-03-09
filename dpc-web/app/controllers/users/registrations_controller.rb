820a76a8aeb011727285061539a5f12a30992306
Sun-Mountain <nzonnenberg@fearless.tech>
2020-03-09 09:27:57 -0400

    Correct error messages

--- dpc-web/app/controllers/users/registrations_controller.rb
+++ dpc-web/app/controllers/users/registrations_controller.rb
@@ -8,11 +8,7 @@ module Users
     def destroy
       @user = User.find(current_user.id)
       if @user.destroy_with_password(user_params[:password_to_delete])
-<<<<<<< HEAD
           redirect_to root_url, notice: 'User account deleted.'
-=======
-        redirect_to root_url, notice: 'User deleted.'
->>>>>>> af3cf596acdb186817d997afd38f801c18910ca7
       else
         redirect_to edit_user_registration_url
         flash[:notice] = 'Could not delete account. Please enter the correct password.'
