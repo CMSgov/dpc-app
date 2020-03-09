72e9eb0ab1a98d4963f70c89bb83bb394eae26c2
Ron Heft <52176593+ronaldheft-usds@users.noreply.github.com>
2020-03-09 16:42:04 -0400

    Fix: Stuck jobs are unable to be restarted due to SQL constraint exception (#674)
    * Reproduce stuck job issue as a unit test
    
    * Update unit test to match reality for how jobs are re-claimed after being stuck
    
    * Add flags onto @JoinColumn to fix Hibernate's delete and update behavior

---
+++ dpc-queue/src/test/java/gov/cms/dpc/queue/DistributedBatchQueueTest.java
@@ -0,0 +1,110 @@
+package gov.cms.dpc.queue;
+
+import com.codahale.metrics.MetricRegistry;
+import gov.cms.dpc.common.hibernate.queue.DPCQueueManagedSessionFactory;
+import gov.cms.dpc.queue.models.JobQueueBatch;
+import gov.cms.dpc.testing.BufferedLoggerHandler;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hl7.fhir.dstu3.model.ResourceType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.time.OffsetDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(BufferedLoggerHandler.class)
+public class DistributedBatchQueueTest {
+
+    private final UUID aggregatorID = UUID.randomUUID();
+    private SessionFactory sessionFactory;
+    private DistributedBatchQueue queue;
+
+    @BeforeEach
+    void setUp() {
+        final Configuration conf = new Configuration();
+        sessionFactory = conf.configure().buildSessionFactory();
+        queue = new DistributedBatchQueue(new DPCQueueManagedSessionFactory(sessionFactory), 100, new MetricRegistry());
+    }
+
+    @AfterEach
+    void shutdown() {
+        try (final Session session = sessionFactory.openSession()) {
+            final Transaction tx = session.beginTransaction();
+            try {
+                session.createQuery("delete from job_queue_batch_file").executeUpdate();
+                session.createQuery("delete from job_queue_batch").executeUpdate();
+            } finally {
+                tx.commit();
+            }
+        }
+        sessionFactory.close();
+    }
+
+    @Test
+    void handleStuckBatchWithClaim() {
+        // One organization id for both jobs
+        final UUID orgID = UUID.randomUUID();
+
+        // Add a job
+        var jobID = queue.createJob(orgID, "test-provider-1", List.of("test-patient-1", "test-patient-2"), Collections.singletonList(ResourceType.Patient));
+
+        // Work the job
+        Optional<JobQueueBatch> workBatch = queue.claimBatch(aggregatorID);
+        assertTrue(workBatch.isPresent(), "Should have a job to work");
+        final UUID firstBatchID = workBatch.orElseThrow().getBatchID();
+
+        // Add a file on the batch
+        workBatch.get().addJobQueueFile(ResourceType.Patient, 0, 1);
+        queue.completePartialBatch(workBatch.get(), aggregatorID);
+
+        // Check that the persisted job is RUNNING
+        final Optional<JobQueueBatch> runningJobOptional = queue.getBatch(firstBatchID);
+        assertTrue(runningJobOptional.isPresent(), "Should have a running job");
+        runningJobOptional.ifPresent(runningJob -> {
+            assertEquals(JobStatus.RUNNING, runningJob.getStatus(), "Should be in the RUNNING state");
+            assertEquals(1, runningJob.getJobQueueBatchFiles().size(), "Should have 1 file on the running job");
+            assertTrue(runningJob.getJobQueueFile(ResourceType.Patient).isPresent(), "Should have a patient job file");
+        });
+
+        // Simulate a stuck job by modifying the update_time
+        try (final Session session = sessionFactory.openSession()) {
+            final Transaction tx = session.beginTransaction();
+            try {
+                session.createQuery("update job_queue_batch set updateTime = :updateTime where jobID = :jobID")
+                        .setParameter("jobID", jobID)
+                        .setParameter("updateTime", OffsetDateTime.now().minusMinutes(15))
+                        .executeUpdate();
+            } finally {
+                tx.commit();
+            }
+        }
+
+        // Fix the stuck job during the claim process
+        Optional<JobQueueBatch> stuckBatch = queue.claimBatch(aggregatorID);
+        assertFalse(stuckBatch.isPresent(), "Should have no job, but the stuck batch was released and is ready to be re-claimed");
+
+        // Re-claim the batch that was in a stuck state
+        stuckBatch = queue.claimBatch(aggregatorID);
+        assertTrue(stuckBatch.isPresent(), "Should have a job to work");
+        final UUID stuckBatchID = stuckBatch.orElseThrow().getBatchID();
+        assertEquals(stuckBatchID, firstBatchID, "Stuck batch should be the same as the initial batch");
+
+        // Check that the stuck job is RUNNING with previous files cleared
+        final Optional<JobQueueBatch> stuckJobOptional = queue.getBatch(stuckBatchID);
+        assertTrue(stuckJobOptional.isPresent(), "Should have a job");
+        stuckJobOptional.ifPresent(stuckJob -> {
+            assertEquals(JobStatus.RUNNING, stuckJob.getStatus(), "Should be in the RUNNING state");
+            assertEquals(0, stuckJob.getJobQueueBatchFiles().size(), "Should have no files on the stuck job (they should be cleared)");
+        });
+    }
+}
