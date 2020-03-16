81678c5b6aac67ea5f120367d3e93fb6d23b40fa
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2020-03-16 19:49:35 -0400

    DPC-1074: Password Required to Delete Account (#668)
    * delete with password
    
    * added devise notifications to edit view
    
    * successful test progress
    
    * tests complete and verified
    
    * Remove whitespace
    
    * Code climate fixes
    
    * fix error messages
    
    * Correct error messages
    
    * Update registrations_controller.rb
    
    * shelby's feedback
    
    * code climate fixes
    
    * Removed redundant '@user = User.find(..)'
    
    * Changed script for delete account button
    
    * Resolved delete account errors
    
    Changed redirect_to to render the edit page. Users will now see 'Current password can't be blank' if left blank or 'Current password is invalid' if they input the wrong password.

---
+++ dpc-web/spec/features/authentication/user_deletes_account_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.feature 'user deletes account' do
+  let(:user) { create :user }
+
+  before(:each) do
+    sign_in user, scope: :user
+    visit edit_user_registration_path
+  end
+
+  context 'when successful' do
+    scenario 'user inputs correct password' do
+      fill_in 'user_password_to_delete', with: '123456'
+
+      find('[data-test="delete-user-account"]').click
+
+      visit new_user_session_path
+      expect(page.body).to include('Bye! Your account has been successfully cancelled. We hope to see you again soon.')
+    end
+  end
+
+  context 'when unsuccessful' do
+    scenario 'user inputs incorrect password' do
+      fill_in 'user_password_to_delete', with: '3v3ryDayPotato'
+
+      find('[data-test="delete-user-account"]').click
+
+      expect(page.body).to include('Current password is invalid')
+    end
+
+    scenario 'user inputs no password' do
+      fill_in 'user_password_to_delete', with: ''
+
+      find('[data-test="delete-user-account"]').click
+
+      expect(page.body).to include('1 error prohibited this user from being saved')
+    end
+  end
+end
