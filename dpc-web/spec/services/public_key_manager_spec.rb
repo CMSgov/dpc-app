ff3b684a2a4c2d543fbe346fc97bda43b8ebd8cd
Sun-Mountain <nzonnenberg@fearless.tech>
2020-04-08 10:39:17 -0400

    Test fixes

--- dpc-web/spec/services/public_key_manager_spec.rb
+++ dpc-web/spec/services/public_key_manager_spec.rb
@@ -18,7 +18,10 @@ RSpec.describe PublicKeyManager do
           allow(api_client).to receive(:response_body).and_return('id' => '570f7a71-0e8f-48a1-83b0-c46ac35d6ef3')
 
           manager = PublicKeyManager.new(api_env: 'sandbox', registered_organization: registered_org)
-          expect(manager.create_public_key(label: 'Test Key 1', public_key: file_fixture('stubbed_key.pem').read)).to eq(true)
+
+          new_public_key = manager.create_public_key(label: 'Test Key 1', public_key: file_fixture('stubbed_key.pem').read)
+
+          expect(new_public_key[:response]).to eq(true)
         end
       end
 
@@ -44,14 +47,18 @@ RSpec.describe PublicKeyManager do
         registered_org = build(:registered_organization, api_env: 'sandbox')
         manager = PublicKeyManager.new(api_env: 'sandbox', registered_organization: registered_org)
 
-        expect(manager.create_public_key(label: 'Test Key 1', public_key: file_fixture('private_key.pem').read)).to eq(false)
+        new_public_key = manager.create_public_key(label: 'Test Key 1', public_key: file_fixture('private_key.pem').read)
+
+        expect(new_public_key[:response]).to eq(false)
       end
 
       it 'returns false when key is not in pem format' do
         registered_org = build(:registered_organization, api_env: 'sandbox')
         manager = PublicKeyManager.new(api_env: 'sandbox', registered_organization: registered_org)
 
-        expect(manager.create_public_key(label: 'Test Key 1', public_key: file_fixture('bad_cert.pub').read)).to eq(false)
+        new_public_key = manager.create_public_key(label: 'Test Key 1', public_key: file_fixture('bad_cert.pub').read)
+
+        expect(new_public_key[:response]).to eq(false)
       end
     end
   end
