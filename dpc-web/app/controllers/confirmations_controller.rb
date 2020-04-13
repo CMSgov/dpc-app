c2591a0e67f0314f7d2ff2c523702da27240a698
Sun-Mountain <nzonnenberg@fearless.tech>
2020-04-13 09:31:28 -0400

    Code climate fixes

--- dpc-web/app/controllers/confirmations_controller.rb
+++ dpc-web/app/controllers/confirmations_controller.rb
@@ -3,7 +3,7 @@
 class ConfirmationsController < Devise::ConfirmationsController
   private
 
-  def after_confirmation_path_for(resource)
+  def after_confirmation_path_for(_resource_name, resource)
     sign_in(resource) # In case you want to sign in the user
     root_path
   end
