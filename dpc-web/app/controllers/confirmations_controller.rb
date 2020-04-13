a1af60fbb7703287f300546ceeae59cf5ed7450f
Sun-Mountain <nzonnenberg@fearless.tech>
2020-04-13 09:30:00 -0400

    Code climate fixes

--- dpc-web/app/controllers/confirmations_controller.rb
+++ dpc-web/app/controllers/confirmations_controller.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 class ConfirmationsController < Devise::ConfirmationsController
   private
-  def after_confirmation_path_for(resource_name, resource)
+
+  def after_confirmation_path_for(resource)
     sign_in(resource) # In case you want to sign in the user
     root_path
   end
--- dpc-web/app/helpers/mailer_helper.rb
+++ dpc-web/app/helpers/mailer_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module MailerHelper
   def last_email
     ActionMailer::Base.deliveries.last
