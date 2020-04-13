15ddfdaec7d82c74b937027fb9ea8198f01489e0
Rick Hawes <richard.hawes@cms.hhs.gov>
2020-04-13 12:48:26 -0400

    Revert "Revert DPC-34 since"
    This reverts commit 9dd05ab323b79f9e8d8f94f63a5a31039d8490cf.

--- dpc-bluebutton/src/main/java/gov/cms/dpc/bluebutton/client/BlueButtonClient.java
+++ dpc-bluebutton/src/main/java/gov/cms/dpc/bluebutton/client/BlueButtonClient.java
@@ -1,25 +1,25 @@
 package gov.cms.dpc.bluebutton.client;
 
 
+import ca.uhn.fhir.rest.param.DateRangeParam;
 import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.CapabilityStatement;
-import org.hl7.fhir.dstu3.model.Patient;
 
 import java.security.GeneralSecurityException;
 
 
 public interface BlueButtonClient {
 
-    Patient requestPatientFromServer(String beneId) throws ResourceNotFoundException;
-
     Bundle requestPatientFromServerByMbi(String mbi) throws ResourceNotFoundException, GeneralSecurityException;
 
     Bundle requestPatientFromServerByMbiHash(String mbiHash) throws ResourceNotFoundException;
 
-    Bundle requestEOBFromServer(String beneId) throws ResourceNotFoundException;
+    Bundle requestPatientFromServer(String beneId, DateRangeParam lastUpdated) throws ResourceNotFoundException;
 
-    Bundle requestCoverageFromServer(String beneId) throws ResourceNotFoundException;
+    Bundle requestEOBFromServer(String beneId, DateRangeParam lastUpdated) throws ResourceNotFoundException;
+
+    Bundle requestCoverageFromServer(String beneId, DateRangeParam lastUpdated) throws ResourceNotFoundException;
 
     Bundle requestNextBundleFromServer(Bundle bundle) throws ResourceNotFoundException;
 
