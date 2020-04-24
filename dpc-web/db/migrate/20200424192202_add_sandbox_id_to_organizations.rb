96bac53908e2c70f2d2ffaa9b440875992864602
Sun-Mountain <nzonnenberg@fearless.tech>
2020-04-24 16:20:48 -0400

    Update all existing organization sandbox_ids

--- dpc-web/db/migrate/20200424192202_add_sandbox_id_to_organizations.rb
+++ dpc-web/db/migrate/20200424192202_add_sandbox_id_to_organizations.rb
@@ -1,5 +1,11 @@
 class AddSandboxIdToOrganizations < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :organizations, :sandbox_id, :string
+
+    Organization.update_all sandbox_id: Luhnacy.generate(15, :prefix => '808403')[-10..-1]
+  end
+
+  def down
+    remove_column :organizations, :sandbox_id
   end
 end
