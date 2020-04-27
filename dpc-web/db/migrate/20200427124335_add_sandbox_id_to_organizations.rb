427907c5b7d39fe17b4af7287b1dd5d69c7ec64e
Sun-Mountain <nzonnenberg@fearless.tech>
2020-04-27 10:20:07 -0400

    Added sandbox_id column and generates id for existing orgs

--- dpc-web/db/migrate/20200427124335_add_sandbox_id_to_organizations.rb
+++ dpc-web/db/migrate/20200427124335_add_sandbox_id_to_organizations.rb
@@ -1,7 +1,14 @@
 class AddSandboxIdToOrganizations < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :organizations, :sandbox_id, :string
 
-    Organization.where(sandbox_id: nil).update_all(sandbox_id: Luhnacy.generate(15, :prefix => '808403')[-10..-1])
+    Organization.find_each do |o|
+      o.sandbox_id = Luhnacy.generate(15, :prefix => '808403')[-10..-1]
+      o.save!
+    end
+  end
+
+  def down
+    remove_column :organizations, :sandbox_id
   end
 end
