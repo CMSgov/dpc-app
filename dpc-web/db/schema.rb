d0e261c2cf1de4fb6ef318ffe77986cb41f70bbc
Sun-Mountain <nzonnenberg@fearless.tech>
2020-04-27 11:43:40 -0400

    Removed provider_id and vendor_id columns

--- dpc-web/db/schema.rb
+++ dpc-web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_124335) do
+ActiveRecord::Schema.define(version: 2020_04_27_154230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,8 +87,6 @@ ActiveRecord::Schema.define(version: 2020_04_27_124335) do
     t.datetime "updated_at", null: false
     t.string "npi"
     t.string "vendor"
-    t.string "vendor_id"
-    t.string "provider_id"
     t.string "sandbox_id"
   end
 
