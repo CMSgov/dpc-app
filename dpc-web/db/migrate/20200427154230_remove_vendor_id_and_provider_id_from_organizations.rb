82198858a958d857d0ad03ac392dc24dbd647fb2
dhgreene <dhgreene@gmail.com>
2020-05-05 09:20:41 -0400

    Revert "DPC-1347: Change auto-generated vendor and provider ids to pass for NPI (#772)" (#781)
    This reverts commit 320e569a80656227dbe87cec9c428b9eca64d7b3.

--- dpc-web/db/migrate/20200427154230_remove_vendor_id_and_provider_id_from_organizations.rb
+++
@@ -1,6 +0,0 @@
-class RemoveVendorIdAndProviderIdFromOrganizations < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :organizations, :vendor_id
-    remove_column :organizations, :provider_id
-  end
-end
