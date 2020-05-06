4277a8203e1b7e65eb63a7f09c912ec48e934bb3
Sun-Mountain <nzonnenberg@fearless.tech>
2020-05-06 09:34:53 -0400

    Removed sandbox_id generate from migration

--- dpc-web/db/schema.rb
+++ dpc-web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_154230) do
+ActiveRecord::Schema.define(version: 2020_05_01_124429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,7 @@ ActiveRecord::Schema.define(version: 2020_04_27_154230) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "session_token"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["created_at"], name: "index_users_on_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
