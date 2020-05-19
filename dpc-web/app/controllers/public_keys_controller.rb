6cc4e85163a5aac9d179f3dc4b9b76d23d1bf30d
Sun-Mountain <nzonnenberg@fearless.tech>
2020-05-19 13:39:48 -0400

    Groundwork for snippet_signature to be passed to the API

--- dpc-web/app/controllers/public_keys_controller.rb
+++ dpc-web/app/controllers/public_keys_controller.rb
@@ -15,7 +15,7 @@ class PublicKeysController < ApplicationController
     reg_org = @organization.registered_organizations.find_by(api_env: params[:api_environment])
     manager = PublicKeyManager.new(api_env: params[:api_environment], registered_organization: reg_org)
 
-    new_public_key = manager.create_public_key(public_key: params[:public_key], label: params[:label])
+    new_public_key = manager.create_public_key(public_key: params[:public_key], label: params[:label], snippet_signature: params[:snippet_signature])
 
     if new_public_key[:response]
       redirect_to dashboard_path
