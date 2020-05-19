878a4d37aacc82f40aea41249ffd8233ba53a3d3
Silvio Luthi <silvio@adhocteam.us>
2020-05-19 15:52:52 -0700

    Add running sidekiq as a daemon to docker entrypoint file

--- dpc-web/docker/entrypoint.sh
+++ dpc-web/docker/entrypoint.sh
@@ -14,6 +14,11 @@ bundle exec rails db:migrate
 # Seed the database
 # This step is not needed, as there is no database seed data yet
 
+# Start background job processing
+# TODO: We should avoid using the dameonize flag in production and, instead, pursue a deployment
+# like provided here https://github.com/mperham/sidekiq/wiki/Deployment
+bundle exec sidekiq -d
+
 # Start the database service (and make accessible outside the Docker container)
 echo "Starting Rails server..."
 if [ -n "$JACOCO" ]; then
