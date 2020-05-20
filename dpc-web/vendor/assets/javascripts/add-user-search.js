03c21525b6f6c146ab5193b6b201cce76debd068
Sun-Mountain <nzonnenberg@fearless.tech>
2020-05-20 14:14:19 -0400

    Dynamic search works

--- dpc-web/vendor/assets/javascripts/add-user-search.js
+++ dpc-web/vendor/assets/javascripts/add-user-search.js
@@ -1,3 +1,21 @@
 function orgUserSearch() {
-  console.log("user search lol");
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('orgUserSearch');
+  filter = input.value;
+  filter = filter.toUpperCase();
+  ul = document.getElementById("orgUserList");
+  li = ul.getElementsByTagName('li');
+
+  if (filter.length < 3) return;
+
+  for (let i = 0; i < li.length; i++) {
+    div = li[i].getElementsByTagName("div")[0];
+
+    txtValue = div.textContent || div.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
 }
