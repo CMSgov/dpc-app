8c36429c16111db8ec986fedf8ae6285d9b9ce71
Silvio Luthi <silvio@adhocteam.us>
2020-05-21 18:26:07 -0700

    Update redis url retrieved via REDIS_URL to be build properly

--- dpc-web/config/initializers/sidekiq.rb
+++ dpc-web/config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
+  config.redis = { url: "redis://#{ENV.fetch('REDIS_URL', 'localhost')}:6379/1" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
+  config.redis = { url: "redis://#{ENV.fetch('REDIS_URL', 'localhost')}:6379/1" }
 end
