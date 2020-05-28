c2fef97bd87ff30e2df1d2ff223e1e8b60b664ef
Will Huang <52384927+whuang85@users.noreply.github.com>
2020-05-27 21:39:51 -0500

    add auth related logging filter

---
+++ dpc-common/src/test/java/gov/cms/dpc/common/logging/filters/AuthRelatedLoggingFilterTest.java
@@ -0,0 +1,43 @@
+package gov.cms.dpc.common.logging.filters;
+
+
+import ch.qos.logback.access.spi.IAccessEvent;
+import ch.qos.logback.core.filter.Filter;
+import ch.qos.logback.core.spi.FilterReply;
+import io.dropwizard.logging.filter.FilterFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mockito;
+
+import java.util.stream.Stream;
+
+public class AuthRelatedLoggingFilterTest {
+
+    private Filter<IAccessEvent> filter;
+    private IAccessEvent event = Mockito.mock(IAccessEvent.class);
+
+    @BeforeEach
+    public void setUp() {
+        FilterFactory<IAccessEvent> filterFactory = new AuthRelatedLoggingFilter();
+        filter = filterFactory.build();
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringSource")
+    public void testQueryString(String queryString, FilterReply expectedReply) {
+        Mockito.when(event.getQueryString()).thenReturn(queryString);
+        FilterReply reply = filter.decide(event);
+        Assertions.assertEquals(expectedReply, reply);
+    }
+
+    private static Stream<Arguments> stringSource() {
+        return Stream.of(
+                Arguments.of("client_assertions=secret_token", FilterReply.DENY),
+                Arguments.of("someotherstuff=stuff&client_assertions=secret_token", FilterReply.DENY),
+                Arguments.of("someotherstuff=stuff", FilterReply.NEUTRAL)
+        );
+    }
+}
