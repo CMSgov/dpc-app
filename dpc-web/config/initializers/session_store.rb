6359278ac68e29e04da0c056fb4170d595f70e71
Sun-Mountain <nzonnenberg@fearless.tech>
2020-06-04 14:26:07 -0400

    redis cache

--- dpc-web/config/initializers/session_store.rb
+++ dpc-web/config/initializers/session_store.rb
@@ -1,7 +1,1 @@
-DpcWebsite::Application.config.session_store :redis_store, {
-  servers: [
-    { host: Rails.application.config.redis_host,
-    port: 6379, db: 0, namespace: "session" },
-  ],
-  expire_after: 1.day
-}
+Rails.application.config.session_store :cache_store, key: '_dpc_app_session'
