4ce8bb0913c371280f3936ecb9b3fe6b579c40de
Will H <52384927+whuang85@users.noreply.github.com>
2020-06-06 23:41:14 -0500

    DPC-162 add auth related logging filter (#822)
    * add auth related logging filter
    
    * add test cases for client_assertion (singular)

---
+++ dpc-common/src/main/java/gov/cms/dpc/common/logging/filters/AuthRelatedLoggingFilter.java
@@ -0,0 +1,33 @@
+package gov.cms.dpc.common.logging.filters;
+
+import ch.qos.logback.access.spi.IAccessEvent;
+import ch.qos.logback.core.filter.Filter;
+import ch.qos.logback.core.spi.FilterReply;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import io.dropwizard.logging.filter.FilterFactory;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Implementation of {@link FilterFactory} that ensures client_tokens are not logged when requests are made to the /Token/auth endpoint
+ */
+@JsonTypeName("token-filter-factory")
+public class AuthRelatedLoggingFilter implements FilterFactory<IAccessEvent> {
+
+    protected AuthRelatedLoggingFilter() {
+        // Not used
+    }
+
+    @Override
+    public Filter<IAccessEvent> build() {
+        return new Filter<>() {
+            @Override
+            public FilterReply decide(IAccessEvent event) {
+                if (StringUtils.containsIgnoreCase(event.getQueryString(), "client_assertion")) {
+                    return FilterReply.DENY;
+                }
+                //Anything else, let other filters do their thing
+                return FilterReply.NEUTRAL;
+            }
+        };
+    }
+}
