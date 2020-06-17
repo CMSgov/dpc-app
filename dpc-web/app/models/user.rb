6e5155254fb97903250844b32c1a04901811699b
SMLuthi <silvio@adhocteam.us>
2020-06-17 13:33:37 -0700

    Dpc 150 validate and properly escape all web model inputs (#859)
    * Add html escaping when creating the csv user file

--- dpc-web/app/models/user.rb
+++ dpc-web/app/models/user.rb
@@ -78,14 +78,25 @@ class User < ApplicationRecord
     )
   end
 
-  def self.to_csv
-    attrs = %w[id first_name last_name email requested_organization requested_organization_type
-               address_1 address_2 city state zip agree_to_terms requested_num_providers created_at updated_at]
+  ATTRS = %w[id first_name last_name email requested_organization requested_organization_type
+             address_1 address_2 city state zip agree_to_terms requested_num_providers created_at updated_at].freeze
 
+  # html escape these fields for XSS protection
+  ESCAPED_ATTRS = %w[first_name last_name requested_organization address_1 address_2 city].freeze
+
+  def self.to_csv
     CSV.generate(headers: true) do |csv|
-      csv << attrs
+      csv << ATTRS
       all.each do |user|
-        csv << user.attributes.values_at(*attrs)
+        attributes = user.attributes
+        escaped_attributes = attributes.map do |k, v|
+          if ESCAPED_ATTRS.include? k
+            v = ERB::Util.html_escape(v)
+          end
+
+          [k, v]
+        end.to_h
+        csv << escaped_attributes.values_at(*ATTRS)
       end
     end
   end
