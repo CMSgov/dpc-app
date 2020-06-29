4a97bf3e84b2f41c3348831e03a3a1a92492b44f
Sun-Mountain <nzonnenberg@fearless.tech>
2020-06-29 14:36:11 -0400

    Automatically add user to requested organization

--- dpc-web/app/controllers/internal/organizations_controller.rb
+++ dpc-web/app/controllers/internal/organizations_controller.rb
@@ -34,17 +34,16 @@ module Internal
 
       if @organization.save
         flash[:notice] = 'Organization created.'
+
+        if from_user_params[:from_user].present?
+          @user = User.find(from_user_params[:from_user])
+
+          @organization.users << @user
+        end
+
         if prod_sbx?
-          if from_user_params[:from_user].present?
-            @user = User.find(from_user_params[:from_user])
-
-            @organization.users << @user
-          end
-
           redirect_to new_internal_organization_registered_organization_path(organization_id: @organization.id,
                                                                              api_env: 'sandbox')
-        elsif from_user_params[:from_user].present?
-          redirect_to edit_internal_user_path(from_user_params[:from_user], user_organization_ids: @organization.id)
         else
           redirect_to internal_organization_path(@organization)
         end
