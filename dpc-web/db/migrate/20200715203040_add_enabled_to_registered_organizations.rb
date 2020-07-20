945b4941d6a711553967aa9ccfaf971198b7456f
Sun-Mountain <nzonnenberg@fearless.tech>
2020-07-20 16:51:35 -0400

    Final final migration

--- dpc-web/db/migrate/20200715203040_add_enabled_to_registered_organizations.rb
+++ dpc-web/db/migrate/20200715203040_add_enabled_to_registered_organizations.rb
@@ -1,7 +1,5 @@
 class AddEnabledToRegisteredOrganizations < ActiveRecord::Migration[6.0]
   def change
     add_column :registered_organizations, :enabled, :boolean, :default => true, null: false
-
-    RegisteredOrganization.where(enabled: nil).update_all(enabled: true)
   end
 end
