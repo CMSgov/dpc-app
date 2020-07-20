957f73992f3c09557218ee3311c0455450198d59
Sun-Mountain <nzonnenberg@fearless.tech>
2020-07-20 15:48:16 -0400

    Registered Enabled update

--- dpc-web/db/migrate/20200715203040_add_enabled_to_registered_organizations.rb
+++ dpc-web/db/migrate/20200715203040_add_enabled_to_registered_organizations.rb
@@ -1,11 +1,7 @@
 class AddEnabledToRegisteredOrganizations < ActiveRecord::Migration[6.0]
-  def up
-    add_column :registered_organizations, :enabled, :boolean, null: false
+  def change
+    add_column :registered_organizations, :enabled, :boolean, :default => true
 
     RegisteredOrganization.where(enabled: nil).update_all(enabled: true)
-  end
-
-  def down
-    remove_column :registered_organizations, :enabled
   end
 end
