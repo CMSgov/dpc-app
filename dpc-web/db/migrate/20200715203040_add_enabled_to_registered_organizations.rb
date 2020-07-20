3438465cd233c193cb877987a539e70d314ded99
Sun-Mountain <nzonnenberg@fearless.tech>
2020-07-20 13:49:42 -0400

    Migration update: default enabled to true

--- dpc-web/db/migrate/20200715203040_add_enabled_to_registered_organizations.rb
+++ dpc-web/db/migrate/20200715203040_add_enabled_to_registered_organizations.rb
@@ -1,7 +1,11 @@
 class AddEnabledToRegisteredOrganizations < ActiveRecord::Migration[6.0]
-  def change
+  def up
     add_column :registered_organizations, :enabled, :boolean, null: false
 
-    RegisteredOrganizations.update_all enabled: true
+    RegisteredOrganization.where(enabled: nil).update_all(enabled: true)
+  end
+
+  def down
+    remove_column :registered_organizations, :enabled
   end
 end
