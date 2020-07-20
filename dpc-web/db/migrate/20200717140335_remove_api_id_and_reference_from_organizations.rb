b1d73dd0e44044eefc1043b595c05ddb44cc76ae
Sun-Mountain <nzonnenberg@fearless.tech>
2020-07-20 09:48:58 -0400

    remove migration

--- dpc-web/db/migrate/20200717140335_remove_api_id_and_reference_from_organizations.rb
+++
@@ -1,6 +0,0 @@
-class RemoveApiIdAndReferenceFromOrganizations < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :organizations, :api_id
-    remove_column :organizations, :api_reference
-  end
-end
