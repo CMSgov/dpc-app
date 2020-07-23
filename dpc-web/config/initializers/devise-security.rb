963f403da3a2075af7805dee6e787a3e9fa22e2b
Sun-Mountain <nzonnenberg@fearless.tech>
2020-07-23 11:10:59 -0400

    Renew reset (2 months)

--- dpc-web/config/initializers/devise-security.rb
+++ dpc-web/config/initializers/devise-security.rb
@@ -5,7 +5,7 @@ Devise.setup do |config|
   # Configure security extension for devise
 
   # Should the password expire (e.g 3.months)
-  config.expire_password_after = 60.days
+  config.expire_password_after = 2.months
 
   # Need 1 char of A-Z, a-z and 0-9
   config.password_complexity = { digit: 1, lower: 1, symbol: 1, upper: 1 }
