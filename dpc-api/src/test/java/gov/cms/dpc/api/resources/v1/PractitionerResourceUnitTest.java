ee8e889b1c9dd8ec2d8695e711d40426f8a5e785
Emily Hart <1923441+embh@users.noreply.github.com>
2020-07-28 16:27:04 -0400

    Unit tests in progress

--- dpc-api/src/test/java/gov/cms/dpc/api/resources/v1/PractitionerResourceUnitTest.java
+++ dpc-api/src/test/java/gov/cms/dpc/api/resources/v1/PractitionerResourceUnitTest.java
@@ -6,8 +6,10 @@ import ca.uhn.fhir.rest.gclient.ICreateTyped;
 import ca.uhn.fhir.rest.gclient.IReadExecutable;
 import ca.uhn.fhir.validation.FhirValidator;
 import gov.cms.dpc.api.auth.OrganizationPrincipal;
+import gov.cms.dpc.fhir.DPCIdentifierSystem;
 import org.hl7.fhir.dstu3.model.Organization;
 import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.dstu3.model.Practitioner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Answers;
@@ -20,7 +22,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PatientResourceUnitTest {
+public class PractitionerResourceUnitTest {
 
     @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     IGenericClient attributionClient;
@@ -28,51 +30,33 @@ public class PatientResourceUnitTest {
     @Mock
     FhirValidator fhirValidator;
 
-    PatientResource resource;
+    PractitionerResource resource;
 
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        resource = new PatientResource(attributionClient, fhirValidator, null, null);
+        resource = new PractitionerResource(attributionClient, fhirValidator);
     }
 
     @Test
-    public void testSubmitPatient() {
+    public void testSubmitProvider() {
         UUID orgId = UUID.randomUUID();
         Organization organization = new Organization();
         organization.setId(orgId.toString());
         OrganizationPrincipal organizationPrincipal = new OrganizationPrincipal(organization);
-        Patient patient = new Patient();
+        Practitioner practitioner = new Practitioner();
 
         ICreateTyped createExec = Mockito.mock(ICreateTyped.class);
-        Mockito.when(attributionClient.create().resource(patient).encodedJson()).thenReturn(createExec);
+        Mockito.when(attributionClient.create().resource(practitioner).encodedJson()).thenReturn(createExec);
 
         MethodOutcome outcome = new MethodOutcome();
-        outcome.setResource(patient);
+        outcome.setResource(practitioner);
         Mockito.when(createExec.execute()).thenReturn(outcome);
 
-        Response response = resource.submitPatient(organizationPrincipal, patient);
-        Patient result = (Patient) response.getEntity();
+        Response response = resource.submitProvider(organizationPrincipal, practitioner);
+        Practitioner result = (Practitioner) response.getEntity();
 
-        assertEquals(patient, result);
-        assertEquals("Organization/"+orgId, result.getManagingOrganization().getReference());
-    }
-
-    @Test
-    public void testGetPatient() {
-        IReadExecutable<Patient> readExec = Mockito.mock(IReadExecutable.class);
-
-        UUID patientId = UUID.randomUUID();
-        Patient patient = new Patient();
-        patient.setId(patientId.toString());
-
-        Mockito.when(attributionClient.read().resource(Patient.class).withId(patientId.toString()).encodedJson())
-                .thenReturn(readExec);
-
-        Mockito.when(readExec.execute()).thenReturn(patient);
-
-        Patient result = resource.getPatient(patientId);
-
-        assertEquals(patient, result);
+        assertEquals(practitioner, result);
+        assertEquals("Organization ID", practitioner.getMeta().getTag(DPCIdentifierSystem.DPC.getSystem(), orgId.toString()).getDisplay());
     }
 }
