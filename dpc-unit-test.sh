b2dfca52937355b1df9068b12b5255fe57069cc3
Emily Hart <1923441+embh@users.noreply.github.com>
2020-08-06 11:43:22 -0400

    DPC-392: Break apart integration and unit tests (#933)
    * Annotate tests relying on db with @IntegrationTest
    
    * Script for running only unit tests
    
    * Simple unit tests for API Patient GET and POST
    
    * Unit tests in progress
    
    * Key resource unit test

---
+++ dpc-unit-test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -Ee
+
+# Current working directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Configure the Maven log level
+export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=info
+
+# Include secure environment variables
+set -o allexport
+[[ -f ${DIR}/ops/config/decrypted/local.env ]] && source ${DIR}/ops/config/decrypted/local.env
+set +o allexport
+
+function _finally {
+  docker-compose down
+}
+trap _finally EXIT
+
+echo "┌──────────────────────────────────────────┐"
+echo "│                                          │"
+echo "│          Running Unit Tests...           │"
+echo "│                                          │"
+echo "└──────────────────────────────────────────┘"
+
+mvn test -Punit-tests
+
+echo "┌──────────────────────────────────────────┐"
+echo "│                                          │"
+echo "│           Unit Tests Complete            │"
+echo "│                                          │"
+echo "└──────────────────────────────────────────┘"
