951a1c984339063a989e8dc45a0e321cfdf32a25
Sun-Mountain <nzonnenberg@fearless.tech>
2020-08-10 15:35:48 -0400

    Migrations

--- dpc-web/db/migrate/20200810161501_create_the_resources.rb
+++ dpc-web/db/migrate/20200810161501_create_the_resources.rb
@@ -1,7 +1,5 @@
 class CreateTheResources < ActiveRecord::Migration[6.0]
   def change
-    drop_table :the_resources
-
     create_table :the_resources do |t|
       t.datetime :password_changed_at
     end
