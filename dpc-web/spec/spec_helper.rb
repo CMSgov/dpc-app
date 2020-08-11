1d1269ef705b65e790739182486cdc74e1fbce7e
Silvio Luthi <silvio@adhocteam.us>
2020-08-11 06:40:31 -0700

    Add initial simplecov settings to spec helper file

--- dpc-web/spec/spec_helper.rb
+++ dpc-web/spec/spec_helper.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter 'app/jobs/application_job.rb'
+  add_filter 'app/channels/application_cable/channel.rb'
+  add_filter 'app/channels/application_cable/connection.rb'
+end
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
