cb449b6c111c4fcf81036e1ae70f142635f406de
Sun-Mountain <nzonnenberg@fearless.tech>
2020-08-14 12:49:06 -0400

    Test fix

--- dpc-web/app/controllers/client_tokens_controller.rb
+++ dpc-web/app/controllers/client_tokens_controller.rb
@@ -15,11 +15,11 @@ class ClientTokensController < ApplicationController
     reg_org = @organization.registered_organization
     manager = ClientTokenManager.new(registered_organization: reg_org)
 
-    elsif manager.create_client_token(label: params[:label])
+    if params_present? && manager.create_client_token(label: params[:label])
       @client_token = manager.client_token
       render :show
     else
-      return render_error 'Label required.' if missing_params
+      return render_error 'Label required.' unless params_present?
 
       render_error 'Client token could not be created.'
     end
@@ -58,8 +58,8 @@ class ClientTokensController < ApplicationController
     render :new
   end
 
-  def missing_params
-    params[:label].blank?
+  def params_present?
+    params[:label].present?
   end
 
   def unauthorized
