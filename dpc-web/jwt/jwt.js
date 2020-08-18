705a08b2ce815e8094b9d1954f2cf1b719859da4
Sun-Mountain <nzonnenberg@fearless.tech>
2020-08-18 18:16:34 -0400

    JWT test

--- dpc-web/jwt/jwt.js
+++ dpc-web/jwt/jwt.js
@@ -1,14 +1,4 @@
-const generateJWT = () => {
-  // User input
-  var clientToken, env, secret, keyId, unsigned, jwt;
-  clientToken = document.getElementById("clientToken").value.trim();
-  env = document.getElementById("environment").value.trim();
-  secret = document.getElementById("privateKey").value.trim();
-  keyId = document.getElementById("keyId").value.trim();
-
-  // Output
-  unsigned = document.getElementById("unsignedJWT");
-  jwt = document.getElementById("JWT");
+function generateJWT() {
 
   var dt = new Date().getTime();
   var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
@@ -18,39 +8,58 @@ const generateJWT = () => {
   });
 
   var data = {
-      "iss": clientToken,
-      "sub": clientToken,
-      "aud": env + "/v1/Token/auth",
-      "exp": Math.round(new Date().getTime() / 1000) + 300,
-      "iat": Math.round(Date.now()),
-      "jti": uuid,
+    "iss": document.getElementById("clientToken").value.trim(),
+    "sub": document.getElementById("clientToken").value.trim(),
+    "aud": document.getElementById("environment").value.trim() + "/v1/Token/auth",
+    "exp": Math.round(new Date().getTime() / 1000) + 300,
+    "iat": Math.round(Date.now()),
+    "jti": uuid,
   };
+
+  var secret = document.getElementById("privateKey").value.trim();
 
   const header = {
       'alg': 'RS384',
-      'kid': keyId,
+      'kid': document.getElementById("keyId").value.trim(),
+  }
+
+  var sPayload = JSON.stringify(data);
+  document.getElementById("unsignedJWT").innerHTML = JSON.stringify(data, null, 2);
+
+  var sJWT = KJUR.jws.JWS.sign("RS384", header, sPayload, secret);
+
+  document.getElementById("JWT").innerHTML = sJWT;
+
+}
+
+function generateJwtTest(env, clientToken, privateKey, keyId) {
+  var dt = new Date().getTime();
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (dt + Math.random() * 16) % 16 | 0;
+    dt = Math.floor(dt / 16);
+    return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+
+  var data = {
+    "iss": clientToken,
+    "sub": clientToken,
+    "aud": env + "/v1/Token/auth",
+    "exp": Math.round(new Date().getTime() / 1000) + 300,
+    "iat": Math.round(Date.now()),
+    "jti": uuid
+  };
+
+  var secret = privateKey;
+
+  const header = {
+    "alg": 'RS384',
+    "kid": keyId
   }
 
   var sPayload = JSON.stringify(data);
   var sJWT = KJUR.jws.JWS.sign("RS384", header, sPayload, secret);
 
-  try {
-    KJUR.jws.JWS.verifyJWT(sJWT, `-----BEGIN PUBLIC KEY-----MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEApwlL/kFlrYMvpc3ksbSsXEYWvBf5qIXnDAeHp0Tj+U6ItZJbjp2uelQUtDfPiMl8N2w4fjBH44J0x8Cp9dk5GkHntzSluhiJiTvf9E4Kqfv/gw2Q+o9lMqTvKcX9x+P9hn8/2c7BFm4LN7WBKbEf
-    r52mp97a1qA69kJGbiyAT2MlTlcfLuAh8E3XAJPPtgkDu+0xUOgIb26xoEYZMy4E
-    /3CZw1yOjd2sF/p87RdojsKSsHhObjIUwby6Q2RuAmSEg382jzkoqfOyhYcViJMa
-    jdOmj+4OSus89dfg12hsRfF8Gs/NW7jIH3v8TXcwzUXeU29Vh4JvoCladuUuHcRC
-    b1yy3jQgC0FMoMdLgW2MoMM1LseeUtlIdQ0MfRtfXJ7axsZLJ7KSlvSZIA7SrYTz
-    IT0Y6HODWi1dl6tw+fDvDjnsCMULgvgFd1PS9sPyYmbGnb1/gSaDLVbo+HyzhcCO
-    dgaXMSBDqIcqAyb2qXVrpyR0pyl6YQyWBo6qkLQjnAGU7kd6AIINZ2b6qtOELLB2
-    3VMrODf46vTqMqsNpxXI3EqfVLkZcb1IQ4jrWdRVfVYZtE+3WQGLX9TQVCmZqVy2
-    yO2uPwGcSGLyNafspG4sICXi4HxQmyJt/FA967o3A1t/7AZv/ICKJ8KrvAvzoihs
-    7FBKpdfdfG7GFVjDWZoowD8CAwEAAQ==
-    -----END PUBLIC KEY-----`,sPayload)
-  }
-
-  unsigned.innerHTML = JSON.stringify(data, null, 2);
-  jwt.innerHTML = sJWT;
-
+  return { sPayload: sPayload, sJWT: sJWT }
 }
 
-module.exports = generateJWT;
+module.exports = generateJwtTest;
--- dpc-web/jwt/jwt.test.js
+++ dpc-web/jwt/jwt.test.js
@@ -1,94 +1,75 @@
-const generateJWT = require('./jwt');
-var jwt = require('jsonwebtoken');
-
-beforeEach(() => {
-  document.body.innerHTML = `
-  <h1>Create a JWT</h1>
-  <p>Environment:<br><input type="text" id="environment" style="width:100%;"></p>
-  <p>Private Key:<br><textarea id="privateKey" rows="20" cols="100"></textarea></p>
-  <p>Client Token:<br><textarea id="clientToken" rows="10" cols="100"></textarea></p>
-  <p>Public Key ID:<br><input type="text" id="keyId" placeholder="xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" style="width:100%;"></p>
-  <p><input type="button" id="JwtButton" value="Generate JWT"></p>
-  <p>Unsigned JWT:<br><textarea id="unsignedJWT" rows="20" cols="100"></textarea></p>
-  <p>Your JWT:<br><textarea id="JWT" rows="20" cols="100"></textarea></p>
-  `;
-
-  document.getElementById('JwtButton').addEventListener('click', generateJWT);
-})
+var KJUR = require('jsrsasign').KJUR;
+window.KJUR = KJUR;
+const generateJwtTest = require('./jwt');
 
 
-test('generates an unsigned JWT and JWT with user input', () => {  
-  const clientToken = document.getElementById("clientToken");
-  const env = document.getElementById("environment");
-  const secret = document.getElementById("privateKey");
-  const keyId = document.getElementById("keyId");
-  
-  const button = document.getElementById("JwtButton");
-  const unsignedJwtOutput = document.getElementById("unsignedJWT");
-  const jwtOutput = document.getElementById("JWT");
-
-  clientToken.value = 'W3sidiI6MiwibCI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwMiIsImkiOiIyZGI5ODhjMy0yNDAwLTQzNDktYmNhOS01ZDNkOWY0YjU5Y2YiLCJjIjpbeyJpNjQiOiJaSEJqWDIxaFkyRnliMjl1WDNabGNuTnBiMjRnUFNBeSJ9LHsiaTY0IjoiWlhod2FYSmxjeUE5SURJd01qRXRNRGd0TVRkVU1qRTZOVGM2TVRndU1UQTBOalkyV2cifSx7Imk2NCI6ImIzSm5ZVzVwZW1GMGFXOXVYMmxrSUQwZ056RTNabVZtT1RjdFptTmlOQzAwTW1Sa0xXSTVPRGN0WlRZelpqSTJOVGsxWmpjMCJ9LHsibCI6ImxvY2FsIiwiaTY0IjoiQW0wa1dzOXRKRnJQLUR1bW1Pd0FSbjMxcDZRTERsNEl2RUcxQlRQakVUbjlCeTBnZmk1SjN1ZzNfMy1nUUFwWjBrT1JhU0JyQ05uSGxqc253bW51dVlTYXRIQXpDRFZ0UWNvVkh4N0FjcTNmZTZDaXNONVFDNUVHQmlNaVp1dUZ5dFF5VGtSTDB3MXc3MWZCR0U2T2cxMnFXSXZZUWliZWx5U2E1ZElpako4cW1FRFJ2cDFlaGNYUk15MXF3eTBQSEl0S2FtOVlUUTJfR2tNUUFBIiwidjY0IjoiaEc1U2ExLWpIenAxdUFrMl8xamhWa1UxUGh1WmxlbzJneFB5OTlwcGpmdWZ3WU9xWlZfYnNUQ1VJLUZpdncydC0zNDJmR3pzR2xLTWRfT201OHRFSk52RGpoM2FNQ3ZxIn1dLCJzNjQiOiIzY2Q1X2ZNbkRfRWhWck1lcjFVUk9SQkRWVGJHZlZLMm04bGRmQ0hRVEw4In1d';
-  env.value = 'prod-sbx';
-  secret.value = `-----BEGIN RSA PRIVATE KEY-----
-  MIIJKQIBAAKCAgEAqCSicQS8voCL/x5R67+oSwlbvowfYCtiMpkdgRU8/7e+Zdlm
-  KU3guE2XLMaiQ+V1xnUE82k6e+088JjZOtrQBxGdclnOugad9iwQya+Ngj9uM1Ea
-  ebwg8e50QPswBERCt91g3/x+635QmCMh1wCU0DWIqsrg5uGtX/QakRavVc/QartP
-  4fT7zgAvLhngX/U/QtmoTBVmyXvY8NTOV38Z3HvX6GVKoOiETTPoeajFJYdbvQ7b
-  PvnnjrBdL6c84CU1q/lKQiAHgCpEHylpr+pcVrCA1IbQ/8DQbNtboEt/w7vUAFp4
-  zxwbH0pKn4YcGGsNmjD7qOPHUAt68XTZ0VB/uYgO6z8IbV4Xi4dylW/sg/VPgj3E
-  ViO6g1t4kB4fjGQnDooZnCfIFduVhKK7L6D/UbJNlaGBw9pQSZJtTgmKi3QWNJR9
-  lNdfOXrO0Ds5Vy+npECGMb6W1CBgoePx7R/VhpWXBQqHvhEFYMly1UrnGgBcHILb
-  zk8X8Q6PYwmS4ZDpZbFr/Gohp8PISVQlhfZVD7ocm+dTT9WRyPIYbOIUCitOuwEY
-  gSNCjjmJGgythctuvj+RRGPyR1kqRDIOBPo2n6TTwnnT5ydz+thfL5KUC5saCRSa
-  tcaJnKi30Ev5kmcQpz9k5Dvmbd2BZ+543RJuYuVqWbdWaA9CtDv56anBoh0CAwEA
-  AQKCAgB80UUs9MFQAYjrR4y0nr/FhBrw5n/cGhh5SySV0DhJ9BKI9Mtb2g35gs+4
-  U3PdRxcYzYFxWCosGL5mNHD9ubU3qiHg4z4M5iCYv7lunr1DdObPfWNT/w3Nyp/O
-  JSlN4YZNiy8A1yU4l7ooVdwnCK1vqm3bq2MswVa8pi6aieZ6oxwqPwZbzEqob4aW
-  iGsBkML3UzAV2sVEyIUQskjCtv1xqRVr1NWDgVfFH8VxmpFO/J5jSAYFR+pXCCEE
-  4PbVxYmEMjinwbMPt1B+eXWVOzp0t+3K1pGLaYzPYj8wTRt5J5OG96I6zLhtj+65
-  YQq2LbIZRkdq5jvs9bi8SWvwb/t/gKEKXOc3qDvMqQK+sLNqsbMbScoAfUjvoLjf
-  esmykUJO//bF7TfTu1584r6WhBJo3PLLRdexRt0w8MpCZpux/USVeMkRRtpIw9HZ
-  90KYSWhCUOxv1cbKpfrhnkjfQ6HgoKH7vI0zAUTRgh+/JY+4loN4y6twfq0eAQ2M
-  2nUCbePIbQw4AWxEE3wIRkJG4KIrodGTb+MBkM1xnPxwqFpA1dY29YdYcM19mKW/
-  Cqz+5WlGE1JhXPaHvacUJdlPCLr4X3vjtfErHbU+OExGTRylbZLqimkBttOcnS0+
-  9qWfFBotft2McTvV0mszdnXzrIiVFxNovP2MUGsYZhALMqnrAQKCAQEA0/yZVF5U
-  qU6euRtIx3rSfiRtvFOvr9Be/Odj6r92T4iM0KCLxudkA3nBHfkbsVeXp58u+8m6
-  4z2SIRPlAlh93pU+3K0rLxoluaZFNKiTQpS+Z5r+uTP8Ug9YjMRPqNwq6Ikfw2FR
-  86nzSWdDXSbg5owE191yDz1ufBYmLkGfHt4eR7RYJhEvXXq+3WxpCEBCDC0hrxx+
-  oDJDBqvtdxa1u5GAQcxS8xuczUTy1caMBzWfm+ccENcYB4TaJ+46tqrqlnii6T/R
-  AVadAq3HK5oH5S/DL7BiOEHJs4s2hJPOVVj3WDLHPe4FEgD9Lr0PNAt6tiFrfdn3
-  RpfTitIHroF+IQKCAQEAyw2vI9ATQfAJ4UbvmzmnwaCxXQe7xJiFwE1gAJnDEk9M
-  9Hj1lX/w8NFoxdpO8QzOaMo/KEl2XwydFHI9shNfPF8nAYW+WHet5rdAb2TEKFfN
-  CSLuZFTq1FxhjOwlj3fgyxZFWaAlociR0xhfCZ1AITYFwqaRJ1r5i/uG975utJDs
-  arFVCAst5oSgJFkAHuBRbm0mde+aaQU2k2ZHosCUAy1RaunPs5P+AlKy/ni3Y3aS
-  qjq2suXIFijJyz7HcXO6OJaGnj6lWK5ujKyAGxpfmIemjTLr3Lh6PtgR6aPZbJZD
-  uEi7uUcaeQzPTxtya/oZUuKtyyjvYQoNyCt2/+2MfQKCAQEArJh6go23Io4nfxDu
-  5bNjlF62leIRJfVikUkYOfYfLsw/0lEU3SJidM355RyAZpipmklp6Ikrx37G7nWT
-  PKCuAr7DCstXYKdKVehBaoliNJCEojg48rOX2XwzHZFsRlDUArY6jUo8fkY+FeKk
-  n0BbVnpkKxlal3vt68vg1EKLeodgYgM8zemqBdM9eWyd66Dd5aoNRdTOaEHj/peW
-  5SIxXwEtSuVAGD3AIgTkLhfUxL3tMPMvD6sBHoYVVPOLDFUGYPLhuUVDN3K8rYSt
-  qBtmGD0WD0rt/V3bCnrrLXBmS2j/Ield/VdA+5KgkSBPM5GxHH8DVNlVkj1leB5f
-  ML6v4QKCAQEAgbN9+fxBMM8pIHL1Pku3smD4qhn0gIGEfe9usTOGfT2WRFq3VAVj
-  XBAHQspNvn+VoS3+5bb3G4OtGpBWhFcHcEK/YaOSkGPx+vt29zCAc9yRpZJggEoV
-  rgxMa320CE5kkpTpO3SmVQ8Oxq48lqGRgyVEzK8k7OSZnibOcZV8lW409XnMfNTX
-  PvXcbRtEjnRuz4B9Hrwr/4VK9SBSBc3JPbZG2Wn8OBMCH22/0/g7/BaTXJUgeml4
-  Q19OdrrlHrzzaI4N8yrE5z6UctsFAUr5YS7U3kB2lIhp3Zaa6oi0qz3Yh/A9qP2Y
-  FVZvXXKExh/86QrORIUjUvLfFstcueF86QKCAQB4M4VQwv+OPMguldu3h62uaf58
-  uzKSpxr+hFWGeuX4X6SSd3N4NgZ3IW3EUOZIwxPF0FtGYnH9K4Rde1yjl0DwBYkc
-  vPSkhbYqB7Sjw3LDwlAO4w7sHI/tyhpnsGgH/Pg9hbiECpXre1OlZGld7u15tg9U
-  p/8iTKuIcOf1Lp50uRKIopPA5Z5c+I9MtTWl2SXWp6ds5uI8TEipI0E4MuwLMjq0
-  pKotjHF17eGLM2QYUGn4nGe1OUdvrKeKF5LZhRDsVPfGQshbLG5JUoUaoP7xF0om
-  qaflITvrJYXIzNRKbzdbq3haFZPosH9dbHav31gRPv8id6PR31mQWIQcvhOI
+test('generates an unsigned JWT and JWT with user input', () => {
+  var env = 'prod-sbx';
+  var clientToken = 'W3sidiI6MiwibCI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwMiIsImkiOiIxZWIxZGM5OS0zYTlmLTQwNTYtYjA5OC03NGFkMzNlNDllY2MiLCJjIjpbeyJpNjQiOiJaSEJqWDIxaFkyRnliMjl1WDNabGNuTnBiMjRnUFNBeSJ9LHsiaTY0IjoiWlhod2FYSmxjeUE5SURJd01qRXRNRGd0TVRoVU1qTTZNVFE2TWpVdU5qQXpNemt3V2cifSx7Imk2NCI6ImIzSm5ZVzVwZW1GMGFXOXVYMmxrSUQwZ056RTNabVZtT1RjdFptTmlOQzAwTW1Sa0xXSTVPRGN0WlRZelpqSTJOVGsxWmpjMCJ9LHsibCI6ImxvY2FsIiwiaTY0IjoiQW0wa1dzOXRKRnJQLUR1bW1Pd0FSbjMxcDZRTERsNEl2RUcxQlRQakVUbjlCeTBnZnQ0QTJKcE9qOFVCZXRLSG93M0xnWkk5VDlnQVNaODV2bmRqSkZHVGxnVTZtVzd3T0pKTmxXck15ZlN1WGRrUnB1SXZVY1JtNWVlbzNmbDlYbkVUcUo1VFVMSWxsWWt5R3drSjlCbV9iT0JkOF94d0ZVVmxBNTMxUjBWYXpnMldRSjlqOXdQY2NOMlRBdHIxb2kyeG5McGk2RUZMSDZuNjZ3IiwidjY0IjoiMUkwTFlrZVRyU21yMHhHV3dSckM1TEZteU9GNThDUi1zbjcxUEQwMW5sWktCZjNZUXRSVnUxWUp6Q2ZQNFJlbGNlbVJRUU9ucUZOOC1zWmJCNEFKYm9ReGRycG5IalNWIn1dLCJzNjQiOiJveXdLVmZVMkt6YmtjbXlNSE9Cb1lvUWUwc1NhZEZYdzFNc1E2dHd1a0lrIn1d';
+  var privateKey = `-----BEGIN RSA PRIVATE KEY-----
+  MIIJKQIBAAKCAgEAtVT4NBbthWNNwcQDlmX1dQBiwFq0uUtx4NaremAuTGJx+1ld
+  tq14DaqUmgeTqvO0OXgfH2lQMtMGbJ1dDwEo3q7S+L9yVBEEFYxTXcrhb2NaM6w4
+  rvrerxFIj+RXoWTx4KDYJuzyWjvD46errz2WuAK7pzej0Ni0U/jX2y+rA+b9/SKb
+  ORyZQcgvY7RVmduyc1txGwZW7UHLSm9d+Ukm6zYqXXiL5wGgnB2/y8JaUXrZrM6h
+  qTNrtsusQDXb2ssVTKfIZh/jGGF6wotDc9VH+hXslE/CQUYic26hRTEZLbKepLAq
+  FfRnHK1vRKC/EinFNZ7ctz4SSL7qsSf0l4wgmnDGimZIXupvhRV4+c2DsqPJZs/n
+  f214qCJrjudNSECp18jQOle8w0zgAAuzrGdgAKxs0T6j7YOqfM/Zjrh0cRWzKKRM
+  +QG2AmK7s2ODfdm8hlux8VqTtIZrPbtG4vrYCdorl8FLuWIbwfFUPCA9gJwsHIX+
+  i+yFlBXm+ENXnQB/xTVADUa94lceYTi8Le8Y1b6BXe6obPpmN2VJOj+ej95pwk6U
+  H6rYIPO6ehHKPzyze6dIYtBGE5JTTzfExO6Q9cufgXnZIAfxAkvT782E21d2az3l
+  c9KAbSTC6lpEn6+4DW5Es+buu4o+GB0lrwTzUGyZ7vxudo/KsSnyxZ+zKdcCAwEA
+  AQKCAgBfaOTr64BwZc68/Fjy4pTjSh+rnE5pDKRPrEQvsLnLoSTeHqhrfRyk/DMS
+  VJhDI9WZQ3YEvhHfnB9OOQKE6ZRKjuwczAbo6sceW2Zl/GDfjgQhGiX9E6zlqo9m
+  frwJBKcsDRWObbAbAEVS3J8+JobOLUn1cxYzxkIjyNQy1QMDm5/aeEH6K+0Lqrwc
+  lv9PZESh4+RAVvaYAb1oXe6xQudCbY6e/dWG59lNK24GVS3Wn3A3N8Xh1FeBp+rr
+  IiubFK4RVlrgOTjxVCEWidYk9XzYvn5rjwfj9nYcDIK4eFwKKmjEuJh8UsuKUEsy
+  be37LZVBYGQ72mIeeq3cJuzQbH+eP68Shfzzdv7Cdu3obq/s1bVyswxuFCQ7P/bs
+  BGhp42lzsu6eSAtqO0u6dGppx7QSu8TwVECHy/VHhqBxCE98pLPo8+yWXU0/WjWx
+  e8iLoBPjWf7uszi8sUgyuVycEhjAgi9L1vb1ZcAE14gMhHDyo2/jvK991rCbiQx7
+  kcDREtz6NHfFRd3BCAy00xArQS69u4ObyzB8Ch78BBSMtaBDR3sGgPHDI26F7W3H
+  ajFIEMIzdEoy0IuavwtKtrhzt3Sbjk5pjgNeGrDON9xM6sS8+xHt7u3v4+qGKL/W
+  gXJ8tMm88ozELx8N0BuFevubIJoZUI7v7r3IFHJW2SjEREeWuQKCAQEA7H7o7Gbd
+  8LgVLt/CfVFy9Bdh5SYUUTrktrWWS4gPIGNg0i7gO6kmaEpnIP/dWNnV4DBNMXNo
+  uOuoGBxaoN0EBLOrYL1R0qRIwCqmr7aemsbDatJy4UCYKR8JFfLXQtPqmRGAcXLh
+  EiVzybs+it+pHdgHjWW1e7M+1sE/f/+XJycviN8iHAqgdCKgWJYXGmEFgEq7D6Fa
+  udjvQeKdwyG4+SzkcO3vsGgvPgs49jQAGYq7mXM3LBhx+JBJX61uJHIL1DW1wZaW
+  lcziFDMv3XVgFpnz7I0KlKmaGfXrMUa+PxmtjuqJ6QNacQqG/rMJCIKoprryDeiY
+  EhoViqAvuhmpSwKCAQEAxEllaJJ3EJw/SJ03AwtBG2R6/6cql4sWmsjo5Z7iOP6V
+  0IGGIPa9ND5eJ7psDrnCp+Qj8nT5fijPxin/HeAwQWP/TZJxPB+Ye9P+Xt6+8nee
+  NUSLLNcDQaqjYnsIe/FdXQ/cVYkI60pRVeEbPvSuUyd+DYQZQydSGBMxw6U1/uVf
+  NT0/nAbOp1GvwfaLOnh+xzR7Id7rvz+xboE0ljoIMyZbfJYkxirzZTJCK2rBVpce
+  1uAzoaLabRd1CjdoQFg/vk75XdCE302zXMfrj3KhUT8bqale3xOpv4eFglqVEzyn
+  sIn2HTl3AVxT0M679Z2jPzLh5Wao/+64zVyCGMvWJQKCAQB66XaLLudYbYPs6bR+
+  Ry+UdvaVDtZuZUprPzMOrHcYIS437s7xkvb4KvswZmqW/BgxPUlRUybxPOEPLuVX
+  R6cH/o0qaRq2fFWnMkORfTsvO/Bie1/jfBvY+11Qf/aqr9L0nOkBjXA2ayqjhOJ7
+  cMTHUviR33OUB9O375XeI2temPoWi8mBg3BjC4AEqnT1a8ldJPnVpMmvX3Tx7CKM
+  QOGO92r+2lrBd1ReTZzcMKiAnd7kYMIi882AHxVTWaKt5GwLbaK6K1a2Ila5ouTU
+  l2VKOddRcPg3zXHe3jXPVXIY/3J5naHxbaG6sHr6svgV6TpX9T9KRUGL3uylMJ29
+  fgrDAoIBAQCRjzw8iCDzmGr1lmCtSTNG9QShgX38U5C3yDWt0l1AlsrSjLrCxzcX
+  /wgjvgNUCD1p860Up9o8zFcIRSyDIzWlyFoTVH3UcDnL2jHQeK5xcHNKXUI4RlqL
+  39a5uTNZdmweDxz98VlD4yDd5sklrp133NmFfGiffBFM+S72PxYDqGUqPfll40Zs
+  ng8Qb5xgB+s8HpcRTxbUBI+3uukcXnymMdO2WEF9E2zbpqGbQNePNxIPlsOzIemI
+  7NRGd0BANzFOF5KlF3Txg9V3AGqXPhKTBZGmB4vrMdG3zvAa8sGKOnjAWg7BLVQr
+  FiSxvB12zye4Pit2yuKYqJcy818mtEJhAoIBAQDSuvI3TqkASbNXoVD+Yck4ZNMK
+  QWqSUKQdwFG5EJNDz6AQd+VeH7oRiUtEgzp2Bu55sEg3/ULrStjWQPA2BWkc5+aw
+  8/L513COhcu4NUXugstyrLrUJS6DlN2e0GjtepcgMgA2/1cb5FnbRTOANREy/3FH
+  0LwW940XkUvyYUtU3i3I+S8T0+S4Bm+Nd0m0w5FJszoMc45OpXz9p6HkipF+tpyP
+  jPgQQ08MucwyRl7SiOmEFKnGYH9mGLufr9KmhQz1s3Jf1z4iEMqvGlpXJFI0Glc7
+  tSvzgcp4MnvYiUtpo6PIMFvDQQcJCLjBhwlwXxLmxvnAyA+UtNCxqtwUEQMh
   -----END RSA PRIVATE KEY-----`;
-  keyId.value = 'a0991262-d368-4cd5-8695-350d6b94f970';
+  var keyId = 'fd38276d-786a-49ec-9987-5e7b258e77cf';
 
-  button.click();
+  const result = generateJwtTest(env, clientToken, privateKey, keyId);
 
-  unsignedJwt = JSON.parse(unsignedJwtOutput.value);
-  jwtFill = jwtOutput.value;
+  var sPayload = JSON.parse(result.sPayload);
+  var sJWT = result.sJWT;
 
-  expect(unsignedJwt.aud).toMatch(env.value);
-  expect(unsignedJwt.iss).toEqual(clientToken.value);
-  expect(unsignedJwt.sub).toEqual(clientToken.value);
-  expect(jwtFill).not.toBe('');
+  expect(sPayload.iss).toEqual(clientToken);
+  expect(sPayload.sub).toEqual(clientToken);
+  expect(sPayload.aud).toContain(env);
+  expect(sJWT).not.toBe('');
+});
+
+test('', () => {
+
 });
