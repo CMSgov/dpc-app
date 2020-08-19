0575c97a385ea702312c8e6f47cfb1e7c393f0f5
Sun-Mountain <nzonnenberg@fearless.tech>
2020-08-19 16:36:19 -0400

    Progress?

--- dpc-web/jwt/jwt.js
+++ dpc-web/jwt/jwt.js
@@ -1,37 +1,3 @@
-function generateJWT() {
-
-  var dt = new Date().getTime();
-  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      var r = (dt + Math.random() * 16) % 16 | 0;
-      dt = Math.floor(dt / 16);
-      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-  });
-
-  var data = {
-    "iss": document.getElementById("clientToken").value.trim(),
-    "sub": document.getElementById("clientToken").value.trim(),
-    "aud": document.getElementById("environment").value.trim() + "/v1/Token/auth",
-    "exp": Math.round(new Date().getTime() / 1000) + 300,
-    "iat": Math.round(Date.now()),
-    "jti": uuid,
-  };
-
-  var secret = document.getElementById("privateKey").value.trim();
-
-  const header = {
-      'alg': 'RS384',
-      'kid': document.getElementById("keyId").value.trim(),
-  }
-
-  var sPayload = JSON.stringify(data);
-  document.getElementById("unsignedJWT").innerHTML = JSON.stringify(data, null, 2);
-
-  var sJWT = KJUR.jws.JWS.sign("RS384", header, sPayload, secret);
-
-  document.getElementById("JWT").innerHTML = sJWT;
-
-}
-
 function generateJwtTest(env, clientToken, privateKey, keyId) {
   var dt = new Date().getTime();
   var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
