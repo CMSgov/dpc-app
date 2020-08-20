3b6d02267c5652e7fc188b50231e121f9e7019b2
Sun-Mountain <nzonnenberg@fearless.tech>
2020-08-20 14:24:08 -0400

    JWT test finished

--- dpc-web/jwt/jwt.js
+++ dpc-web/jwt/jwt.js
@@ -7,15 +7,15 @@ function generateJwtTest(env, clientToken, privateKey, keyId) {
   });
 
   var data = {
-    "iss": clientToken,
-    "sub": clientToken,
-    "aud": env + "/v1/Token/auth",
+    "iss": clientToken.trim(),
+    "sub": clientToken.trim(),
+    "aud": env.trim() + "/v1/Token/auth",
     "exp": Math.round(new Date().getTime() / 1000) + 300,
     "iat": Math.round(Date.now()),
     "jti": uuid
   };
 
-  var secret = privateKey;
+  var secret = privateKey.trim();
 
   const header = {
     "alg": 'RS384',
@@ -26,6 +26,6 @@ function generateJwtTest(env, clientToken, privateKey, keyId) {
   var sJWT = KJUR.jws.JWS.sign("RS384", header, sPayload, secret);
 
   return { sPayload: sPayload, sJWT: sJWT }
-}
+};
 
 module.exports = generateJwtTest;
