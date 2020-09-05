58a58c6e159e6778b8d37d5026fe66e00eff9cbd
Silvio Luthi <silvio@adhocteam.us>
2020-09-04 18:25:34 -0700

    Move redis url prefix to default localhost and expect it to be present in REDIS_URL env var

--- dpc-web/config/environments/production.rb
+++ dpc-web/config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   # Use a different cache store in production.
   config.action_controller.perform_caching = true
 
-  config.cache_store = :redis_cache_store, { url: "rediss://#{ENV.fetch('REDIS_URL', 'localhost')}:6379/1" }
+  config.cache_store = :redis_cache_store, { url: "#{ENV.fetch('REDIS_URL', 'redis://localhost')}:6379/1" }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
