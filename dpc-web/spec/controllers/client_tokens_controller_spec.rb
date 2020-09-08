75c12764350d8d237a5c0bef5c99ee0c5de20877
SMLuthi <silvio@adhocteam.us>
2020-09-08 11:29:58 -0700

    Silvio/add simplecov to spec runs (#968)
    * Add simplecov configuration
    
    * remove internal users passwords controller as this was auto generated and isnt used
    
    * Add some missing tests to pass coverage requirements

--- dpc-web/spec/controllers/client_tokens_controller_spec.rb
+++ dpc-web/spec/controllers/client_tokens_controller_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe ClientTokensController, type: :controller do
         )
         allow(stub).to receive(:get_public_keys).and_return(stub)
         allow(stub).to receive(:response_body).and_return(default_org_creation_response, { 'entities' => [] })
-    
+
         org = create(:organization, :api_enabled)
         user.organizations << org
 
@@ -34,6 +34,57 @@ RSpec.describe ClientTokensController, type: :controller do
           other_org = create(:organization)
           get :new, params: { organization_id: other_org.id }
           expect(response.location).to include(portal_path)
+        end
+      end
+    end
+  end
+
+  describe 'GET #destroy' do
+    let!(:user) { create(:user, :assigned) }
+
+    context 'authenticated user' do
+      before(:each) do
+        sign_in user, scope: :user
+      end
+
+      context 'with a successful call to the api' do
+        it 'returns http success' do
+          stub = stub_api_client(
+            message: :create_organization,
+            success: true,
+            response: default_org_creation_response
+          )
+          allow(stub).to receive(:get_public_keys).and_return(stub)
+          allow(stub).to receive(:response_body).and_return(default_org_creation_response, { 'entities' => [] })
+
+          org = create(:organization, :api_enabled)
+          user.organizations << org
+
+          allow(stub).to receive(:delete_client_token).and_return(true)
+
+          get :destroy, params: { id: 1, organization_id: org.id }
+          expect(response.location).to include(request.host + root_path)
+          expect(response).to have_http_status(:found)
+        end
+      end
+
+      context 'with a failed call to the api' do
+        it 'renders new' do
+          stub = stub_api_client(
+            message: :create_organization,
+            success: true,
+            response: default_org_creation_response
+          )
+          allow(stub).to receive(:get_public_keys).and_return(stub)
+          allow(stub).to receive(:response_body).and_return(default_org_creation_response, { 'entities' => [] })
+
+          org = create(:organization, :api_enabled)
+          user.organizations << org
+
+          allow(stub).to receive(:delete_client_token).and_return(false)
+
+          get :destroy, params: { id: 1, organization_id: org.id }
+          expect(response).to render_template(:new)
         end
       end
     end
@@ -57,7 +108,7 @@ RSpec.describe ClientTokensController, type: :controller do
           )
           allow(stub).to receive(:get_public_keys).and_return(stub)
           allow(stub).to receive(:response_body).and_return(default_org_creation_response, { 'entities' => [] })
-      
+
           org = create(:organization, :api_enabled)
           user.organizations << org
 
