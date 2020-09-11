14212e7420fef0f49b472fc52c3f2e437ea9175d
Sun-Mountain <nzonnenberg@fearless.tech>
2020-09-11 10:03:57 -0400

    Update tests & change password reset layout

--- dpc-web/spec/features/authentication/user_resets_password_spec.rb
+++ dpc-web/spec/features/authentication/user_resets_password_spec.rb
@@ -59,6 +59,14 @@ RSpec.feature 'user resets password' do
       expect(page.body).to include('1 error prohibited this user from being saved:')
       expect(page.body).to include('Password is too short (minimum is 15 characters)')
 
+      fill_in 'user_password', with: 'CrabwardTentacles'
+      fill_in 'user_password_confirmation', with: 'CrabwardTentacles'
+      find('input[data-test="submit"]').click
+
+      expect(page.body).to include('1 error prohibited this user from being saved:')
+      expect(page.body).to include('Password must include at least one number, one lowercase letter')
+      expect(page.body).to include('one uppercase letter, and one special character (!@#$&amp;*)')
+
       fill_in 'user_password', with: 'CrabW0rd$_B00m#'
       fill_in 'user_password_confirmation', with: 'CrabW0rd$_B00m#'
       find('input[data-test="submit"]').click
