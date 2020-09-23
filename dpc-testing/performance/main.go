8d2adefc7e1cc17e686790a806bd011f4da252e0
Emily Hart <1923441+embh@users.noreply.github.com>
2020-09-23 14:24:59 -0400

    DPC-731: Basic performance test setup for /Key endpoints (#1057)
    * Key tests in progress
    
    * Response bodies from test requests; custom targeters
    
    * Rename key/sig function
    
    * Remove panic on non-200 response (errors are logged in metrics JSON)

--- dpc-testing/performance/main.go
+++ dpc-testing/performance/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/rsa"
+	"encoding/json"
 	"fmt"
 	"os"
 	"time"
@@ -13,6 +14,7 @@ import (
 var (
 	apiURL, adminURL string
 	goldenMacaroon   []byte
+	keyIDs           []string
 )
 
 func init() {
@@ -22,14 +24,15 @@ func init() {
 }
 
 func main() {
-	orgID := createOrg()
-
-	// pubKeyStr, privateKey, signature := getKeyPairAndSignature()
-	// keyID := uploadKey(pubKeyStr, signature, orgID)
-	// clientToken := getClientToken(orgID)
-	// accessToken := refreshAccessToken(privateKey, keyID, clientToken)
-
 	testMetadata()
+
+	orgID := createOrg()
+	pubKeyStr, privateKey, signature := generateKeyPairAndSignature()
+	keyID := uploadKey(pubKeyStr, signature, orgID)
+	clientToken := getClientToken(orgID)
+
+	accessToken := refreshAccessToken(privateKey, keyID, clientToken)
+	testKeyEndpoints(accessToken)
 
 	cleanUp(orgID)
 }
@@ -46,6 +49,71 @@ func testMetadata() {
 	runTest(getMetadataTarget, 5, 5)
 }
 
+func testKeyEndpoints(accessToken string) {
+
+	resps := runTestWithTargeter(fmt.Sprintf("POST %s/Key", apiURL), newPOSTKeyTargeter(accessToken), 5, 5)
+	for _, resp := range resps {
+		var result Resource
+		json.Unmarshal(resp, &result)
+		keyIDs = append(keyIDs, result.ID)
+	}
+
+	getKeysTarget := vegeta.Target{
+		Method: "GET",
+		URL:    fmt.Sprintf("%s%s", apiURL, "/Key"),
+		Header: map[string][]string{
+			"Accept":        {"application/json"},
+			"Authorization": {fmt.Sprintf("Bearer %s", accessToken)},
+		},
+	}
+	runTest(getKeysTarget, 5, 5)
+
+	getKeyTarget := vegeta.Target{
+		Method: "GET",
+		URL:    fmt.Sprintf("%s%s%s", apiURL, "/Key/", keyIDs[0]),
+		Header: map[string][]string{
+			"Accept":        {"application/json"},
+			"Authorization": {fmt.Sprintf("Bearer %s", accessToken)},
+		},
+	}
+	runTest(getKeyTarget, 5, 5)
+
+	deleteKeyTargeter := newDELETEKeyTargeter(accessToken, func() string {
+		keyID := keyIDs[0]
+		keyIDs = keyIDs[1:]
+		return keyID
+	})
+	runTestWithTargeter(fmt.Sprintf("DELETE %s/Key/{id}", apiURL), deleteKeyTargeter, 5, 5)
+}
+
+func newPOSTKeyTargeter(accessToken string) vegeta.Targeter {
+	return func(t *vegeta.Target) error {
+		t.Method = "POST"
+		t.URL = fmt.Sprintf("%s/Key", apiURL)
+		t.Header = map[string][]string{
+			"Content-Type":  {"application/json"},
+			"Authorization": {fmt.Sprintf("Bearer %s", accessToken)},
+		}
+
+		pubKeyStr, _, signature := generateKeyPairAndSignature()
+		body := fmt.Sprintf("{ \"key\": \"%s\", \"signature\": \"%s\"}", pubKeyStr, signature)
+		t.Body = []byte(body)
+
+		return nil
+	}
+}
+
+func newDELETEKeyTargeter(accessToken string, nextKeyID func() string) vegeta.Targeter {
+	return func(t *vegeta.Target) error {
+		t.Method = "DELETE"
+		t.URL = fmt.Sprintf("%s/Key/%s", apiURL, nextKeyID())
+		t.Header = map[string][]string{
+			"Authorization": {fmt.Sprintf("Bearer %s", accessToken)},
+		}
+		return nil
+	}
+}
+
 func refreshAccessToken(privateKey *rsa.PrivateKey, keyID string, clientToken []byte) string {
 	authToken, err := dpcclient.GenerateAuthToken(privateKey, keyID, clientToken, apiURL)
 	if err != nil {
@@ -60,21 +128,30 @@ func refreshAccessToken(privateKey *rsa.PrivateKey, keyID string, clientToken []
 	return accessToken
 }
 
-func runTest(target vegeta.Target, duration, frequency int) {
-	fmt.Printf("Running performance test on %s %s...\n", target.Method, target.URL)
+func runTest(target vegeta.Target, duration, frequency int) [][]byte {
+	return runTestWithTargeter(fmt.Sprintf("%s %s", target.Method, target.URL), vegeta.NewStaticTargeter(target), duration, frequency)
+}
+
+func runTestWithTargeter(name string, targeter vegeta.Targeter, duration, frequency int) [][]byte {
+
+	fmt.Printf("\nRunning performance test on %s...\n", name)
 
 	d := time.Second * time.Duration(duration)
 	r := vegeta.Rate{Freq: frequency, Per: time.Second}
 
 	attacker := vegeta.NewAttacker()
 	var metrics vegeta.Metrics
-	for results := range attacker.Attack(vegeta.NewStaticTargeter(target), r, d, fmt.Sprintf("%dps:", r.Freq)) {
+	var respBodies [][]byte
+	for results := range attacker.Attack(targeter, r, d, fmt.Sprintf("%dps:", r.Freq)) {
 		metrics.Add(results)
+		respBodies = append(respBodies, results.Body)
 	}
 	metrics.Close()
 
 	reporter := vegeta.NewJSONReporter(&metrics)
 	reporter.Report(os.Stdout)
+
+	return respBodies
 }
 
 func cleanAndPanic(err error) {
