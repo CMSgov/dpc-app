5de4f5b02b065b2b1eec872f5f0bb5b89fc2155d
Silvio Luthi <silvio@adhocteam.us>
2020-10-20 13:21:51 -0700

    Remove id generation from go routine, pair org create down to one and include PUT and GET request tests

--- dpc-testing/performance/organization.go
+++ dpc-testing/performance/organization.go
@@ -14,23 +14,18 @@ type orgTargetConfig struct {
 	BaseURL     string
 	Path        string
 	AccessToken string
-	FilePattern string   // Optional file pattern location(s)
-	IDs         []string // Optional list of ids to append url
+	FilePattern string // Optional file pattern location(s)
+	ID          string // Optional id to append to url
 }
 
 type orgTargeter struct {
-	reqs chan Req
+	bodies chan []byte
 	orgTargetConfig
-}
-
-type Req struct {
-	id   *string
-	body []byte
 }
 
 func NewOrgTargeter(config orgTargetConfig) *orgTargeter {
 	targeter := &orgTargeter{
-		reqs:            make(chan Req),
+		bodies:          make(chan []byte),
 		orgTargetConfig: config,
 	}
 
@@ -40,7 +35,7 @@ func NewOrgTargeter(config orgTargetConfig) *orgTargeter {
 }
 
 func (o *orgTargeter) genReqs() {
-	reqs := make([]Req, 0)
+	bodies := make([][]byte, 0)
 
 	if o.FilePattern != "" {
 		fileNames, err := filepath.Glob(o.FilePattern)
@@ -53,24 +48,14 @@ func (o *orgTargeter) genReqs() {
 			if err != nil {
 				panic(err)
 			}
-			reqs = append(reqs, Req{body: body})
-		}
-	}
-
-	update := len(reqs) != 0
-
-	for i := range o.IDs {
-		if update {
-			reqs[i].id = &o.IDs[i]
-		} else {
-			reqs = append(reqs, Req{id: &o.IDs[i]})
+			bodies = append(bodies, body)
 		}
 	}
 
 	go func() {
 		for {
-			for _, req := range reqs {
-				o.reqs <- req
+			for _, body := range bodies {
+				o.bodies <- body
 			}
 		}
 	}()
@@ -79,9 +64,12 @@ func (o *orgTargeter) genReqs() {
 func (o *orgTargeter) buildTarget(t *vegeta.Target) error {
 	t.Method = o.Method
 
-	req := o.nextRequest()
+	var body []byte
+	if o.FilePattern != "" {
+		body = o.nextRequest()
+	}
 
-	t.URL = o.FullURL(req.id)
+	t.URL = o.FullURL(o.ID)
 
 	t.Header = map[string][]string{"Authorization": {fmt.Sprintf("Bearer %s", o.AccessToken)}}
 	if o.Method != "DELETE" {
@@ -90,8 +78,8 @@ func (o *orgTargeter) buildTarget(t *vegeta.Target) error {
 		// if neither GET or DELETE, must be POST or PUT
 		if o.Method != "GET" {
 			t.Header.Add("Content-Type", "application/fhir+json")
-			if req.body != nil {
-				t.Body = req.body
+			if body != nil {
+				t.Body = body
 			}
 		}
 	}
@@ -100,51 +88,71 @@ func (o *orgTargeter) buildTarget(t *vegeta.Target) error {
 }
 
 func (o *orgTargeter) Name() string {
-	var id *string
+	var id string
 	if o.Method != "POST" {
-		s := "{id}"
-		id = &s
+		id = "{id}"
 	}
 	return fmt.Sprintf("%s %s", o.Method, o.FullURL(id))
 }
 
-func (o *orgTargeter) FullURL(id *string) string {
+func (o *orgTargeter) FullURL(id string) string {
 	url := fmt.Sprintf("%s/%s", o.BaseURL, o.Path)
-	if id != nil {
-		url = url + "/" + *id
+	if id != "" {
+		url = url + "/" + id
 	}
 	return url
 }
 
-func (o *orgTargeter) nextRequest() Req {
-	return <-o.reqs
+func (o *orgTargeter) nextRequest() []byte {
+	return <-o.bodies
 }
 
-func testOrganizationEndpoints(accessToken string) {
+func testOrganizationEndpoints() {
 	const endpoint = "Organization"
 
 	o := NewOrgTargeter(orgTargetConfig{
 		Method:      "POST",
 		BaseURL:     apiURL,
 		Path:        endpoint + "/$submit",
-		AccessToken: string(goldenMacaroon), // accessToken doesn't work
-		FilePattern: "../../src/main/resources/organizations/organization-*.json",
+		AccessToken: string(goldenMacaroon), // Users cannot create orgs so the golden macaroon is required
+		FilePattern: "../../src/main/resources/organizations/base-organization.json",
 	})
-	resps := runTestWithTargeter(o.Name(), o.buildTarget, 1, 1)
+	resp := runTestWithTargeter(o.Name(), o.buildTarget, 1, 1)
 
-	orgIDs := make([]string, 0)
-	for _, resp := range resps {
-		var result Resource
-		json.Unmarshal(resp, &result)
-		orgIDs = append(orgIDs, result.ID)
-	}
+	var resource Resource
+	json.Unmarshal(resp[0], &resource)
+
+	// Every org requires its own access token
+	pubKeyStr, privateKey, signature := generateKeyPairAndSignature()
+	keyID := uploadKey(pubKeyStr, signature, resource.ID)
+	clientToken := getClientToken(resource.ID)
+	accessToken := refreshAccessToken(privateKey, keyID, clientToken)
+
+	o = NewOrgTargeter(orgTargetConfig{
+		Method:      "GET",
+		BaseURL:     apiURL,
+		Path:        endpoint,
+		AccessToken: accessToken, // Users cannot create orgs so the golden macaroon is required
+		ID:          resource.ID,
+	})
+	runTestWithTargeter(o.Name(), o.buildTarget, 5, 2)
+
+	o = NewOrgTargeter(orgTargetConfig{
+		Method:      "PUT",
+		BaseURL:     apiURL,
+		Path:        endpoint,
+		AccessToken: accessToken, // Users cannot create orgs so the golden macaroon is required
+		FilePattern: "../../src/main/resources/organizations/organization-*.json",
+		ID:          resource.ID,
+	})
+	runTestWithTargeter(o.Name(), o.buildTarget, 5, 2)
 
 	o = NewOrgTargeter(orgTargetConfig{
 		Method:      "DELETE",
 		BaseURL:     apiURL,
 		Path:        endpoint,
-		AccessToken: string(goldenMacaroon), // accessToken doesn't work
-		IDs:         orgIDs,
+		AccessToken: string(goldenMacaroon),
+		ID:          resource.ID,
 	})
 	runTestWithTargeter(o.Name(), o.buildTarget, 1, 1)
 }
