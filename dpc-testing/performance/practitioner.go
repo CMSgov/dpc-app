003c5efac54e3fc1ac00f807ef05ab14833945d7
Sun-Mountain <nzonnenberg@fearless.tech>
2020-10-22 15:47:13 -0400

    Practioner 3

--- dpc-testing/performance/practitioner.go
+++ dpc-testing/performance/practitioner.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"container/list"
+	"encoding/json"
 	"fmt"
 
 	vegeta "github.com/tsenart/vegeta/lib"
@@ -8,13 +10,44 @@ import (
 
 func testPractitionerEndpoints(accessToken string) {
 	// POST /Practitioner/$validate
-	postPractitionerValidateTargeter := newPOSTPractitionerTargeter("/$validate", accessToken, nextPartParams, 1)
+	postPractitionerValidateTargeter := newPOSTPractitionerTargeter("/$validate", accessToken, nextPractParams, 1)
 	// Higher numbers of requests cause timeouts
-	runTestWithTargeter(fmt.Sprintf("POST %s/Practitioner/$validate", apiURL), postPractitionerValidateTargeter, 5, 2)
+	runTestWithTargeter(fmt.Sprintf("POST %s/Practitioner/$validate", apiURL), postPractitionerValidateTargeter, 2, 2)
+
+	// POST /Practitioner
+	postPractitionerTargeter := newPOSTPractitionerTargeter("", accessToken, nextPractitioner, 1)
+	resps := runTestWithTargeter(fmt.Sprintf("POST %s/Practitioner", apiURL), postPractitionerTargeter, 2, 2)
+
+	var practitionerIDs = list.New()
+	for _, resp := range resps {
+		var result Resource
+		json.Unmarshal(resp, &result)
+		practitionerIDs.PushBack(result.ID)
+	}
+
+	// POST /Patient/$submit
+	postPractitionerSubmitTargeter := newPOSTPractitionerTargeter("/$submit", accessToken, nextPractParams, 1)
+	// Higher numbers of requests cause timeouts
+	resps = runTestWithTargeter(fmt.Sprintf("POST %s/Practitioner/$submit", apiURL), postPractitionerSubmitTargeter, 2, 2)
+
+	// PUT /Patient/{id}
+	currentPractitionerID := practitionerIDs.Front()
+	putPractitionerTargeter := newPUTPractitionerTargeter(func() string {
+		s, ok := currentPractitionerID.Value.(string)
+		if !ok {
+			cleanAndPanic(fmt.Errorf("not a valid string: %v", currentPractitionerID.Value))
+		}
+		currentPractitionerID = currentPractitionerID.Next()
+		return s
+	})
 }
 
-func nextPartParams(fileNum *int) []byte {
+func nextPractParams(fileNum *int) []byte {
 	return nextFile("../../src/main/resources/parameters/bundles/practitioners/practitioner-%v.json", fileNum)
+}
+
+func nextPractitioner(fileNum *int) []byte {
+	return nextFile("../../src/main/resources/practitioners/practitioner-%v.json", fileNum)
 }
 
 func newPOSTPractitionerTargeter(operation, accessToken string, nextBody func(*int) []byte, fileNum int) vegeta.Targeter {
@@ -22,7 +55,7 @@ func newPOSTPractitionerTargeter(operation, accessToken string, nextBody func(*i
 		t.Method = "POST"
 		t.URL = fmt.Sprintf("%s/Practitioner%s", apiURL, operation)
 		t.Header = map[string][]string{
-			"Content-Type":  {"application/fhirs+json"},
+			"Content-Type":  {"application/fhir+json"},
 			"Authorization": {fmt.Sprintf("Bearer %s", accessToken)},
 			"Accept":        {"application/fhir+json"},
 		}
