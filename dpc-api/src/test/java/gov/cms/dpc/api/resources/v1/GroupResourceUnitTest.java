d281f81a2ed52003c34ac43cf46f9709e5c1fe3d
Jon Fulk <jonfulk@gmail.com>
2020-10-22 10:15:42 -0500

    Revert "DPC-554 fix: Require `Accept` header (#1091)"
    This reverts commit 29fc06907510d70c7ae27da9fcf16d48cd80e696.

--- dpc-api/src/test/java/gov/cms/dpc/api/resources/v1/GroupResourceUnitTest.java
+++ dpc-api/src/test/java/gov/cms/dpc/api/resources/v1/GroupResourceUnitTest.java
@@ -3,15 +3,11 @@ package gov.cms.dpc.api.resources.v1;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.gclient.ICreateTyped;
-import ca.uhn.fhir.rest.gclient.IOperationUntypedWithInput;
 import ca.uhn.fhir.rest.gclient.IReadExecutable;
 import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
 import gov.cms.dpc.api.auth.OrganizationPrincipal;
-import gov.cms.dpc.bluebutton.client.BlueButtonClient;
 import gov.cms.dpc.common.utils.NPIUtil;
 import gov.cms.dpc.fhir.DPCIdentifierSystem;
-import gov.cms.dpc.fhir.FHIRMediaTypes;
-import gov.cms.dpc.queue.IJobQueue;
 import org.hl7.fhir.dstu3.model.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,13 +17,11 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import javax.ws.rs.BadRequestException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.UUID;
 
-import static gov.cms.dpc.fhir.FHIRMediaTypes.FHIR_JSON;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GroupResourceUnitTest {
@@ -35,18 +29,12 @@ public class GroupResourceUnitTest {
     @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     IGenericClient attributionClient;
 
-    @Mock
-    IJobQueue queue;
-
-    @Mock
-    BlueButtonClient blueButtonClient;
-
     GroupResource resource;
 
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        resource = new GroupResource(queue, attributionClient, null, blueButtonClient);
+        resource = new GroupResource(null, attributionClient, null, null);
     }
 
     @Test
@@ -167,81 +155,6 @@ public class GroupResourceUnitTest {
         Mockito.when(readExec.execute()).thenThrow(new ResourceNotFoundException("practitioner not found"));
 
         Assertions.assertThrows(WebApplicationException.class, () -> resource.createRoster(organizationPrincipal, provenance, group));
-    }
-
-    @Test
-    public void testExportRequestHeaders() {
-        UUID practitionerId = UUID.randomUUID();
-        UUID orgId = UUID.randomUUID();
-        Organization organization = new Organization();
-        organization.setId(orgId.toString());
-        OrganizationPrincipal organizationPrincipal = new OrganizationPrincipal(organization);
-
-        IReadExecutable<Group> readExec = Mockito.mock(IReadExecutable.class);
-        Group fakeGroup = new Group();
-        fakeGroup.getMember().add(new Group.GroupMemberComponent());
-        Mockito.when(attributionClient
-                .read()
-                .resource(Group.class)
-                .withId(Mockito.any(IdType.class))
-                .encodedJson())
-                .thenReturn(readExec);
-
-        Mockito.when(readExec.execute())
-                .thenReturn(fakeGroup);
-
-        IOperationUntypedWithInput<Bundle> operationInput = Mockito.mock(IOperationUntypedWithInput.class);
-        Patient fakePatient = new Patient();
-        fakePatient.getIdentifier().add(new Identifier().setSystem(DPCIdentifierSystem.MBI.getSystem()).setValue("2S51C00AA00"));
-        Bundle fakeBundle = new Bundle();
-        fakeBundle.getEntry().add(new Bundle.BundleEntryComponent().setResource(fakePatient));
-        Mockito.when(attributionClient
-                .operation()
-                .onInstance(Mockito.any(IdType.class))
-                .named("patients")
-                .withParameters(Mockito.any(Parameters.class))
-                .returnResourceType(Bundle.class)
-                .useHttpGet()
-                .encodedJson())
-                .thenReturn(operationInput);
-        Mockito.when(operationInput.execute())
-                .thenReturn(fakeBundle);
-
-        Mockito.when(blueButtonClient.requestPatientFromServer(Mockito.anyString(), Mockito.any()))
-                .thenReturn(new Bundle());
-
-        Assertions.assertDoesNotThrow(() -> {
-            resource.export(organizationPrincipal, "roster-id", "Coverage", FHIRMediaTypes.FHIR_NDJSON, "2017-01-01T00:00:00Z", "respond-async", FHIR_JSON);
-        });
-
-        Assertions.assertThrows(BadRequestException.class, () -> {
-            resource.export(organizationPrincipal, "roster-id", "Coverage", FHIRMediaTypes.FHIR_NDJSON, "2017-01-01T00:00:00Z", null, FHIR_JSON);
-        });
-
-        Assertions.assertThrows(BadRequestException.class, () -> {
-            resource.export(organizationPrincipal, "roster-id", "Coverage", FHIRMediaTypes.FHIR_NDJSON, "2017-01-01T00:00:00Z", "asdfasdf", FHIR_JSON);
-        });
-
-        Assertions.assertThrows(BadRequestException.class, () -> {
-            resource.export(organizationPrincipal, "roster-id", "Coverage", FHIRMediaTypes.FHIR_NDJSON, "2017-01-01T00:00:00Z", "", FHIR_JSON);
-        });
-
-        Assertions.assertThrows(BadRequestException.class, () -> {
-            resource.export(organizationPrincipal, "roster-id", "Coverage", FHIRMediaTypes.FHIR_NDJSON, "2017-01-01T00:00:00Z", " ", FHIR_JSON);
-        });
-
-        Assertions.assertThrows(BadRequestException.class, () -> {
-            resource.export(organizationPrincipal, "roster-id", "Coverage", FHIRMediaTypes.FHIR_NDJSON, "2017-01-01T00:00:00Z", "respond-async", "asdfasdf");
-        });
-
-        Assertions.assertThrows(BadRequestException.class, () -> {
-            resource.export(organizationPrincipal, "roster-id", "Coverage", FHIRMediaTypes.FHIR_NDJSON, "2017-01-01T00:00:00Z", "respond-async", "");
-        });
-
-        Assertions.assertThrows(BadRequestException.class, () -> {
-            resource.export(organizationPrincipal, "roster-id", "Coverage", FHIRMediaTypes.FHIR_NDJSON, "2017-01-01T00:00:00Z", "respond-async", " ");
-        });
-
     }
 
 }
