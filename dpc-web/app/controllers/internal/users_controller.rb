cfcc88c5e357b088c61d07641cccf7139a8f9276
Sun-Mountain <nzonnenberg@fearless.tech>
2020-11-30 12:35:27 -0500

    CSV filter enabled

--- dpc-web/app/controllers/internal/users_controller.rb
+++ dpc-web/app/controllers/internal/users_controller.rb
@@ -48,13 +48,39 @@ module Internal
     end
 
     def download
+      users = params[:users]
+      users_obj = JSON.parse(users, object_class: User)
       respond_to do |format|
         filename = "users-#{Time.now.strftime('%Y%m%dT%H%M')}.csv"
-        format.csv { send_data User.all.to_csv, filename: filename }
+        format.csv { send_data cvs_convert(users_obj), filename: filename }
       end
     end
 
     private
+
+
+    ATTRS = %w[id first_name last_name email requested_organization requested_organization_type
+      address_1 address_2 city state zip agree_to_terms requested_num_providers created_at updated_at].freeze
+
+    # html escape these fields for XSS protection
+    ESCAPED_ATTRS = %w[first_name last_name requested_organization address_1 address_2 city].freeze
+
+    def cvs_convert(users)
+      CSV.generate(headers:true) do |csv|
+        csv << ATTRS
+        users.each do |user|
+          attributes = user.attributes
+          escaped_attributes = attributes.map do |k, v|
+            if ESCAPED_ATTRS.include? k
+              v = ERB::Util.html_escape(v)
+            end
+  
+            [k, v]
+          end.to_h
+          csv << escaped_attributes.values_at(*ATTRS)
+        end
+      end
+    end
 
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :organization_ids)
