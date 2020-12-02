b7b837584f5e48a2e81548abff7e24908a72d441
Sun-Mountain <nzonnenberg@fearless.tech>
2020-12-02 14:14:17 -0500

    Add tags to csv file

--- dpc-web/app/models/user.rb
+++ dpc-web/app/models/user.rb
@@ -83,10 +83,10 @@ class User < ApplicationRecord
   end
 
   ATTRS = %w[id first_name last_name email requested_organization requested_organization_type
-             address_1 address_2 city state zip agree_to_terms requested_num_providers created_at updated_at].freeze
+             address_1 address_2 city state zip agree_to_terms requested_num_providers created_at updated_at tags].freeze
 
   # html escape these fields for XSS protection
-  ESCAPED_ATTRS = %w[first_name last_name requested_organization address_1 address_2 city].freeze
+  ESCAPED_ATTRS = %w[first_name last_name requested_organization address_1 address_2 city tags].freeze
 
   def self.to_csv(user_ids)
     users = User.find(user_ids)
@@ -94,9 +94,16 @@ class User < ApplicationRecord
       csv << ATTRS
       users.each do |user|
         attributes = user.attributes
+        attributes["tags"] = user.tags.map(&:name)
         escaped_attributes = attributes.map do |k, v|
           if ESCAPED_ATTRS.include? k
             v = ERB::Util.html_escape(v)
+
+            if k == "tags"
+              v.gsub!("&quot;", '')
+              v.gsub!("[", '')
+              v.gsub!("]", '')
+            end
           end
 
           [k, v]
