cbcbd86616cac42f499c23bb271f13d295b9111a
Sun-Mountain <nzonnenberg@fearless.tech>
2020-12-14 11:28:17 -0500

    config routes

--- dpc-web/app/models/organization.rb
+++ dpc-web/app/models/organization.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Organization < ApplicationRecord
-  include LuhnacyLib
   include OrganizationsHelper
   include OrganizationTypable
   include Taggable
--- dpc-web/config/application.rb
+++ dpc-web/config/application.rb
@@ -25,6 +25,8 @@ Bundler.require(*Rails.groups)
 
 module DpcWebsite
   class Application < Rails::Application
+    config.autoload_paths += %W(#{config.root}/lib)
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
