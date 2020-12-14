07fe12b6627ea9a1c7a35d0221aa7f1440f09d79
Sun-Mountain <nzonnenberg@fearless.tech>
2020-12-14 12:09:29 -0500

    Change load paths

--- dpc-web/config/application.rb
+++ dpc-web/config/application.rb
@@ -25,7 +25,7 @@ Bundler.require(*Rails.groups)
 
 module DpcWebsite
   class Application < Rails::Application
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.eager_load_paths << "#{Rails.root}/lib"
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
