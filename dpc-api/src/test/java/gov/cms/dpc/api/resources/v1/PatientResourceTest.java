ed0544735c40e052c3bf553f2c30644198e4031d
Sal Gutierrez <43150956+SalGutierrez@users.noreply.github.com>
2020-12-16 11:51:36 -0800

    fixed typo in patient resource test

--- dpc-api/src/test/java/gov/cms/dpc/api/resources/v1/PatientResourceTest.java
+++ dpc-api/src/test/java/gov/cms/dpc/api/resources/v1/PatientResourceTest.java
@@ -368,7 +368,7 @@ class PatientResourceTest extends AbstractSecureApplicationTest {
     }
 
     @Test
-    public void tesDeletePatient() throws GeneralSecurityException, IOException, URISyntaxException {
+    public void testDeletePatient() throws GeneralSecurityException, IOException, URISyntaxException {
         final TestOrganizationContext orgAContext = registerAndSetupNewOrg();
         final TestOrganizationContext orgBContext = registerAndSetupNewOrg();
         final IGenericClient orgAClient = APIAuthHelpers.buildAuthenticatedClient(ctx, getBaseURL(), orgAContext.getClientToken(), UUID.fromString(orgAContext.getPublicKeyId()), orgAContext.getPrivateKey());
