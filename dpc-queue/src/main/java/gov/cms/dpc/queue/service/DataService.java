220c0d514ccbc6a3d62b13328a34665efa45ad6c
Pat Kelsh <pkelsh@gmail.com>
2021-01-29 15:56:45 -0500

    DPC-917 added since parameter to logging

--- dpc-queue/src/main/java/gov/cms/dpc/queue/service/DataService.java
+++ dpc-queue/src/main/java/gov/cms/dpc/queue/service/DataService.java
@@ -67,7 +67,7 @@ public class DataService {
                                  OffsetDateTime transactionTime,
                                  String requestingIP, ResourceType... resourceTypes) {
         UUID jobID = this.queue.createJob(organizationID, providerID.toString(), patientMBIs, List.of(resourceTypes), since, transactionTime, requestingIP, false);
-        LOGGER.info("Patient everything export job created with job_id={}",jobID.toString());
+        LOGGER.info("Patient everything export job created with job_id={} _since={}",jobID.toString(), since);
  
         Optional<List<JobQueueBatch>> optionalBatches = waitForJobToComplete(jobID, organizationID, this.queue);
 
