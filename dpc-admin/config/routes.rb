623698428aa7132eede65e54e42e6af8b8d9b0a1
Sun-Mountain <nzonnenberg@fearless.tech>
2021-02-08 14:22:06 -0500

    Progress

--- dpc-admin/config/routes.rb
+++ dpc-admin/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :edit, :update, :destroy] do
+    collection { get :download }
+  end
+
+  root to: 'users#index', via: :get
 end
