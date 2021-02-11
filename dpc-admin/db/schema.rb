f63bc56af06562ecbdc202426e1efa558382fb3a
Sun-Mountain <nzonnenberg@fearless.tech>
2021-02-11 15:13:31 -0500

    Remove double quotes

--- dpc-admin/db/schema.rb
+++ dpc-admin/db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_153523) do
+# frozen_string_literal: true
 
+ActiveRecord::Schema.define(version: 2020_09_09_153523) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -147,5 +148,4 @@ ActiveRecord::Schema.define(version: 2020_09_09_153523) do
     t.index ['requested_organization'], name: 'index_users_on_requested_organization'
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
-
 end
