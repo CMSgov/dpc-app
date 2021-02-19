1617cc202c64d867991341e22670a09de2620b14
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2021-02-19 15:09:48 -0500

    DPC-1071: Admin site separation: create new admin site (#1187)
    * Init
    
    * Add dotenv file
    
    * Add schema
    
    * Clean slate
    
    * Restart
    
    * Connection to database
    
    * Progress
    
    * Progress
    
    * Save progress
    
    * adding styling
    
    * javascripts compiling
    
    * scss now compiles
    
    * Added devise
    
    * Session view up
    
    * Sign in thru github complete
    
    * Sign out added
    
    * Upgrade actionpack
    
    * Bundle update and localhost changed
    
    * Upgrade actionpack and activerecord
    
    * Update bundle
    
    * Changed double quotes to single quotes in schema to appease code climate
    
    * Change web app default from 3000 to 3500
    
    * Trying to appease code climate
    
    * Added frozen string comment
    
    * Remove double quotes
    
    * remove double quotes
    
    * Styling update
    
    * Starting to add tests
    
    * Tests for sign in internal user and internal user model
    
    * Add dotrspec file
    
    * Add README documentation on existing admin app
    
    * Update README
    
    * Update ruby version and add rubocop
    
    * Add rubocop yml
    
    * Update gemlock file

---
+++ dpc-admin/app/models/organization.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+require './lib/luhnacy_lib/luhnacy_lib'
+
+class Organization < ApplicationRecord
+  include OrganizationsHelper
+  include OrganizationTypable
+  include Taggable
+
+  has_many :taggings, as: :taggable
+  has_many :tags, through: :taggings
+  has_one :address, as: :addressable, dependent: :destroy
+  has_many :organization_user_assignments, dependent: :destroy
+  has_many :users, through: :organization_user_assignments
+  has_one :registered_organization, dependent: :destroy
+
+  enum organization_type: ORGANIZATION_TYPES
+
+  validates :organization_type, inclusion: { in: ORGANIZATION_TYPES.keys }
+  validates :name, uniqueness: true, presence: true
+  validates :npi, uniqueness: { allow_blank: true }
+
+  delegate :street, :street_2, :city, :state, :zip, to: :address, allow_nil: true, prefix: true
+  accepts_nested_attributes_for :address, reject_if: :all_blank
+
+  before_save :assign_id, if: -> { prod_sbx? }
+  before_save :npi_valid?
+
+  after_update :update_registered_organization
+
+  scope :vendor, -> { where(organization_type: ORGANIZATION_TYPES['health_it_vendor']) }
+
+  scope :provider, -> { where.not(organization_type: ORGANIZATION_TYPES['health_it_vendor']) }
+
+  scope :is_registered, -> {
+    where('id IN(SELECT DISTINCT(organization_id) FROM registered_organizations WHERE enabled IS true)')
+  }
+
+  scope :is_not_registered, -> {
+    where('id IN(SELECT DISTINCT(organization_id) FROM registered_organizations WHERE enabled IS NOT true)')
+  }
+
+  def address_type
+    address&.address_type
+  end
+
+  def address_use
+    address&.address_use
+  end
+
+  def npi=(input)
+    super(input.blank? ? nil : input)
+  end
+
+  def assign_id
+    return true if npi.present?
+
+    self.npi = LuhnacyLib.generate_npi
+  end
+
+  def notify_users_of_sandbox_access
+    organization_user_assignments.each(&:send_organization_sandbox_email)
+  end
+
+  def npi_valid?
+    return if npi.blank?
+
+    validate_npi
+  end
+
+  def prod_sbx?
+    ENV['ENV'] == 'prod-sbx'
+  end
+
+  def update_registered_organization
+    return unless npi.present?
+
+    return registered_organization.update_api_organization if registered_organization.present?
+  end
+
+  def reg_org
+    return registered_organization if registered_organization.present?
+  end
+
+  def fhir_endpoint
+    registered_organization.fhir_endpoint
+  end
+end
+
+private
+
+def validate_npi
+  npi = '80840' + self.npi
+
+  return if LuhnacyLib.validate_npi(npi)
+
+  errors.add :npi, 'must be valid.'
+  throw(:abort)
+end
