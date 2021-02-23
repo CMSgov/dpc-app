1c92ed2aa4270f4e395bfbaa2e45a61000ee94e7
Jon Fulk <jon.fulk@adhocteam.us>
2021-02-23 11:37:37 -0600

    chore: remove unused swagger annotations from attribution (#1194)
    we use swagger annotations in api to autogenerate docs
    for public endpoints. however, we do not want to generate
    docs for the underlying attribution methods.
    For this reason, we need to remove the unecessary annotations
    as well as the test in attribution that was checking for them.
    This commit also includes some whitespace cleanup.
    DPC-1095

--- dpc-attribution/src/main/java/gov/cms/dpc/attribution/resources/AbstractAttributionResource.java
+++ dpc-attribution/src/main/java/gov/cms/dpc/attribution/resources/AbstractAttributionResource.java
@@ -1,13 +1,10 @@
 package gov.cms.dpc.attribution.resources;
 
 import gov.cms.dpc.common.utils.PropertiesProvider;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-@Api(value = "Metadata")
 public abstract class AbstractAttributionResource {
 
     private final PropertiesProvider pp;
@@ -33,16 +30,12 @@ public abstract class AbstractAttributionResource {
 
     @GET
     @Path("/_healthy")
-    @ApiOperation(value = "Check is healthy", notes = "Returns whether or not the application is in a healthy state." +
-            "\n\nMeaning, are all endpoints functioning and is the attribution database reachable.")
     public boolean checkHealth() {
         return true;
     }
 
     @GET
     @Path("/version")
-    @ApiOperation(value = "Get application build version", notes = "Returns the application build version. " +
-            "Which is the git sha abbreviation and the build timestamp.")
     public String getVersion() {
         return this.pp.getBuildVersion();
     }
