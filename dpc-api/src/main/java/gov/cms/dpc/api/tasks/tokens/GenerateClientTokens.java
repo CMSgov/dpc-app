6b8ec19afc557c992923cdb65726c62a25407dd5
SalGutierrez <43150956+SalGutierrez@users.noreply.github.com>
2021-02-22 18:32:26 -0800

    Fixed bug where token generator was throwing a parse exception when an empty token exipiration value was passed in (#1196)

--- dpc-api/src/main/java/gov/cms/dpc/api/tasks/tokens/GenerateClientTokens.java
+++ dpc-api/src/main/java/gov/cms/dpc/api/tasks/tokens/GenerateClientTokens.java
@@ -10,6 +10,7 @@ import gov.cms.dpc.api.resources.v1.TokenResource;
 import gov.cms.dpc.macaroons.MacaroonBakery;
 import io.dropwizard.jersey.jsr310.OffsetDateTimeParam;
 import io.dropwizard.servlets.tasks.Task;
+import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.dstu3.model.Organization;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,7 +43,7 @@ public class GenerateClientTokens extends Task {
     @Override
     public void execute(ImmutableMultimap<String, String> parameters, PrintWriter output) {
         final ImmutableCollection<String> expirationCollection = parameters.get("expiration");
-        final ImmutableCollection<String> tokenCollection = parameters.get("label");
+        final ImmutableCollection<String> labelCollection = parameters.get("label");
         final ImmutableCollection<String> organizationCollection = parameters.get("organization");
         if (organizationCollection.isEmpty()) {
             logger.warn("CREATING UNRESTRICTED MACAROON. ENSURE THIS IS OK");
@@ -52,9 +53,11 @@ public class GenerateClientTokens extends Task {
             final String organization = organizationCollection.asList().get(0);
             final Organization orgResource = new Organization();
             orgResource.setId(organization);
-            final String tokenLabel = tokenCollection.isEmpty() ? null : tokenCollection.asList().get(0);
-            final Optional<OffsetDateTimeParam> expiration = expirationCollection.isEmpty() ?
-                    Optional.empty() : Optional.of(new OffsetDateTimeParam(expirationCollection.asList().get(0)));
+            final String tokenLabel = labelCollection.isEmpty() ? null : labelCollection.asList().get(0);
+            Optional<OffsetDateTimeParam> expiration = Optional.empty();
+            if(!expirationCollection.isEmpty() && !StringUtils.isBlank(expirationCollection.asList().get(0))){
+                expiration = Optional.of(new OffsetDateTimeParam(expirationCollection.asList().get(0)));
+            }
             final TokenEntity tokenResponse = this.resource
                     .createOrganizationToken(
                             new OrganizationPrincipal(orgResource), null,
