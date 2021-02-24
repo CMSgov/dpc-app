c7c1bb99a6ec970fc4acdbea5b62347438917694
Jonathan Fulk <jon.fulk@adhocteam.us>
2021-02-24 11:35:05 -0600

    fix: Add checks for null or missing accept headers as well as the wildcard header

--- dpc-common/src/main/java/gov/cms/dpc/fhir/dropwizard/filters/FHIRRequestFilter.java
+++ dpc-common/src/main/java/gov/cms/dpc/fhir/dropwizard/filters/FHIRRequestFilter.java
@@ -28,9 +28,13 @@ public class FHIRRequestFilter implements ContainerRequestFilter {
     }
 
     private void checkAccepts(ContainerRequestContext requestContext) {
-        final List<MediaType> contentHeader = requestContext.getAcceptableMediaTypes();
+        final List<MediaType> acceptHeaders = requestContext.getAcceptableMediaTypes();
 
-        if (contentHeader == null || !shortCircuitBooleanCheck(contentHeader, FHIRMediaTypes::isFHIRContent)) {
+        if (requestContext.getUriInfo().getRequestUri().toString().contains("$export") && acceptHeaders == null) {
+            throw new WebApplicationException("`Accept:` header is required.", Response.SC_BAD_REQUEST);
+        }
+
+        if (!shortCircuitBooleanCheck(acceptHeaders, FHIRMediaTypes::isFHIRContent) || acceptHeaders.contains(MediaType.WILDCARD_TYPE)) {
             throw new WebApplicationException("`Accept:` header must specify valid FHIR content type", Response.SC_UNSUPPORTED_MEDIA_TYPE);
         }
     }
