1611ae9e16522156a435dc2777eb1be69a458e97
Sun-Mountain <nzonnenberg@fearless.tech>
2021-02-25 15:45:02 -0500

    Create org function

--- dpc-admin/app/controllers/application_controller.rb
+++ dpc-admin/app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  def model_error_string(resource)
+    resource.errors.full_messages.join(', ')
+  end
+
   private
 
   def id_param
     params.require(:id)
   end
+
+  def prod_sbx?
+    ENV['ENV'] == 'prod-sbx'
+  end
+  helper_method :prod_sbx?
 end
