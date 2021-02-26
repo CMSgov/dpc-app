43f46679548b56f97b72b7e2b4f8eb49d3fd72de
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2021-02-26 11:27:11 -0500

    DPC-1078: Admin site separation: add user dashboard tests (#1198)
    * Init
    
    * Fix deprication for Ruby 2.7
    
    * Add model_error_string method
    
    * User management spec intro
    
    * User spec test complete
    
    * Add BaseSearch, Luhnacy Lib, and Address specs
    
    * Finish searching/filtering spec
    
    * Remove address model spec
    
    * Remove address model spec test

---
+++ dpc-admin/spec/factories/organizations.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "./lib/luhnacy_lib/luhnacy_lib"
+
+FactoryBot.define do
+  factory :organization do
+    sequence(:name) { |n| "The Health Factory #{n}" }
+    organization_type { 0 }
+    num_providers { 5 }
+    npi {
+      loop do
+        npi = LuhnacyLib.generate_npi
+        break npi unless Organization.where(npi: npi).exists?
+      end
+    }
+
+    after(:create) do |org|
+      create(:address, addressable: org)
+    end
+
+    trait :api_enabled do
+      after(:create) do |org|
+        create(:registered_organization, organization: org, enabled: true)
+      end
+    end
+  end
+end
