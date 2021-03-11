6452e621ce67c51371ceceae40384bc08c7d62ea
Sun-Mountain <nzonnenberg@fearless.tech>
2021-03-11 10:30:00 -0500

    Add tags helper

---
+++ dpc-admin/app/helpers/tags_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module TagsHelper
+  def confirm_text(tag)
+    "Are you sure? #{tag.taggings.count} records have this tag."
+  end
+
+  def available_tags(tag_type)
+    Tag.where.not(id: tag_type.tag_ids)
+  end
+end
