c17832c9a5c103f3a82123d4127e4deb167e4999
Sun-Mountain <nzonnenberg@fearless.tech>
2021-03-24 14:44:29 -0400

    fix dpc-web-test file

--- dpc-web-test.sh
+++ dpc-web-test.sh
@@ -12,8 +12,8 @@ make website
 
 # Run the tests
 docker-compose -f dpc-web/docker-compose.yml up start_core_dependencies
-docker-compose -f dpc-web/docker-compose.yml run --entrypoint "bundle exec rails db:create db:migrate RAILS_ENV=test" dpc_web
-docker-compose -f dpc-web/docker-compose.yml run --entrypoint "bundle exec rails spec" dpc_web
+docker-compose -f dpc-web/docker-compose.yml run --entrypoint "bundle exec rails db:create db:migrate RAILS_ENV=test" web
+docker-compose -f dpc-web/docker-compose.yml run --entrypoint "bundle exec rails spec" web
 
 echo "┌──────────────────────────────────┐"
 echo "│                                  │"
