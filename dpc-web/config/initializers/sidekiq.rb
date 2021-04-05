86af47feefc70c95a38411e617ef55b5f107310d
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2021-04-05 10:59:56 -0400

    DPC-1059: Address "bugs" identified in Sonar (#1247)
    * Init
    
    * Change font weight and nav bar semi-colons
    
    * DPC Web - trap focus variables
    
    * DPC admin - trap focus var
    
    * Tool tip text
    
    * dpc-admin Remove hover duplicate
    
    * dpc-web update mimemagic gem
    
    * dpc-web remove to do for deleting api endpoint
    
    * dpc-admin remove todo for reg org api delete
    
    * dpc-web lang tags
    
    * dpc-admin lang tags
    
    * dpc-web mailer
    
    * Update mailer
    
    * Mailer progress
    
    * Mailer progress
    
    * Organization filter modal
    
    * Update mimemagic
    
    * Update mimemagic
    
    * Update ruby version to 2.7.2
    
    * Add shared-mime-info to Dockerfile
    
    * Change <b> tags to <strong> tags
    
    * Remove youtube video
    
    * Add language to emails
    
    * Add sidekiq namespace
    
    * Remove valign from mailer
    
    * Remove width from mailer
    
    * Removed cellpadding and cellspacing from mailer
    
    * Remove align attribute
    
    * Removed zip file methods
    
    * Nav helper constants
    
    * API Client Update
    
    * Add th headers
    
    * Update font family in templateBody
    
    * remove th tags
    
    * Remove TODO in registered_organization model
    
    * Added table description

--- dpc-web/config/initializers/sidekiq.rb
+++ dpc-web/config/initializers/sidekiq.rb
@@ -14,7 +14,7 @@ Sidekiq.configure_server do |config|
 
   config.log_formatter = DPCJSON.new
 
-  config.redis = { url: "#{ENV.fetch('REDIS_URL', 'redis://localhost')}:6379/1" }
+  config.redis = { url: "#{ENV.fetch('REDIS_URL', 'redis://localhost')}:6379/1", namespace: "sidekiq-web-app" }
 end
 
 Sidekiq.configure_client do |config|
