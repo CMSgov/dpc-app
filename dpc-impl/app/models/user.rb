52b7996c16cd3ad20a1bc242f0ebbc725120fe0f
Sun-Mountain <nzonnenberg@fearless.tech>
2021-04-12 10:08:39 -0400

    User model set up

--- dpc-impl/app/models/user.rb
+++ dpc-impl/app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :last_name, :first_name, :email, presence: true
+  validates :agree_to_terms, inclusion: {
+    in: [true], message: 'you must agree to the terms of service to create an account'
+  }
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
