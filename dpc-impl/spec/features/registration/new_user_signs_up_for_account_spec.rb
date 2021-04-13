6f3551f3faac893010ce6d425e9b7cade02e7b11
Sun-Mountain <nzonnenberg@fearless.tech>
2021-04-13 12:46:41 -0400

    Unsuccessful sign in scenario

--- dpc-impl/spec/features/registration/new_user_signs_up_for_account_spec.rb
+++ dpc-impl/spec/features/registration/new_user_signs_up_for_account_spec.rb
@@ -10,30 +10,37 @@ RSpec.feature 'new user signs up for account' do
   end
 
   context 'when successful' do
-    before(:each) do
-      fill_in :user_first_name, with: 'Clarissa'
-      fill_in :user_last_name, with: 'Dalloway'
-      fill_in :user_email, with: 'clarissa@example.com'
+    scenario 'create an account and sign out' do
+      fill_in :user_first_name, with: 'Samuel'
+      fill_in :user_last_name, with: 'Vimes'
+      fill_in :user_email, with: 'vimes@example.com'
       fill_in :user_password, with: '3veryDay#P0tato'
       fill_in :user_password_confirmation, with: '3veryDay#P0tato'
-      fill_in :user_requested_organization, with: 'London Health System'
-      select 'Primary Care Clinic', from: :user_requested_organization_type
-      fill_in :user_requested_num_providers, visible: false, with: '777'
-      fill_in :user_address_1, with: '1 Hampton Heath Drive'
-      fill_in :user_address_2, with: 'Suite 5'
-      fill_in :user_city, with: 'London'
-      select 'New York', from: :user_state
-      fill_in :user_zip, with: '10033'
+      fill_in :user_implementer, with: 'Night Watch Clinic'
       check :user_agree_to_terms
-    end
-
-    scenario 'create an account' do
       click_on('Sign up')
 
-      expect(page).to have_content('Welcome Clarissa Dalloway')
+      expect(page).to have_content('Samuel Vimes')
+
+      find('#sign-out', visible: false).click
+
+      expect(page).to have_content('Log in')
+      expect(page).to have_content('Request access')
     end
   end
 
   context 'when unsuccessful' do
+    scenario 'returns errors for missing fields' do
+      click_on('Sign up')
+
+      expect(page).to have_content('7 errors prohibited this user from being saved:')
+      expect(page).to have_content("Email can't be blank")
+      expect(page).to have_content("Password can't be blank")
+      expect(page).to have_content("First name can't be blank")
+      expect(page).to have_content("Last name can't be blank")
+      expect(page).to have_content("Implementer can't be blank")
+      expect(page).to have_content('Password must include at least one number, one lowercase letter, one uppercase letter, and one special character (!@#$&*-)')
+      expect(page).to have_content('Agree to terms you must agree to the terms of service to create an account')
+    end
   end
 end
