538c415be70cbf30234a4070e9f05d1a684ea1b7
Pat Kelsh <pkelsh@gmail.com>
2021-04-15 11:45:38 -0400

    DPC-1245 switched order of rubocop and rspec tests

--- dpc-portals-test.sh
+++ dpc-portals-test.sh
@@ -21,16 +21,16 @@ echo "│                         │"
 echo "│  Running DPC Web Tests  │"
 echo "│                         │"
 echo "└─────────────────────────┘"
-docker-compose -p dpc-portals -f docker-compose.portals.yml run --entrypoint "bundle exec rails spec" dpc_web
 docker-compose -p dpc-portals -f docker-compose.portals.yml run --entrypoint "rubocop" dpc_web
+docker-compose -p dpc-portals -f docker-compose.portals.yml run --entrypoint "bundle exec rails spec" dpc_web
 
 echo "┌───────────────────────────┐"
 echo "│                           │"
 echo "│  Running DPC Admin Tests  │"
 echo "│                           │"
 echo "└───────────────────────────┘"
-docker-compose -p dpc-portals -f docker-compose.portals.yml run --entrypoint "bundle exec rails spec" dpc_admin
 docker-compose -p dpc-portals -f docker-compose.portals.yml run --entrypoint "rubocop" dpc_admin
+docker-compose -p dpc-portals -f docker-compose.portals.yml run --entrypoint "bundle exec rails spec" dpc_admin
 
 echo "┌──────────────────────────────────────────┐"
 echo "│                                          │"
