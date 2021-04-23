ceceb061326bc0b89b50509c62d11faa6b8a7dde
Oren Fromberg <oren@adhocteam.us>
2021-04-23 10:59:01 -0400

    Using different prefix for assets

--- dpc-admin/config/environments/test.rb
+++ dpc-admin/config/environments/test.rb
@@ -46,4 +46,7 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+  config.assets.prefix = '/admin_assets'
+  config.assets.paths << Rails.root.join("app", "assets", "fonts")
 end
