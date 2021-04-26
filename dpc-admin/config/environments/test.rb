2e52c65c894b29180be262eabe79fc68de75ad63
Oren Fromberg <oren@adhocteam.us>
2021-04-26 16:16:34 -0400

    Removing redundant config

--- dpc-admin/config/environments/test.rb
+++ dpc-admin/config/environments/test.rb
@@ -46,7 +46,4 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
-
-  config.assets.prefix = '/admin_assets'
-  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 end
