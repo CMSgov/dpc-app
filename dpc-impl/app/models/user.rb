72d9847ec2b235f76b9c694406e3c635bf1f584f
Sun-Mountain <nzonnenberg@fearless.tech>
2021-04-29 11:13:58 -0400

    Install devise invitable

--- dpc-impl/app/models/user.rb
+++ dpc-impl/app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :validatable,
          :trackable, :timeoutable, :confirmable,
          :password_expirable, :password_archivable
