afc963f29e77dd6c8a54fe26f87651f021d13dd6
Sun-Mountain <nzonnenberg@fearless.tech>
2021-05-06 12:27:23 -0400

    Portal controller refactor

--- dpc-impl/app/controllers/portal_controller.rb
+++ dpc-impl/app/controllers/portal_controller.rb
@@ -4,11 +4,25 @@ class PortalController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users_accepted = User.invitation_accepted.where(implementer_id: current_user.implementer_id)
-    @users_pending = User.invitation_not_accepted.where(implementer_id: current_user.implementer_id)
+    @users_accepted = users_accepted
+    @users_pending = users_pending
   end
 
   def show
     @user = current_user
+  end
+
+  private
+
+  def current_user_imp_id
+    current_user.implementer_id
+  end
+
+  def users_accepted
+    User.invitation_accepted.where(implementer_id: current_user_imp_id)
+  end
+
+  def users_pending
+    User.invitation_not_accepted.where(implementer_id: current_user_imp_id)
   end
 end
