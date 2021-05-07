58ad9add045a66d81b43db8530e0184c64ab95b9
Sun-Mountain <nzonnenberg@fearless.tech>
2021-05-07 15:25:20 -0400

    Readd check_impl

--- dpc-impl/app/models/user.rb
+++ dpc-impl/app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   before_save :assign_implementer_id
+  before_create :check_impl
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -17,6 +18,27 @@ class User < ApplicationRecord
   validates :agree_to_terms, inclusion: {
     in: [true], message: 'you must agree to the terms of service to create an account'
   }
+
+  def check_impl
+    @host = self.invited_by_id
+
+    return if @host.nil?
+
+    @user = self
+    @invite = User.where(id: @host).first
+    user_id = @user.implementer_id
+    invite_id = @invite.implementer_id
+    user_imp = @user.implementer
+    invite_imp = @invite.implementer
+
+    if user_id != invite_id
+      @user.implementer_id = invite_id
+    end
+
+    if user_imp != invite_imp
+      @user.implementer = invite_imp
+    end
+  end
 
   def name
     "#{first_name} #{last_name}"
