4c04d0048e3115f1f0f50f8c2ede5089a9416bcc
Sal Gutierrez <43150956+SalGutierrez@users.noreply.github.com>
2021-05-13 23:37:31 -0700

    PR changes

--- dpc-go/dpc-attribution/src/model/managed_org.go
+++ dpc-go/dpc-attribution/src/model/managed_org.go
@@ -2,8 +2,8 @@ package model
 
 // ManagedOrg is a struct that models the relationship status of an org and implementer
 type ManagedOrg struct {
-	OrganizationID string        `db:"id" json:"org_id"`
-	Name           string        `db:"name" json:"org_name"`
+	OrganizationID string `db:"id" json:"org_id"`
+	Name           string `db:"name" json:"org_name"`
 	Status         string `db:"created_at" json:"status"`
-	NPI            string    `db:"updated_at" json:"npi"`
+	NPI            string `db:"updated_at" json:"npi"`
 }
