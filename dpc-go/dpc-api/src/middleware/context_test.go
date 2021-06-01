494a7ddb561af1f21bb0f44b3b28f846cb9d4229
Jonathan Fulk <jon.fulk@adhocteam.us>
2021-06-01 15:56:13 -0500

    add test for context middleware

---
+++ dpc-go/dpc-api/src/middleware/context_test.go
@@ -0,0 +1,48 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type ContextTestSuite struct {
+	suite.Suite
+}
+
+func TestContextTestSuite(t *testing.T) {
+	suite.Run(t, new(ContextTestSuite))
+}
+
+func (suite *ContextTestSuite) TestExportTypesParam() {
+	var types string
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		types, _ = r.Context().Value(ContextKeyResourceTypes).(string)
+		assert.Equal(suite.T(), "Coverage,ExplanationOfBenefit", types)
+		_, _ = w.Write([]byte(""))
+	})
+
+	req := httptest.NewRequest(http.MethodGet, "http://www.example.com/Group/some-id$export?_type=Coverage,ExplanationOfBenefit&_since=2012-01-02T12:12:12-00:00", nil)
+	res := httptest.NewRecorder()
+
+	e := ExportTypesParamCtx(nextHandler)
+	e.ServeHTTP(res, req)
+}
+
+func (suite *ContextTestSuite) TestExportTypesParamDefault() {
+	var types string
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		types, _ = r.Context().Value(ContextKeyResourceTypes).(string)
+		assert.Equal(suite.T(), AllResources, types)
+		_, _ = w.Write([]byte(""))
+	})
+
+	req := httptest.NewRequest(http.MethodGet, "http://www.example.com/Group/some-id$export?_since=2012-01-02T12:12:12-00:00", nil)
+	res := httptest.NewRecorder()
+
+	e := ExportTypesParamCtx(nextHandler)
+	e.ServeHTTP(res, req)
+}
