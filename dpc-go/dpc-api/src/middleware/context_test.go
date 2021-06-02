912c4e60a7d2ccf7466a2dd3135d53dfb58660bc
Jonathan Fulk <jon.fulk@adhocteam.us>
2021-06-02 07:49:15 -0500

    add `ExportSinceParamCtx` function

--- dpc-go/dpc-api/src/middleware/context_test.go
+++ dpc-go/dpc-api/src/middleware/context_test.go
@@ -146,3 +146,33 @@ func (suite *ContextTestSuite) TestExportSinceParamInvalid() {
 	e.ServeHTTP(res, req)
 	assert.Equal(suite.T(), "", since)
 }
+
+func (suite *ContextTestSuite) TestExportSinceParam() {
+	var since string
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		since, _ = r.Context().Value(ContextKeySince).(string)
+		assert.Equal(suite.T(), "2012-01-02T12:12:12-00:00", since)
+		_, _ = w.Write([]byte(""))
+	})
+
+	req := httptest.NewRequest(http.MethodGet, "http://www.example.com/Group/some-id$export?_type=Coverage,ExplanationOfBenefit&_since=2012-01-02T12:12:12-00:00", nil)
+	res := httptest.NewRecorder()
+
+	e := ExportSinceParamCtx(nextHandler)
+	e.ServeHTTP(res, req)
+}
+
+func (suite *ContextTestSuite) TestExportSinceParamDefault() {
+	var since string
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		since, _ = r.Context().Value(ContextKeySince).(string)
+		assert.Equal(suite.T(), "", since)
+		_, _ = w.Write([]byte(""))
+	})
+
+	req := httptest.NewRequest(http.MethodGet, "http://www.example.com/Group/some-id$export?_type=Coverage,ExplanationOfBenefit", nil)
+	res := httptest.NewRecorder()
+
+	e := ExportSinceParamCtx(nextHandler)
+	e.ServeHTTP(res, req)
+}
