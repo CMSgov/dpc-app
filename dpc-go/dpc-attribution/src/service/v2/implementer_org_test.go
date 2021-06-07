c4376f817ac31d5cec471d8e2d28a72c2bef497c
Salvador <43150956+MrMorie@users.noreply.github.com>
2021-06-07 00:22:27 -0700

    DPC-1296 GET implementer/:id/org (#1295)
    * added GET implementer/:id/org

--- dpc-go/dpc-attribution/src/service/v2/implementer_org_test.go
+++ dpc-go/dpc-attribution/src/service/v2/implementer_org_test.go
@@ -2,13 +2,8 @@ package v2
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
-	"strings"
-	"testing"
-
 	"github.com/CMSgov/dpc/attribution/middleware"
 	v2 "github.com/CMSgov/dpc/attribution/model/v2"
 	"github.com/bxcodec/faker/v3"
@@ -17,6 +12,11 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 )
 
 type MockImplementerOrgRepo struct {
@@ -36,6 +36,14 @@ func (m *MockImplementerOrgRepo) FindRelation(ctx context.Context, implId string
 		return nil, args.Error(1)
 	}
 	return args.Get(0).(*v2.ImplementerOrgRelation), args.Error(1)
+}
+
+func (m *MockImplementerOrgRepo) FindManagedOrgs(ctx context.Context, implId string) ([]v2.ImplementerOrgRelation, error) {
+	args := m.Called(ctx, implId)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).([]v2.ImplementerOrgRelation), args.Error(1)
 }
 
 type ImplementerOrgServiceTestSuite struct {
@@ -102,6 +110,57 @@ func (suite *ImplementerOrgServiceTestSuite) TestPost() {
 	assert.Equal(suite.T(), http.StatusOK, res.StatusCode)
 }
 
+func (suite *ImplementerOrgServiceTestSuite) TestGetOrgs() {
+
+	implOrg := v2.ImplementerOrgRelation{}
+	_ = faker.FakeData(&implOrg)
+	implOrg.Status = v2.Active
+
+	implOrg2 := v2.ImplementerOrgRelation{}
+	_ = faker.FakeData(&implOrg)
+	implOrg.Status = v2.Active
+
+	relations := []v2.ImplementerOrgRelation{implOrg, implOrg2}
+	suite.implOrgRepo.On("FindManagedOrgs", mock.Anything, mock.Anything).Return(relations, nil)
+
+	impl := v2.Implementer{}
+	_ = faker.FakeData(&impl)
+	suite.implRepo.On("FindByID", mock.Anything, mock.Anything).Return(&impl, nil)
+
+	org1 := v2.Organization{}
+	inf1 := `{
+	"resourceType": "Organization",
+          "identifier": [
+               {
+                   "system": "http://hl7.org/fhir/sid/us-npi",
+                   "value": "00010"
+               }
+          ],
+          "name": "Some org name"}`
+	var org1Info v2.Info
+	err := json.Unmarshal([]byte(inf1), &org1Info)
+	assert.NoError(suite.T(), err)
+	org1.Info = org1Info
+
+	suite.orgRepo.On("FindByID", mock.Anything, mock.Anything).Return(&org1, nil)
+
+	req := httptest.NewRequest("GET", "http://example.com/foo", strings.NewReader(""))
+	ctx := req.Context()
+	ctx = context.WithValue(ctx, middleware.ContextKeyImplementer, implOrg.ImplementerID)
+	req = req.WithContext(ctx)
+	w := httptest.NewRecorder()
+
+	suite.service.Get(w, req)
+	res := w.Result()
+
+	resp, _ := ioutil.ReadAll(res.Body)
+	respS := string(resp)
+
+	assert.Contains(suite.T(), respS, implOrg.OrganizationID)
+	assert.Contains(suite.T(), respS, implOrg2.OrganizationID)
+	assert.Equal(suite.T(), http.StatusOK, res.StatusCode)
+}
+
 func (suite *ImplementerOrgServiceTestSuite) TestSaveRepoError() {
 	ja := jsonassert.New(suite.T())
 
@@ -154,14 +213,6 @@ func (suite *ImplementerOrgServiceTestSuite) TestSaveRepoError() {
         "message": "error",
         "statusCode": 422
     }`)
-}
-
-func (suite *ImplementerOrgServiceTestSuite) TestGetNotImplemented() {
-	req := httptest.NewRequest(http.MethodGet, "http://example.com/foo", nil)
-	w := httptest.NewRecorder()
-	suite.service.Get(w, req)
-	res := w.Result()
-	assert.Equal(suite.T(), http.StatusNotImplemented, res.StatusCode)
 }
 
 func (suite *ImplementerOrgServiceTestSuite) TestDeleteNotImplemented() {
