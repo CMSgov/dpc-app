a562ea05987d0f8a4aaf89bbb9180dc238cce02b
Sun-Mountain <nzonnenberg@fearless.tech>
2021-06-10 13:33:38 -0400

    Set up head

--- dpc-adminv2/app/helpers/application_helper.rb
+++ dpc-adminv2/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def yield_meta_tag(tag, default_text = '')
+    content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
+  end
 end
--- dpc-adminv2/config/routes.rb
+++ dpc-adminv2/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   scope 'adminv2' do
+
+    root to: 'implementers#index', via: :get
+
   end
 end
