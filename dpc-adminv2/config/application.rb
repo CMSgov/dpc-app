82348d0a17bdf30a5b757c7502e5bb9ce180045e
Sun-Mountain <nzonnenberg@fearless.tech>
2021-06-10 15:24:30 -0400

    Finalize styling including focus

--- dpc-adminv2/config/application.rb
+++ dpc-adminv2/config/application.rb
@@ -8,9 +8,15 @@ Bundler.require(*Rails.groups)
 
 module DpcAdminv2
   class Application < Rails::Application
+    # Check for STATIC_SITE_URL environment variable
+    ENV['STATIC_SITE_URL'].present? ? ENV['STATIC_SITE_URL'] : ENV['STATIC_SITE_URL'] = 'https://dpc.cms.gov'
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # Add fonts to asset pipeline
+    config.assets.prefix = '/adminv2/assets'
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
