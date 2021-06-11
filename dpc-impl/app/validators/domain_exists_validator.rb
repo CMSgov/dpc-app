fdf04f10b7113ce8c5d414b26ed82f12d586dcf5
Sun-Mountain <nzonnenberg@fearless.tech>
2021-06-11 14:55:39 -0400

    Email validator deprecation fixed

--- dpc-impl/app/validators/domain_exists_validator.rb
+++ dpc-impl/app/validators/domain_exists_validator.rb
@@ -3,13 +3,11 @@
 require 'mail'
 class DomainExistsValidator < ActiveModel::EachValidator#add
   def validate_each(record, attribute, value)
-    unless [record, attribute, value].present?
-      begin
-        r = Truemail.validate(value).result.success
-      rescue StandardError
-        r = false
-      end
-      record.errors.add << (options[:message] || 'is invalid') unless r
+    begin
+      r = Truemail.validate(value).result.success
+    rescue StandardError
+      r = false
     end
+    record.errors.add attribute, (options[:message] || ' is invalid') unless r
   end
 end
