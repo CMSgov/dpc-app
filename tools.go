b7fb3b760bc769898771933f75a1fb79f7f27f69
Salvador <43150956+MrMorie@users.noreply.github.com>
2021-06-22 15:18:33 -0700

    Update tools.go
    go fmt

--- tools.go
+++ tools.go
@@ -3,9 +3,9 @@
 package main
 
 import (
-    // bcda/worker/ssas dependencies
-    _ "github.com/BurntSushi/toml"
-    _ "github.com/go-delve/delve/cmd/dlv"
-    _ "github.com/howeyc/fsnotify"
-    _ "github.com/mattn/go-colorable"
+	// bcda/worker/ssas dependencies
+	_ "github.com/BurntSushi/toml"
+	_ "github.com/go-delve/delve/cmd/dlv"
+	_ "github.com/howeyc/fsnotify"
+	_ "github.com/mattn/go-colorable"
 )
