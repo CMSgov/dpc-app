4353ef28ef30f652758af798958e1c4682c20e75
Sun-Mountain <nzonnenberg@fearless.tech>
2021-06-23 10:39:13 -0400

    Count

--- dpc-impl/app/assets/javascripts/components/_search-filter.js
+++ dpc-impl/app/assets/javascripts/components/_search-filter.js
@@ -5,6 +5,8 @@ function searchFunc() {
   ul = document.getElementById('searchList');
   li = ul.getElementsByTagName('li');
 
+  var liDisplayCount = 0;
+
   for (i = 0; i < li.length; i++) {
     div = li[i].getElementsByTagName('div')[0];
 
@@ -14,6 +16,21 @@ function searchFunc() {
       li[i].style.display = 'none';
     } else {
       li[i].style.display = '';
+      liDisplayCount++
     }
+  }
+
+  var numCount = document.getElementById('searchCount')
+  
+  if (liDisplayCount == 0) {
+    numCount.innerHTML = "There are no results that match your search query."
+  } else if (liDisplayCount > 1) {
+    numCount.innerHTML = "There are " + liDisplayCount + " results that match your search query."
+  } else {
+    numCount.innerHTML = "There is " + liDisplayCount + " result that matches your search query."
+  }
+
+  if (filter.length == 0) {
+    numCount.innerHTML = ""
   }
 }
