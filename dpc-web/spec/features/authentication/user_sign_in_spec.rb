33639938cf7f6c0b9c09209bd646bff0bc3b6af5
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2021-06-23 15:39:26 -0400

    DPC-1521: Web app: remove admin code (#1328)
    * Init - remove basesearch spec
    
    * remove internal specs
    
    * Remove controller concerns
    
    * Remove controllers, helpers
    
    * Remove pages
    
    * remove routes
    
    * Update Gemlock file
    
    * Remove last internal route
    
    * Update minimum coverage by file
    
    * Remove user helper
    
    * Update gemlock

--- dpc-web/spec/features/authentication/user_sign_in_spec.rb
+++ dpc-web/spec/features/authentication/user_sign_in_spec.rb
@@ -14,20 +14,6 @@ RSpec.feature 'user signs in' do
     expect(page).to have_css('[data-test="my-account-menu"]')
   end
 
-  scenario 'user cannot then sign in as internal user' do
-    visit new_user_session_path
-    fill_in 'user_email', with: user.email
-    fill_in 'user_password', with: '12345ABCDEfghi!'
-    find('[data-test="submit"]').click
-
-    expect(page).to have_css('[data-test="my-account-menu"]')
-
-    visit new_internal_user_session_path
-
-    expect(page).not_to have_css('[data-test="internal-user-sign-in-form"]')
-    expect(page).to have_css('[data-test="my-account-menu"]')
-  end
-
   scenario 'user cannot sign in if account not confirmed' do
     unconfirmed = create(:user, confirmed_at: nil)
 
