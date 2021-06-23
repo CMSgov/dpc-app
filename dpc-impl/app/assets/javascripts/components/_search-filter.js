b3bfb32465eb89d0348b8aa28a014fba5b728960
Sun-Mountain <nzonnenberg@fearless.tech>
2021-06-23 09:43:38 -0400

    Dynamic search

--- dpc-impl/app/assets/javascripts/components/_search-filter.js
+++ dpc-impl/app/assets/javascripts/components/_search-filter.js
@@ -1,7 +1,19 @@
 function searchFunc() {
-  var input;
+  var input, filter, ul, li, div, txtValue;
   input = document.getElementById('searchInput');
   filter = input.value.toUpperCase();
+  ul = document.getElementById('searchList');
+  li = ul.getElementsByTagName('li');
 
-  console.log(filter);
+  for (i = 0; i < li.length; i++) {
+    div = li[i].getElementsByTagName('div')[0];
+
+    txtValue = div.textContent || div.innerText;
+
+    if (txtValue.toUpperCase().indexOf(filter) <= -1) {
+      li[i].style.display = 'none';
+    } else {
+      li[i].style.display = '';
+    }
+  }
 }
