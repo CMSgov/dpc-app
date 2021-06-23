6a9cfe5a605e65e74b0c31a759186022d39e4a3e
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2021-06-23 17:09:28 -0400

    DPC-1479: Replicate in admin portal - login page (#1333)
    * Install devise/omniauth gems
    
    * Add devise/omniauth
    
    * Add AdminUser table in database
    
    * Remove AdminUser table
    
    * Add Admin model, omniauth callbacks, views
    
    * Revert "Install devise/omniauth gems"
    
    This reverts commit 99e14e1ed3e228813008563ef23a82c32cfc559f.
    
    * Create admin table
    
    * Set up controllers and view
    
    * Install devise
    
    * Redirects to github signin page
    
    * Sign in thru github
    
    * Sign out from admin portal
    
    * Add end ul tag
    
    * Init omniauth spec tests
    
    * Admin model tests
    
    * Update rails helper
    
    * Add simplecov
    
    * Remove default_table_access_method
    
    * Impl README
    
    * READMEs
    
    * Remove READMEs (for now)
    
    * Remove unused devise views
    
    * remove controller disctinction in navbar

--- dpc-adminv2/config/routes.rb
+++ dpc-adminv2/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   scope 'adminv2' do
+    devise_for :admin, path: '', controllers: {
+      sessions: 'auth/sessions',
+      omniauth_callbacks: 'auth/omniauth_callbacks'
+    }
 
     root to: 'implementers#index', via: :get
 
