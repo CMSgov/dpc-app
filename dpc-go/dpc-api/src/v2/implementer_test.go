b3497bb679f50d854ed35579a6cc84f15f5eedd4
Gavin St. Ours <gst-ours@fearless.tech>
2021-06-28 14:27:20 -0400

    gofmt test file

--- dpc-go/dpc-api/src/v2/implementer_test.go
+++ dpc-go/dpc-api/src/v2/implementer_test.go
@@ -1,54 +1,54 @@
 package v2
 
 import (
-    "context"
-    "github.com/CMSgov/dpc/api/apitest"
-    "github.com/go-chi/chi/middleware"
-    "github.com/kinbiko/jsonassert"
-    "github.com/stretchr/testify/assert"
-    "github.com/stretchr/testify/mock"
-    "github.com/stretchr/testify/suite"
-    "io/ioutil"
-    "net/http"
-    "net/http/httptest"
-    "strings"
-    "testing"
+	"context"
+	"github.com/CMSgov/dpc/api/apitest"
+	"github.com/go-chi/chi/middleware"
+	"github.com/kinbiko/jsonassert"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 )
 
 type ImplementerControllerTestSuite struct {
-    suite.Suite
-    impl *ImplementerController
-    mac *MockAttributionClient
+	suite.Suite
+	impl *ImplementerController
+	mac  *MockAttributionClient
 }
 
 func (suite *ImplementerControllerTestSuite) SetupTest() {
-    mac := new(MockAttributionClient)
-    suite.mac = mac
-    suite.impl = NewImplementerController(mac)
+	mac := new(MockAttributionClient)
+	suite.mac = mac
+	suite.impl = NewImplementerController(mac)
 }
 
 func TestImplementerControllerTestSuite(t *testing.T) {
-    suite.Run(t, new(ImplementerControllerTestSuite))
+	suite.Run(t, new(ImplementerControllerTestSuite))
 }
 
 func (suite *ImplementerControllerTestSuite) TestCreateImplementerBadJson() {
-    ja := jsonassert.New(suite.T())
-    req := httptest.NewRequest(http.MethodPost, "http://example.com/foo", nil)
-    ctx := req.Context()
-    ctx = context.WithValue(ctx, middleware.RequestIDKey, "12345")
-    req = req.WithContext(ctx)
+	ja := jsonassert.New(suite.T())
+	req := httptest.NewRequest(http.MethodPost, "http://example.com/foo", nil)
+	ctx := req.Context()
+	ctx = context.WithValue(ctx, middleware.RequestIDKey, "12345")
+	req = req.WithContext(ctx)
 
-    w := httptest.NewRecorder()
+	w := httptest.NewRecorder()
 
-    suite.impl.Create(w, req)
+	suite.impl.Create(w, req)
 
-    res := w.Result()
+	res := w.Result()
 
-    assert.Equal(suite.T(), http.StatusBadRequest, res.StatusCode)
+	assert.Equal(suite.T(), http.StatusBadRequest, res.StatusCode)
 
-    resp, _ := ioutil.ReadAll(res.Body)
+	resp, _ := ioutil.ReadAll(res.Body)
 
-    ja.Assertf(string(resp), `
+	ja.Assertf(string(resp), `
     {
         "issue": [
             {
@@ -65,16 +65,16 @@ func (suite *ImplementerControllerTestSuite) TestCreateImplementerBadJson() {
 }
 
 func (suite *ImplementerControllerTestSuite) TestCreateImplementer() {
-    suite.mac.On("Post", mock.Anything, mock.Anything, mock.Anything).Return(apitest.AttributionToFHIRResponse(apitest.ImplJSON), nil)
+	suite.mac.On("Post", mock.Anything, mock.Anything, mock.Anything).Return(apitest.AttributionToFHIRResponse(apitest.ImplJSON), nil)
 
-    req := httptest.NewRequest(http.MethodPost, "http://example.com/foo", strings.NewReader(apitest.ImplJSON))
-    ctx := req.Context()
-    ctx = context.WithValue(ctx, middleware.RequestIDKey, "12345")
-    req = req.WithContext(ctx)
+	req := httptest.NewRequest(http.MethodPost, "http://example.com/foo", strings.NewReader(apitest.ImplJSON))
+	ctx := req.Context()
+	ctx = context.WithValue(ctx, middleware.RequestIDKey, "12345")
+	req = req.WithContext(ctx)
 
-    w := httptest.NewRecorder()
-    suite.impl.Create(w, req)
-    res := w.Result()
+	w := httptest.NewRecorder()
+	suite.impl.Create(w, req)
+	res := w.Result()
 
-    assert.Equal(suite.T(), http.StatusOK, res.StatusCode)
+	assert.Equal(suite.T(), http.StatusOK, res.StatusCode)
 }
