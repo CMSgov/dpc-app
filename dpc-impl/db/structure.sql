766bbd3c63de6e13dcd36f5acdd53a8fd513701b
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2021-06-29 13:02:59 -0400

    DPC-1475: As a user, I want to search through provider orgs (#1337)
    * Set up search input
    
    * Set up JavaScript
    
    * Dynamic search
    
    * Count
    
    * Update search function
    
    * Declare variable i
    
    * SearchFunc refactor
    
    * structure.sql
    
    * Reset db
    
    * Delete sql structure
    
    * New db
    
    * Remove default_table_access_method

--- dpc-impl/db/structure.sql
+++ dpc-impl/db/structure.sql
@@ -362,7 +362,6 @@ CREATE UNIQUE INDEX index_users_on_reset_password_token ON public.users USING bt
 SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
-('0'),
 ('20210409181131'),
 ('20210423155520'),
 ('20210423155611'),
