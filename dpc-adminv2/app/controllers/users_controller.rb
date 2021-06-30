fe6f91d62697bb0cac6ba4c6f9f851d5064ad08a
Sun-Mountain <nzonnenberg@fearless.tech>
2021-06-30 17:31:10 -0400

    User show page + user delete

--- dpc-adminv2/app/controllers/users_controller.rb
+++ dpc-adminv2/app/controllers/users_controller.rb
@@ -4,10 +4,23 @@ class UsersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @users = User.all
+    @users = User.all.order('created_at DESC')
   end
 
   def show
     @user = User.find(id_param)
+  end
+
+  def destroy
+    @user = User.find(id_param)
+    @user.destroy
+
+    if @user.destroy
+      flash[:notice] = 'User successfully deleted.'
+      redirect_to users_path
+    else
+      flash[:alert] = 'Unable to delete user.'
+      render user_path(@user)
+    end
   end
 end
