7ed0df46c2a64bee2b3205fecd267560d746519d
Sun-Mountain <nzonnenberg@fearless.tech>
2021-07-01 10:51:20 -0400

    User info validators

--- dpc-adminv2/app/models/user.rb
+++ dpc-adminv2/app/models/user.rb
@@ -2,6 +2,9 @@
 
 class User < ApplicationRecord
 
+  validates :email, presence: true, domain_exists: true
+  validates :last_name, :first_name, presence: true
+
   def name
     "#{first_name} #{last_name}"
   end
