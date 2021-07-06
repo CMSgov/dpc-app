52ea1910a7a802d2e310d3bc41efe2c3857e74e2
Salvador <43150956+MrMorie@users.noreply.github.com>
2021-07-05 19:59:12 -0700

    DPC-1442-1542 Added SSAS group and system id to implementer and implementer/org (#1345)
    * Added ssas group and system id to implementer and implementer/org

--- dpc-go/dpc-attribution/src/service/v2/implementer_test.go
+++ dpc-go/dpc-attribution/src/service/v2/implementer_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/CMSgov/dpc/attribution/middleware"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -25,6 +26,13 @@ type MockImplementerRepo struct {
 
 func (m *MockImplementerRepo) Insert(ctx context.Context, body []byte) (*v2.Implementer, error) {
 	args := m.Called(ctx, body)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*v2.Implementer), args.Error(1)
+}
+func (m *MockImplementerRepo) Update(ctx context.Context, id string, body []byte) (*v2.Implementer, error) {
+	args := m.Called(ctx, id, body)
 	if args.Get(0) == nil {
 		return nil, args.Error(1)
 	}
@@ -79,6 +87,85 @@ func (suite *ImplementerServiceTestSuite) TestPost() {
 	ja.Assertf(string(resp), string(b))
 }
 
+func (suite *ImplementerServiceTestSuite) TestPut() {
+	ja := jsonassert.New(suite.T())
+
+	impl := v2.Implementer{}
+	err := faker.FakeData(&impl)
+	if err != nil {
+		fmt.Printf("ERR %v\n", err)
+	}
+	implUpdated := v2.Implementer{}
+	err = faker.FakeData(&impl)
+	if err != nil {
+		fmt.Printf("ERR %v\n", err)
+	}
+	suite.repo.On("FindByID", mock.Anything, mock.Anything).Return(&impl, nil)
+
+	suite.repo.On("Update", mock.Anything, mock.Anything, mock.Anything).Return(&implUpdated, nil)
+
+	//Send update request
+	req := httptest.NewRequest(http.MethodPut, "http://example.com/Implementer/123456789", strings.NewReader(`{"name":"test-name"}`))
+	ctx := req.Context()
+	ctx = context.WithValue(ctx, middleware.ContextKeyImplementer, "123456789")
+	req = req.WithContext(ctx)
+	w := httptest.NewRecorder()
+	suite.service.Put(w, req)
+
+	res := w.Result()
+	assert.Equal(suite.T(), http.StatusOK, res.StatusCode)
+	resp, _ := ioutil.ReadAll(res.Body)
+
+	b, _ := json.Marshal(implUpdated)
+	ja.Assertf(string(resp), string(b))
+}
+
+func (suite *ImplementerServiceTestSuite) TestUpdateWithoutName() {
+	impl := v2.Implementer{}
+	err := faker.FakeData(&impl)
+	if err != nil {
+		fmt.Printf("ERR %v\n", err)
+	}
+	suite.repo.On("FindByID", mock.Anything, mock.Anything).Return(&impl, nil)
+
+	//Send update request
+	req := httptest.NewRequest(http.MethodPut, "http://example.com/Implementer/123456789", strings.NewReader(`{}`))
+	ctx := req.Context()
+	ctx = context.WithValue(ctx, middleware.ContextKeyImplementer, "123456789")
+	req = req.WithContext(ctx)
+	w := httptest.NewRecorder()
+	suite.service.Put(w, req)
+
+	res := w.Result()
+	assert.Equal(suite.T(), http.StatusUnprocessableEntity, res.StatusCode)
+	resp, _ := ioutil.ReadAll(res.Body)
+
+	assert.Contains(suite.T(), string(resp), "Missing name in body")
+}
+
+func (suite *ImplementerServiceTestSuite) TestUpdateNotFound() {
+	impl := v2.Implementer{}
+	err := faker.FakeData(&impl)
+	if err != nil {
+		fmt.Printf("ERR %v\n", err)
+	}
+	suite.repo.On("FindByID", mock.Anything, mock.Anything).Return(nil, nil)
+
+	//Send update request
+	req := httptest.NewRequest(http.MethodPut, "http://example.com/Implementer/123456789", strings.NewReader(`{}`))
+	ctx := req.Context()
+	ctx = context.WithValue(ctx, middleware.ContextKeyImplementer, "123456789")
+	req = req.WithContext(ctx)
+	w := httptest.NewRecorder()
+	suite.service.Put(w, req)
+
+	res := w.Result()
+	assert.Equal(suite.T(), http.StatusNotFound, res.StatusCode)
+	resp, _ := ioutil.ReadAll(res.Body)
+
+	assert.Contains(suite.T(), string(resp), "Implementer not found")
+}
+
 func (suite *ImplementerServiceTestSuite) TestSaveRepoError() {
 	ja := jsonassert.New(suite.T())
 
@@ -116,14 +203,6 @@ func (suite *ImplementerServiceTestSuite) TestDeleteNotImplemented() {
 	req := httptest.NewRequest(http.MethodDelete, "http://example.com/foo", nil)
 	w := httptest.NewRecorder()
 	suite.service.Delete(w, req)
-	res := w.Result()
-	assert.Equal(suite.T(), http.StatusNotImplemented, res.StatusCode)
-}
-
-func (suite *ImplementerServiceTestSuite) TestPutNotImplemented() {
-	req := httptest.NewRequest(http.MethodPut, "http://example.com/foo", nil)
-	w := httptest.NewRecorder()
-	suite.service.Put(w, req)
 	res := w.Result()
 	assert.Equal(suite.T(), http.StatusNotImplemented, res.StatusCode)
 }
