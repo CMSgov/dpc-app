0187588d3691930ba1471e5178a0b747e71f089c
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2021-07-08 11:38:10 -0400

    DPC-1298: Automatically generate NPI for sandbox provider organizations (#1347)
    * Provider org routes
    
    * New Porg Button
    
    * Connection error shared
    
    * Luhnacy install
    
    * stopping point
    
    * REvert
    
    * Revert "Connection error shared"
    
    This reverts commit 2e9b7dc304b16aa55857a4b86db5a6be2a4075e7.
    
    * Home button
    
    * Connection error shared
    
    * add porg form added
    
    * Able to make post request for new po
    
    * NPI check
    
    * Add Provider Org styling
    
    * Api errors
    
    * Change show to new
    
    * Bundle update
    
    * Provider Org #add test
    
    * Add provider org spec successful request
    
    * unsuccessful api connection

--- dpc-impl/spec/support/api_client_support.rb
+++ dpc-impl/spec/support/api_client_support.rb
@@ -18,4 +18,13 @@ module ApiClientSupport
       'updated_at' => 00000
     }
   end
+
+  def default_provider_org_response
+    {:status=>"Active",
+     :id=>"2e449356-903f-4dbe-9a32-9a45d08c26a9",
+     :implementer_id=>"923a4f7b-eade-494a-8ca4-7a685edacfad",
+     :organization_id=>"587ed9cf-6bd6-4860-9de3-077277b2c824",
+     :created_at=>"2021-07-06T17:46:59.365253Z",
+     :updated_at=>"2021-07-06T17:46:59.365253Z"}
+  end
 end
