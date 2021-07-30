8c86cef978a3d6ea68189210d0fb8180f8aafd21
Sun-Mountain <nzonnenberg@fearless.tech>
2021-07-30 12:02:01 -0400

    Reverting before system

--- dpc-impl/app/controllers/public_keys_controller.rb
+++ dpc-impl/app/controllers/public_keys_controller.rb
@@ -9,20 +9,16 @@ class PublicKeysController < ApplicationController
 
   def create
     @org_id = org_id
-    return render_error('Required values missing.', @org_id) if missing_key
 
     manager = PublicKeyManager.new(imp_id: imp_id, org_id: @org_id)
 
     new_public_key = manager.create_public_key(
-      org_name: params[:org_name],
-      label: params[:label],
       public_key: params[:public_key],
+      label: params[:label],
       snippet_signature: params[:snippet_signature]
     )
 
     if new_public_key[:response]
-      flash[:notice] = 'Public Key Added'
-      redirect_to provider_orgs_path(org_id: @org_id)
     else
       render_error(new_public_key[:message], @org_id)
     end
