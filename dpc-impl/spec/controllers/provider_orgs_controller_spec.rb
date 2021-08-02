c73dd7f8389ad5839732e0b3f29f5e3fa5adecd6
Sun-Mountain <nzonnenberg@fearless.tech>
2021-08-02 15:12:59 -0400

    Reset provider org spec test

--- dpc-impl/spec/controllers/provider_orgs_controller_spec.rb
+++ dpc-impl/spec/controllers/provider_orgs_controller_spec.rb
@@ -6,29 +6,4 @@ require './lib/luhnacy_lib/luhnacy_lib'
 RSpec.describe ProviderOrgsController, type: :controller do
   include ApiClientSupport
 
-  describe 'callbacks' do
-    describe '#add_provider_org' do
-      before do
-        # stub default value
-        allow(ENV).to receive(:[]).and_call_original
-      end
-
-      it 'invokes ApiClient and returns the response body' do
-        api_client = stub_api_client(
-          message: :create_provider_org,
-          success: true,
-          response: default_provider_org_response
-        )
-
-        npi = LuhnacyLib.generate_npi
-
-        expect(api_client).to have_received(:create_provider_org)
-          .with(default_provider_org_response)
-          .once
-        binding.pry
-
-        expect().to eq(default_provider_org_response)
-      end
-    end
-  end
 end
