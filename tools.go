5b1fc948b39caade631be7366c72db29e49db46a
Matt Snook <matt.snook@adhocteam.us>
2021-08-03 20:14:54 -0400

    Remove delve dependency from SSAS image

--- tools.go
+++ tools.go
@@ -5,7 +5,6 @@ package main
 import (
 	// bcda/worker/ssas dependencies
 	_ "github.com/BurntSushi/toml"
-	_ "github.com/go-delve/delve/cmd/dlv"
 	_ "github.com/howeyc/fsnotify"
 	_ "github.com/mattn/go-colorable"
 )
