4dc0f089c9dd52f4dd908316c6f09b6bc5a7b278
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2021-08-05 18:56:18 -0400

    DPC-1302: Replicate in implementer portal - Provider Organization Management page (#1388)
    * Set up get request for single provider org
    
    * Refactor to show
    
    * Provider show page
    
    * Repurpose dashboard styling for provider org show page
    
    * provider org dashboard
    
    * Start provider org page view test
    
    * Provider org spec test update
    
    * Update org route
    
    * Get client tokens and public keys api request
    
    * token/keys if else conditional
    
    * View client tokens
    
    * Remove private key
    
    * Client token controller set up
    
    * Client token/public key styling
    
    * Put client token list in partial
    
    * New public key page
    
    * How to create public key page
    
    * New public key form
    
    * Public key form progress
    
    * Public key errors
    
    * Public key manager
    
    * System form
    
    * Set up create_system in public_key_manager
    
    * Remove unnecessary files
    
    * System progress
    
    * Reverting before system
    
    * Update ips to ignore empty
    
    * Update snippet_signature to signature
    
    * Fix show view
    
    * Create public key
    
    * Delete public key button
    
    * Delete public key error
    
    * Delete public key error
    
    * Delete public key
    
    * Client token progress
    
    * Add public key instructions
    
    * Download snippet
    
    * public key intructions
    
    * Add delete token button
    
    * Client token reset
    
    * Change new portal page
    
    * Show token
    
    * Can add label to create client token
    
    * Delete client tokens
    
    * Fix system form workflow
    
    * Show initial token
    
    * Reset provider org spec test
    
    * Test to view all provider orgs
    
    * Move provider org spec to model
    
    * Update gitignore
    
    * Update title of new provider org page
    
    * remove concern
    
    * remove include ApiErrorSimplify on user model
    
    * Update public key upload route
    
    * Fix client token label error
    
    * Client token message update
    
    * Todo for public keys added
    
    * UPdate docsv1 to docsv2

--- dpc-impl/spec/support/api_client_support.rb
+++ dpc-impl/spec/support/api_client_support.rb
@@ -14,17 +14,48 @@ module ApiClientSupport
     {
       'id' => '923a4f7b-eade-494a-8ca4-7a685edacfad',
       'name' => 'Surreal Kayak',
-      'created_at' => 00000,
-      'updated_at' => 00000
+      'created_at' => DateTime.now,
+      'updated_at' => DateTime.now
     }
   end
 
-  def default_provider_org_response
+  def default_add_provider_org_response
     {:status=>"Active",
      :id=>"2e449356-903f-4dbe-9a32-9a45d08c26a9",
      :implementer_id=>"923a4f7b-eade-494a-8ca4-7a685edacfad",
      :organization_id=>"587ed9cf-6bd6-4860-9de3-077277b2c824",
      :created_at=>"2021-07-06T17:46:59.365253Z",
      :updated_at=>"2021-07-06T17:46:59.365253Z"}
+  end
+
+  def default_get_provider_org_response
+    {'id' =>'58bb33bd-8c5a-4ff9-8113-0a8996b9c11e',
+     'identifier' => [{'system' => 'http://hl7.org/fhir/sid/us-npi', 'value' => '3103929527'}],
+     'meta' =>
+      {'id' => 'Organization/58bb33bd-8c5a-4ff9-8113-0a8996b9c11e',
+       'lastUpdated' => '2021-08-02T19:01:28.878+00:00',
+       'versionId' => '0'},
+     'name' => 'Purple Elephant Healthcare',
+     'resourceType' =>'Organization'}
+  end
+
+  def default_provider_orgs_list
+    [
+      {"org_id": "58bb33bd-8c5a-4ff9-8113-0a8996b9c11e",
+       "org_name": "Purple Elephant Healthcare",
+       "status": "Active",
+       "npi": "3103929527",
+       "ssas_system_id": ""},
+      {"org_id": "28f3a00d-d714-44df-8844-7ec792adcd88",
+       "org_name": "Festive Kaftan Healthcare",
+       "status": "Active",
+       "npi": "3092016294",
+       "ssas_system_id": ""},
+      {"org_id": "6c9a5685-7cbe-42f1-9668-f139e67654ea",
+       "org_name": "Gregarious Capitol Healthcare",
+       "status": "Active",
+       "npi": "3718986623",
+       "ssas_system_id": ""}
+    ]
   end
 end
