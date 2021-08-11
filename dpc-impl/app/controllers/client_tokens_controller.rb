926689fce27f79c53fa54106953d99c5b91e3b91
Gavin St. Ours <gst-ours@fearless.tech>
2021-08-11 15:27:11 -0400

    Satisfy the number of arguments for render_error (#1411)

--- dpc-impl/app/controllers/client_tokens_controller.rb
+++ dpc-impl/app/controllers/client_tokens_controller.rb
@@ -20,7 +20,7 @@ class ClientTokensController < ApplicationController
     else
       return render_error('Label required.', @org_id) unless label.present?
 
-      render_error 'Client token could not be created.'
+      render_error('Client token could not be created.', @org_id)
     end
   end
 
@@ -38,7 +38,7 @@ class ClientTokensController < ApplicationController
       flash[:notice] = 'Client token successfully deleted.'
       redirect_to provider_orgs_path(org_id: @org_id)
     else
-      render_error 'Client token could not be deleted.'
+      render_error('Client token could not be deleted.', @org_id)
     end
   end
 
