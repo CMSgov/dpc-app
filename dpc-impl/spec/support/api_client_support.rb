80057efe99309ee9b1c1d76b031a58b904b60407
Sun-Mountain <nzonnenberg@fearless.tech>
2021-08-26 10:36:40 -0400

    Set up files

--- dpc-impl/spec/support/api_client_support.rb
+++ dpc-impl/spec/support/api_client_support.rb
@@ -20,12 +20,16 @@ module ApiClientSupport
   end
 
   def default_add_provider_org_response
-    {:status=>"Active",
-     :id=>"2e449356-903f-4dbe-9a32-9a45d08c26a9",
+    {:id=>"2e449356-903f-4dbe-9a32-9a45d08c26a9",
+     :org_id=>"587ed9cf-6bd6-4860-9de3-077277b2c824",
      :implementer_id=>"923a4f7b-eade-494a-8ca4-7a685edacfad",
-     :organization_id=>"587ed9cf-6bd6-4860-9de3-077277b2c824",
-     :created_at=>"2021-07-06T17:46:59.365253Z",
-     :updated_at=>"2021-07-06T17:46:59.365253Z"}
+     :ssas_system_id=>"5",
+     :status=>"Active",
+     :npi=>"7619918395"}
+  end
+
+  def default_create_system_response
+    
   end
 
   def default_get_provider_org_response
