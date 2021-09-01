39f186c631b95fc5d4137dce826352436afb3edd
Sun-Mountain <nzonnenberg@fearless.tech>
2021-09-01 09:42:32 -0400

    One write ca crt if production

--- dpc-impl/config/initializers/write_ca_cert.rb
+++ dpc-impl/config/initializers/write_ca_cert.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
-File.write('ca.crt', Base64.decode64(ENV.fetch('DPC_CA_CERT')))
+if !(Rails.env.development? || Rails.env.test?)
+  File.write('ca.crt', Base64.decode64(ENV.fetch('DPC_CA_CERT')))
+end
