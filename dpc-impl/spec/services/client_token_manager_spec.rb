9cb13131190cfed01af7958a8d06699073418af2
Sun-Mountain <nzonnenberg@fearless.tech>
2021-09-02 10:30:29 -0400

    Failed create client token test

--- dpc-impl/spec/services/client_token_manager_spec.rb
+++ dpc-impl/spec/services/client_token_manager_spec.rb
@@ -3,15 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe ClientTokenManager do
-  describe '#create_client_token' do
-    before(:each) do
-      @user = create(:user)
-      @imp_id = @user.implementer_id
-      @org_id = "77233b56-555c-4c29-8d29-dc7c615c8c31"
-    end
+  before(:each) do
+    @user = create(:user)
+    @imp_id = @user.implementer_id
+    @org_id = "77233b56-555c-4c29-8d29-dc7c615c8c31"
+  end
 
+  describe '#create_client_token' do
     context 'successful API request' do
-      it 'respons true with @client_token set' do
+      it 'responds true with @client_token set' do
         token = { 'token' => 'exampleToken' }
         api_client = instance_double(ApiClient)
         allow(ApiClient).to receive(:new).and_return(api_client)
@@ -24,6 +24,22 @@ RSpec.describe ClientTokenManager do
         manager = ClientTokenManager.new(imp_id: @imp_id, org_id: @org_id)
         expect(manager.create_client_token(label: 'Test Token 1')).to eq(true)
         expect(manager.client_token).to eq(token)
+      end
+    end
+
+    context 'failed API request' do
+      it 'responds false' do
+        api_client = instance_double(ApiClient)
+        allow(ApiClient).to receive(:new).and_return(api_client)
+        allow(api_client).to receive(:create_client_token)
+          .with(@imp_id, @org_id, label: 'Test Token 1')
+          .and_return(api_client)
+        allow(api_client).to receive(:response_successful?).and_return(false)
+        allow(api_client).to receive(:response_body).and_return(nil)
+
+        manager = ClientTokenManager.new(imp_id: @imp_id, org_id: @org_id)
+        expect(manager.create_client_token(label: 'Test Token 1')).to eq(false)
+        expect(manager.client_token).to eq(nil)
       end
     end
   end
