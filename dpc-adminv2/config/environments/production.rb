e85172707d4dfa825bdf68635a0601d5c9147099
Sun-Mountain <nzonnenberg@fearless.tech>
2021-09-08 11:16:42 -0400

    Health check added

--- dpc-adminv2/config/environments/production.rb
+++ dpc-adminv2/config/environments/production.rb
@@ -16,6 +16,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.cache_store = :redis_cache_store, { url: "#{ENV.fetch('REDIS_URL', 'redis://localhost')}:6379/1" }
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
