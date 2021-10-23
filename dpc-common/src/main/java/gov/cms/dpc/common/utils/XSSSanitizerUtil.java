95033c8012d32962db83225def8934de4de33fa5
Will Huang <52384927+whuang85@users.noreply.github.com>
2021-10-23 17:35:27 -0500

    sanitize potential areas that might not be sanitized

---
+++ dpc-common/src/main/java/gov/cms/dpc/common/utils/XSSSanitizerUtil.java
@@ -0,0 +1,13 @@
+package gov.cms.dpc.common.utils;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.safety.Whitelist;
+
+public class XSSSanitizerUtil {
+
+    public static String sanitize(String unsanitized) {
+        String s1 = unsanitized.replaceAll("(\\s&\\s)", "   ");
+        return Jsoup.clean(s1, "", Whitelist.none(), new Document.OutputSettings().prettyPrint(false));
+    }
+}
