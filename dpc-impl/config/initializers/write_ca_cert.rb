c4910772ea60e43cccfd51705936da9140e55c76
Salvador <43150956+MrMorie@users.noreply.github.com>
2021-12-08 10:33:55 -0800

    Change the way AWS env is looked up in write_ca_cert.rb

--- dpc-impl/config/initializers/write_ca_cert.rb
+++ dpc-impl/config/initializers/write_ca_cert.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-if !(Rails.env.development? || Rails.env.test?)
+if (ENV.fetch('ENV') == "prod-sbx" || ENV.fetch('ENV') == "prod")
   File.write('ca.crt', Base64.decode64(ENV.fetch('DPC_CA_CERT')))
 end
