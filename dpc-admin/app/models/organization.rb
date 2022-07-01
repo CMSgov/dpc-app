6190d72954fe438b491ea30a54f343d4164ab1fd
Jeff Stewart <jeffcodes@duck.com>
2022-07-01 14:23:49 -0400

    DPC-2579: Adjust filter query for un-registered orgs
    This query includes un-registered orgs as well as disabled in the api orgs into the same query for the "not enabled in API" org filter. This fixes the original bug DPC-2351

--- dpc-admin/app/models/organization.rb
+++ dpc-admin/app/models/organization.rb
@@ -37,7 +37,7 @@ class Organization < ApplicationRecord
   }
 
   scope :is_not_registered, -> {
-    where('id IN(SELECT DISTINCT(organization_id) FROM registered_organizations WHERE enabled IS NOT true)')
+    where('id IN(SELECT DISTINCT(o.id) FROM organizations as o LEFT JOIN registered_organizations as ro ON ro.organization_id = o.id WHERE ro.organization_id IS NULL or ro.enabled IS NOT true)')
   }
 
   def address_type
