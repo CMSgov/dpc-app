15fe2b7a390bf807f3259340c4abba596dda83b2
Luke Garceau <luke.garceau@gsa.gov>
2022-08-30 13:57:37 -0400

    Update portal_controller.rb

--- dpc-web/app/controllers/portal_controller.rb
+++ dpc-web/app/controllers/portal_controller.rb
@@ -9,18 +9,25 @@ class PortalController < ApplicationController
       @client_tokens = []
       @public_keys = []
     else
-      @public_keys = if params.key?(:key_page)
-                       Kaminari.paginate_array(@user.primary_organization.reg_org.public_keys).page(params[:key_page]).per(10)
-                     else
-                       Kaminari.paginate_array(@user.primary_organization.reg_org.public_keys).page(0).per(10)
-                     end
-
-      @client_tokens = if params.key?(:token_page)
-                         Kaminari.paginate_array(@user.primary_organization.reg_org.client_tokens).page(params[:token_page]).per(10)
-                       else
-                         Kaminari.paginate_array(@user.primary_organization.reg_org.client_tokens).page(0).per(10)
-                       end
-
+      get_public_keys(params)
+      get_client_tokens(params) 
     end
   end
+
+  def get_public_keys(params)
+    @public_keys = if params.key?(:key_page)
+                     Kaminari.paginate_array(@user.primary_organization.reg_org.public_keys).page(params[:key_page]).per(10)
+                   else
+                     Kaminari.paginate_array(@user.primary_organization.reg_org.public_keys).page(0).per(10)
+                   end
+  end
+
+  def get_client_tokens(params)
+    @client_tokens = if params.key?(:token_page)
+                       Kaminari.paginate_array(@user.primary_organization.reg_org.client_tokens).page(params[:token_page]).per(10)
+                     else
+                       Kaminari.paginate_array(@user.primary_organization.reg_org.client_tokens).page(0).per(10)
+                     end
+  end
+
 end
