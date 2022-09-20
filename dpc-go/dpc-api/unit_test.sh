6cbfb8afa78e213afff134d76947b74349391c25
Tianna <twoodson@fearless.tech>
2022-09-20 11:53:24 -0400

    Add go mod tidy - which we lost when we switched to go install

--- dpc-go/dpc-api/unit_test.sh
+++ dpc-go/dpc-api/unit_test.sh
@@ -11,6 +11,7 @@ mkdir -p test_results/latest
 
 cd src
 echo -e "-------------- DPC-API LINTING STARTED --------------"
+go mod tidy
 golangci-lint --timeout 5m run && echo "*********** DPC-API IS LINT FREE!! ***********" || echo -e "*********** LINTING FAILED!! ***********"
 echo -e "-------------- SECURITY SCAN STARTED --------------"
 gosec -fmt=json -out=../results.json ./...
