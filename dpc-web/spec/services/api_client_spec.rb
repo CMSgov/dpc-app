4721362758dc714ed99a746671d0b18c324dbd5b
Luke Garceau <luke.garceau@gsa.gov>
2022-10-03 09:40:48 -0400

    Update api_client_spec.rb

--- dpc-web/spec/services/api_client_spec.rb
+++ dpc-web/spec/services/api_client_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 require './lib/luhnacy_lib/luhnacy_lib'
 
-RSpec.describe APIClient do
+RSpec.describe ApiClient do
   include OrganizationsHelper
 
   let!(:org) { create(:organization, npi: LuhnacyLib.generate_npi) }
