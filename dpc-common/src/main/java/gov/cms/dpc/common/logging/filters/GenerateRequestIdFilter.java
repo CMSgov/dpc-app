27be9c54cd3e34b52429d1417d24e23c8e7d9963
Jeff Stewart <jeffcodes@duck.com>
2022-11-08 16:31:58 -0500

    DPC-2968 Simplify, throw error to prevent MDC copying to another call.

--- dpc-common/src/main/java/gov/cms/dpc/common/logging/filters/GenerateRequestIdFilter.java
+++ dpc-common/src/main/java/gov/cms/dpc/common/logging/filters/GenerateRequestIdFilter.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
 import javax.annotation.Priority;
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import java.io.IOException;
@@ -27,20 +28,18 @@ public class GenerateRequestIdFilter implements ContainerRequestFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        String requestId = requestContext.getHeaderString(Constants.DPC_REQUEST_ID_HEADER);
-        String resource_requested = XSSSanitizerUtil.sanitize(requestContext.getUriInfo().getPath());
-        String method = requestContext.getMethod();
-        String media_type = requestContext.getMediaType().getType();
         try {
             MDC.clear();
         } catch(Error e) {
-            logger.info("resource_requested={}, method={}, media_type={}, mdc_clear_error={}", resource_requested, method, media_type, e.getMessage());
+            logger.info("mdc_clear_error={}", e.getMessage());
+            throw new InternalServerErrorException("Something went wrong, please try again. If this continues, contact DPC admin.");
         }
+        String requestId = requestContext.getHeaderString(Constants.DPC_REQUEST_ID_HEADER);
         if(requestId!=null && useProvidedRequestId) {
             MDC.put(MDCConstants.DPC_REQUEST_ID, requestId);
         }else{
             MDC.put(MDCConstants.DPC_REQUEST_ID, UUID.randomUUID().toString());
         }
-        logger.info("resource_requested={}, method={}, media_type={}, use_provided_request_id={}", resource_requested, method, media_type, useProvidedRequestId );
+        logger.info("resource_requested={}, method={}", XSSSanitizerUtil.sanitize(requestContext.getUriInfo().getPath()),requestContext.getMethod());
     }
 }
