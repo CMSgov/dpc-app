799c9b4f31e662996f976d8a8ddf3750f1e30022
Luke Garceau <luke.garceau@gsa.gov>
2022-11-08 14:23:16 -0500

    Update client_tokens_controller.rb

--- dpc-web/app/controllers/client_tokens_controller.rb
+++ dpc-web/app/controllers/client_tokens_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class ClientTokensController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :organization_enabled?
   rescue_from ActiveRecord::RecordNotFound, with: :unauthorized
 
