d9b7f46976217a34129ac347d0d537520864c024
Jordan Nasim <jordannasim@jordans-mbp.myfiosgateway.com>
2022-12-16 13:40:22 -0500

    initial checkin

--- dpc-common/src/main/java/gov/cms/dpc/common/Constants.java
+++ dpc-common/src/main/java/gov/cms/dpc/common/Constants.java
@@ -1,13 +1,19 @@
 package gov.cms.dpc.common;
-
 public final class Constants {
     private Constants() {}
-
     public static final String INCLUDE_IDENTIFIERS_HEADER = "IncludeIdentifiers";
     public static final String BULK_CLIENT_ID_HEADER = "BULK-CLIENTID";
     public static final String BULK_JOB_ID_HEADER = "BULK-JOBID";
     public static final String DPC_CLIENT_ID_HEADER = "DPC_CLIENTID";
-    public static final String BFD_ORIGINAL_QUERY_ID_HEADER = "BFD-OriginalQueryId";
-    public static final String DPC_REQUEST_ID_HEADER = "X-Request-Id";
 
+    public static final String BFD_ORIGINAL_QUERY_ID_HEADER = "BlueButton-OriginalQueryId";  //TODO: REMOVE ME
+    public static final String DPC_REQUEST_ID_HEADER = "X-Request-Id";
+    public interface BlueButton {
+        public static final String ORIGINAL_QUERY_ID_HEADER = "BlueButton-OriginalQueryId";
+        public static final String ORIGINAL_QUERY_TIME_STAMP_HEADER = "BlueButton-OriginalQueryTimestamp";
+        public static final String BULK_CLIENTNAME_HEADER = "BULK-CLIENTNAME";
+        public static final String APPLICATION_NAME_HEADER = "BlueButton-Application";
+        public static final String APPLICATION_ID_HEADER = "BlueButton-ApplicationId";
+        public static final String APPLICATION_NAME_DESC = "DPC";
+    }
 }
