95380d1656cd4239710fde4d38bbbab51fc20403
Luke Garceau <luke.garceau@gsa.gov>
2022-12-23 11:21:00 -0500

    Update SplunkTimestamp.java

--- dpc-common/src/main/java/gov/cms/dpc/common/logging/SplunkTimestamp.java
+++ dpc-common/src/main/java/gov/cms/dpc/common/logging/SplunkTimestamp.java
@@ -3,6 +3,6 @@ import java.time.Instant;
 
 public class SplunkTimestamp {    
     public static String getSplunkTimestamp() {
-        return Instant.now().toString().replace("T", " ").substring(0, 22);
+        return Instant.now().toString();
     }
 }
