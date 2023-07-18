581c44086531a13b431cac59144bda95462785b9
Ashley Weaver <ashleyweaver@navapbc.com>
2023-07-18 17:30:53 -0400

    Update Ruby version for dpc-web

--- dpc-web/config/application.rb
+++ dpc-web/config/application.rb
@@ -1,10 +1,6 @@
-require_relative 'boot'
-
-# Enable stout syncing for Docker
-$stdout.sync = true
+require_relative "boot"
 
 require "rails"
-require "luhnacy"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
@@ -12,11 +8,11 @@ require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
-require "sprockets/railtie"
+# require "action_cable/engine"
 # require "rails/test_unit/railtie"
-require "./lib/dpc_middleware/ig_fix"
-require 'macaroons'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,47 +20,18 @@ Bundler.require(*Rails.groups)
 
 module DpcWebsite
   class Application < Rails::Application
-
-    #Dump and read as sql
-    config.active_record.schema_format = :sql
-
-    # Check for STATIC_SITE_URL environment variable
-    ENV['STATIC_SITE_URL'].present? ? ENV['STATIC_SITE_URL'] : ENV['STATIC_SITE_URL'] = 'https://dpc.cms.gov'
-    config.autoload_paths << Rails.root.join('lib')
-    config.autoload_paths << Rails.root.join('lib/luhnacy_lib')
-
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Add fonts to asset pipeline
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    # field_with_errors support, avoid that nasty line break on errors
-    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
-      html_tag
-    }
-
-    # Add middleware to fix issue with /ig links breaking
-    config.middleware.insert_before ActionDispatch::Static, DpcMiddleware::IgFix
-
-    config.active_job.queue_adapter = :sidekiq
-
-    # Sending mail with`DeliveryJob` has been deprecated. Work has been moved to `MailDeliveryJob`
-    config.action_mailer.delivery_job = "ActionMailer::MailDeliveryJob"
-
-    config.to_prepare { Devise::Mailer.layout "mailer" }
-
-    # Mail throttling
-    # Default limit to 5 emails before hard stop
-    config.x.mail_throttle.limit = ENV.fetch('MAIL_THROTTLE_LIMIT', 5)
-    config.x.mail_throttle.expiration = 300 # In seconds
   end
 end
--- dpc-web/config/environments/production.rb
+++ dpc-web/config/environments/production.rb
@@ -1,3 +1,5 @@
+require "active_support/core_ext/integer/time"
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -20,81 +22,42 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(:harmony => true)
+  # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+  config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  # config.asset_host = "http://assets.example.com"
 
   # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
+  # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
+  # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
-  config.ssl_options = { redirect: { exclude: -> request { request.path =~ /health_check/ } } }
+  # config.force_ssl = true
 
-  # Lograge config
-  config.lograge.enabled = true
-
-  # This specifies to log in JSON format
-  config.lograge.formatter = Lograge::Formatters::Json.new
-
-  ## Disables log coloration
-  config.colorize_logging = false
-
-  # Log to a dedicated file
-  config.lograge.logger = ActiveSupport::Logger.new(STDOUT)
-
-  # This is useful if you want to log query parameters
-  config.lograge.custom_options = lambda do |event|
-    { :ddsource => 'ruby',
-      :params => event.payload[:params].reject { |k| %w(controller action).include? k }
-    }
-  end
-
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
-  config.log_level = ENV["LOG_LEVEL"] || :info
+  # Include generic and useful information about system operation, but avoid logging too much
+  # information to avoid inadvertent exposure of personally identifiable information (PII).
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  config.action_controller.perform_caching = true
+  # config.cache_store = :mem_cache_store
 
-  config.cache_store = :redis_cache_store, { url: "#{ENV.fetch('REDIS_URL', 'redis://localhost')}:6379/1" }
-
-  # Use a real queuing backend for Active Job (and separate queues per environment)
+  # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "dpc-website_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "dpc_website_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: ENV['HOST_NAME'], port: ENV['PORT'] }
-  config.action_mailer.asset_host = "https://#{ENV['HOST_NAME']}:#{ENV['PORT']}"
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              ENV['SMTP_ADDRESS'],
-    port:                 ENV['SMTP_PORT'],
-    domain:               ENV['SMTP_DOMAIN'],
-    user_name:            ENV['SMTP_USER_NAME'],
-    password:             ENV['SMTP_PASSWORD'],
-    authentication:       ENV['SMTP_AUTH'],
-    openssl_verify_mode:  ENV['SMTP_SSL_VERIFY'],
-    enable_starttls_auto: true
-  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -102,10 +65,23 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = [I18n.default_locale]
+  config.i18n.fallbacks = true
 
-  # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
+  # Don't log any deprecations.
+  config.active_support.report_deprecations = false
+
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
+
+  # Use a different logger for distributed setups.
+  # require "syslog/logger"
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
