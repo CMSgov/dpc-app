34f13658766dbe66588d09a172866b4546a5ce7d
Ashley Weaver <ashleyweaver@navapbc.com>
2023-07-18 17:19:49 -0400

    Upgrade dpc-admin to Ruby 3.0.6

--- dpc-admin/config/application.rb
+++ dpc-admin/config/application.rb
@@ -1,6 +1,18 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,31 +20,15 @@ Bundler.require(*Rails.groups)
 
 module Admin
   class Application < Rails::Application
-    # Dump and read as sql
-    config.active_record.schema_format = :sql
-
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Add fonts to asset pipeline
-    config.assets.prefix = '/admin/assets'
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    config.active_job.queue_adapter = :sidekiq
-
-    # Sending mail with`DeliveryJob` has been deprecated. Work has been moved to `MailDeliveryJob`
-    config.action_mailer.delivery_job = 'ActionMailer::MailDeliveryJob'
-
-    config.to_prepare { Devise::Mailer.layout 'mailer' }
-
-    # Mail throttling
-    # Default limit to 5 emails before hard stop
-    config.x.mail_throttle.limit = ENV.fetch('MAIL_THROTTLE_LIMIT', 5)
-    config.x.mail_throttle.expiration = 300 # In seconds
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
