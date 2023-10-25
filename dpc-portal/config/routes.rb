bb285d4027bc6cfccd63be416ddec460e7e3e40f
Kevin Yeh <kevinyeah@utexas.edu>
2023-10-25 15:08:05 -0400

    Update for rails deployment

--- dpc-portal/config/routes.rb
+++ dpc-portal/config/routes.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Defines the root path route ("/")
-  root 'main#welcome'
+  scope 'portal' do
+    # Defines the root path route ("/")
+    root 'main#welcome'
 
-  if Rails.env.development?
-    require 'sidekiq/web'
-    mount Sidekiq::Web, at: '/sidekiq'
+    if Rails.env.development?
+      require 'sidekiq/web'
+      mount Sidekiq::Web, at: '/sidekiq'
+    end
   end
 end
