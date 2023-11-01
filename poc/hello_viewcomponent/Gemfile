6dcfbcbdf2b133cf443f90acb4bdf8e97fb39f0a
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2023-11-01 15:48:48 -0400

    Added functional header to viewcomponent PoC

--- poc/hello_viewcomponent/Gemfile
+++ poc/hello_viewcomponent/Gemfile
@@ -43,7 +43,7 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
 
 # Use Sass to process CSS
-# gem "sassc-rails"
+gem "sassc-rails"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- poc/hello_viewcomponent/config/routes.rb
+++ poc/hello_viewcomponent/config/routes.rb
@@ -4,4 +4,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'hello#index'
+
+  # For the purposes of this PoC, set everything to link to the main page
+    get '/home', to: 'hello#index'
+    get '/docs', to: 'hello#index'
+    get '/faq', to: 'hello#index'
+    get '/terms-of-service', to: 'hello#index'
+
+    get '/users/new', to: 'hello#index#new'
+    get '/session/new', to: 'hello#index#new'
 end
