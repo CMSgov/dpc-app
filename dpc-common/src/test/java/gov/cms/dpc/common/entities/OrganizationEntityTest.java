d8f0d0b0ac4645eb7016ab547c0cceb6f7a5048b
Alex Dzeda <alex@e14s.com>
2023-11-03 13:58:59 -0500

    remove old test

--- dpc-common/src/test/java/gov/cms/dpc/common/entities/OrganizationEntityTest.java
+++ dpc-common/src/test/java/gov/cms/dpc/common/entities/OrganizationEntityTest.java
@@ -2,40 +2,16 @@
 package gov.cms.dpc.common.entities;
 
 import org.junit.Test;
-import org.junit.jupiter.api.Disabled;
-import gov.cms.dpc.fhir.converters.AbstractEntityConversionTest;
 import gov.cms.dpc.fhir.DPCIdentifierSystem;
 import org.hl7.fhir.dstu3.model.Identifier;
-import org.hl7.fhir.dstu3.model.Organization;
-import gov.cms.dpc.fhir.converters.FHIRConverter;
-import gov.cms.dpc.fhir.converters.entities.OrganizationEntityConverter;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.Collections;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-public class OrganizationEntityTest extends AbstractEntityConversionTest{
-
-	@Test
-    @Disabled // Disabled until DPC-935
-    void testSimpleRoundTrip() {
-        final OrganizationEntity entity = new OrganizationEntity();
-        entity.setId(UUID.randomUUID());
-        entity.setOrganizationName("Test Organization");
-        entity.setOrganizationID(new OrganizationEntity.OrganizationID(DPCIdentifierSystem.NPPES, "1234"));
-
-        final Organization organization = super.converter.toFHIR(Organization.class, entity);
-        assertEquals(entity, super.converter.fromFHIR(OrganizationEntity.class, organization), "Should be equal");
-    }
-
-
-    @Override
-    protected List<FHIRConverter<?, ?>> registerConverters() {
-        return Collections.singletonList(new OrganizationEntityConverter());
-    }
+public class OrganizationEntityTest {
 
 	@Test
 	public void testGettersAndSetters() {
