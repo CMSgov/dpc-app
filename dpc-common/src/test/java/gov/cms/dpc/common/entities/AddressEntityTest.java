f08d6b29bd32ae515baa08cac8d7279c6e97f3ff
Alex Dzeda <alex@e14s.com>
2023-11-07 11:25:34 -0600

    remove test constants from overall constants

--- dpc-common/src/test/java/gov/cms/dpc/common/entities/AddressEntityTest.java
+++ dpc-common/src/test/java/gov/cms/dpc/common/entities/AddressEntityTest.java
@@ -3,33 +3,38 @@ package gov.cms.dpc.common.entities;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-import gov.cms.dpc.common.Constants;
 import org.hl7.fhir.dstu3.model.Address;
 
 public class AddressEntityTest {
+	private String addressCity = "Galveston";
+    private String addressCountry = "US";
+    private String addressDistrict = "Southern";
+    private String addressZip = "77550";
+    private String addressState = "TX";
+    private String addressLine1 = "416 21st St";
+    private String addressLine2 = "N/A";
 
 	@Test
 	public void testGettersAndSetters() {
 		//Create an object, and set / get the objects in the Entity.
 		AddressEntity address = new AddressEntity();
-
-        address.setCity(Constants.ADDRESS_ENTITY_CITY);
-        address.setCountry(Constants.ADDRESS_ENTITY_COUNTRY);
-        address.setDistrict(Constants.ADDRESS_ENTITY_DISTRICT);
-        address.setPostalCode(Constants.ADDRESS_ENTITY_ZIP);
-        address.setState(Constants.ADDRESS_ENTITY_STATE);
+        address.setCity(addressCity);
+        address.setCountry(addressCountry);
+        address.setDistrict(addressDistrict);
+        address.setPostalCode(addressZip);
+        address.setState(addressState);
         address.setUse(Address.AddressUse.WORK);
         address.setType(Address.AddressType.BOTH);
-		address.setLine1(new String(Constants.ADDRESS_ENTITY_LINE1));
-		address.setLine2(new String(Constants.ADDRESS_ENTITY_LINE2));
+		address.setLine1(addressLine1);
+		address.setLine2(addressLine2);
 
-		assertEquals(Constants.ADDRESS_ENTITY_CITY, address.getCity());
-		assertEquals(Constants.ADDRESS_ENTITY_COUNTRY, address.getCountry());
-		assertEquals(Constants.ADDRESS_ENTITY_DISTRICT, address.getDistrict());
-		assertEquals(Constants.ADDRESS_ENTITY_LINE1, address.getLine1());
-		assertEquals(Constants.ADDRESS_ENTITY_LINE2, address.getLine2());
-		assertEquals(Constants.ADDRESS_ENTITY_ZIP, address.getPostalCode());
-		assertEquals(Constants.ADDRESS_ENTITY_STATE, address.getState());
+		assertEquals(addressCity, address.getCity());
+		assertEquals(addressCountry, address.getCountry());
+		assertEquals(addressDistrict, address.getDistrict());
+		assertEquals(addressLine1, address.getLine1());
+		assertEquals(addressLine2, address.getLine2());
+		assertEquals(addressZip, address.getPostalCode());
+		assertEquals(addressState, address.getState());
 
 	}
 
@@ -37,28 +42,28 @@ public class AddressEntityTest {
 	public void testToFHIR(){
 		AddressEntity address = new AddressEntity();
 
-        address.setCity(Constants.ADDRESS_ENTITY_CITY);
-        address.setCountry(Constants.ADDRESS_ENTITY_COUNTRY);
-        address.setDistrict(Constants.ADDRESS_ENTITY_DISTRICT);
-        address.setPostalCode(Constants.ADDRESS_ENTITY_ZIP);
-        address.setState(Constants.ADDRESS_ENTITY_STATE);
+        address.setCity(addressCity);
+        address.setCountry(addressCountry);
+        address.setDistrict(addressDistrict);
+        address.setPostalCode(addressZip);
+        address.setState(addressState);
         address.setUse(Address.AddressUse.WORK);
         address.setType(Address.AddressType.BOTH);
-		address.setLine1(new String(Constants.ADDRESS_ENTITY_LINE1));
-		address.setLine2(new String(Constants.ADDRESS_ENTITY_LINE2));
+		address.setLine1(addressLine1);
+		address.setLine2(addressLine2);
 
 		Address address2 = address.toFHIR();
 
-		assertEquals(Constants.ADDRESS_ENTITY_CITY, address2.getCity());
-		assertEquals(Constants.ADDRESS_ENTITY_COUNTRY, address2.getCountry());
-		assertEquals(Constants.ADDRESS_ENTITY_DISTRICT, address2.getDistrict());
+		assertEquals(addressCity, address2.getCity());
+		assertEquals(addressCountry, address2.getCountry());
+		assertEquals(addressDistrict, address2.getDistrict());
 		String l1 = address2.getLine().get(0).toString();
 		String l2 = address2.getLine().get(1).toString();
 
-		assertEquals(Constants.ADDRESS_ENTITY_LINE1, l1);
-		assertEquals(Constants.ADDRESS_ENTITY_LINE2, l2);
-		assertEquals(Constants.ADDRESS_ENTITY_ZIP, address2.getPostalCode());
-		assertEquals(Constants.ADDRESS_ENTITY_STATE, address2.getState());
+		assertEquals(addressLine1, l1);
+		assertEquals(addressLine2, l2);
+		assertEquals(addressZip, address2.getPostalCode());
+		assertEquals(addressState, address2.getState());
 
 
 	}
