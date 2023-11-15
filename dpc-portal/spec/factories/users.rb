9703dc47e209f2bc980bd190bca83fa521b94476
Ashley Weaver <ashleyweaver@navapbc.com>
2023-11-15 15:43:21 -0500

    Update users.rb

--- dpc-portal/spec/factories/users.rb
+++ dpc-portal/spec/factories/users.rb
@@ -3,7 +3,5 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "user#{n}@example.com" }
-    sequence(:last_name) { |n| "last_name_#{n}" }
-    sequence(:first_name) { |n| "first_name_#{n}" }
   end
 end
