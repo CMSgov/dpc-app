44face2b009b00b1f6b88b866ce0efd224bfbc0f
Mikaelie Johnson <mikaelie@verdance.co>
2023-11-16 14:37:55 -0700

    Use random uuid and add assert check

--- dpc-api/src/test/java/gov/cms/dpc/api/resources/v1/AdminResourceTest.java
+++ dpc-api/src/test/java/gov/cms/dpc/api/resources/v1/AdminResourceTest.java
@@ -1,15 +1,18 @@
 package gov.cms.dpc.api.resources.v1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.UUID;
 
 import javax.ws.rs.HttpMethod;
 
 import org.apache.http.HttpHeaders;
+import org.eclipse.jetty.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
 import gov.cms.dpc.api.AbstractSecureApplicationTest;
@@ -18,7 +21,9 @@ public class AdminResourceTest extends AbstractSecureApplicationTest{
 
     @Test
     void testGetOrganizations() throws IOException, URISyntaxException {
-        URL url = new URL(getBaseURL() + "admin/organizations?ids=123,345");
+        UUID orgID1 = UUID.randomUUID();
+        UUID orgID2 = UUID.randomUUID();
+        URL url = new URL(getBaseURL() + "admin/organizations?ids="+orgID1.toString() + "," + orgID2.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod(HttpMethod.GET);
         conn.setRequestProperty(HttpHeaders.CONTENT_TYPE, "application/fhir+json");
@@ -27,5 +32,6 @@ public class AdminResourceTest extends AbstractSecureApplicationTest{
         conn.setDoOutput(true);
 
         assertNotNull(conn.getResponseCode());
+        assertEquals(HttpStatus.OK_200, conn.getResponseCode());
     }
 }
