d683657acc9c79b0a89199f9eb06bf52197141d6
Kevin Yeh <kevinyeah@utexas.edu>
2023-11-24 15:06:41 -0500

    fix spec

--- dpc-portal/spec/components/alert_component_spec.rb
+++ dpc-portal/spec/components/alert_component_spec.rb
@@ -5,141 +5,141 @@ RSpec.describe Alert::Component, type: :component do
 
   def render_component
     render_inline(component) do
-      "Check six"
+      'Check six'
     end
   end
 
   info_alert = <<~HTML
-    <div class="usa-alert usa-alert--info usa-alert--slim" >
-      <div class="usa-alert__body">
-        <p class="usa-alert__text">
+    <div class='usa-alert usa-alert--info usa-alert--slim' >
+      <div class='usa-alert__body'>
+        <p class='usa-alert__text'>
           Check six
         </p>
       </div>
     </div>
   HTML
 
-  it "matches the html" do
+  it 'matches the html' do
     render_component
     expect(rendered_content).to match_html_fragment(info_alert)
   end
 
-  it "is an alert" do
+  it 'is an alert' do
     render_component
     expect(page).to have_selector('div.usa-alert')
   end
 
-  it "has a body" do
+  it 'has a body' do
     render_component
     expect(page).to have_selector('.usa-alert div.usa-alert__body')
   end
 
-  it "includes the content" do
+  it 'includes the content' do
     render_component
-    expect(page.find('p.usa-alert__text')).to have_content("Check six")
+    expect(page.find('p.usa-alert__text')).to have_content('Check six')
   end
 
-  describe "heading option" do
-    context "when no heading is given" do
-      subject(:component) { described_class.new heading: "" }
+  describe 'heading option' do
+    context 'when no heading is given' do
+      subject(:component) { described_class.new heading: '' }
 
-      it "is a slim alert" do
+      it 'is a slim alert' do
         render_component
         expect(page).to have_selector('.usa-alert.usa-alert--slim')
       end
 
-      it "does not include a heading" do
+      it 'does not include a heading' do
         render_component
         expect(page).not_to have_selector('.usa-alert__heading')
       end
     end
 
-    context "when a heading is given" do
-      subject(:component) { described_class.new heading: "Look" }
+    context 'when a heading is given' do
+      subject(:component) { described_class.new heading: 'Look' }
 
-      it "includes the heading" do
+      it 'includes the heading' do
         render_component
         expect(page)
           .to have_selector('.usa-alert .usa-alert__body h3.usa-alert__heading')
       end
 
-      it "includes the heading text" do
+      it 'includes the heading text' do
         render_component
-        expect(page.find('.usa-alert__heading')).to have_content("Look")
+        expect(page.find('.usa-alert__heading')).to have_content('Look')
       end
     end
   end
 
-  describe "status option" do
-    context "when no status is given" do
+  describe 'status option' do
+    context 'when no status is given' do
       subject(:component) { described_class.new status: '' }
 
-      it "is an info alert" do
+      it 'is an info alert' do
         render_component
         expect(page).to have_selector('.usa-alert.usa-alert--info')
       end
     end
 
-    context "when the status is info" do
+    context 'when the status is info' do
       subject(:component) { described_class.new status: 'info' }
 
-      it "is an info alert" do
+      it 'is an info alert' do
         render_component
         expect(page).to have_selector('.usa-alert.usa-alert--info')
       end
     end
 
-    context "when the status is warning" do
+    context 'when the status is warning' do
       subject(:component) { described_class.new status: 'warning' }
 
-      it "is an warning alert" do
+      it 'is an warning alert' do
         render_component
         expect(page).to have_selector('.usa-alert.usa-alert--warning')
       end
     end
 
-    context "when the status is error" do
+    context 'when the status is error' do
       subject(:component) { described_class.new status: 'error' }
 
-      it "is an error alert" do
+      it 'is an error alert' do
         render_component
         expect(page).to have_selector('.usa-alert.usa-alert--error')
       end
     end
 
-    context "when the status is success" do
+    context 'when the status is success' do
       subject(:component) { described_class.new status: 'success' }
 
-      it "is an success alert" do
+      it 'is an success alert' do
         render_component
         expect(page).to have_selector('.usa-alert.usa-alert--success')
       end
     end
   end
 
-  describe "icon option" do
-    context "when the icon option is not set" do
+  describe 'icon option' do
+    context 'when the icon option is not set' do
       subject(:component) { described_class.new }
 
-      it "includes an icon" do
+      it 'includes an icon' do
         render_component
         expect(page).not_to have_selector('.usa-alert--no-icon')
       end
     end
 
-    context "when the icon option is true" do
+    context 'when the icon option is true' do
       subject(:component) { described_class.new include_icon: true }
 
-      it "includes an icon" do
+      it 'includes an icon' do
         render_component
         expect(page).not_to have_selector('.usa-alert--no-icon')
       end
     end
 
-    context "when the icon option is false" do
+    context 'when the icon option is false' do
       subject(:component) { described_class.new include_icon: false }
 
-      it "does not include an icon" do
+      it 'does not include an icon' do
         render_component
         expect(page).to have_selector('.usa-alert.usa-alert--no-icon')
       end
