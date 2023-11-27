89492119a33e842a608222cb7ff12c2ac7945b68
Kevin Yeh <kevinyeah@utexas.edu>
2023-11-27 09:34:44 -0500

    Configure test helpers

--- dpc-portal/spec/components/alert_component_spec.rb
+++ dpc-portal/spec/components/alert_component_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'view_component/test_helpers'
 
 RSpec.describe Alert::Component, type: :component do
   subject(:component) { described_class.new }
