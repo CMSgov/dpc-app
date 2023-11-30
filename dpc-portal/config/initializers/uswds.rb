18f57ece7cd03466aa102077c4bd6eaec62b0ce7
Kevin Yeh <kevinyeah@utexas.edu>
2023-11-30 00:23:32 -0500

    Fix asset loading for USWDS

--- dpc-portal/config/initializers/uswds.rb
+++ dpc-portal/config/initializers/uswds.rb
@@ -1,2 +1,6 @@
+# Ensure the USWDS dist folder is compiled as part of assets, since their CSS 
+# relies on specific images and other provided assets for the design system.
+#
+# See also: app/assets/stylesheets/uswds-theme.scss
 Rails.application.config.assets.paths <<
   Rails.root.join('node_modules/@uswds/uswds/dist')
