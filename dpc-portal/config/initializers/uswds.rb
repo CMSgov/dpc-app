fae888f2e5c36bc7f43acc16c40ab718764d969e
Kevin Yeh <kevinyeah@utexas.edu>
2023-11-30 00:33:09 -0500

    Update comment

--- dpc-portal/config/initializers/uswds.rb
+++ dpc-portal/config/initializers/uswds.rb
@@ -1,5 +1,9 @@
-# Ensure the USWDS dist folder is compiled as part of assets, since their CSS 
-# relies on specific images and other provided assets for the design system.
+# Ensure the USWDS dist folder is compiled as part of assets. 
+# 
+# This serves a couple different purposes:
+# - Ensures that images and fonts are exposed properly on the client-side,
+#   since USWDS CSS references specific assets for their design components.
+# - Ensures that the js files are exposed properly for use in the application layout.
 #
 # See also: app/assets/stylesheets/uswds-theme.scss
 Rails.application.config.assets.paths <<
