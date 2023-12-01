576a471ba1941157cd67228011769cf231e6f574
Ashley Weaver <ashleyweaver@navapbc.com>
2023-12-01 15:50:05 -0500

    Create SeedCommandTest.java

---
+++ dpc-attribution/src/test/java/gov/cms/dpc/attribution/cli/SeedCommandTest.java
@@ -0,0 +1,66 @@
+package gov.cms.dpc.attribution.cli;
+
+import gov.cms.dpc.attribution.DPCAttributionConfiguration;
+import gov.cms.dpc.attribution.DPCAttributionService;
+import io.dropwizard.cli.Cli;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.util.JarLocation;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class SeedCommandTest {
+
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    private final ByteArrayOutputStream stdOut = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream stdErr = new ByteArrayOutputStream();
+    private Cli cli;
+
+    SeedCommandTest() {
+        // Not used
+    }
+
+    @BeforeEach
+    void cliSetup() {
+        final JarLocation location = mock(JarLocation.class);
+        when(location.getVersion()).thenReturn(Optional.of("1.0.0"));
+
+        final Bootstrap<DPCAttributionConfiguration> bootstrap = new Bootstrap<>(new DPCAttributionService());
+        bootstrap.addCommand(new SeedCommand(bootstrap.getApplication()));
+
+        // Redirect stdout and stderr to our byte streams
+        System.setOut(new PrintStream(stdOut));
+        System.setErr(new PrintStream(stdErr));
+
+        cli = new Cli(location, bootstrap, stdOut, stdErr);
+    }
+
+    @AfterEach
+    void teardown() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    void testSeedCommand() {
+        final String timestamp = "2020-01-01T12:00:00+03:00";
+        final LocalDateTime offsetDateTime = OffsetDateTime.parse(timestamp).toLocalDateTime();
+        final Optional<Throwable> success = cli.run("seed", "-t", timestamp);
+        assertAll(() -> assertTrue(success.isEmpty(), "Should have succeeded"),
+                () -> assertEquals("", stdErr.toString(), "Should not have errors"),
+                () -> assertTrue(stdOut.toString().contains(("Seeding attribution at time " + offsetDateTime))),
+                () -> assertTrue(stdOut.toString().contains("Finished loading seeds")));
+    }
+}
