e84979715df7770ced71af6c18437778daf795f6
Ashley Weaver <ashleyweaver@navapbc.com>
2023-12-04 12:30:01 -0500

    Try setting application

--- dpc-attribution/src/test/java/gov/cms/dpc/attribution/cli/SeedCommandTest.java
+++ dpc-attribution/src/test/java/gov/cms/dpc/attribution/cli/SeedCommandTest.java
@@ -2,8 +2,10 @@ package gov.cms.dpc.attribution.cli;
 
 import gov.cms.dpc.attribution.DPCAttributionConfiguration;
 import gov.cms.dpc.attribution.DPCAttributionService;
+import io.dropwizard.Application;
 import io.dropwizard.cli.Cli;
 import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.testing.DropwizardTestSupport;
 import io.dropwizard.util.JarLocation;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,17 +28,22 @@ public class SeedCommandTest {
     private final ByteArrayOutputStream stdErr = new ByteArrayOutputStream();
     private Cli cli;
 
+    private final DropwizardTestSupport<DPCAttributionConfiguration> APPLICATION =
+            new DropwizardTestSupport<>(DPCAttributionService.class, "ci.application.conf");
+
     SeedCommandTest() {
         // Not used
     }
 
     @BeforeEach
-    void cliSetup() {
+    void cliSetup() throws Exception {
         final JarLocation location = mock(JarLocation.class);
         when(location.getVersion()).thenReturn(Optional.of("1.0.0"));
 
-        final Bootstrap<DPCAttributionConfiguration> bootstrap = new Bootstrap<>(new DPCAttributionService());
-        bootstrap.addCommand(new SeedCommand(bootstrap.getApplication()));
+        APPLICATION.before();
+        final Application<DPCAttributionConfiguration> application = APPLICATION.getApplication();
+        final Bootstrap<DPCAttributionConfiguration> bootstrap = new Bootstrap<>(application);
+        bootstrap.addCommand(new SeedCommand(application));
 
         // Redirect stdout and stderr to our byte streams
         System.setOut(new PrintStream(stdOut));
@@ -47,13 +54,14 @@ public class SeedCommandTest {
 
     @AfterEach
     void teardown() {
+        APPLICATION.after();
         System.setOut(originalOut);
         System.setErr(originalErr);
     }
 
     @Test
     void testSeedCommand() {
-        final Optional<Throwable> success = cli.run("seed", "ci.application.conf");
+        final Optional<Throwable> success = cli.run("seed", "application.conf");
         assertAll(() -> assertTrue(success.isEmpty(), "Should have succeeded"),
                 () -> assertEquals("", stdErr.toString(), "Should not have errors"),
                 () -> assertTrue(stdOut.toString().contains("Seeding attribution at time ")),
