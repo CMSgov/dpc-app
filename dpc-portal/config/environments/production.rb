45d05c17cd719d919416187904c755649c6f7b8c
Kevin Yeh <kevinyeah@utexas.edu>
2023-12-04 16:49:43 -0500

    match other rails apps

--- dpc-portal/config/environments/production.rb
+++ dpc-portal/config/environments/production.rb
@@ -23,9 +23,11 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # TODO: 
+  # We should disable serving static files from the `/public` folder and
+  # configure a production-ready static file server separate from Rails.
+  # This is recommended to improve static file download performance.
+  config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
