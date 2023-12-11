b3c37edc46bf3781030d7bf25b1d744f3308a65f
Mikaelie Johnson <mikaelie@verdance.co>
2023-12-11 10:18:00 -0700

    Test npi is getting set

--- dpc-attribution/src/test/java/gov/cms/dpc/attribution/resources/OrganizationResourceTest.java
+++ dpc-attribution/src/test/java/gov/cms/dpc/attribution/resources/OrganizationResourceTest.java
@@ -196,13 +196,14 @@ class OrganizationResourceTest extends AbstractAttributionTest {
     void testGetOrganizationsByIds() {
         List<String> ids = new ArrayList<String>();
         final IGenericClient client = AttributionTestHelpers.createFHIRClient(ctx, getServerURL());
-        Organization organization1 = OrganizationHelpers.createOrganization(ctx, AttributionTestHelpers.createFHIRClient(ctx, getServerURL()), "1633101112", true);
+        Organization organization1 = OrganizationHelpers.createOrganization(ctx, AttributionTestHelpers.createFHIRClient(ctx, getServerURL()), "1633101112", false);
         Organization organization2 = OrganizationHelpers.createOrganization(ctx, AttributionTestHelpers.createFHIRClient(ctx, getServerURL()), "1235567892", false);
         ids.add(organization1.getIdentifierFirstRep().getId());
         ids.add(organization2.getIdentifierFirstRep().getId());
 
         Map<String, List<String>> searchParams = new HashMap<>();
         System.out.println("org1: "+organization1.getIdentifierFirstRep().getId());
+        System.out.println("org2: "+organization2.getIdentifierFirstRep().getId());
         searchParams.put("identifier", Collections.singletonList("id|"+organization1.getIdentifierFirstRep().getId()+","+organization2.getIdentifierFirstRep().getId()));
         final Bundle organizations = client
                 .search()
