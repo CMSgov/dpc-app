63ece096ba99fab0eda81620199e7f0dc0b3ae68
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2023-12-11 15:40:24 -0500

    Org card

--- dpc-portal/app/components/core/card/organization_card_component.rb
+++ dpc-portal/app/components/core/card/organization_card_component.rb
@@ -3,6 +3,13 @@
 module Core
     module Card
         class Core::Card::OrganizationCardComponent < ViewComponent::Base
+            def initialize(name:, npi:, status:, status_color:)
+                super
+                @name = name
+                @npi = npi
+                @status = status
+                @status_color = status_color
+              end
         end
     end
 end
--- dpc-portal/app/components/core/card/organization_card_component_preview.rb
+++ dpc-portal/app/components/core/card/organization_card_component_preview.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
-class Core::Card::OrganizationCardComponentPreview < ViewComponent::Preview
-  def default
-    render(Core::Card::OrganizationCardComponent.new)
-  end
+module Core
+    module Card
+        # Organization Card Component
+        # ----------------
+        #
+        # [See at USWDS](https://designsystem.digital.gov/components/card/)
+        #
+        class OrganizationCardComponentPreview < ViewComponent::Preview
+            # @param name "Name of the organization"
+            # @param npi "NPI of the organization"
+            def good_status(name: "Org Name", npi: "npi_123_abc")
+                render(Core::Card::OrganizationCardComponent.new(name: name, npi: npi, status: 'Good', status_color: 'LightSkyBlue'))
+            end
+
+            # @param name "Name of the organization"
+            # @param npi "NPI of the organization"
+            def bad_status(name: "Org Name", npi: "npi_123_abc")
+                render(Core::Card::OrganizationCardComponent.new(name: name, npi: npi, status: 'Bad', status_color: 'Red'))
+            end
+        end
+    end
 end
