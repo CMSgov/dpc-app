d77e7d14bcae140736bcef48114de434970e7d66
Christopher Ronning <127991809+chris-ronning-ny@users.noreply.github.com>
2023-12-14 09:51:26 -0500

    [DPC-3684] CPI API Gateway requests (#1993)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-3684
    
    ## 🛠 Changes
    
    This creates the three API requests that are called for in the JIRA
    ticket. It also sets up the Dockerfile for local development, and
    imports some CA certs that are required.
    
    ## ℹ️ Context for reviewers
    
    (Background context, more in-depth details of the implementation, and
    anything else you'd like to call out or ask reviewers.)
    
    ## ✅ Acceptance Validation
    
    (How were the changes verified? Did you fully test the acceptance
    criteria in the ticket? Provide reproducible testing instructions and
    screenshots if applicable.)
    
    ## 🔒 Security Implications
    
    - [ ] This PR adds a new software dependency or dependencies.
    - [ ] This PR modifies or invalidates one or more of our security
    controls.
    - [ ] This PR stores or transmits data that was not stored or
    transmitted before.
    - [ ] This PR requires additional review of its security implications
    for other reasons.
    
    If any security implications apply, add Jason Ashbaugh (GitHub username:
    StewGoin) as a reviewer and do not merge this PR without his approval.

---
+++ dpc-portal/app/services/cpi_api_gateway_client.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require 'oauth2'
+
+# A client for requests to the CPI API Gateway
+class CpiApiGatewayClient
+  attr_accessor :access
+
+  # rubocop:disable Metrics/MethodLength
+  def initialize
+    env = ENV.fetch('ENV', nil)
+    client_id = ENV.fetch('CPI_API_GW_CLIENT_ID', nil)
+    client_secret = ENV.fetch('CPI_API_GW_CLIENT_SECRET', nil)
+    cms_idm_url = ENV.fetch('CMS_IDM_OAUTH_URL', nil)
+    @cpi_api_gateway_url = ENV.fetch('CPI_API_GW_BASE_URL', nil)
+    @cpi_api_gateway_url += '/' unless @cpi_api_gateway_url.end_with?('/')
+    @client = OAuth2::Client.new(client_id, client_secret,
+                                 site: cms_idm_url,
+                                 token_url: '/oauth2/aus2151jb0hszrbLU297/v1/token',
+                                 ssl: {
+                                   verify: env != 'local'
+                                 })
+    fetch_token
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  # fetch data about an organization, including enrollment_id
+  def fetch_enrollment(npi)
+    body = { providerID: { npi: npi.to_s } }.to_json
+    response = request_client.post("#{@cpi_api_gateway_url}api/1.0/ppr/providers/enrollments",
+                                   headers: { 'Content-Type': 'application/json' },
+                                   body: body)
+    response.parsed
+  end
+
+  # fetch a list of roles, roughly corresponding to associated individuals
+  def fetch_enrollment_roles(enrollment_id)
+    response = request_client.get("#{@cpi_api_gateway_url}api/1.0/ppr/providers/enrollments/#{enrollment_id}/roles",
+                                  headers: { 'Content-Type': 'application/json' })
+    response.parsed
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  # fetch info about the authorized official, including a list of med sanctions
+  def fetch_authorized_official_med_sanctions(ssn)
+    body = {
+      providerID: {
+        providerType: 'ind',
+        identity: {
+          idType: 'ssn',
+          id: ssn.to_s
+        }
+      },
+      dataSets: {
+        subjectAreas: {
+          medSanctions: true
+        }
+      }
+    }.to_json
+    response = request_client.post("#{@cpi_api_gateway_url}api/1.0/ppr/providers",
+                                   headers: { 'Content-Type': 'application/json' },
+                                   body: body)
+    response.parsed
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  private
+
+  def fetch_token
+    @access = @client.client_credentials.get_token(scope: 'READ')
+  end
+
+  def request_client
+    fetch_token if @access.nil? || @access.expired?
+    @access
+  end
+end
