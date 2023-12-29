c4f54ef606ff50ebbacc9537ff888a24e8afc059
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2023-12-29 11:10:17 -0500

    Switched to mock server based tests

--- dpc-api/src/test/java/gov/cms/dpc/api/cli/keys/KeyListUnitTest.java
+++ dpc-api/src/test/java/gov/cms/dpc/api/cli/keys/KeyListUnitTest.java
@@ -1,25 +1,28 @@
 package gov.cms.dpc.api.cli.keys;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.cms.dpc.api.APITestHelpers;
 import gov.cms.dpc.api.DPCAPIConfiguration;
 import gov.cms.dpc.api.DPCAPIService;
 import gov.cms.dpc.api.entities.PublicKeyEntity;
 import gov.cms.dpc.api.models.CollectionResponse;
+import gov.cms.dpc.testing.APIAuthHelpers;
+import gov.cms.dpc.testing.NoExitSecurityManager;
 import io.dropwizard.cli.Cli;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.util.JarLocation;
 import org.apache.http.HttpStatus;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import gov.cms.dpc.testing.NoExitSecurityManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Answers;
-import org.mockito.MockedStatic;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.Parameter;
 
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -31,46 +34,39 @@ import static org.mockito.Mockito.*;
 class KeyListUnitTest {
     private final PrintStream originalOut = System.out;
     private final PrintStream originalErr = System.err;
-    private final InputStream originalIn = System.in;
 
     private final ByteArrayOutputStream stdOut = new ByteArrayOutputStream();
     private final ByteArrayOutputStream stdErr = new ByteArrayOutputStream();
     private Cli cli;
+    private ClientAndServer mockServer;
+    private URI taskUri;
 
     @BeforeEach
-    void setUp() throws Exception {
-        // Setup necessary mock
-        final JarLocation location = mock(JarLocation.class);
-        when(location.getVersion()).thenReturn(Optional.of("1.0.0"));
+    void setUp() throws URISyntaxException {
+        // Redirect stdout and stderr to our byte streams
+        System.setOut(new PrintStream(stdOut));
+        System.setErr(new PrintStream(stdErr));
 
         // Add commands you want to test
         final Bootstrap<DPCAPIConfiguration> bootstrap = new Bootstrap<>(new DPCAPIService());
         bootstrap.addCommand(new KeyList());
 
-        // Redirect stdout and stderr to our byte streams
-        System.setOut(new PrintStream(stdOut));
-        System.setErr(new PrintStream(stdErr));
+        cli = new Cli(mock(JarLocation.class), bootstrap, stdOut, stdErr);
 
-        // Build what'll run the command and interpret arguments
-        cli = new Cli(location, bootstrap, stdOut, stdErr);
+        taskUri = new URI(APIAuthHelpers.TASK_URL);
+        mockServer = ClientAndServer.startClientAndServer(taskUri.getPort());
     }
 
     @AfterEach
     void teardown() {
+        mockServer.stop();
+
         System.setOut(originalOut);
         System.setErr(originalErr);
-        System.setIn(originalIn);
     }
 
     @Test
     public void testListKeys_happyPath() throws IOException {
-        // Build a mocked http client that returns a successful key list response
-        CloseableHttpClient mockClient = mock(CloseableHttpClient.class);
-        CloseableHttpResponse mockResponse = mock(CloseableHttpResponse.class, Answers.RETURNS_DEEP_STUBS);
-
-        when(mockClient.execute(any())).thenReturn(mockResponse);
-        when(mockResponse.getStatusLine().getStatusCode()).thenReturn(HttpStatus.SC_OK);
-
         PublicKeyEntity publicKeyEntity = new PublicKeyEntity();
         publicKeyEntity.setId(UUID.randomUUID());
         publicKeyEntity.setLabel("test public key");
@@ -78,48 +74,53 @@ class KeyListUnitTest {
         CollectionResponse collectionResponse = new CollectionResponse(List.of(publicKeyEntity));
 
         ObjectMapper mapper = new ObjectMapper();
-        InputStream inputStream = new ByteArrayInputStream(mapper.writeValueAsBytes(collectionResponse));
+        String payload = mapper.writeValueAsString(collectionResponse);
 
-        when(mockResponse.getEntity().getContent()).thenReturn(inputStream);
+        new MockServerClient(taskUri.getHost(), taskUri.getPort())
+            .when(
+                HttpRequest.request()
+                    .withMethod("POST")
+                    .withPath(taskUri.getPath() + "list-keys")
+                    .withQueryStringParameters(List.of(Parameter.param("organization", "org_id")))
+            )
+            .respond(
+                org.mockserver.model.HttpResponse.response()
+                    .withStatusCode(HttpStatus.SC_OK)
+                    .withBody(payload)
+            );
 
-        // Sub in our mocked http client and run the command
-        try(MockedStatic<HttpClients> httpClients = mockStatic(HttpClients.class)) {
-            httpClients.when(HttpClients::createDefault).thenReturn(mockClient);
-
-            Optional<Throwable> errors = cli.run("list", "org_id");
-            assertTrue(errors.isEmpty());
-        }
+        Optional<Throwable> errors = cli.run("list", "org_id");
+        assertTrue(errors.isEmpty());
 
         String results = stdOut.toString();
         assertTrue(results.contains("│ test public key │"));
-        assertEquals("", stdErr.toString());
     }
 
     @Test
     public void testListKeys_badResponse() throws IOException {
-        // Build a mocked http client that returns a successful key list response
-        CloseableHttpClient mockClient = mock(CloseableHttpClient.class);
-        CloseableHttpResponse mockResponse = mock(CloseableHttpResponse.class, Answers.RETURNS_DEEP_STUBS);
-
-        when(mockClient.execute(any())).thenReturn(mockResponse);
-        when(mockResponse.getStatusLine().getStatusCode()).thenReturn(HttpStatus.SC_BAD_REQUEST);
+        new MockServerClient(taskUri.getHost(), taskUri.getPort())
+            .when(
+                HttpRequest.request()
+                    .withMethod("POST")
+                    .withPath(taskUri.getPath() + "list-keys")
+                    .withQueryStringParameters(List.of(Parameter.param("organization", "org_id")))
+            )
+            .respond(
+                org.mockserver.model.HttpResponse.response()
+                    .withStatusCode(HttpStatus.SC_BAD_REQUEST)
+            );
 
         // This is kind of kludgey and isn't guaranteed to work for all versions of Java, but it allows us to test error
         // cases that call System.exit()
         SecurityManager originalSecurityManager = System.getSecurityManager();
         System.setSecurityManager(new NoExitSecurityManager());
 
-        // Sub in our mocked http client and run the command
-        try(MockedStatic<HttpClients> httpClients = mockStatic(HttpClients.class)) {
-            httpClients.when(HttpClients::createDefault).thenReturn(mockClient);
+        Optional<Throwable> errors = cli.run("list", "org_id");
+        assertFalse(errors.isEmpty());
 
-            Optional<Throwable> errors = cli.run("list", "org_id");
-            assertFalse(errors.isEmpty());
-
-            Throwable throwable = errors.get();
-            assertInstanceOf(RuntimeException.class, throwable);
-            assertEquals("1", throwable.getMessage());
-        }
+        Throwable throwable = errors.get();
+        assertInstanceOf(RuntimeException.class, throwable);
+        assertEquals("1", throwable.getMessage());
 
         System.setSecurityManager(originalSecurityManager);
         assertFalse(stdErr.toString().isEmpty());
