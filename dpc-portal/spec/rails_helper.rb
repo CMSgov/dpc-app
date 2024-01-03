391653e068f9f7e9a847d5cd545d23cb19d73bc4
Ashley Weaver <ashleyweaver@navapbc.com>
2024-01-03 15:21:01 -0500

    Clean up

--- dpc-portal/spec/rails_helper.rb
+++ dpc-portal/spec/rails_helper.rb
@@ -66,6 +66,5 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ViewComponent::TestHelpers, type: :component
 end
