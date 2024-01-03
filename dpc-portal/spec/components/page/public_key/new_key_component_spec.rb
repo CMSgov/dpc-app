8ec404543d6a7d79908bac098c20727e89a8ab7c
Ashley Weaver <ashleyweaver@navapbc.com>
2024-01-03 14:52:41 -0500

    Fix expected HTML

--- dpc-portal/spec/components/page/public_key/new_key_component_spec.rb
+++ dpc-portal/spec/components/page/public_key/new_key_component_spec.rb
@@ -18,28 +18,29 @@ RSpec.describe Page::PublicKey::NewKeyComponent, type: :component do
         <div>
           <div class="margin-bottom-5">← <a href="/portal/organizations/#{org.path_id}">#{org.name}</a></div>
           <h1>Add Public Key</h1>
-          <div>
-            <h2>New Public Key for #{org.name}</h2>
-            <hr/>
-            <form action="/portal/organizations/#{org.path_id}/public_keys" accept-charset="UTF-8" method="post">
-              <div class="margin-bottom-4">
-                <label class="usa-label" for="label">Label</label>
-                <p class="usa-hint">Choose a descriptive name to make your key easily identifiable to you.</p>
-                <input type="text" name="label" id="label" max_length="25" class="usa-input" />
-              </div>
-              <div class="margin-bottom-4">
-                <label class="usa-public-key" for="public-key">Public Key</label>
-                <p class="usa-hint">Must include the "BEGIN PUBLIC KEY" and "END PUBLIC KEY" tags from your public.pem file.</p>
-                <input type="textarea" name="public-key" id="public-key" class="usa-public-key" />
-              </div>
-              <div class="margin-bottom-4">
-                <label class="usa-signature-snippet" for="signature-snippet">Signature Snippet</label>
-                <p class="usa-hint">Must yield "Verified Ok" results in order to generate the signature.sig file.</p>
-                <input type="textarea" name="signature-snippet" id="signature-snippet" class="usa-signature-snippet" />
-              </div>
-              <input type="submit" name="commit" value="Create key" class="usa-button" data-test="form-submit" data-disable-with="Create key" />
-            </form>
-          </div>
+          <section class="box">
+            <div>
+              <h2>New Public Key for #{org.name}</h2>
+              <form action="/portal/organizations/#{org.path_id}/public_keys" accept-charset="UTF-8" method="post">
+                <div class="margin-bottom-4">
+                  <label class="usa-label" for="label">Label</label>
+                  <p class="usa-hint">Choose a descriptive name to make your key easily identifiable to you.</p>
+                  <input type="text" name="label" id="label" max_length="25" class="usa-input">
+                </div>
+                <div class="margin-bottom-4">
+                  <label class="usa-label" for="public_key">Public Key</label>
+                  <p class="usa-hint">Must include the "BEGIN PUBLIC KEY" and "END PUBLIC KEY" tags from your public.pem file.</p>
+                  <textarea name="public_key" id="public_key" class="usa-textarea"></textarea>
+                </div>
+                <div class="margin-bottom-4">
+                  <label class="usa-label" for="snippet_signature">Signature Snippet</label>
+                  <p class="usa-hint">Must yield "Verified Ok" results in order to generate the signature.sig file.</p>
+                  <textarea name="snippet_signature" id="snippet_signature" class="usa-textarea"></textarea>
+                </div>
+                <input type="submit" name="commit" value="Add key" class="usa-button" data-test="form:submit" data-disable-with="Add key">
+              </form>
+            </div>
+          </section>
         </div>
       HTML
     end
