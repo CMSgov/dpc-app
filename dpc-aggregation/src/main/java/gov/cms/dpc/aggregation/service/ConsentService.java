2e6fb2428faf835d30ca916e82e1bc8aa96ac3eb
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2024-01-18 15:20:50 -0500

    ConsentService can now take a List of mbis

--- dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/service/ConsentService.java
+++ dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/service/ConsentService.java
@@ -5,4 +5,5 @@ import java.util.Optional;
 
 public interface ConsentService {
     Optional<List<ConsentResult>> getConsent(String mbi);
+    Optional<List<ConsentResult>> getConsent(List<String> mbis);
 }
