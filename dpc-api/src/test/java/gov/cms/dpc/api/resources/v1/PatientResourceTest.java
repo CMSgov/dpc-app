ffd8a38b9b075a7f2b8b2416dc8981745d4354ab
Ashley Weaver <ashleyweaver@navapbc.com>
2024-02-13 08:33:55 -0500

    Debug

--- dpc-api/src/test/java/gov/cms/dpc/api/resources/v1/PatientResourceTest.java
+++ dpc-api/src/test/java/gov/cms/dpc/api/resources/v1/PatientResourceTest.java
@@ -493,6 +493,8 @@ class PatientResourceTest extends AbstractSecureApplicationTest {
                 .withAdditionalHeader("X-Provenance", generateProvenance(ORGANIZATION_ID, practitioner.getId()));
 
         InternalErrorException exception = assertThrows(InternalErrorException.class, getEverythingOperation::execute, "Expected Internal server error when retrieving opted out patient.");
+        System.out.println("EVERYTHING RESPONSE BODY");
+        System.out.println(exception.getResponseBody());
         assertTrue(exception.getResponseBody().contains("\"text\":\"Data not available for opted out patient\""), "Incorrect or missing operation outcome in response body.");
     }
 
