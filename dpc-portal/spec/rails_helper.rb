3e70c83621c69a97f8026a13c67442829e7b125e
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2024-02-21 09:53:45 -0500

    DPC-3858: Persist user on login (#2040)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-3858
    ## 🛠 Changes
    
    Actually saving a user after logging in via login-dot-gov.
    
    ## ℹ️ Context for reviewers
    
    Devise needs the User to have a password, even if it is never used, so
    we set it using a Devise convenience method.
    
    ## ✅ Acceptance Validation
    
    Manual testing shows user created in db

--- dpc-portal/spec/rails_helper.rb
+++ dpc-portal/spec/rails_helper.rb
@@ -35,6 +35,8 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{Rails.root}/spec/fixtures"
 
