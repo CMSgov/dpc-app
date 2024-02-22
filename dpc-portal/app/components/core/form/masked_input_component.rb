a4397fc4a6c502ebc07be7a57354b34eb08a6c77
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2024-02-22 14:56:23 -0500

    DPC-3872: AO invites CD (#2038)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-3872
    
    ## 🛠 Changes
    
    - Updated TextInputComponent to show errors
    - Added MaskedInputComponent to support Phone Mask (must be child of
    form tag)
    - Added a ModalComponent for use by NewInvitationComponent and
    RowComponent
    - Added components for credential delegate invitation page and success
    page
    - Added mock CD Invitation until we have a db schema to handle
    validations
    - Added CredentialDelegateInvitation controller (with authentication
    required)
    
    ## ℹ️ Context for reviewers
    
    Just getting the views working. No actual logic beyond validations.
    
    ## ✅ Acceptance Validation
    
    Matches screenshots

--- dpc-portal/app/components/core/form/masked_input_component.rb
+++ dpc-portal/app/components/core/form/masked_input_component.rb
@@ -2,22 +2,26 @@
 
 module Core
   module Form
-    # Renders <input type="text"...
-    class TextInputComponent < ViewComponent::Base
+    # Renders masked input fields
+    class MaskedInputComponent < ViewComponent::Base
       attr_accessor :label, :attribute, :default, :input_options, :hint, :error_msg
 
       # rubocop:disable Metrics/ParameterLists
-      def initialize(label:, attribute:, default: '', input_options: {}, hint: '', error_msg: '')
+      def initialize(label:, attribute:, mask:, default: '', hint: '', error_msg: '')
         super
         @label = label
         @attribute = attribute
-        @input_options = input_options
+        @input_options = { class: %w[usa-input usa-masked] }
         @hint = hint
         @error_msg = error_msg
         @default = default
-        @input_options[:class] ||= []
-        @input_options[:class] << 'usa-input' unless @input_options[:class].include?('usa-input')
         @input_options[:class] << 'usa-input--error' if error_msg.present?
+        case mask
+        when 'us-phone'
+          @input_options[:placeholder] = '___-___-____'
+          @input_options[:pattern] = '\d{3}-\d{3}-\d{4}'
+          @input_options['aria-describedby'] = 'telHint'
+        end
       end
       # rubocop:enable Metrics/ParameterLists
     end
