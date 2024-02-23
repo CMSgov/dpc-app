6df820bc5a2d044c2cfea78de52d00e7c59b47f8
Christopher Ronning <cronning90@gmail.com>
2024-02-23 10:38:35 -0500

    update synthetic test data

--- lambda/opt-out-import/populate_s3.sh
+++ lambda/opt-out-import/populate_s3.sh
@@ -2,6 +2,5 @@
 aws --endpoint-url=http://localhost:4566 s3 mb s3://demo-bucket
 aws --endpoint-url=http://localhost:4566 s3api put-bucket-acl --bucket demo-bucket --acl public-read
 aws --endpoint-url=http://localhost:4566 s3api put-bucket-acl --bucket demo-bucket --acl public-write
-aws --endpoint-url=http://localhost:4566 s3api put-object --bucket demo-bucket --key T#EFT.ON.ACO.NGD1800.DPRF.D181120.T1000009 --body=synthetic_test_data/T#EFT.ON.ACO.NGD1800.DPRF.D181120.T1000009
+aws --endpoint-url=http://localhost:4566 s3api put-object --bucket demo-bucket --key P#EFT.ON.DPC.NGD.RSP.D240123.T1122001 --body=synthetic_test_data/P#EFT.ON.DPC.NGD.RSP.D240123.T1122001
 aws --endpoint-url=http://localhost:4566 s3api put-object --bucket demo-bucket --key dummyfile.txt --body=dummyfile.txt
-
