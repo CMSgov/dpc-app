f09f61605b682477c2117aefcf1c71e284666ceb
Miki543 <61986745+Miki543@users.noreply.github.com>
2024-02-28 11:04:28 -0700

    DPC-3862: Add landing page for AO (#2046)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-3862
    
    ## 🛠 Changes
    
    - AO landing component, html, and preview
    - Prose component
    - update to button component to add additional input
    - update to organization card component to match new button look
    
    ## ℹ️ Context for reviewers
    
    (Background context, more in-depth details of the implementation, and
    anything else you'd like to call out or ask reviewers.)
    
    ## ✅ Acceptance Validation
    
    <img width="1009" alt="image"
    src="https://github.com/CMSgov/dpc-app/assets/61986745/04bcb894-28e7-4bf0-ac25-5174144d4c2e">
    
    <img width="1009" alt="Screenshot 2024-02-27 at 3 54 47 PM"
    src="https://github.com/CMSgov/dpc-app/assets/61986745/0c4887d4-5eb8-4893-88d2-1b2296e57c9b">
    
    
    ## 🔒 Security Implications
    
    - [ ] This PR adds a new software dependency or dependencies.
    - [ ] This PR modifies or invalidates one or more of our security
    controls.
    - [ ] This PR stores or transmits data that was not stored or
    transmitted before.
    - [ ] This PR requires additional review of its security implications
    for other reasons.
    
    If any security implications apply, add Jason Ashbaugh (GitHub username:
    StewGoin) as a reviewer and do not merge this PR without his approval.

--- dpc-portal/app/components/core/button/button_component.rb
+++ dpc-portal/app/components/core/button/button_component.rb
@@ -4,10 +4,19 @@ module Core
   module Button
     # Render a USWDS-styled button.
     class ButtonComponent < ViewComponent::Base
-      def initialize(label:, destination:)
+      attr_accessor :label, :destination, :additional_classes
+
+      def initialize(label:, destination:, additional_classes: nil)
         super
         @label = label
         @destination = destination
+        @additional_classes = additional_classes
+      end
+
+      def button_classes
+        classes = @additional_classes || []
+        classes << 'usa-button'
+        classes.uniq.join(' ')
       end
     end
   end
--- dpc-portal/spec/components/core/button/button_component_spec.rb
+++ dpc-portal/spec/components/core/button/button_component_spec.rb
@@ -4,22 +4,46 @@ require 'rails_helper'
 
 RSpec.describe Core::Button::ButtonComponent, type: :component do
   describe 'html' do
-    subject(:html) do
-      render_inline(component)
-      rendered_content
+    context 'with default button class' do
+      subject(:html) do
+        render_inline(component)
+        rendered_content
+      end
+
+      let(:component) { described_class.new(label: 'label', destination: 'destination') }
+      let(:expected_html) do
+        <<~HTML
+          <form class="button_to" method="get" action="destination"><button class="usa-button" type="submit">label</button></form>
+        HTML
+      end
+
+      before do
+        render_inline(component)
+      end
+
+      it { is_expected.to match_html_fragment(expected_html) }
     end
 
-    let(:component) { described_class.new(label: 'label', destination: 'destination') }
-    let(:expected_html) do
-      <<~HTML
-        <form class="button_to" method="get" action="destination"><button class="usa-button" type="submit">label</button></form>
-      HTML
-    end
+    context 'with outline button class' do
+      subject(:html) do
+        render_inline(component)
+        rendered_content
+      end
 
-    before do
-      render_inline(component)
-    end
+      let(:component) do
+        described_class.new(label: 'label', destination: 'destination', additional_classes: ['usa-button--outline'])
+      end
+      let(:expected_html) do
+        <<~HTML
+          <form class="button_to" method="get" action="destination"><button class="usa-button--outline usa-button" type="submit">label</button></form>
+        HTML
+      end
 
-    it { is_expected.to match_html_fragment(expected_html) }
+      before do
+        render_inline(component)
+      end
+
+      it { is_expected.to match_html_fragment(expected_html) }
+    end
   end
 end
