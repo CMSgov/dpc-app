9221a7327557ee9577f65d43d9667a121c1a1f24
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2024-03-06 13:54:53 -0500

    Removed old test

--- dpc-api/src/test/java/gov/cms/dpc/api/resources/v1/AdminResourceTest.java
+++ dpc-api/src/test/java/gov/cms/dpc/api/resources/v1/AdminResourceTest.java
@@ -19,26 +19,8 @@ import java.net.URL;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AdminResourceTest extends AbstractSecureApplicationTest{
-
-    @Test
-    void testGetOrganizations() throws IOException, URISyntaxException {
-        UUID orgID1 = UUID.randomUUID();
-        UUID orgID2 = UUID.randomUUID();
-        URL url = new URL(getBaseURL() + "/Admin/Organization/?ids=id|"+orgID1.toString() + "," + orgID2.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod(HttpMethod.GET);
-        conn.setRequestProperty(HttpHeaders.CONTENT_TYPE, "application/fhir+json");
-        conn.setRequestProperty(HttpHeaders.AUTHORIZATION, "Bearer " + GOLDEN_MACAROON);
-
-        conn.setDoOutput(true);
-
-        assertNotNull(conn.getResponseCode());
-        assertEquals(HttpStatus.OK_200, conn.getResponseCode());
-        conn.disconnect();
-    }
 
     @Test
     void testNoGoldenMacaroon() throws IOException, URISyntaxException {
