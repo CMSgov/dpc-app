403aeba9f1b149a9d6f1220194c0d674b6a207fa
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2024-03-07 09:13:52 -0500

    [DPC-3905] Add admin endpoint for fetching an organization by NPI (#2061)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-3905
    
    ## 🛠 Changes
    
    Renamed the current `/Admin/Organization` end point to indicate that
    it's searching by NPI and not id.
    
    ## ℹ️ Context for reviewers
    
    Despite the fact that it said it was searching by id, the admin end
    point we already have is actually searching by NPI. The initial tests we
    had never caught it because they were only checking for a 200, not
    verifying the results. I added a new integration test to
    AdminResourceTest that performs the search by NPI and ensures that a
    valid org is returned.
    
    ## ✅ Acceptance Validation
    
    Passes new integration tests.
    
    ## 🔒 Security Implications
    
    - [ ] This PR adds a new software dependency or dependencies.
    - [ ] This PR modifies or invalidates one or more of our security
    controls.
    - [ ] This PR stores or transmits data that was not stored or
    transmitted before.
    - [ ] This PR requires additional review of its security implications
    for other reasons.
    
    If any security implications apply, add Jason Ashbaugh (GitHub username:
    StewGoin) as a reviewer and do not merge this PR without his approval.
    
    ---------
    
    Co-authored-by: Kevin Yeh <kevin@navapbc.com>

--- dpc-api/src/main/java/gov/cms/dpc/api/resources/v1/AdminResource.java
+++ dpc-api/src/main/java/gov/cms/dpc/api/resources/v1/AdminResource.java
@@ -1,30 +1,25 @@
 package gov.cms.dpc.api.resources.v1;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.Timed;
+import com.google.inject.name.Named;
+import gov.cms.dpc.api.auth.annotations.AdminOperation;
+import gov.cms.dpc.api.resources.AbstractAdminResource;
+import gov.cms.dpc.fhir.annotations.FHIR;
+import io.swagger.annotations.*;
+import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.Organization;
 
 import javax.inject.Inject;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
-
-import com.codahale.metrics.annotation.ExceptionMetered;
-import com.codahale.metrics.annotation.Timed;
-import com.google.inject.name.Named;
-
-import ca.uhn.fhir.rest.client.api.IGenericClient;
-import gov.cms.dpc.api.auth.annotations.AdminOperation;
-import gov.cms.dpc.api.resources.AbstractAdminResource;
-import gov.cms.dpc.fhir.annotations.FHIR;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.Authorization;
-import org.hl7.fhir.dstu3.model.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @Api(value = "Admin", authorizations = @Authorization(value = "access_token"))
@@ -43,14 +38,14 @@ public class AdminResource extends AbstractAdminResource{
     @Timed
     @AdminOperation
     @ExceptionMetered
-    @ApiOperation(value = "Get organizations by UUIDs",
+    @ApiOperation(value = "Get organizations by NPIs",
             notes = "FHIR endpoint which returns list of Organization resources that are currently registered with the application.",
             authorizations = @Authorization(value = "access_token"))
     @ApiResponses(value = {
             @ApiResponse(code = 401, message = "Only administrators can use this endpoint")})
-    public Bundle getOrganizations(@NotNull @QueryParam(value="ids") String ids) {
+    public Bundle getOrganizations(@NotNull @QueryParam(value="npis") String npis) {
         Map<String, List<String>> searchParams = new HashMap<>();
-        searchParams.put("identifier", Collections.singletonList(ids));
+        searchParams.put("identifier", Collections.singletonList(npis));
         Bundle bundle = this.client
                     .search()
                     .forResource(Organization.class)
