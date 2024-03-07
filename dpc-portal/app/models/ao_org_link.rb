c6c03fadcbea5e2c45c76d38742e58ef9cb5504d
Ashley Weaver <ashleyweaver@navapbc.com>
2024-03-07 17:53:13 -0500

    Fix line length

--- dpc-portal/app/models/ao_org_link.rb
+++ dpc-portal/app/models/ao_org_link.rb
@@ -4,7 +4,8 @@
 class AoOrgLink < ApplicationRecord
   attr_reader :user_id, :provider_organization_id
 
-  validates :user_id, uniqueness: { scope: :provider_organization_id, message: 'User already exists for this provider.' }
+  validates :user_id,
+            uniqueness: { scope: :provider_organization_id, message: 'User already exists for this provider.' }
 
   belongs_to :user, required: true
   belongs_to :provider_organization, required: true
