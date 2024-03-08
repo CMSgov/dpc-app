6601584d0227abe29f2ebb2e88125b4b5ff86851
Ashley Weaver <ashleyweaver@navapbc.com>
2024-03-08 08:33:47 -0500

    Update specs

--- dpc-portal/spec/models/ao_org_link_spec.rb
+++ dpc-portal/spec/models/ao_org_link_spec.rb
@@ -3,8 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe AoOrgLink, type: :model do
+  let(:organization) { build(:provider_organization) }
+  let(:authorized_official) { build(:user) }
+  let(:ao_org_link) { build(:ao_org_link) }
+
   it 'has foreign keys' do
-    ao_org_link = AoOrgLink.new
     expect(ao_org_link.user_id).to exist
     expect(ao_org_link.provider_organization_id).to exist
   end
--- dpc-portal/spec/models/cd_org_link_spec.rb
+++ dpc-portal/spec/models/cd_org_link_spec.rb
@@ -3,8 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe CdOrgLink, type: :model do
+  let(:organization) { build(:provider_organization) }
+  let(:credentialed_delegate) { build(:user) }
+  let(:new_cd_invite) { build(:invitation) }
+  let(:cd_org_link) { build(:cd_org_link) }
+
   it 'has foreign keys' do
-    cd_org_link = CdOrgLink.new
     expect(cd_org_link.user_id).to exist
     expect(cd_org_link.provider_organization_id).to exist
     expect(cd_org_link.invitation_id).to exist
