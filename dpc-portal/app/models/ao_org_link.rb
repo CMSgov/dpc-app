9448dc29efa61d187ed245e1ad23fdf326dedb25
Ashley Weaver <ashleyweaver@navapbc.com>
2024-03-08 08:04:39 -0500

    Add show attributes

--- dpc-portal/app/models/ao_org_link.rb
+++ dpc-portal/app/models/ao_org_link.rb
@@ -9,4 +9,8 @@ class AoOrgLink < ApplicationRecord
 
   belongs_to :user, required: true
   belongs_to :provider_organization, required: true
+
+  def show_attributes
+    { user_id:, provider_organization_id: }.with_indifferent_access
+  end
 end
--- dpc-portal/app/models/cd_org_link.rb
+++ dpc-portal/app/models/cd_org_link.rb
@@ -7,4 +7,8 @@ class CdOrgLink < ApplicationRecord
   belongs_to :user, required: true
   belongs_to :provider_organization, required: true
   belongs_to :invitation, required: true
+
+  def show_attributes
+    { user_id:, provider_organization_id:, invitation_id: }.with_indifferent_access
+  end
 end
