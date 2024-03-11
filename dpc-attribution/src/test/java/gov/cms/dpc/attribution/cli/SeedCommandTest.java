0c61892e9fda635416dc2c98728f2f3edbcf40ec
Ashley Weaver <ashleyweaver@navapbc.com>
2024-03-11 13:31:35 -0400

    Update SeedCommandTest.java

--- dpc-attribution/src/test/java/gov/cms/dpc/attribution/cli/SeedCommandTest.java
+++ dpc-attribution/src/test/java/gov/cms/dpc/attribution/cli/SeedCommandTest.java
@@ -69,7 +69,7 @@ public class SeedCommandTest extends AbstractAttributionTest {
     @Test
     void testSeedCommand() {
         final Optional<Throwable> success = cli.run("seed", "src/test/resources/test.application.yml");
-        //assertTrue(success.isEmpty(), "Should have succeeded");
+        assertTrue(success.isEmpty(), "Should have succeeded");
         assertEquals("", stdErr.toString(), "Should not have errors");
         assertTrue(stdOut.toString().contains("Seeding attribution at time "));
         assertTrue(stdOut.toString().contains("Finished loading seeds"));
