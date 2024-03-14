c01f61fe25d7aef835c492d5c3d5757ed09000ea
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2024-03-14 12:29:13 -0400

    Ip Addr auth

--- dpc-portal/app/controllers/ip_addresses_controller.rb
+++ dpc-portal/app/controllers/ip_addresses_controller.rb
@@ -11,11 +11,11 @@ class IpAddressesController < ApplicationController
 
   # rubocop:disable Metrics/AbcSize
   def create
-    manager = IpAddressManager.new(params[:organization_id])
+    manager = IpAddressManager.new(@organization.dpc_api_organization_id)
     new_ip_address = manager.create_ip_address(ip_address: params[:ip_address], label: params[:label])
     if new_ip_address[:response]
       flash[:notice] = 'IP address successfully created.'
-      redirect_to organization_path(params[:organization_id])
+      redirect_to organization_path(@organization)
     else
       flash[:alert] = "IP address could not be created: #{manager.errors.join(', ')}."
       render Page::IpAddress::NewAddressComponent.new(@organization)
@@ -23,28 +23,13 @@ class IpAddressesController < ApplicationController
   end
 
   def destroy
-    manager = IpAddressManager.new(params[:organization_id])
+    manager = IpAddressManager.new(@organization.dpc_api_organization_id)
     if manager.delete_ip_address(params)
       flash[:notice] = 'IP address successfully deleted.'
     else
       flash[:alert] = "IP address could not be deleted: #{manager.errors.join(', ')}."
     end
-    redirect_to organization_path(params[:organization_id])
+    redirect_to organization_path(@organization)
   end
   # rubocop:enable Metrics/AbcSize
-
-  private
-
-  def load_organization
-    @organization = case ENV.fetch('ENV', nil)
-                    when 'prod-sbx'
-                      redirect_to root_url
-                    when 'test'
-                      Organization.new('6a1dbf47-825b-40f3-b81d-4a7ffbbdc270')
-                    when 'dev'
-                      Organization.new('78d02106-2837-4d07-8c51-8d73332aff09')
-                    else
-                      Organization.new(params[:organization_id])
-                    end
-  end
 end
--- dpc-portal/spec/requests/ip_addresses_spec.rb
+++ dpc-portal/spec/requests/ip_addresses_spec.rb
@@ -12,16 +12,28 @@ RSpec.describe 'IpAddresses', type: :request do
     end
   end
 
+  describe 'GET /new no link to org' do
+    let!(:user) { create(:user) }
+    let!(:org) { create(:provider_organization) }
+    before { sign_in user }
+    it 'redirects to organizations' do
+      get "/organizations/#{org.id}/client_tokens/new"
+      expect(response).to redirect_to('/organizations')
+    end
+  end
+
   describe 'GET /new' do
     let!(:user) { create(:user) }
-    before { sign_in user }
+    let!(:org) { create(:provider_organization) }
+    
+    before do
+      create(:cd_org_link, provider_organization: org, user:)
+      sign_in user
+    end
 
     it 'returns success' do
-      api_id = SecureRandom.uuid
-      stub_api_client(message: :get_organization,
-                      response: default_get_org_response(api_id))
-      get "/organizations/#{api_id}/ip_addresses/new"
-      expect(assigns(:organization).api_id).to eq api_id
+      get "/organizations/#{org.id}/ip_addresses/new"
+      expect(assigns(:organization)).to eq org
       expect(response).to have_http_status(200)
     end
   end
@@ -35,62 +47,63 @@ RSpec.describe 'IpAddresses', type: :request do
 
   describe 'POST /create' do
     let!(:user) { create(:user) }
-    before { sign_in user }
+    let(:org_api_id) { SecureRandom.uuid }
+    let!(:org) { create(:provider_organization, dpc_api_organization_id: org_api_id) }
+    
+    before do
+      create(:cd_org_link, provider_organization: org, user:)
+      sign_in user
+    end
 
     it 'succeeds with valid params' do
-      org_api_id = SecureRandom.uuid
       api_client = stub_api_client(message: :get_organization,
                                    response: default_get_org_response(org_api_id))
       stub_self_returning_api_client(message: :create_ip_address,
                                      response: default_get_ip_addresses['entities'].first,
                                      api_client:)
-      post "/organizations/#{org_api_id}/ip_addresses", params: { label: 'Public IP 1', ip_address: '136.226.19.87' }
-      expect(assigns(:organization).api_id).to eq org_api_id
+      post "/organizations/#{org.id}/ip_addresses", params: { label: 'Public IP 1', ip_address: '136.226.19.87' }
+      expect(assigns(:organization)).to eq org
     end
 
     it 'fails if missing params' do
-      org_api_id = SecureRandom.uuid
       stub_api_client(message: :get_organization,
                       response: default_get_org_response(org_api_id))
-      post "/organizations/#{org_api_id}/ip_addresses"
-      expect(assigns(:organization).api_id).to eq org_api_id
+      post "/organizations/#{org.id}/ip_addresses"
+      expect(assigns(:organization)).to eq org
       expect(flash[:alert]).to eq('IP address could not be created: missing label, missing IP address.')
     end
 
     it 'fails if invalid IP' do
-      org_api_id = SecureRandom.uuid
       api_client = stub_api_client(message: :get_organization,
                                    response: default_get_org_response(org_api_id))
       stub_self_returning_api_client(message: :create_ip_address,
                                      response: default_get_ip_addresses['entities'].first,
                                      api_client:)
-      post "/organizations/#{org_api_id}/ip_addresses", params: { label: 'Public IP 1', ip_address: '333.333.333.333' }
-      expect(assigns(:organization).api_id).to eq org_api_id
+      post "/organizations/#{org.id}/ip_addresses", params: { label: 'Public IP 1', ip_address: '333.333.333.333' }
+      expect(assigns(:organization)).to eq org
       expect(flash[:alert]).to eq('IP address could not be created: invalid IP address.')
     end
 
     it 'fails if label over 25 characters' do
-      org_api_id = SecureRandom.uuid
       api_client = stub_api_client(message: :get_organization,
                                    response: default_get_org_response(org_api_id))
       stub_self_returning_api_client(message: :create_ip_address,
                                      response: default_get_ip_addresses['entities'].first,
                                      api_client:)
-      post "/organizations/#{org_api_id}/ip_addresses",
+      post "/organizations/#{org.id}/ip_addresses",
            params: { label: 'aaaaabbbbbcccccdddddeeeeefffff', ip_address: '136.226.19.87' }
-      expect(assigns(:organization).api_id).to eq org_api_id
+      expect(assigns(:organization)).to eq org
       expect(flash[:alert]).to eq('IP address could not be created: label cannot be over 25 characters.')
     end
 
     it 'shows error if problem' do
-      org_api_id = SecureRandom.uuid
       api_client = stub_api_client(message: :get_organization,
                                    response: default_get_org_response(org_api_id))
       stub_self_returning_api_client(message: :create_ip_address,
                                      success: false,
                                      response: nil,
                                      api_client:)
-      post "/organizations/#{org_api_id}/ip_addresses", params: { label: 'Public IP 1', ip_address: '136.226.19.87' }
+      post "/organizations/#{org.id}/ip_addresses", params: { label: 'Public IP 1', ip_address: '136.226.19.87' }
       expect(flash[:alert]).to eq('IP address could not be created: failed to create IP address.')
     end
   end
@@ -104,10 +117,15 @@ RSpec.describe 'IpAddresses', type: :request do
 
   describe 'DELETE /destroy' do
     let!(:user) { create(:user) }
-    before { sign_in user }
+    let(:org_api_id) { SecureRandom.uuid }
+    let!(:org) { create(:provider_organization, dpc_api_organization_id: org_api_id) }
+    
+    before do
+      create(:cd_org_link, provider_organization: org, user:)
+      sign_in user
+    end
 
     it 'flashes success if succeeds' do
-      org_api_id = SecureRandom.uuid
       addr_guid = SecureRandom.uuid
       api_client = stub_api_client(message: :get_organization,
                                    response: default_get_org_response(org_api_id))
@@ -115,13 +133,12 @@ RSpec.describe 'IpAddresses', type: :request do
                                      response: nil,
                                      with: [org_api_id, addr_guid],
                                      api_client:)
-      delete "/organizations/#{org_api_id}/ip_addresses/#{addr_guid}"
+      delete "/organizations/#{org.id}/ip_addresses/#{addr_guid}"
       expect(flash[:notice]).to eq('IP address successfully deleted.')
-      expect(response).to redirect_to(organization_path(org_api_id))
+      expect(response).to redirect_to(organization_path(org))
     end
 
     it 'renders error if error' do
-      org_api_id = SecureRandom.uuid
       addr_guid = SecureRandom.uuid
       api_client = stub_api_client(message: :get_organization,
                                    response: default_get_org_response(org_api_id))
@@ -130,9 +147,9 @@ RSpec.describe 'IpAddresses', type: :request do
                                      success: false,
                                      with: [org_api_id, addr_guid],
                                      api_client:)
-      delete "/organizations/#{org_api_id}/ip_addresses/#{addr_guid}"
+      delete "/organizations/#{org.id}/ip_addresses/#{addr_guid}"
       expect(flash[:alert]).to eq('IP address could not be deleted: failed to delete IP address.')
-      expect(response).to redirect_to(organization_path(org_api_id))
+      expect(response).to redirect_to(organization_path(org))
     end
   end
 end
