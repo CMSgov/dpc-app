ab46221962add9259e75ec030dce4d7092769f7f
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2024-03-19 11:45:19 -0400

    HAPIExceptionHandler now returns the correct http status for all errors

--- dpc-common/src/test/java/gov/cms/dpc/fhir/dropwizard/handlers/exceptions/HAPIExceptionHandlerTest.java
+++ dpc-common/src/test/java/gov/cms/dpc/fhir/dropwizard/handlers/exceptions/HAPIExceptionHandlerTest.java
@@ -1,25 +1,19 @@
 package gov.cms.dpc.fhir.dropwizard.handlers.exceptions;
 
 import ca.uhn.fhir.rest.server.exceptions.BaseServerResponseException;
-import gov.cms.dpc.fhir.annotations.FHIR;
 import org.eclipse.jetty.http.HttpStatus;
 import org.hl7.fhir.dstu3.model.OperationOutcome;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.Response;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class HAPIExceptionHandlerTest {
 
     @Test
-    void testToResponse_fhirException() {
-        ResourceInfo info = Mockito.mock(ResourceInfo.class);
-        Mockito.when(info.getResourceClass()).thenAnswer(answer -> FHIRResourceClass.class);
-        final HAPIExceptionHandler handler = new HAPIExceptionHandler(info);
+    void testToResponse_noOperationOutcome() {
+        final HAPIExceptionHandler handler = new HAPIExceptionHandler();
 
         String errMsg = "FHIR exception";
         Response response = handler.toResponse(new ServerResponseException(HttpStatus.NOT_FOUND_404, errMsg));
@@ -32,25 +26,35 @@ public class HAPIExceptionHandlerTest {
     }
 
     @Test
-    void testToResponse_nonFhirException() {
-        final HAPIExceptionHandler handler = new HAPIExceptionHandler(Mockito.mock(ResourceInfo.class));
+    void testToResponse_hasOperationOutcome() {
+        final HAPIExceptionHandler handler = new HAPIExceptionHandler();
 
-        try {
-            handler.toResponse(new ServerResponseException(HttpStatus.NOT_FOUND_404, ""));
-            fail("This call is supposed to fail.");
-        } catch (IllegalStateException exception) {
-            assertEquals("Cannot return HAPI exception for non-FHIR method", exception.getMessage());
-        }
+        String errMsg = "FHIR exception";
+
+        OperationOutcome outcome = new OperationOutcome();
+        outcome.addIssue()
+                .setCode(OperationOutcome.IssueType.EXCEPTION)
+                .setSeverity(OperationOutcome.IssueSeverity.ERROR)
+                .setDiagnostics(errMsg);
+
+        Response response = handler.toResponse(new ServerResponseException(HttpStatus.NOT_FOUND_404, errMsg));
+        assertEquals(HttpStatus.NOT_FOUND_404, response.getStatus());
+
+        OperationOutcome.OperationOutcomeIssueComponent issue = ((OperationOutcome) response.getEntity()).getIssueFirstRep();
+        assertEquals(OperationOutcome.IssueType.EXCEPTION, issue.getCode());
+        assertEquals(OperationOutcome.IssueSeverity.ERROR, issue.getSeverity());
+        assertEquals(errMsg, issue.getDiagnostics());
     }
 
     static class ServerResponseException extends BaseServerResponseException {
+        OperationOutcome outcome;
+
         public ServerResponseException(int theStatusCode, String theMessage) {
             super(theStatusCode, theMessage);
         }
-    }
-
-    @FHIR
-    static class FHIRResourceClass {
-
+        public ServerResponseException(int theStatusCode, String theMessage, OperationOutcome outcome) {
+            super(theStatusCode, theMessage);
+            this.outcome = outcome;
+        }
     }
 }
