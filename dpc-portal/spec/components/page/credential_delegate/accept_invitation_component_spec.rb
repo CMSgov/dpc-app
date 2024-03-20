bee38320dd179f2008c8d3bd129379c12fe6fb18
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2024-03-20 09:40:17 -0400

    component specs

--- dpc-portal/spec/components/page/credential_delegate/accept_invitation_component_spec.rb
+++ dpc-portal/spec/components/page/credential_delegate/accept_invitation_component_spec.rb
@@ -3,13 +3,77 @@
 require 'rails_helper'
 
 RSpec.describe Page::CredentialDelegate::AcceptInvitationComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ComponentSupport
+  describe 'html' do
+    subject(:html) do
+      render_inline(component)
+      normalize_space(rendered_content)
+    end
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+
+    let(:org) { ComponentSupport::MockOrg.new }
+    let(:cd_id) { '4' }
+    let(:ao) { build(:user, given_name: 'Bob', family_name: 'Hodges') }
+    let(:cd_invite) { Invitation.new(id: cd_id, invited_by: ao) }
+
+    let(:component) { described_class.new(org, cd_invite) }
+
+    before do
+      render_inline(component)
+    end
+
+    context 'New form' do
+      it 'should match header' do
+        header = <<~HTML
+          <h1>Accept organization invite</h1>
+            <div class="usa-alert usa-alert--info margin-bottom-4">
+              <div class="usa-alert__body">
+                <h4 class="usa-alert__heading">Key information</h4>
+                <p class="usa-alert__text">
+        HTML
+        is_expected.to include(normalize_space(header))
+      end
+
+      it 'should match form tag' do
+        form_tag = ['<form class="usa-form" id="cd-accept-form"',
+                    %(action="/portal/organizations/#{org.path_id}/credential_delegate_invitations/#{cd_invite.id}/confirm"),
+                    'accept-charset="UTF-8" method="post">'].join(' ')
+        is_expected.to include(form_tag)
+      end
+
+      it 'should mention org name' do
+        is_expected.to include(org.name)
+      end
+
+      it 'should mention AO name' do
+        is_expected.to include('Bob Hodges')
+      end
+
+      it 'should have empty verification_code stanza' do
+        verification_code = <<~HTML
+          <div class="margin-bottom-4">
+            <label class="usa-label" for="verification_code">Enter the invite code:</label>
+            <input type="text" name="verification_code" id="verification_code" maxlength="6" class="usa-input" />
+          </div>
+        HTML
+        is_expected.to include(normalize_space(verification_code))
+      end
+    end
+
+    context 'Errors' do
+      let(:error_msg) { 'Some error message' }
+      
+      before { cd_invite.errors.add(:verification_code, :is_bad, message: error_msg) }
+      it 'should have errored verification_code stanza' do
+        verification_code = <<~HTML
+          <div class="margin-bottom-4">
+            <label class="usa-label" for="verification_code">Enter the invite code:</label>
+            <p style="color: #b50909;">#{error_msg}</p>
+            <input type="text" name="verification_code" id="verification_code" maxlength="6" class="usa-input usa-input--error" />
+          </div>
+        HTML
+        is_expected.to include(normalize_space(verification_code))
+      end
+    end
+  end
 end
--- dpc-portal/spec/components/page/credential_delegate/bad_invitation_component_spec.rb
+++ dpc-portal/spec/components/page/credential_delegate/bad_invitation_component_spec.rb
@@ -3,13 +3,38 @@
 require 'rails_helper'
 
 RSpec.describe Page::CredentialDelegate::BadInvitationComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ComponentSupport
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  describe 'html' do
+    subject(:html) do
+      render_inline(component)
+      normalize_space(rendered_content)
+    end
+
+    before do
+      render_inline(component)
+    end
+
+    context 'invalid invitation' do
+      let(:component) { described_class.new('invalid') }
+      it 'should match header' do
+        header = <<~HTML
+          <h1>Invitation is invalid</h1>
+            <div class="usa-alert usa-alert--warning margin-bottom-4">
+        HTML
+        is_expected.to include(normalize_space(header))
+      end
+    end
+
+    context 'PII mismatch' do
+      let(:component) { described_class.new('pii_mismatch') }
+      it 'should match header' do
+        header = <<~HTML
+          <h1>Invitation denied</h1>
+            <div class="usa-alert usa-alert--error margin-bottom-4">
+        HTML
+        is_expected.to include(normalize_space(header))
+      end
+    end
+  end
 end
