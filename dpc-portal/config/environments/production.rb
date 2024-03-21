995f7489c6d113d3de59af3ff14978e43ece8969
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2024-03-21 14:39:48 -0700

    update config

--- dpc-portal/config/environments/production.rb
+++ dpc-portal/config/environments/production.rb
@@ -87,10 +87,10 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              ENV['SMTP_ADDRESS'],
-    port:                 ENV['SMTP_PORT'],
-    domain:               ENV['SMTP_DOMAIN'],
-    enable_starttls_auto: true
+    address:         ENV['SMTP_ADDRESS'],
+    port:            ENV['SMTP_PORT'],
+    domain:          ENV['SMTP_DOMAIN'],
+    enable_starttls: true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
