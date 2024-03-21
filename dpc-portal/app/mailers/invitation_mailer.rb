9f488ec36775e059ffff9328ee7ebd86aae58016
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2024-03-21 13:57:13 -0700

    Send invite when created by controller

--- dpc-portal/app/mailers/invitation_mailer.rb
+++ dpc-portal/app/mailers/invitation_mailer.rb
@@ -7,6 +7,6 @@ class InvitationMailer < ApplicationMailer
     mail(
       to: @invitation.invited_email,
       subject: 'You have been granted credential delegate authority in Data at the Point of Care'
-    )     
+    )
   end
 end
