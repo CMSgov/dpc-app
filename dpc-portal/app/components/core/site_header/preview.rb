533d91e79d0cffd3bf81ca0a18afe02adb58c6a2
Kevin Yeh <kevinyeah@utexas.edu>
2024-03-29 14:11:16 -0400

    Add param

--- dpc-portal/app/components/core/site_header/preview.rb
+++ dpc-portal/app/components/core/site_header/preview.rb
@@ -6,8 +6,9 @@ module Core
     # ----------------
     #
     class Preview < ViewComponent::Preview
-      def default
-        render Core::SiteHeader::Component.new
+      # @param logged_in toggle "Indicate whether user is logged in"
+      def default(logged_in: false)
+        render Core::SiteHeader::Component.new(logged_in:)
       end
     end
   end
