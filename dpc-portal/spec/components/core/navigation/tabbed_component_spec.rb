a8757df1a5ed3f622d0e8cd46d1bd4c0cfc35b17
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2024-04-05 14:15:35 -0400

    use custom layout

--- dpc-portal/spec/components/core/navigation/tabbed_component_spec.rb
+++ dpc-portal/spec/components/core/navigation/tabbed_component_spec.rb
@@ -24,127 +24,116 @@ RSpec.describe Core::Navigation::TabbedComponent, type: :component do
     context 'when has one link' do
       let(:links) { [['Link 1', '.area1', false]] }
       let(:expected_selectors) { '".area1"' }
-      let(:expected_links) do
+      let(:expected_buttons) do
         <<~HTML
-          <li class="usa-nav__primary-item">
-            <a class="usa-nav-link" href="#" onclick="make_current(0); return false">Link 1</a>
+          <li>
+            <button type="button" style="box-shadow: none; border-radius: 0;" class="usa-button usa-button--outline" onclick="make_current(0); return false">
+              Link 1
+            </button>
           </li>
         HTML
       end
 
-      it { is_expected.to match_html_fragment(expected_html(expected_selectors, expected_start_index, expected_links)) }
+      it {
+        is_expected.to match_html_fragment(expected_html(expected_selectors, expected_start_index, expected_buttons))
+      }
     end
 
     context 'when has two links' do
-      let(:links) { [['Link 1', '.area1', false], ['Link 2', '.area2', false]] }
-      let(:expected_selectors) { '".area1", ".area2"' }
-      let(:expected_links) do
+      let(:expected_buttons) do
         <<~HTML
-          <li class="usa-nav__primary-item">
-            <a class="usa-nav-link" href="#" onclick="make_current(0); return false">Link 1</a>
+          <li>
+            <button type="button" style="box-shadow: none; border-radius: 0;" class="usa-button usa-button--outline" onclick="make_current(0); return false">
+              Link 1
+            </button>
           </li>
-          <li class="usa-nav__primary-item">
-            <a class="usa-nav-link" href="#" onclick="make_current(1); return false">Link 2</a>
+          <li>
+            <button type="button" style="box-shadow: none; border-radius: 0;" class="usa-button usa-button--outline" onclick="make_current(1); return false">
+              Link 2
+            </button>
           </li>
         HTML
       end
 
-      it { is_expected.to match_html_fragment(expected_html(expected_selectors, expected_start_index, expected_links)) }
-    end
+      context 'none current' do
+        let(:links) { [['Link 1', '.area1', false], ['Link 2', '.area2', false]] }
+        let(:expected_selectors) { '".area1", ".area2"' }
 
-    context 'when has two links, first current' do
-      let(:links) { [['Link 1', '.area1', true], ['Link 2', '.area2', false]] }
-      let(:expected_selectors) { '".area1", ".area2"' }
-      let(:expected_start_index) { 0 }
-      let(:expected_links) do
-        <<~HTML
-          <li class="usa-nav__primary-item">
-            <a class="usa-nav-link" href="#" onclick="make_current(0); return false">Link 1</a>
-          </li>
-          <li class="usa-nav__primary-item">
-            <a class="usa-nav-link" href="#" onclick="make_current(1); return false">Link 2</a>
-          </li>
-        HTML
+        it {
+          is_expected.to match_html_fragment(expected_html(expected_selectors, expected_start_index, expected_buttons))
+        }
       end
 
-      it { is_expected.to match_html_fragment(expected_html(expected_selectors, expected_start_index, expected_links)) }
-    end
+      context 'first current' do
+        let(:links) { [['Link 1', '.area1', true], ['Link 2', '.area2', false]] }
+        let(:expected_selectors) { '".area1", ".area2"' }
+        let(:expected_start_index) { 0 }
 
-    context 'when has two links, second current' do
-      let(:links) { [['Link 1', '.area1', false], ['Link 2', '.area2', true]] }
-      let(:expected_selectors) { '".area1", ".area2"' }
-      let(:expected_start_index) { 1 }
-      let(:expected_links) do
-        <<~HTML
-          <li class="usa-nav__primary-item">
-            <a class="usa-nav-link" href="#" onclick="make_current(0); return false">Link 1</a>
-          </li>
-          <li class="usa-nav__primary-item">
-            <a class="usa-nav-link" href="#" onclick="make_current(1); return false">Link 2</a>
-          </li>
-        HTML
+        it {
+          is_expected.to match_html_fragment(expected_html(expected_selectors, expected_start_index, expected_buttons))
+        }
       end
 
-      it { is_expected.to match_html_fragment(expected_html(expected_selectors, expected_start_index, expected_links)) }
-    end
+      context 'second current' do
+        let(:links) { [['Link 1', '.area1', false], ['Link 2', '.area2', true]] }
+        let(:expected_selectors) { '".area1", ".area2"' }
+        let(:expected_start_index) { 1 }
 
-    context 'when has two links, both current' do
-      let(:links) { [['Link 1', '.area1', true], ['Link 2', '.area2', true]] }
-      let(:expected_selectors) { '".area1", ".area2"' }
-      let(:expected_start_index) { 1 }
-      let(:expected_links) do
-        <<~HTML
-          <li class="usa-nav__primary-item">
-            <a class="usa-nav-link" href="#" onclick="make_current(0); return false">Link 1</a>
-          </li>
-          <li class="usa-nav__primary-item">
-            <a class="usa-nav-link" href="#" onclick="make_current(1); return false">Link 2</a>
-          </li>
-        HTML
+        it {
+          is_expected.to match_html_fragment(expected_html(expected_selectors, expected_start_index, expected_buttons))
+        }
       end
 
-      it { is_expected.to match_html_fragment(expected_html(expected_selectors, expected_start_index, expected_links)) }
+      context 'both current' do
+        let(:links) { [['Link 1', '.area1', true], ['Link 2', '.area2', true]] }
+        let(:expected_selectors) { '".area1", ".area2"' }
+        let(:expected_start_index) { 1 }
+
+        it {
+          is_expected.to match_html_fragment(expected_html(expected_selectors, expected_start_index, expected_buttons))
+        }
+      end
     end
   end
 
-  def expected_html(expected_selectors, expected_start_index, expected_links)
+  def expected_html(expected_selectors, expected_start_index, expected_buttons)
     <<~HTML
       <script>
         //<![CDATA[
-          function make_current(link_index) {
-            const selectors = [#{expected_selectors}];
-            var current_index = 0;
-            for (anchor of document.querySelector('.usa-nav__primary').getElementsByTagName("a")) {
-              if (current_index == link_index){
-                anchor.className = 'usa-nav-link usa-current';
-                if (document.querySelector(selectors[current_index])) {
-                  document.querySelector(selectors[current_index]).style.display = 'block';
-                }
-              } else {
-                anchor.className = 'usa-nav-link';
-                if (document.querySelector(selectors[current_index])) {
-                  document.querySelector(selectors[current_index]).style.display = 'none';
-                }
+        function make_current(link_index) {
+          const selectors = [#{expected_selectors}];
+          var current_index = 0;
+          for (anchor of document.querySelector('#header_id').getElementsByTagName("button")) {
+            if (current_index == link_index){
+              anchor.className = 'usa-button usa-button--outline usa-button--active';
+              anchor.style.borderBottom = '5px solid #162E51';
+            if (document.querySelector(selectors[current_index])) {
+                document.querySelector(selectors[current_index]).style.display = 'block';
               }
-              current_index++;
+            } else {
+              anchor.className = 'usa-button usa-button--outline';
+              anchor.style.borderBottom = '5px solid transparent';
+              if (document.querySelector(selectors[current_index])) {
+                document.querySelector(selectors[current_index]).style.display = 'none';
+              }
             }
+            current_index++;
           }
-          document.addEventListener("DOMContentLoaded", function() {
+        }
+        document.addEventListener("DOMContentLoaded", function() {
             if (document.querySelector("#header_id")) {
               document.querySelector("#header_id").style.display = 'block';
             }
             make_current(#{expected_start_index});
-          });
+        });
         //]]>
       </script>
       <div class="usa-overlay"></div>
       <header id="header_id" class="usa-header usa-header--extended margin-bottom-2" style="display: none">
-        <nav aria-label="Primary navigation" class="usa-nav">
-          <div class="usa-nav__inner">
-            <ul class="usa-nav__primary usa-accordion">
-            #{expected_links}
-            </ul>
-          </div>
+        <nav>
+          <ul class="usa-button-group">
+            #{expected_buttons}
+          </ul>
         </nav>
       </header>
     HTML
