464ded80f29e2a8741eb09370ef9f926593430f0
Ashley Weaver <ashleyweaver@navapbc.com>
2024-04-08 10:54:42 -0400

    Fetch patient only if lastUpdated is null

--- dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/engine/ResourceFetcher.java
+++ dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/engine/ResourceFetcher.java
@@ -121,7 +121,7 @@ class ResourceFetcher {
     /**
      * Based on resourceType, fetch a resource or a bundle of resources.
      *
-     * @param patient   {@link Patient} we're fetching resrouc
+     * @param patient   {@link Patient} we're fetching resources for
      * @return the first bundle of resources
      */
     private Bundle fetchFirst(Patient patient, Map<String, String> headers) {
@@ -133,20 +133,29 @@ class ResourceFetcher {
         String patientId = patient.getIdElement().getIdPart();
 
         final var lastUpdated = formLastUpdatedParam();
+        Bundle resultBundle = new Bundle();
         switch (resourceType) {
             // Why are we reloading the Patient, because on reload we're passing a lastUpdated parameter.  If the user
             // only wants resources from the last month and our patient hasn't been updated in a year, this method will
             // return an empty bundle.
             // TODO: Implement the since parameter ourselves using meta.lastUpdated and avoid pulling Patient and EoB twice.
             case Patient:
-                return blueButtonClient.requestPatientFromServer(patientId, lastUpdated, headers);
+                Date patientLastUpdated = patient.getMeta().getLastUpdated();
+                if (patientLastUpdated == null) {
+                    return blueButtonClient.requestPatientFromServer(patientId, lastUpdated, headers);
+                } else if (since != null && patientLastUpdated.toInstant().isBefore(since.toInstant())) {
+                    break;
+                } else {
+                    resultBundle.addEntry().setResource(patient);
+                }
             case ExplanationOfBenefit:
                 return blueButtonClient.requestEOBFromServer(patientId, lastUpdated, headers);
             case Coverage:
                 return blueButtonClient.requestCoverageFromServer(patientId, lastUpdated, headers);
             default:
-                throw new JobQueueFailure(jobID, batchID, "Unexpected resource type: " + resourceType.toString());
+                throw new JobQueueFailure(jobID, batchID, "Unexpected resource type: " + resourceType);
         }
+        return resultBundle;
     }
 
     /**
