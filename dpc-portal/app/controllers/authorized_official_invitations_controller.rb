3ba901dc1e018bf6e7dee409353d165e2baef8f3
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2024-04-11 16:43:14 -0400

    Add AO invitations controller (no-op)

---
+++ dpc-portal/app/controllers/authorized_official_invitations_controller.rb
@@ -0,0 +1,6 @@
+class AuthorizedOfficialInvitationsController < ApplicationController
+  before_action :load_organization
+  def accept
+    render inline: '<h1>Not implemented</h1>', layout: :default, status: :not_implemented
+  end
+end
