bc2c8626da698d406b7c0ab790486f6815a54318
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2024-04-12 11:33:30 -0400

    create ao invitation via service

--- dpc-portal/app/services/ao_invitation_service.rb
+++ dpc-portal/app/services/ao_invitation_service.rb
@@ -2,9 +2,30 @@
 
 # A service that verifies generates an ao invitation
 class AoInvitationService
-  def initialize
-    @cpi_api_gw_client = CpiApiGatewayClient.new
+  def create_invitation(ao_given_name, ao_family_name, ao_email, org_npi)
+    organization = ProviderOrganization.find_or_create_by(npi: org_npi) do |org|
+      org.name = org_name(org_npi)
+    end
+    Invitation.create(invited_given_name: ao_given_name,
+                      invited_family_name: ao_family_name,
+                      invited_email: ao_email,
+                      invited_email_confirmation: ao_email,
+                      provider_organization: organization,
+                      invitation_type: 'authorized_official')
   end
 
-  def org_name(npi); end
+  def org_name(npi)
+    org_info = client.org_info(npi)
+    raise AoInvitationServiceError, 'No such organization' if org_info['code'].to_s == '404'
+
+    org_info.dig('provider', 'orgName')
+  end
+
+  private
+
+  def client
+    @client ||= CpiApiGatewayClient.new
+  end
 end
+
+class AoInvitationServiceError < StandardError; end
--- dpc-portal/spec/services/ao_invitation_service_spec.rb
+++ dpc-portal/spec/services/ao_invitation_service_spec.rb
@@ -4,31 +4,91 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe AoInvitationService do
-  describe 'create invitation' do
-    it 'verifies the npi' do
-      expect(cpi_api_gw_client).to receive(:org_info)
+  let(:client) { instance_double(CpiApiGatewayClient) }
+  let(:service) { AoInvitationService.new }
+  let(:organization_npi) { '11111111111' }
+  before do
+    allow(CpiApiGatewayClient).to receive(:new).and_return(client)
+  end
+  describe 'utilities' do
+    describe 'org_name'
+    it 'fetches the name' do
+      expect(client).to receive(:org_info)
         .with(organization_npi)
-        .and_return({
-                      'provider' => {
-                        'providerType' => 'org',
-                        'orgName' => 'Health Hut',
-                        'npi' => organization_npi.to_s,
-                        'addresses' => [
-                          {
-                            'addressLine1' => '1234 Company Ln.',
-                            'addressLine2' => '#2222',
-                            'city' => 'Fairfax',
-                            'stateCode' => 'VA',
-                            'zip' => '23230',
-                            'dataIndicator' => 'CURRENT'
-                          }
-                        ]
-                      }
-                    })
+        .and_return(org_stanza)
+      name = service.org_name(organization_npi)
+      expect(name).to eq 'Health Hut'
     end
-    it 'does not create if org with npi exists'
-    it 'creates org wich PECOS name'
-    it 'creates invitation'
+
+    it 'raises exception if no org' do
+      expect(client).to receive(:org_info)
+        .with(organization_npi)
+        .and_return({ 'code' => '404' })
+      expect do
+        service.org_name(organization_npi)
+      end.to raise_error(AoInvitationServiceError)
+    end
+  end
+
+  describe 'create invitation' do
+    let(:params) { %w[Bob Hoskins bob@example.com] }
+    it 'creates org if not exist' do
+      expect(client).to receive(:org_info)
+        .with(organization_npi)
+        .and_return(org_stanza)
+      expect do
+        service.create_invitation(*params, organization_npi)
+      end.to change { ProviderOrganization.count }.by 1
+    end
+    it 'does not create if org with npi exists' do
+      create(:provider_organization, npi: organization_npi)
+      expect do
+        service.create_invitation(*params, organization_npi)
+      end.to change { ProviderOrganization.count }.by 0
+    end
+    it 'creates org wich PECOS name' do
+      expect(client).to receive(:org_info)
+        .with(organization_npi)
+        .and_return(org_stanza)
+      service.create_invitation(*params, organization_npi)
+      organization = ProviderOrganization.find_by(npi: organization_npi)
+      expect(organization.name).to eq 'Health Hut'
+    end
+    it 'creates invitation' do
+      expect(client).to receive(:org_info)
+        .with(organization_npi)
+        .and_return(org_stanza)
+      expect do
+        service.create_invitation(*params, organization_npi)
+      end.to change { Invitation.count }.by 1
+      organization = ProviderOrganization.find_by(npi: organization_npi)
+      matching_invitation = Invitation.where(invited_given_name: params[0],
+                                             invited_family_name: params[1],
+                                             invited_email: params[2],
+                                             provider_organization: organization,
+                                             invitation_type: 'authorized_official')
+      expect(matching_invitation.count).to eq 1
+    end
     it 'sends email'
+  end
+
+  def org_stanza
+    {
+      'provider' => {
+        'providerType' => 'org',
+        'orgName' => 'Health Hut',
+        'npi' => organization_npi.to_s,
+        'addresses' => [
+          {
+            'addressLine1' => '1234 Company Ln.',
+            'addressLine2' => '#2222',
+            'city' => 'Fairfax',
+            'stateCode' => 'VA',
+            'zip' => '23230',
+            'dataIndicator' => 'CURRENT'
+          }
+        ]
+      }
+    }
   end
 end
