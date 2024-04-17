a87c21a0908a45963afb5d0d010677fa90a5dcd0
Ashley Weaver <ashleyweaver@navapbc.com>
2024-04-17 11:17:44 -0400

    Update JobBatchProcessor.java

--- dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/engine/JobBatchProcessor.java
+++ dpc-aggregation/src/main/java/gov/cms/dpc/aggregation/engine/JobBatchProcessor.java
@@ -357,7 +357,7 @@ public class JobBatchProcessor {
             if (consentResults.isEmpty()) {
                 return false;
             }
-            final ConsentResult latestConsent = Collections.max(consentResults, Comparator.comparing(ConsentResult::getConsentDate));
+            final ConsentResult latestConsent = Collections.max(consentResults, Comparator.comparing(consent -> consent.getConsentDate()));
             final boolean isActive = latestConsent.isActive();
             final boolean isOptOut = ConsentResult.PolicyType.OPT_OUT.equals(latestConsent.getPolicyType());
             final boolean isFutureConsent = latestConsent.getConsentDate().after(new Date());
