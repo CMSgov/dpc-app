92933108d11887d57d991580df1ed3f78306e61d
Ashley Weaver <ashleyweaver@navapbc.com>
2024-04-23 16:31:59 -0400

    Update schema.rb

--- dpc-portal/db/schema.rb
+++ dpc-portal/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_23_174750) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_01_144012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,7 +71,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_23_174750) do
     t.string "family_name"
     t.string "provider", limit: 50, default: "", null: false
     t.string "uid", limit: 50, default: "", null: false
-    t.datetime "remembered_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
