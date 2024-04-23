b4d33ca70cb37a9e0d2d7dd5c313a8c78ebd2a31
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2024-04-23 13:47:58 -0400

    Setup activerecord-session_store gem

--- dpc-portal/config/environments/test.rb
+++ dpc-portal/config/environments/test.rb
@@ -63,3 +63,5 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 end
+# Session serializer
+ActiveRecord::SessionStore::Session.serializer = :json
