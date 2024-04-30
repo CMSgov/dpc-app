eff719e34d97a74cc4804819072b3c5f77be15d7
Ashley Weaver <134093673+ashley-weaver@users.noreply.github.com>
2024-04-30 13:20:34 -0400

    [DPC-3980] Implement idle timeout and max session length (#2145)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-3980
    
    ## 🛠 Changes
    
    Adds idle timeout and max session length for User model.
    
    ## ℹ️ Context for reviewers
    
    (Background context, more in-depth details of the implementation, and
    anything else you'd like to call out or ask reviewers.)
    
    ## ✅ Acceptance Validation
    
    After inactive time exceeded:
    
    ![image](https://github.com/CMSgov/dpc-app/assets/134093673/11f9c3ee-d815-417d-8a06-1124ca40ea6e)
    
    After max session length exceeded:
    
    ![image](https://github.com/CMSgov/dpc-app/assets/134093673/4bbed2eb-2c18-4f8e-a0a9-29de9bcc0776)
    
    ## 🔒 Security Implications
    
    - [ ] This PR adds a new software dependency or dependencies.
    - [ ] This PR modifies or invalidates one or more of our security
    controls.
    - [ ] This PR stores or transmits data that was not stored or
    transmitted before.
    - [ ] This PR requires additional review of its security implications
    for other reasons.
    
    If any security implications apply, add Jason Ashbaugh (GitHub username:
    StewGoin) as a reviewer and do not merge this PR without his approval.

--- dpc-portal/config/initializers/devise.rb
+++ dpc-portal/config/initializers/devise.rb
@@ -197,7 +197,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
-  # config.remember_for = 2.weeks
+  config.remember_for = 12.hours
 
   # Invalidates all the remember me tokens when the user signs out.
   config.expire_all_remember_me_on_sign_out = true
@@ -221,7 +221,7 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  # config.timeout_in = 30.minutes
+  config.timeout_in = 30.minutes
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
