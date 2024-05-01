b7e83f6e5c4c8b37073973c3700a3db52863447f
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2024-05-01 12:11:30 -0400

    Me/dpc 4024 add verification fields to db (#2160)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-4024
    
    ## 🛠 Changes
    
    Added verification fields to portal db as described
    [here](https://confluence.cms.gov/display/DAPC/DPC+Portal%3A+Data+Model+for+Tracking+PECOS+Verification+Statuses).
    
    ## ℹ️ Context for reviewers
    
    - Added `verification_status`, `verification_reason` and
    `last_checked_at` fields to the `ao_org_links`, `provider_organizations`
    and `user` tables.
    - Added tests to make sure validations work.
    
    ## ✅ Acceptance Validation
    
    Ran the migration locally and made sure the columns were added to the db
    correctly.
    
    ## 🔒 Security Implications
    
    - [ ] This PR adds a new software dependency or dependencies.
    - [ ] This PR modifies or invalidates one or more of our security
    controls.
    - [ ] This PR stores or transmits data that was not stored or
    transmitted before.
    - [ ] This PR requires additional review of its security implications
    for other reasons.
    
    If any security implications apply, add Jason Ashbaugh (GitHub username:
    StewGoin) as a reviewer and do not merge this PR without his approval.

--- dpc-portal/app/models/ao_org_link.rb
+++ dpc-portal/app/models/ao_org_link.rb
@@ -6,6 +6,10 @@ class AoOrgLink < ApplicationRecord
             uniqueness: { scope: :provider_organization_id, message: 'already exists for this provider.' }
   validates :invitation_id, allow_nil: true,
                             uniqueness: { scope: :invitation_id, message: 'already used by another AoOrgLink.' }
+  validates :verification_reason, allow_nil: true, allow_blank: true,
+                                  inclusion: { in: :verification_reason }
+
+  enum verification_reason: %i[ao_removal user_med_sanction no_approved_enrollments org_med_sanction]
 
   belongs_to :user, required: true
   belongs_to :provider_organization, required: true
