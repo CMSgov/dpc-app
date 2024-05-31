8a6a9df3fec08daea742d1deebcdb96ef2a124f2
Ashley Weaver <ashleyweaver@navapbc.com>
2024-05-31 16:48:20 -0400

    Update dpc-portals-test.sh

--- dpc-portals-test.sh
+++ dpc-portals-test.sh
@@ -4,7 +4,7 @@ set -e
 echo "┌───────────────────────────────┐"
 echo "│                               │"
 echo "│  Running Web, Admin & Portal  |"
-echo "|            Tests              │"
+echo "|             Tests             │"
 echo "│                               │"
 echo "└───────────────────────────────┘"
 
