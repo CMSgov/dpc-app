96b25d2c899f51bf9573fed23990810c5cddba04
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2024-06-18 11:36:17 -0400

    clean up data and docker queue after tests

--- dpc-test.sh
+++ dpc-test.sh
@@ -13,8 +13,10 @@ set -o allexport
 set +o allexport
 
 function _finally {
-  docker-compose down
+  docker-compose -p start-v1-app down
+  docker volume rm start-v1-app_pgdata14
 }
+
 trap _finally EXIT
 
 if [ -n "$REPORT_COVERAGE" ]; then
@@ -42,28 +44,28 @@ if [ -n "$REPORT_COVERAGE" ]; then
   mvn jacoco:report -ntp
 fi
 
-docker-compose down
+docker-compose -p start-v1-app down
 docker volume rm start-v1-app_pgdata14
 docker-compose -p start-v1-app up start_core_dependencies
-docker-compose up start_api_dependencies
+docker-compose -p start-v1-app up start_api_dependencies
 
 # Run the integration tests
-docker-compose up --exit-code-from tests tests
+docker-compose -p start-v1-app up --exit-code-from tests tests
 
-docker-compose down
+docker-compose -p start-v1-app down
 docker volume rm start-v1-app_pgdata14
 docker-compose -p start-v1-app up start_core_dependencies
-docker-compose up start_api_dependencies
+docker-compose -p start-v1-app up start_api_dependencies
 
 # Start the API server
-AUTH_DISABLED=true docker-compose up start_api start_consent
+AUTH_DISABLED=true docker-compose -p start-v1-app up start_api start_consent
 
 # Run the Postman tests
 npm install
 npm run test
 
 # Wait for Jacoco to finish writing the output files
-docker-compose down -t 60
+docker-compose -p start-v1-app down -t 60
 
 # Collect the coverage reports for the Docker integration tests
 if [ -n "$REPORT_COVERAGE" ]; then
