df51ba0b1ee4b9f73aa05aebcc581d5c00607aa3
Kevin Yeh <kevinyeah@utexas.edu>
2024-06-21 11:26:12 -0400

    Revert logging config in local dev

--- dpc-portal/config/initializers/logging.rb
+++ dpc-portal/config/initializers/logging.rb
@@ -3,8 +3,10 @@
 require './app/lib/dpc_json_logger'
 
 Rails.application.configure do
+  unless ENV['DISABLE_JSON_LOGGER'] == 'true'
     Rails.logger = DpcJsonLogger.new($stdout)
     config.logger = Rails.logger
     config.logger.formatter = DpcJsonLogger.formatter
     config.log_formatter = DpcJsonLogger.formatter
+  end
 end
