678173bbddc61693c136adfe3e57372184881577
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2024-07-26 16:48:06 -0400

    use dpc logo in email

--- dpc-portal/app/mailers/invitation_mailer.rb
+++ dpc-portal/app/mailers/invitation_mailer.rb
@@ -14,15 +14,10 @@ class InvitationMailer < ApplicationMailer
     @invitation = params[:invitation]
     @given_name = params[:given_name]
     @family_name = params[:family_name]
-    attach_logos
+    attachments.inline['dpc-logo.svg'] = File.read("#{Rails.root}/app/assets/images/dpc.svg")
     mail(
       to: @invitation.invited_email,
       subject: 'Time to register your organization with DPC'
     )
-  end
-
-  def attach_logos
-    attachments.inline['dpc-logo-mail.png'] = File.read("#{Rails.root}/app/assets/images/site-logo.png")
-    attachments.inline['cms-logo-mail.png'] = File.read("#{Rails.root}/app/assets/images/site-logo.png")
   end
 end
