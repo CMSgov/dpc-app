2cf27c7552c651245b0cdb3d5126d111eb76270b
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2024-08-02 14:14:33 -0400

    Comment formatting

--- dpc-testing/src/main/java/gov/cms/dpc/testing/MBIUtil.java
+++ dpc-testing/src/main/java/gov/cms/dpc/testing/MBIUtil.java
@@ -3,8 +3,6 @@ package gov.cms.dpc.testing;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public final class MBIUtil {
-	// Creates a fake MBI that matches the format defined in our PatientEntity
-
 	/**
 	 * Creates a fake MBI that matches the format defined in PatientEntity
 	 * @return the MBI
