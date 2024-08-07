c7d37725381c0e23492482e37b055dc45101d7f1
Ashley Weaver <ashleyweaver@navapbc.com>
2024-08-07 15:46:46 -0400

    Update accessibility_spec.rb

--- dpc-portal/spec/features/accessibility_spec.rb
+++ dpc-portal/spec/features/accessibility_spec.rb
@@ -5,21 +5,12 @@ require 'rails_helper'
 RSpec.describe 'Accessibility', type: :request, js: true, accessibility: true do
   include DpcClientSupport
 
-  context 'client tokens' do
-    let(:terms_of_service_accepted_by) { create(:user) }
-    let!(:org) { create(:provider_organization, terms_of_service_accepted_by:) }
-    let!(:user) { create(:user) }
-
-    before do
-      create(:cd_org_link, provider_organization: org, user:)
-      sign_in user
-      WebMock.allow_net_connect!
-    end
-
+  context 'not signed in' do
+    before { WebMock.allow_net_connect! }
     after { WebMock.disable_net_connect! }
 
-    it 'GET /new is accessible' do
-      visit "/organizations/#{org.id}/client_tokens/new"
+    it 'is accessible' do
+      visit '/portal/users/sign_in'
       expect(page).to be_axe_clean
     end
   end
