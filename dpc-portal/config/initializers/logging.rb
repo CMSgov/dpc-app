3ad2bd26e5818e813758e29237c7f7de4aab0911
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2024-08-09 11:44:57 -0400

    move logging configuration to application.rb because otherwise Rails.logger != logger

--- dpc-portal/config/initializers/logging.rb
+++ dpc-portal/config/initializers/logging.rb
@@ -2,15 +2,6 @@
 
 require './app/lib/dpc_json_logger'
 
-Rails.application.configure do
-  unless ENV['DISABLE_JSON_LOGGER'] == 'true'
-    Rails.logger = DpcJsonLogger.new($stdout)
-    config.logger = Rails.logger
-    config.logger.formatter = DpcJsonLogger.formatter
-    config.log_formatter = DpcJsonLogger.formatter
-  end
-end
-
 module LoggingConstants
   module ActionContext
     Registration = 'Registration'
