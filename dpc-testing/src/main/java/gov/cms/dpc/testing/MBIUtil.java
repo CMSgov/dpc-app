c9f4ab76ee73bfb5ac5e12c22e18bec0a7f7c94c
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2024-08-09 12:35:51 -0400

    Me/dpc 4174 attribution group performance (#2229)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-4174
    
    ## 🛠 Changes
    
    Updated Group endpoint so that the `POST`, `PUT`, and `POST $add`
    methods all batch their DB requests to improve performance.
    
    Individual _select_ statements have been bundled together into one
    _select in_ statement, and Hibernate has been configured to batch
    inserts/updates together and send them to the DB in one transaction.
    
    ## ℹ️ Context
    
    Performance, especially in our `$add` operation, was particularly bad,
    with some operations taking 5 minutes or more. This is because each
    patient was processed one at a time, first checking if the patient
    exists, then checking if they're already part of the group, then
    updating their attribution to the group. We currently allow 1350
    patients per group, and with 3 transactions each that's potentially 4k
    DB transactions per each $add.
    
    Now we combine the existence and group checks for all patients into 2
    _select_ statements, and have Hibernate batch the _inserts/updates_ into
    groups of 100. This gives us a worst case scenario of 16 DB transactions
    per each `$add`.
    
    ## 🧪 Validation
    
    Turned on Hibernate batch logging and verified that inserts/updates were
    being batched in groups of 100.
    
    Created integration tests that create and add groups of 1350 patients
    and verified they only took a few seconds.

---
+++ dpc-testing/src/main/java/gov/cms/dpc/testing/MBIUtil.java
@@ -0,0 +1,25 @@
+package gov.cms.dpc.testing;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public final class MBIUtil {
+	/**
+	 * Creates a fake MBI that matches the format defined in PatientEntity
+	 * @return the MBI
+	 */
+	public static String generateMBI() {
+		String mbi;
+
+		mbi = RandomStringUtils.randomNumeric(1);
+		mbi += RandomStringUtils.randomAlphabetic(1);
+		mbi += RandomStringUtils.randomAlphanumeric(1);
+		mbi += RandomStringUtils.randomNumeric(1);
+		mbi += RandomStringUtils.randomAlphabetic(1);
+		mbi += RandomStringUtils.randomAlphanumeric(1);
+		mbi += RandomStringUtils.randomNumeric(1);
+		mbi += RandomStringUtils.randomAlphabetic(2);
+		mbi += RandomStringUtils.randomNumeric(2);
+
+		return mbi;
+	}
+}
