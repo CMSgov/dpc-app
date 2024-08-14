c30cf60b8202c513906d3ddb57d1bca54e50aa0b
Luke Short <lukeshortemail@gmail.com>
2024-08-14 15:17:49 -0700

    add a docstring for refresh()

--- dpc-common/src/main/java/gov/cms/dpc/common/hibernate/attribution/DPCAbstractDAO.java
+++ dpc-common/src/main/java/gov/cms/dpc/common/hibernate/attribution/DPCAbstractDAO.java
@@ -8,6 +8,15 @@ public class DPCAbstractDAO<E> extends AbstractDAO<E> {
         super(sessionFactory);
     }
 
+    /**
+     * Use this method to get the latest changes of the hibernate database object,
+     * bypassing the session cache.
+     * Default behavior for hibernate is to cache queries for duration of a session.
+     * Meaning, if you call rosterDAO.getEntity() more than once within the same session
+     * Hibernate may return the cached value without the latest updates.
+     *
+     * @param databaseObject instance of database model representing table being queried
+     */
     public void refresh(E databaseObject) {
         currentSession().flush();
         currentSession().refresh(databaseObject);
