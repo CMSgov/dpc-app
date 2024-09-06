753379ec88a6117c0a8d45acc896be50286e85ee
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2024-09-06 11:56:33 -0400

    DPC-4230 Code-first Credential Delegate Flow (#2258)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-4230
    
    ## 🛠 Changes
    
    - Accept and Confirm endpoints now AO only (as is the Accept component)
    - Code, VerifyCode, and ConfirmCD endpoints added (along with
    OtpComponent)
    - Start component has button to new CD flow
    - Invitation now checks for match with email instead of all_emails
    - Invitations controller and request-spec reordered for better
    understanding
    
    ## ℹ️ Context
    
    The CD flow was rearranged to have the confirmation code before rather
    than after identity-proofing.
    The move from email to all_emails was due to client requirements
    
    ## 🧪 Validation
    
    Automated and Manual testing

--- dpc-portal/app/controllers/invitations_controller.rb
+++ dpc-portal/app/controllers/invitations_controller.rb
@@ -5,28 +5,65 @@ class InvitationsController < ApplicationController
   before_action :load_organization
   before_action :load_invitation
   before_action :validate_invitation, except: %i[renew]
-  before_action :check_for_token, except: %i[login renew show]
-  before_action :invitation_matches_user, only: %i[accept]
-  before_action :invitation_matches_conditions, only: %i[confirm]
+  before_action :verify_ao_invitation, only: %i[accept confirm]
+  before_action :verify_cd_invitation, only: %i[code verify_code confirm_cd]
+  before_action :check_for_token, only: %i[accept confirm confirm_cd register]
 
   def show
     render(Page::Invitations::StartComponent.new(@organization, @invitation))
   end
 
+  # AO Flow
   def accept
-    session["invitation_status_#{@invitation.id}"] = 'identity_verified'
+    invitation_matches_user
+    return if performed?
+
     render(Page::Invitations::AcceptInvitationComponent.new(@organization, @invitation, @given_name, @family_name))
   end
 
   def confirm
-    session["invitation_status_#{@invitation.id}"] = 'conditions_verified'
+    unless session["invitation_status_#{@invitation.id}"] == 'identity_verified'
+      return redirect_to accept_organization_invitation_url(@organization, @invitation)
+    end
+
+    verify_user_is_ao
+    return if performed?
+
+    session["invitation_status_#{@invitation.id}"] = 'verification_complete'
     render(Page::Invitations::RegisterComponent.new(@organization, @invitation))
   end
 
+  # CD Flow
+  def code
+    render(Page::Invitations::OtpComponent.new(@organization, @invitation))
+  end
+
+  def verify_code
+    unless params[:verification_code] == @invitation.verification_code
+      @invitation.errors.add(:verification_code, :bad_code, message: 'tbd')
+      return render(Page::Invitations::OtpComponent.new(@organization, @invitation), status: :bad_request)
+    end
+
+    session["invitation_status_#{@invitation.id}"] = 'code_verified'
+    render(Page::Invitations::InvitationLoginComponent.new(@invitation))
+  end
+
+  def confirm_cd
+    unless session["invitation_status_#{@invitation.id}"] == 'code_verified'
+      return redirect_to code_organization_invitation_url(@organization, @invitation)
+    end
+
+    invitation_matches_user
+    return if performed?
+
+    session["invitation_status_#{@invitation.id}"] = 'verification_complete'
+    render(Page::Invitations::RegisterComponent.new(@organization, @invitation))
+  end
+
+  # Everybody
   def register
-    unless session["invitation_status_#{@invitation.id}"] == 'conditions_verified'
-      return redirect_to accept_organization_invitation_url(@organization,
-                                                            @invitation)
+    unless session["invitation_status_#{@invitation.id}"] == 'verification_complete'
+      return redirect_to organization_invitation_url(@organization, @invitation)
     end
 
     return unless create_link
@@ -67,6 +104,57 @@ class InvitationsController < ApplicationController
   end
 
   private
+
+  def invitation_matches_user
+    user_info = UserInfoService.new.user_info(session)
+    unless @invitation.match_user?(user_info)
+      render(Page::Invitations::BadInvitationComponent.new(@invitation, 'pii_mismatch'),
+             status: :forbidden)
+    end
+    session["invitation_status_#{@invitation.id}"] = 'identity_verified'
+    @given_name = user_info['given_name']
+    @family_name = user_info['family_name']
+  rescue UserInfoServiceError => e
+    handle_user_info_service_error(e, 1)
+  end
+
+  def verify_user_is_ao
+    user_info = UserInfoService.new.user_info(session)
+    result = @invitation.ao_match?(user_info) # raises if does not match
+    session[:user_pac_id] = result.dig(:ao_role, 'pacId')
+    log_waivers(result)
+  rescue VerificationError => e
+    status = AoVerificationService::SERVER_ERRORS.include?(e.message) ? :service_unavailable : :forbidden
+    log_ao_verification_error(e, status == :service_unavailable)
+    render(Page::Invitations::AoFlowFailComponent.new(@invitation, e.message, 2), status:)
+  rescue UserInfoServiceError => e
+    handle_user_info_service_error(e, 2)
+  end
+
+  def handle_user_info_service_error(error, step)
+    logger.error(['User Info Service unavailable',
+                  { actionContext: LoggingConstants::ActionContext::Registration, error: error.message }])
+
+    if error.message == 'unauthorized'
+      render(Page::Invitations::InvitationLoginComponent.new(@invitation))
+    elsif @invitation.credential_delegate?
+      render(Page::Invitations::BadInvitationComponent.new(@invitation, error.message),
+             status: :service_unavailable)
+    else
+      render(Page::Invitations::AoFlowFailComponent.new(@invitation, error.message, step),
+             status: :service_unavailable)
+    end
+  end
+
+  def login_session
+    session[:user_return_to] = if @invitation.authorized_official?
+                                 accept_organization_invitation_url(@organization, params[:id])
+                               else
+                                 confirm_cd_organization_invitation_url(@organization, params[:id])
+                               end
+    session['omniauth.nonce'] = @nonce = SecureRandom.hex(16)
+    session['omniauth.state'] = @state = SecureRandom.hex(16)
+  end
 
   def create_link
     if @invitation.credential_delegate?
@@ -122,89 +210,6 @@ class InvitationsController < ApplicationController
     @user.save
   end
 
-  def check_for_token
-    if session[:login_dot_gov_token].present? &&
-       session[:login_dot_gov_token_exp].present? &&
-       session[:login_dot_gov_token_exp] > Time.now
-      return
-    end
-
-    render(Page::Invitations::InvitationLoginComponent.new(@invitation))
-  end
-
-  def invitation_matches_user
-    user_info = UserInfoService.new.user_info(session)
-    unless @invitation.match_user?(user_info)
-      render(Page::Invitations::BadInvitationComponent.new(@invitation, 'pii_mismatch'),
-             status: :forbidden)
-    end
-    @given_name = user_info['given_name']
-    @family_name = user_info['family_name']
-  rescue UserInfoServiceError => e
-    handle_user_info_service_error(e, 1)
-  end
-
-  def invitation_matches_conditions
-    unless session["invitation_status_#{@invitation.id}"] == 'identity_verified'
-      return redirect_to accept_organization_invitation_url(@organization, @invitation)
-    end
-
-    return check_code if @invitation.credential_delegate?
-
-    check_ao
-  rescue UserInfoServiceError => e
-    handle_user_info_service_error(e, 2)
-  rescue VerificationError => e
-    status = AoVerificationService::SERVER_ERRORS.include?(e.message) ? :service_unavailable : :forbidden
-    log_ao_verification_error(e, status == :service_unavailable)
-    render(Page::Invitations::AoFlowFailComponent.new(@invitation, e.message, 2), status:)
-  end
-
-  def check_code
-    return if params[:verification_code] == @invitation.verification_code
-
-    @invitation.errors.add(:verification_code, :bad_code, message: 'tbd')
-    render(Page::Invitations::AcceptInvitationComponent.new(@organization, @invitation, @given_name, @family_name),
-           status: :bad_request)
-  end
-
-  def check_ao
-    user_info = UserInfoService.new.user_info(session)
-    result = @invitation.ao_match?(user_info)
-
-    log_waivers(result)
-    session[:user_pac_id] = result.dig(:ao_role, 'pacId') if result[:success]
-    result[:success]
-  end
-
-  def handle_user_info_service_error(error, step)
-    logger.error(['User Info Service unavailable',
-                  { actionContext: LoggingConstants::ActionContext::Registration, error: error.message }])
-
-    if error.message == 'unauthorized'
-      render(Page::Invitations::InvitationLoginComponent.new(@invitation))
-    elsif @invitation.credential_delegate?
-      render(Page::Invitations::BadInvitationComponent.new(@invitation, error.message),
-             status: :service_unavailable)
-    else
-      render(Page::Invitations::AoFlowFailComponent.new(@invitation, error.message, step),
-             status: :service_unavailable)
-    end
-  end
-
-  def log_ao_verification_error(error, service_unavailable)
-    if service_unavailable
-      logger.error(['CPI API Gateway unavailable',
-                    { actionContext: LoggingConstants::ActionContext::Registration, error: error.message }])
-    else
-      logger.info(['AO Check Fail',
-                   { actionContext: LoggingConstants::ActionContext::Registration,
-                     actionType: LoggingConstants::ActionType::FailCpiApiGwCheck,
-                     verificationReason: error.message,
-                     invitation: @invitation.id }])
-    end
-  end
-
   def load_invitation
     @invitation = Invitation.find(params[:id])
     if @organization != @invitation.provider_organization
@@ -230,10 +235,35 @@ class InvitationsController < ApplicationController
            status: :forbidden)
   end
 
-  def login_session
-    session[:user_return_to] = accept_organization_invitation_url(@organization, params[:id])
-    session['omniauth.nonce'] = @nonce = SecureRandom.hex(16)
-    session['omniauth.state'] = @state = SecureRandom.hex(16)
+  def verify_ao_invitation
+    redirect_to organization_invitation_url(@organization, @invitation) unless @invitation.authorized_official?
+  end
+
+  def verify_cd_invitation
+    redirect_to organization_invitation_url(@organization, @invitation) unless @invitation.credential_delegate?
+  end
+
+  def check_for_token
+    if session[:login_dot_gov_token].present? &&
+       session[:login_dot_gov_token_exp].present? &&
+       session[:login_dot_gov_token_exp] > Time.now
+      return
+    end
+
+    render(Page::Invitations::InvitationLoginComponent.new(@invitation))
+  end
+
+  def log_ao_verification_error(error, service_unavailable)
+    if service_unavailable
+      logger.error(['CPI API Gateway unavailable',
+                    { actionContext: LoggingConstants::ActionContext::Registration, error: error.message }])
+    else
+      logger.info(['AO Check Fail',
+                   { actionContext: LoggingConstants::ActionContext::Registration,
+                     actionType: LoggingConstants::ActionType::FailCpiApiGwCheck,
+                     verificationReason: error.message,
+                     invitation: @invitation.id }])
+    end
   end
 
   def log_create_user
@@ -247,6 +277,21 @@ class InvitationsController < ApplicationController
                            actionType: LoggingConstants::ActionType::AoCreated }])
     end
   end
+
+  def log_waivers(role_and_waivers)
+    if role_and_waivers[:has_org_waiver]
+      Rails.logger.info(['Organization has a waiver',
+                         { actionContext: LoggingConstants::ActionContext::Registration,
+                           actionType: LoggingConstants::ActionType::OrgHasWaiver,
+                           invitation: @invitation.id }])
+    end
+    return unless role_and_waivers[:has_ao_waiver]
+
+    Rails.logger.info(['Authorized official has a waiver',
+                       { actionContext: LoggingConstants::ActionContext::Registration,
+                         actionType: LoggingConstants::ActionType::AoHasWaiver,
+                         invitation: @invitation.id }])
+  end
 end
 
 def redirect_host
@@ -258,19 +303,4 @@ def redirect_host
   else
     "https://#{ENV.fetch('ENV', nil)}.dpc.cms.gov"
   end
-end
-
-def log_waivers(role_and_waivers)
-  if role_and_waivers[:has_org_waiver]
-    Rails.logger.info(['Organization has a waiver',
-                       { actionContext: LoggingConstants::ActionContext::Registration,
-                         actionType: LoggingConstants::ActionType::OrgHasWaiver,
-                         invitation: @invitation.id }])
-  end
-  return unless role_and_waivers[:has_ao_waiver]
-
-  Rails.logger.info(['Authorized official has a waiver',
-                     { actionContext: LoggingConstants::ActionContext::Registration,
-                       actionType: LoggingConstants::ActionType::AoHasWaiver,
-                       invitation: @invitation.id }])
 end
