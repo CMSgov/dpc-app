a870f3edbc7c0613af872fbf587ea9bacee423b2
Christopher Ronning <cronning90@gmail.com>
2024-09-13 11:12:29 -0400

    create accept_invitation_component for CD

--- dpc-portal/spec/components/page/invitations/accept_invitation_component_spec.rb
+++ dpc-portal/spec/components/page/invitations/accept_invitation_component_spec.rb
@@ -31,14 +31,27 @@ RSpec.describe Page::Invitations::AcceptInvitationComponent, type: :component do
         expect(page.find('.usa-step-indicator__current-step').text).to eq '2'
       end
 
-      it 'should not have verification_code stanza' do
-        verification_code = <<~HTML
-          <div class="margin-bottom-4">
-            <label class="usa-label" for="verification_code">Enter the invite code:</label>
-            <input type="text" name="verification_code" id="verification_code" value="" maxlength="6" class="usa-input" />
-          </div>
-        HTML
-        is_expected.to_not include(normalize_space(verification_code))
+      it 'should have Continue to register button' do
+        is_expected.to include('Continue to register')
+      end
+    end
+
+    context 'credential delegate' do
+      let(:cd_invite) { Invitation.new(id: 6, invitation_type: :credential_delegate) }
+      let(:component) { described_class.new(org, cd_invite, given_name, family_name) }
+      it 'should match form method and action' do
+        form_url = "/portal/organizations/#{org.path_id}/invitations/#{cd_invite.id}/accept"
+        form_method_action = %(method="post" action="#{form_url}")
+        is_expected.to include(form_method_action)
+      end
+
+      it 'should have step component at step 2' do
+        expect(page).to have_selector('.usa-step-indicator__current-step')
+        expect(page.find('.usa-step-indicator__current-step').text).to eq '2'
+      end
+
+      it 'should have Accept Invite button' do
+        is_expected.to include('Accept Invite')
       end
     end
   end
