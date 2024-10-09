f720795c446b9347dc0b964c4ed6b76d5eeec6ef
Luke Short <lukeshortemail@gmail.com>
2024-10-09 08:12:10 -0700

    [DPC-4278] Content updates for default welcome page (#2284)
    ## 🎫 Ticket
    
    [DPC-4278](https://jira.cms.gov/browse/DPC-4278)
    
    ## 🛠 Changes
    
    - remove lorem ipsum placeholder text for `/portal/users/sign_in` and
    use latest content provided by design
    - Source of truth for content under "Page for returning users to sign
    in" on Confluence doc
    [HERE](https://confluence.cms.gov/display/DAPC/DPC+Portal+MVP+Copy#:~:text=Page%20for%20returning%20users%20to%20sign%20in).
    - link to sandbox on the right side of the page
    
    <!-- What was added, updated, or removed in this PR? -->
    
    ## ℹ️ Context
    
    <!-- Why were these changes made? Add background context suitable for a
    non-technical audience. -->
    
    <!-- If any of the following security implications apply, this PR must
    not be merged without Stephen Walter's approval. Explain in this section
    and add @SJWalter11 as a reviewer.
      - Adds a new software dependency or dependencies.
      - Modifies or invalidates one or more of our security controls.
      - Stores or transmits data that was not stored or transmitted before.
    - Requires additional review of security implications for other reasons.
    -->
     - Lorem ipsum placeholder text was used beforehand.
    
    ## 🧪 Validation
    
    <!-- How were the changes verified? Did you fully test the acceptance
    criteria in the ticket? Provide reproducible testing instructions and
    screenshots if applicable. -->
     - manually verified the content updates by running 
    - `make portal && make start-portal` then navigating to
    `http://localhost:3100/portal/users/sign_in`
    - see also updates to
    `dpc-portal/spec/components/page/session/login_component_spec.rb`

--- dpc-portal/spec/components/page/session/login_component_spec.rb
+++ dpc-portal/spec/components/page/session/login_component_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Page::Session::LoginComponent, type: :component do
   include ComponentSupport
   describe 'login component' do
     let(:url) { '/portal/' }
+    let(:sandbox_url) { 'https://sandbox.dpc.cms.gov/' }
     let(:component) { described_class.new(url) }
     before { render_inline(component) }
     it 'should be a usa section' do
@@ -14,14 +15,19 @@ RSpec.describe Page::Session::LoginComponent, type: :component do
 
     it 'should have a login button' do
       expect(page).to have_selector('button.usa-button')
-      expect(page.find('button.usa-button')).to have_content('Sign in with')
+      expect(page.find('button.usa-button.width-full')).to have_content('Sign in with')
       expect(page).to have_selector('button.usa-button span.login-button__logo')
       expect(page.find('button.usa-button span.login-button__logo')).to have_content('Login.gov')
     end
 
-    it 'should post to appropriate url' do
-      expect(page.find('form')[:action]).to eq url
-      expect(page.find('form')[:method]).to eq 'post'
+    it 'login.gov button should post to appropriate url' do
+      expect(page.find('form', match: :first)[:action]).to eq url
+      expect(page.find('form', match: :first)[:method]).to eq 'post'
+    end
+
+    it 'test data button should post to sandbox url' do
+      expect(page.find('form.sandbox_url')[:action]).to eq sandbox_url
+      expect(page.find('form.sandbox_url')[:method]).to eq 'get'
     end
 
     it 'should have two columns' do
