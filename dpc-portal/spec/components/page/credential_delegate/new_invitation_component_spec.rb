5ed3a06d80c9ea73375cea9b564f0d70e8d631bb
Ashley Weaver <ashleyweaver@navapbc.com>
2024-10-31 12:47:31 -0400

    Update error

--- dpc-portal/spec/components/page/credential_delegate/new_invitation_component_spec.rb
+++ dpc-portal/spec/components/page/credential_delegate/new_invitation_component_spec.rb
@@ -232,8 +232,8 @@ RSpec.describe Page::CredentialDelegate::NewInvitationComponent, type: :componen
           error = <<~HTML
             <div class="usa-alert usa-alert--error margin-bottom-4">
               <div class="usa-alert__body">
-                <h2 class="usa-alert__heading">#{I18n.t('attributes.base.duplicate_cd.status')}</h2>
-                <p class="usa-alert__text">#{I18n.t('attributes.base.duplicate_cd.text')}</p>
+                <h2 class="usa-alert__heading">#{I18n.t('errors.attributes.base.duplicate_cd.status')}</h2>
+                <p class="usa-alert__text">#{I18n.t('errors.attributes.base.duplicate_cd.text')}</p>
               </div>
             </div>
             <h1>Invite new user</h1>
