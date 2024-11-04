a3fc25f453c0b4745e10a9cd1eb63ec7fe77da74
ceh1 <ceh1@192.168.8.168>
2024-11-04 11:59:49 -0500

    next experiment with threading problems in cicd

--- ops/scripts/is_aws_ec2.sh
+++ ops/scripts/is_aws_ec2.sh
@@ -4,35 +4,10 @@
 #
 # Josh Kupershmidt (https://serverfault.com/users/223639/josh-kupershmidt) 3/20/2018
 
-# This first, simple check will work for many older instance types.
-if [ -f /sys/hypervisor/uuid ]; then
-  # File should be readable by non-root users.
-  if [ "$(head -c 3 /sys/hypervisor/uuid)" = "ec2" ]; then
-    echo "yes"
-    exit 0
-  else
-    echo "no"
-    exit 1
-  fi
-
-# This check will work on newer m5/c5 instances, but only if you have root!
-elif [ -r /sys/devices/virtual/dmi/id/product_uuid ]; then
-  # If the file exists AND is readable by us, we can rely on it.
-  if [ "$(head -c 3 /sys/devices/virtual/dmi/id/product_uuid)" = "EC2" ]; then
-    echo "yes"
-    exit 0
-  else
-    echo "no"
-    exit 1
-  fi
-
+if [ -n "$ECS_CONTAINER_METADATA_URI" ]; then 
+  echo "yes"
+  exit 0
 else
-  # Fallback check of http://169.254.169.254/. 
-  if curl -s -m 1 http://169.254.169.254/latest/dynamic/instance-identity/document | grep -q availabilityZone; then
-    echo "yes"
-    exit 0
-  else
-    echo "no"
-    exit 1
-  fi
+  echo "no"
+  exit 1
 fi
