e65542a4890d6f30f686caae312d1acc26f1d09a
ceh1 <ceh1@192.168.8.168>
2024-11-04 18:56:25 -0500

    next experiment with threading problems in cicd

--- scripts/build-custom-jersey2-guice.sh
+++ scripts/build-custom-jersey2-guice.sh
@@ -66,16 +66,9 @@ else
   echo "done!"
 fi
 
-# Step 3: Build the Jersey2-Guice projects
-echo -n "	Step 3: Building the custom Jersey2-Guice v1.0.6-DPC...";
+# Step 3: Build the Jersey2-Guice spi project
+echo -n "	Step 3: Building the custom Jersey2-Guice SPI v1.0.6-DPC...";
 (cd tmp/jersey2-guice/jersey2-guice-spi && mvn clean package -DskipTests > /dev/null 2>&1)
-
-if [ $? -ne 0 ]; then
-  echo "build failed!"
-  exit 1
-fi
-
-(cd tmp/jersey2-guice/jersey2-guice-impl && mvn clean package -ntp -DskipTests)# > /dev/null 2>&1)
 
 if [ $? -ne 0 ]; then
   echo "build failed!"
@@ -84,7 +77,7 @@ else
   echo "done!"
 fi
 
-# Step 4: Install the artifacts in the custom repository (./repo)
+# Step 4: Install the jersey2-guice-spi artifacts in the custom repository (./repo)
 echo -n "	Step 4: Installing the artifacts in the project repository...";
 (cd tmp/jersey2-guice/jersey2-guice-spi && mvn install:install-file \
   -Dfile=target/jersey2-guice-spi-1.0.6-DPC.jar \
@@ -94,6 +87,25 @@ echo -n "	Step 4: Installing the artifacts in the project repository...";
   -Dpackaging=jar \
   -DlocalRepositoryPath=$REPO_DIR > /dev/null 2>&1)
 
+# Step 5: Generate checksums
+echo -n "	Step 5: Generating checksums for the jersey2-guice-spi project..."
+(cd repo/com/squarespace/jersey2-guice/jersey2-guice-spi/1.0.6-DPC && md5sum jersey2-guice-spi-1.0.6-DPC.pom > jersey2-guice-spi-1.0.6-DPC.pom.md5)
+(cd repo/com/squarespace/jersey2-guice/jersey2-guice-spi/1.0.6-DPC && sha1sum jersey2-guice-spi-1.0.6-DPC.pom > jersey2-guice-spi-1.0.6-DPC.pom.sha1)
+echo "done!" 
+
+# Step 6: Build the Jersey2-Guice Impl project
+echo -n "	Step 6: Building the custom Jersey2-Guice implementation v1.0.6-DPC...";
+(cd tmp/jersey2-guice/jersey2-guice-impl && mvn clean package -ntp -DskipTests)# > /dev/null 2>&1)
+
+if [ $? -ne 0 ]; then
+  echo "build failed!"
+  exit 1
+else
+  echo "done!"
+fi
+
+# Step 7: Install the jersey2-guice-impl artifacts in the custom repository (./repo)
+echo -n "	Step 7: Installing the jersey2-guice-impl artifacts in the project repository...";
 (cd tmp/jersey2-guice/jersey2-guice-impl && mvn install:install-file \
   -Dfile=target/jersey2-guice-impl-1.0.6-DPC.jar \
   -DgroupId=com.squarespace.jersey2-guice \
@@ -109,16 +121,14 @@ else
   echo "done!"
 fi
 
-# Step 5: Generate checksums
-echo -n "	Step 5: Generating checksums for the project..."
-(cd repo/com/squarespace/jersey2-guice/jersey2-guice-spi/1.0.6-DPC && md5sum jersey2-guice-spi-1.0.6-DPC.pom > jersey2-guice-spi-1.0.6-DPC.pom.md5)
-(cd repo/com/squarespace/jersey2-guice/jersey2-guice-spi/1.0.6-DPC && sha1sum jersey2-guice-spi-1.0.6-DPC.pom > jersey2-guice-spi-1.0.6-DPC.pom.sha1)
+# Step 8: Generate checksums
+echo -n "	Step 8: Generating checksums for the jersey2-guice-impl project..."
 (cd repo/com/squarespace/jersey2-guice/jersey2-guice-impl/1.0.6-DPC && md5sum jersey2-guice-impl-1.0.6-DPC.jar > jersey2-guice-impl-1.0.6-DPC.jar.md5)
 (cd repo/com/squarespace/jersey2-guice/jersey2-guice-impl/1.0.6-DPC && sha1sum jersey2-guice-impl-1.0.6-DPC.jar > jersey2-guice-impl-1.0.6-DPC.jar.sha1)
 echo "done!" 
 
-# Step 7: Cleanup the cloned jersey2-guice repo
-echo -n "	Step 7: Removing the cloned jersey2-guice repo...";
+# Step 9: Cleanup the cloned jersey2-guice repo
+echo -n "	Step 9: Removing the cloned jersey2-guice repo...";
 rm -rf tmp/jersey2-guice;
 echo "done!"
 
