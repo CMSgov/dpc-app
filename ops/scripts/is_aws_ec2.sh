cfa8d69c9469423192e0e1700880779652d487f8
ceh1 <ceh1@192.168.8.168>
2024-11-04 14:02:45 -0500

    next experiment with threading problems in cicd

--- ops/scripts/is_aws_ec2.sh
+++ ops/scripts/is_aws_ec2.sh
@@ -4,6 +4,10 @@
 #
 # Josh Kupershmidt (https://serverfault.com/users/223639/josh-kupershmidt) 3/20/2018
 
+#temporarily disabling this check until understood when its really needed.
+echo "no"
+exit 1
+
 # Check for ECS environment variable
 if [ -n "$ECS_CONTAINER_METADATA_URI" ]; then 
   echo "yes"
