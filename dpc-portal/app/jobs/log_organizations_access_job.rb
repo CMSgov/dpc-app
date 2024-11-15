fdc2787d22eada8ef80e5750b369f1f9b1d362da
Luke Short <lukeshortemail@gmail.com>
2024-11-15 14:33:46 -0800

    update sidekiq job to include aggregate org credential status in logs

--- dpc-portal/app/jobs/log_organizations_access_job.rb
+++ dpc-portal/app/jobs/log_organizations_access_job.rb
@@ -5,20 +5,39 @@ class LogOrganizationsAccessJob < ApplicationJob
 
   def perform
     @start = Time.now
+    organizations_credential_aggregate_status = {
+      have_active_credentials: 0,
+      have_incomplete_or_no_credentials: 0,
+      have_no_credentials: 0,
+    }
     ProviderOrganization.where.not(terms_of_service_accepted_by: nil).find_each do |organization|
       credential_status = fetch_credential_status?(organization)
       Rails.logger.info(['Credential status for organization',
                          { name: organization.name, id: organization.id,
-                         has_prod_credentials: credential_status}])
+                           credential_status: credential_status }])
+      credential_status_values_as_arr = [credential_status["num_tokens"], credential_status["num_keys"], credential_status["num_ips"]]
+      if credential_status_values_as_arr.all?
+          organizations_credential_aggregate_status[:have_active_credentials] += 1
+      elsif credential_status_values_as_arr.any?
+          organizations_credential_aggregate_status[:have_incomplete_or_no_credentials] += 1
+      else
+          organizations_credential_aggregate_status[:have_incomplete_or_no_credentials] += 1
+          organizations_credential_aggregate_status[:have_no_credentials] += 1
+      end
     end
+    Rails.logger.info(['Organizations API credential status', organizations_credential_aggregate_status])
   end
 
   def fetch_credential_status?(organization)
-      tokens = dpc_client.get_client_tokens(organization.id)
-      pub_keys = dpc_client.get_public_keys(organization.id)
-      ip_addresses = dpc_client.get_ip_addresses(organization.id)
-      does_org_have_access = tokens["count"] >= 1 && pub_keys["count"] >= 1 && ip_addresses["count"] >= 1
-      return does_org_have_access
+    tokens = dpc_client.get_client_tokens(organization.id)
+    pub_keys = dpc_client.get_public_keys(organization.id)
+    ip_addresses = dpc_client.get_ip_addresses(organization.id)
+
+    return {
+      "num_tokens": tokens['count'],
+      "num_keys": pub_keys['count'],
+      "num_ips": ip_addresses['count'],
+    }
   end
 
   def dpc_client
