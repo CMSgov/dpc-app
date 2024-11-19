2238a64ee8dd966286ed8b73b796712b12888537
Luke Short <lukeshortemail@gmail.com>
2024-11-19 09:51:26 -0800

    add unit tests for full and no credentials

--- dpc-portal/spec/jobs/log_organizations_access_spec.rb
+++ dpc-portal/spec/jobs/log_organizations_access_spec.rb
@@ -25,12 +25,12 @@ RSpec.describe LogOrganizationsAccessJob, type: :job do
   end
 
   describe 'perform' do
-    it 'updates log with 1 organization for have_incomplete_or_no_credentials' do
+    it 'organization has partial credentials' do
       provider_organization.save!
 
-      expect(mock_dpc_client).to receive(:get_client_tokens).and_return({ count: 0 }).once
-      expect(mock_dpc_client).to receive(:get_public_keys).and_return({ count: 0 }).once
-      expect(mock_dpc_client).to receive(:get_ip_addresses).and_return({ count: 0 }).once
+      expect(mock_dpc_client).to receive(:get_client_tokens).and_return({ 'count' => 0 }).once
+      expect(mock_dpc_client).to receive(:get_public_keys).and_return({ 'count' => 0 }).once
+      expect(mock_dpc_client).to receive(:get_ip_addresses).and_return({ 'count' => 0 }).once
       allow(Rails.logger).to receive(:info)
       expect(Rails.logger).to receive(:info).with(['Organizations API credential status',
                                                    { have_active_credentials: 0,
@@ -39,6 +39,34 @@ RSpec.describe LogOrganizationsAccessJob, type: :job do
 
       described_class.perform_now
     end
+  end
+  it 'updates log with 1 organization that has all 3 credentials' do
+    provider_organization.save!
+
+    expect(mock_dpc_client).to receive(:get_client_tokens).and_return({ 'count' => 1 }).once
+    expect(mock_dpc_client).to receive(:get_public_keys).and_return({ 'count' => 2 }).once
+    expect(mock_dpc_client).to receive(:get_ip_addresses).and_return({ 'count' => 3 }).once
+    allow(Rails.logger).to receive(:info)
+    expect(Rails.logger).to receive(:info).with(['Organizations API credential status',
+                                                 { have_active_credentials: 1,
+                                                   have_incomplete_or_no_credentials: 0,
+                                                   have_no_credentials: 0 }])
+
+    described_class.perform_now
+  end
+  it 'updates log with 1 organization that has partial credentials' do
+    provider_organization.save!
+
+    expect(mock_dpc_client).to receive(:get_client_tokens).and_return({ 'count' => 1 }).once
+    expect(mock_dpc_client).to receive(:get_public_keys).and_return({ 'count' => 2 }).once
+    expect(mock_dpc_client).to receive(:get_ip_addresses).and_return({ 'count' => 0 }).once
+    allow(Rails.logger).to receive(:info)
+    expect(Rails.logger).to receive(:info).with(['Organizations API credential status',
+                                                 { have_active_credentials: 0,
+                                                   have_incomplete_or_no_credentials: 1,
+                                                   have_no_credentials: 0 }])
+
+    described_class.perform_now
   end
 end
 
