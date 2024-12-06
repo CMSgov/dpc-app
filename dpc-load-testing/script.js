356d0d3ed4c2fad0d42ba680a0d7f499f01dc0d3
Christopher Ronning <cronning90@gmail.com>
2024-12-06 11:15:16 -0500

    remove on: push: parameters on YML

--- dpc-load-testing/script.js
+++ dpc-load-testing/script.js
@@ -1,6 +1,8 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+// See https://grafana.com/docs/k6/latest/using-k6/k6-options/reference for
+// details on this configuration object.
 export const options = {
   // A number specifying the number of VUs to run concurrently.
   vus: 10,
