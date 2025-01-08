faa57ddd93d55da3f0c5381d80f2e0876f12c85f
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2025-01-08 14:41:44 -0500

    Me/dpc 4437 add docker healthchecks (#2369)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-4437
    
    ## 🛠 Changes
    
    - Removed `wait-for-dependencies` images and replaced with docker
    compose health checks and wait flags.
    - Updated the makefile to use the new docker compose changes.
    - Added a `USE_BFD_MOCK` flag to dpc-api.
    - Added _curl_ to our docker images that didn't already have it.
    
    ### Note:
    
    Assuming you're running things through the makefile, there should be no
    functionality changes whatsoever. If you call `docker compose up`
    manually, though, you'll have to call the services you want directly
    instead of through their `wait-for-dependencies` services.
    
    ## ℹ️ Context
    
    _dadarek/wait-for-dependencies_ hasn't been updated in about seven
    years, and in that time Docker Compose has added better functionality
    natively, in the form of health checks and the `--wait` flag.
    
    In the past, you would call _dadarek/wait-for-dependencies_ and it would
    start the service you wanted, then wait for it to open up a particular
    port indicating the service was up, then exit and return to the caller.
    Now, you call `docker compose up SERVICE --wait`, and it waits for the
    service to start and pass its health check before returning to the
    caller. This way we know the service is up and definitely ready to
    handle requests. Services that depend on one another will also now wait
    until the services they depend on are healthy, not just running, before
    they start.
    
    ## 🧪 Validation
    
    Ran all makefile targets locally.

--- dpc-portal-test.sh
+++ dpc-portal-test.sh
@@ -17,7 +17,7 @@ echo "└───────────────────────�
 make portal
 
 # Prepare the environment 
-docker compose -p start-v1-portals -f docker-compose.yml -f docker-compose.portals.yml up start_core_dependencies
+docker compose -p start-v1-portals -f docker-compose.yml -f docker-compose.portals.yml up db --wait
 docker compose -p start-v1-portals -f docker-compose.yml -f docker-compose.portals.yml run --entrypoint "bundle exec rails db:create db:migrate RAILS_ENV=test" dpc_portal
 
 # Run the tests
