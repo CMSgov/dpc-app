1c8d23f587f21356cd8d7ef4c56eda59e7950608
Ashley Weaver <ashleyweaver@navapbc.com>
2025-01-17 12:22:08 -0500

    Update Docker logfile

--- dpc-test.sh
+++ dpc-test.sh
@@ -46,7 +46,6 @@ fi
 
 docker compose -p start-v1-app down
 docker volume rm start-v1-app_pgdata16
-docker logs -f start-v1-app-api-1 &> docker-logs.txt &
 USE_BFD_MOCK=true docker compose -p start-v1-app up db attribution aggregation --wait
 
 # Run the integration tests
@@ -57,7 +56,7 @@ docker volume rm start-v1-app_pgdata16
 
 echo "Starting Postman tests"
 # Start the API server
-USE_BFD_MOCK=true AUTH_DISABLED=true docker compose -p start-v1-app up db attribution aggregation consent api --wait
+USE_BFD_MOCK=true AUTH_DISABLED=true docker compose -p start-v1-app up db attribution aggregation consent api --wait &> docker-compose.log
 
 # Run the Postman tests
 npm install
