ad96c0339d0c3a18eb0356105cdfd2f5b2a3b49c
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2025-01-22 11:59:17 -0500

    More coverage.

--- dpc-attribution/src/test/java/gov/cms/dpc/attribution/utils/RESTUtilsUnitTest.java
+++ dpc-attribution/src/test/java/gov/cms/dpc/attribution/utils/RESTUtilsUnitTest.java
@@ -1,14 +1,17 @@
 package gov.cms.dpc.attribution.utils;
 
 import gov.cms.dpc.common.hibernate.attribution.DPCAbstractDAO;
+import org.apache.commons.lang3.exception.UncheckedException;
 import org.hl7.fhir.dstu3.model.Patient;
 import org.hl7.fhir.dstu3.model.Resource;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import javax.ws.rs.WebApplicationException;
 import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 
+import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 
@@ -30,6 +33,14 @@ class RESTUtilsUnitTest {
 		RESTUtils.bulkResourceHandler(Stream.of(), action, dao, 10);
 
 		Mockito.verify(action, Mockito.never()).apply(any());
+	}
+
+	@Test
+	void test_bulkResourceHandler_error_on_action() {
+		Mockito.when(action.apply(any())).thenThrow(UncheckedException.class);
+
+		assertThrows(WebApplicationException.class, () ->
+			RESTUtils.bulkResourceHandler(Stream.of(new Patient()), action, dao, 10));
 	}
 
 	@Test
