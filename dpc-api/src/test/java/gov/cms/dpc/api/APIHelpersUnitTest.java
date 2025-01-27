c927a18f95460fd4bfca35dffcfcecb35815acad
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2025-01-27 15:26:18 -0500

    Pull in changes from PR 2394

--- dpc-api/src/test/java/gov/cms/dpc/api/APIHelpersUnitTest.java
+++ dpc-api/src/test/java/gov/cms/dpc/api/APIHelpersUnitTest.java
@@ -11,7 +11,7 @@ import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
-import javax.ws.rs.WebApplicationException;
+import jakarta.ws.rs.WebApplicationException;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Function;
