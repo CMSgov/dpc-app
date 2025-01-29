a80bd3a96bade1b81810c1ebfcee1cad212b5ef0
Ashley Weaver <ashleyweaver@navapbc.com>
2025-01-29 09:35:31 -0500

    Update 20250128130628_drop_fhir_endpoints_table.rb

--- dpc-web/db/migrate/20250128130628_drop_fhir_endpoints_table.rb
+++ dpc-web/db/migrate/20250128130628_drop_fhir_endpoints_table.rb
@@ -1,5 +1,12 @@
 class DropFhirEndpointsTable < ActiveRecord::Migration[7.2]
   def change
-    drop_table :fhir_endpoints
+    drop_table :fhir_endpoints, force: :cascade do |t|
+      t.string "name", null: false
+      t.integer "status", null: false
+      t.string "uri", null: false
+      t.integer "organization_id"
+      t.integer "registered_organization_id"
+      t.index ["registered_organization_id"], name: "index_fhir_endpoints_on_registered_organization_id"
+    end
   end
 end
