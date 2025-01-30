a3f931fcab5b3556012c183c978ea414517d9ace
Luke Short <lukeshortemail@gmail.com>
2025-01-29 21:42:20 -0800

    add comments explaining next steps when disk space issues resolved

--- dpc-test.sh
+++ dpc-test.sh
@@ -75,7 +75,3 @@ echo "│                                          │"
 echo "│             All Tests Complete           │"
 echo "│                                          │"
 echo "└──────────────────────────────────────────┘"
-
-echo "images here!"
-docker image ls
-docker compose images
