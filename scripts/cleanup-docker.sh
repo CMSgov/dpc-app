4c19f2274702ace16fb74f8c89dc4a67d608c363
Luke Short <lukeshortemail@gmail.com>
2025-01-31 11:49:26 -0800

    see if only removing dangling fixes this???

--- scripts/cleanup-docker.sh
+++ scripts/cleanup-docker.sh
@@ -4,4 +4,5 @@ RUNNING=`docker ps -q`
 if [ -n "$RUNNING" ]; then
     docker stop $(docker ps -q)
 fi
-docker system prune -a --volumes -f
+#docker system prune -a --volumes -f
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
