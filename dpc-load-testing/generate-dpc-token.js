c3fb307b288db141c47b80e27a98337110a01239
Christopher Ronning <cronning90@gmail.com>
2025-02-05 10:43:32 -0500

    use admin url from secrets

--- dpc-load-testing/generate-dpc-token.js
+++ dpc-load-testing/generate-dpc-token.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 
-const adminUrl = __ENV.ENVIRONMENT == 'local' ? 'http://host.docker.internal:9903' : 'http://frontend-internal.dpc-test.local:9900';
+const adminUrl = __ENV.ENVIRONMENT == 'local' ? 'http://host.docker.internal:9903' : __ENV.API_ADMIN_URL;
 const testOrgId = __ENV.LOAD_TEST_ORGANIZATION_ID;
 
 const fetchTokenURL = `${adminUrl}/tasks/generate-token?organization=${testOrgId}`;
