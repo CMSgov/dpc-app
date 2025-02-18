fd0e37eccdba0b5cfe5795251e995d9b3dc8abf3
Ashley Weaver <ashleyweaver@navapbc.com>
2025-02-18 10:00:34 -0500

    Revert "Update DAO tests"
    This reverts commit 115b1b1168ebe12e30634451c6d448c0d87ebd98.

--- dpc-testing/src/main/java/gov/cms/dpc/testing/AbstractDAOTest.java
+++ dpc-testing/src/main/java/gov/cms/dpc/testing/AbstractDAOTest.java
@@ -3,7 +3,7 @@ package gov.cms.dpc.testing;
 import io.dropwizard.testing.junit5.DAOTestExtension;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import io.dropwizard.util.Generics;
-import org.hibernate.cfg.JdbcSettings;
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.dialect.PostgreSQLDialect;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -30,7 +30,7 @@ public abstract class AbstractDAOTest<E> {
         postgreSql.start();
 
         db = DAOTestExtension.newBuilder()
-            .customizeConfiguration(c -> c.setProperty(JdbcSettings.DIALECT, PostgreSQLDialect.class.getName()))
+            .customizeConfiguration(c -> c.setProperty(AvailableSettings.DIALECT, PostgreSQLDialect.class.getName()))
             .setDriver(postgreSql.getDriverClassName())
             .setUrl(postgreSql.getJdbcUrl())
             .setUsername(postgreSql.getUsername())
