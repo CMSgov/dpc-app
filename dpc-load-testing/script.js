407ff939e5ed44d4492e41c2d387b76852dd8406
Christopher Ronning <cronning90@gmail.com>
2025-03-03 13:20:39 -0500

    create setup script for POST Organization/

--- dpc-load-testing/script.js
+++ dpc-load-testing/script.js
@@ -1,6 +1,8 @@
+import { check, sleep } from 'k6';
 import http from 'k6/http';
-import { sleep } from 'k6';
+import exec from 'k6/execution'
 import generateDPCToken from './generate-dpc-token.js';
+import { createOrganization } from './dpc-api-client.js';
 
 // See https://grafana.com/docs/k6/latest/using-k6/k6-options/reference for
 // details on this configuration object.
@@ -8,11 +10,45 @@ export const options = {
   // A number specifying the number of VUs to run concurrently.
   vus: 1,
   // A string specifying the total duration of the test run.
-  duration: '1m',
+  duration: '10s',
   thresholds: {
     http_req_failed: ['rate<0.01']
   }
 };
+
+let org1Id;
+let org2Id;
+
+// Sets up two test organizations and saves their UUIDs for use later.
+export function setup() {
+  // Fake NPIs generated online: https://jsfiddle.net/alexdresko/cLNB6
+  const org1 = createOrganization('2782823019', 'Test Org 1');
+  const org2 = createOrganization('8197402604', 'Test Org 2');
+
+  const checkOutput1 = check(
+    org1,
+    { 
+      'response code was 200': res => res.status === 200,
+      'response has id field': res => res.json().hasOwnProperty('id'),
+      'id field is not null or undefined': res => res.json().id != null && res.json().id != undefined 
+    }
+  );
+  const checkOutput2 = check(
+    org2,
+    { 
+      'response code was 200': res => res.status === 200,
+      'response has id field': res => res.json().hasOwnProperty('id'),
+      'id field is not null or undefined': res => res.json().id != null && res.json().id != undefined
+    }
+  )
+
+  if (!checkOutput1 || !checkOutput2) {
+    exec.test.abort('failed to create organizations on setup')
+  }
+
+  org1Id = org1.json().id;
+  org2Id = org2.json().id;
+}
 
 let bearerToken;
 
