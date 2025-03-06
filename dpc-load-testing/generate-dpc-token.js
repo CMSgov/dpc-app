f5ee0bde5e8a553ba501666396da1685b60e3aff
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2025-03-06 09:58:41 -0500

    tidy up

--- dpc-load-testing/generate-dpc-token.js
+++ dpc-load-testing/generate-dpc-token.js
@@ -3,9 +3,6 @@ import http from 'k6/http';
 const adminUrl = __ENV.ENVIRONMENT == 'local' ? 'http://host.docker.internal:9903' : __ENV.API_ADMIN_URL;
 const testOrgId = __ENV.LOAD_TEST_ORGANIZATION_ID;
 
-const fetchTokenURL = `${adminUrl}/tasks/generate-token?organization=${testOrgId}`;
-
-
 export default function generateDPCToken(orgId) {
   const fetchTokenURL = `${adminUrl}/tasks/generate-token?organization=${orgId}`;
   const headers = { 
