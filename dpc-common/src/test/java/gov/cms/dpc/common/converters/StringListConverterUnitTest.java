cc704aed1278d05f68e7010a8df30244004631ec
Ashley Weaver <ashleyweaver@navapbc.com>
2025-03-18 13:17:47 -0400

    Create StringListConverterUnitTest.java

---
+++ dpc-common/src/test/java/gov/cms/dpc/common/converters/StringListConverterUnitTest.java
@@ -0,0 +1,39 @@
+package gov.cms.dpc.common.converters;
+
+import gov.cms.dpc.common.converters.hibernate.StringListConverter;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StringListConverterUnitTest {
+
+    static StringListConverter converter = new StringListConverter();
+
+    @Test
+    void convertToDatabaseColumn() {
+        List<String> attributes = List.of("foo", "bar", "baz");
+        String expected = String.join(",", attributes);
+        assertEquals(expected, converter.convertToDatabaseColumn(attributes));
+    }
+
+    @Test
+    void convertToDatabaseColumn_nullValue() {
+        assertEquals("", converter.convertToDatabaseColumn(null));
+    }
+
+    @Test
+    void convertToEntityAttribute() {
+        String dbData = "foo,bar,baz";
+        List<String> expected = new ArrayList<>(Arrays.asList(dbData.split(",")));
+        assertEquals(expected, converter.convertToEntityAttribute(dbData));
+    }
+
+    @Test
+    void convertToEntityAttribute_emptyString() {
+        assertEquals(List.of(), converter.convertToEntityAttribute(""));
+    }
+}
