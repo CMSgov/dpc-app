fe80ec06053b14450fe8b72e38f162af443a687d
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2025-03-19 14:32:06 -0400

    Basic formatting setup

--- dpc-portal/app/components/page/organization/compound_show_component_preview.rb
+++ dpc-portal/app/components/page/organization/compound_show_component_preview.rb
@@ -5,12 +5,12 @@ module Page
     # Shows tabbed credential delegates and credentials
     class CompoundShowComponentPreview < ViewComponent::Preview
       def authorized_official
-        org = ProviderOrganization.new(name: 'Health Hut', npi: '1111111111', id: 2)
+        org = ProviderOrganization.new(name: 'Health Hut', npi: '1111111111', id: 2, dpc_api_organization_id: '09106579-d3bd-49d4-bd40-03b3ae5e142d')
         render(Page::Organization::CompoundShowComponent.new(org, { active: [], pending: [], expired: [] }, true))
       end
 
       def credential_delegate
-        org = ProviderOrganization.new(name: 'Health Hut', npi: '1111111111', id: 2)
+        org = ProviderOrganization.new(name: 'Health Hut', npi: '1111111111', id: 2, dpc_api_organization_id: '09106579-d3bd-49d4-bd40-03b3ae5e142d')
         render(Page::Organization::CompoundShowComponent.new(org, {}, true))
       end
     end
