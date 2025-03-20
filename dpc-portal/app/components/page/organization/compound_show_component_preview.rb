3338cb2cf7752a4b1b8c58cedd0cfb8a31227990
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2025-03-20 14:00:26 -0400

    Add user tests

--- dpc-portal/app/components/page/organization/compound_show_component_preview.rb
+++ dpc-portal/app/components/page/organization/compound_show_component_preview.rb
@@ -6,7 +6,7 @@ module Page
     class CompoundShowComponentPreview < ViewComponent::Preview
       def authorized_official
         org = ProviderOrganization.new(name: 'Health Hut', npi: '1111111111', id: 2, dpc_api_organization_id: '09106579-d3bd-49d4-bd40-03b3ae5e142d')
-        render(Page::Organization::CompoundShowComponent.new(org, { active: [], pending: [], expired: [] }, true, 'Authorized Official', 'accepted'))
+        render(Page::Organization::CompoundShowComponent.new(org, { active: [], pending: [], expired: [] }, true, 'Authorized Official', 'Accepted'))
       end
 
       def credential_delegate
