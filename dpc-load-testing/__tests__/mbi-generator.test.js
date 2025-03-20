29be771a02e13585265bacccde46d3bdc7f6e259
Christopher Ronning <cronning90@gmail.com>
2025-03-20 11:16:34 -0400

    create npi generator; refactor unit tests for mbi-generator

--- dpc-load-testing/__tests__/mbi-generator.test.js
+++ dpc-load-testing/__tests__/mbi-generator.test.js
@@ -1,40 +1,37 @@
 import { generateMBI } from "../utils/mbi-generator";
-
-function isArrayUnique(arr) {
-  return Array.isArray(arr) && new Set(arr).size === arr.length;
-}
+import { isArrayUnique } from "../utils/test-utils";
 
 describe('generateMBI', () => {
+  const mbis = [];
+  beforeAll(() => {
+    for (let i = 0; i <= 10000; i++){
+      mbis.push(generateMBI(i));
+    }
+  });
+
   test('generates an 11 character string', () => {
-    expect(generateMBI(0)).toHaveLength(11);
+    mbis.every(mbi => {
+      expect(mbi).toHaveLength(11);
+    })
   });
 
   test('generates strings with the correct character placement', () => {
-    const mbis = [];
-    for (let i = 0; i <= 1000; i++){
-      mbis.push(generateMBI(i));
-    }
-    mbis.every(result => {
-      expect(result[0]).toMatch(/^[1-9]$/); // first character is a number 1-9
-      expect(result[1]).toMatch(/[A-Z]/); // second character is an uppercase letter
-      expect(result[2]).toMatch(/^[0-9A-Z]+$/); // third character is alphanumeric
-      expect(result[3]).toMatch(/^[0-9]$/); // fourth character is a number 0-9
-      expect(result[4]).toMatch(/[A-Z]/); // fifth character is an uppercase letter
-      expect(result[5]).toMatch(/^[0-9A-Z]+$/); // sixth character is alphanumeric
-      expect(result[6]).toMatch(/^[0-9]$/); // seventh character is a number 0-9
-      expect(result[7]).toMatch(/[A-Z]/); // eighth character is an uppercase letter
-      expect(result[8]).toMatch(/[A-Z]/); // ninth character is an uppercase letter
-      expect(result[9]).toMatch(/^[0-9]$/); // tenth character is a number 0-9
-      expect(result[10]).toMatch(/^[0-9]$/); // eleventh character is a number 0-9
+    mbis.every(mbi => {
+      expect(mbi[0]).toMatch(/^[1-9]$/); // first character is a number 1-9
+      expect(mbi[1]).toMatch(/[A-Z]/); // second character is an uppercase letter
+      expect(mbi[2]).toMatch(/^[0-9A-Z]+$/); // third character is alphanumeric
+      expect(mbi[3]).toMatch(/^[0-9]$/); // fourth character is a number 0-9
+      expect(mbi[4]).toMatch(/[A-Z]/); // fifth character is an uppercase letter
+      expect(mbi[5]).toMatch(/^[0-9A-Z]+$/); // sixth character is alphanumeric
+      expect(mbi[6]).toMatch(/^[0-9]$/); // seventh character is a number 0-9
+      expect(mbi[7]).toMatch(/[A-Z]/); // eighth character is an uppercase letter
+      expect(mbi[8]).toMatch(/[A-Z]/); // ninth character is an uppercase letter
+      expect(mbi[9]).toMatch(/^[0-9]$/); // tenth character is a number 0-9
+      expect(mbi[10]).toMatch(/^[0-9]$/); // eleventh character is a number 0-9
     });
   });
 
   test('generates only unique MBIs', () => {
-    const mbis = [];
-    for (let i = 0; i <= 1000; i++){
-      mbis.push(generateMBI(i));
-    }
-
     expect(isArrayUnique(mbis)).toBeTruthy();
   });
 });
---
+++ dpc-load-testing/utils/npi-generator.js
@@ -0,0 +1,32 @@
+/**
+ * Returns a Luhn check digit, which should be the last digit of an NPI. This serves as a 
+ * validation mechanism. 
+ * @param {string} nineDigitNumber 
+ * @returns {string} a digit to be appended to the nineDigitNumber
+ */
+function generateLuhnCheckDigit(nineDigitNumber) {
+  const appendedNineDigitNumber = "80840" + nineDigitNumber;
+  const reversed = appendedNineDigitNumber.split('').reverse();
+  let total = 0;
+  for (const [index, value] of reversed.entries()) {
+    let x = parseInt(value);
+    if (index % 2 === 0) {
+      x = x * 2;
+      // if the result has two digits, add the digits together to get a single digit number
+      if (x > 9) {
+        x.toString().split('').map(Number).reduce((a, b) => a + b);
+      }
+    }
+
+    total += x;
+  }
+
+  const unitsDigit = total % 10;
+  const checkDigit = unitsDigit === 0 ? unitsDigit : (10 - (total % 10));
+  return checkDigit.toString();
+}
+
+export function generateNPI(counter) {
+  const paddedNumber = counter.toString().padStart(9, '0');
+  return paddedNumber + generateLuhnCheckDigit(paddedNumber);
+}
