c20c1c688c2f558879ad973d5ed093a31adc310b
Luke Short <lukeshortemail@gmail.com>
2025-03-31 15:07:58 -0700

    Organization list status column updates

--- dpc-portal/app/components/core/card/organization_card_component.rb
+++ dpc-portal/app/components/core/card/organization_card_component.rb
@@ -4,7 +4,9 @@ module Core
   module Card
     # Render a USWDS-styled card for an organization.
     class OrganizationCardComponent < ViewComponent::Base
+      include OrganizationUtils
       with_collection_parameter :link
+
       def initialize(link:)
         super
         @link = link
@@ -13,17 +15,7 @@ module Core
 
       def before_render
         message_prefix = @link.is_a?(AoOrgLink) ? 'verification' : 'cd_access'
-        @icon, @classes, @status = if @organization.rejected?
-                                     ['lock', %i[text-gray-50],
-                                      t("#{message_prefix}.#{@organization.verification_reason}_status")]
-                                   elsif !@link.verification_status?
-                                     ['lock', %i[text-gray-50],
-                                      t("verification.#{@link.verification_reason}_status")]
-                                   elsif @organization.terms_of_service_accepted_at.blank?
-                                     ['warning', %i[text-gold], t("#{message_prefix}.tos_not_signed")]
-                                   else
-                                     ['verified', %i[text-accent-cool], t("#{message_prefix}.manage_org")]
-                                   end
+        @icon, @classes, @status = org_status(@organization, @link)
       end
     end
   end
