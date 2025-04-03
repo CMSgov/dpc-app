343da4b413e6febc0f1e7dcfda25e24d4b9b09cb
Ashley Weaver <ashleyweaver@navapbc.com>
2025-04-03 14:29:13 -0400

    Add ESLint to workflow

---
+++ dpc-load-testing/eslint.config.js
@@ -0,0 +1,15 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import js from "@eslint/js";
+
+export default defineConfig([
+    [ globalIgnores(["__tests__", "macaroonTests.js"]) ],
+	{
+		files: ["**/*.js"],
+		plugins: { js },
+		extends: ["js/recommended"],
+		rules: {
+		    "no-undef": "warn",
+		    "no-unused-vars": "warn",
+		},
+	},
+]);
