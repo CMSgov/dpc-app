947bb5c6bc46cedc3d240b5afb2e3c3fdbbb135c
Luke Short <lukeshortemail@gmail.com>
2025-04-05 20:11:12 -0700

    remove commented out code

--- dpc-portal/app/concerns/organization_utils.rb
+++ dpc-portal/app/concerns/organization_utils.rb
@@ -7,7 +7,6 @@ module OrganizationUtils
   included do
     def org_status(organization, link)
       message_prefix = link.is_a?(AoOrgLink) ? 'verification' : 'cd_access'
-#       organization_config_complete = organization.check_config_complete
       organization_config_complete = organization.public_ips.present? && organization.public_keys.present? && organization.client_tokens.present?
       if organization.terms_of_service_accepted_at.blank?
         ['warning', %i[text-gold], t("#{message_prefix}.sign_tos")]
