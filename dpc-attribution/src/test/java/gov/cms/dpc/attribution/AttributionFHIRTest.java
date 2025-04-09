0db6a017a4732228dc94ac29cd1bb4e31e7b1c20
Ashley Weaver <ashleyweaver@navapbc.com>
2025-04-09 14:29:56 -0400

    Update AttributionFHIRTest.java

--- dpc-attribution/src/test/java/gov/cms/dpc/attribution/AttributionFHIRTest.java
+++ dpc-attribution/src/test/java/gov/cms/dpc/attribution/AttributionFHIRTest.java
@@ -106,8 +106,8 @@ class AttributionFHIRTest extends AbstractAttributionTest {
         // Remove meta so we can do equality between the two resources
         fetchedGroup.setMeta(null);
 
-        System.out.println("CREATED: " + createdGroup);
-        System.out.println("FETCHED: " + fetchedGroup);
+        System.out.println("CREATED: " + createdGroup.children());
+        System.out.println("FETCHED: " + fetchedGroup.children());
         assertAll(() -> assertTrue(createdGroup.equalsDeep(fetchedGroup), "Groups should be equal"),
                 () -> assertEquals(bundle.getEntry().size() - 1, fetchedGroup.getMember().size(), "Should have the same number of benes"));
 
