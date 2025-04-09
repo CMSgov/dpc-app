798a22d624d76398a631aaf439c5d46c1d1a589f
Ashley Weaver <ashleyweaver@navapbc.com>
2025-04-09 16:50:24 -0400

    Update AttributionFHIRTest.java

--- dpc-attribution/src/test/java/gov/cms/dpc/attribution/AttributionFHIRTest.java
+++ dpc-attribution/src/test/java/gov/cms/dpc/attribution/AttributionFHIRTest.java
@@ -106,17 +106,8 @@ class AttributionFHIRTest extends AbstractAttributionTest {
         // Remove meta so we can do equality between the two resources
         fetchedGroup.setMeta(null);
 
-        assertAll(
-                () -> assertEquals(createdGroup.getIdentifier(), fetchedGroup.getIdentifier()),
-                () -> assertEquals(createdGroup.getActive(), fetchedGroup.getActive()),
-                () -> assertEquals(createdGroup.getType(), fetchedGroup.getType()),
-                () -> assertEquals(createdGroup.getActual(), fetchedGroup.getActual()),
-                //() -> assertEquals(createdGroup.getCode(), fetchedGroup.getCode()),
-                () -> assertEquals(createdGroup.getName(), fetchedGroup.getName()),
-                () -> assertEquals(createdGroup.getQuantity(), fetchedGroup.getQuantity())
-                //() -> assertEquals(createdGroup.getCharacteristic(), fetchedGroup.getCharacteristic()),
-                //() -> assertEquals(createdGroup.getMember(), fetchedGroup.getMember())
-        );
+        System.out.println("CREATED: " + createdGroup.listChildrenByName("*"));
+        System.out.println("FETCHED: " + fetchedGroup.listChildrenByName("*"));
         assertAll(() -> assertTrue(createdGroup.equalsDeep(fetchedGroup), "Groups should be equal"),
                 () -> assertEquals(bundle.getEntry().size() - 1, fetchedGroup.getMember().size(), "Should have the same number of benes"));
 
