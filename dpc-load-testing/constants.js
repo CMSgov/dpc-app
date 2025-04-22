4abb684301997b2cdd66c16fe41b1de8d102f5f9
Christopher Ronning <cronning90@gmail.com>
2025-04-22 14:31:59 -0400

    reduce number of VUs

--- dpc-load-testing/constants.js
+++ dpc-load-testing/constants.js
@@ -2,7 +2,7 @@ function getVUsCount() {
   if (__ENV.TEST_TYPE === 'single-iteration' || __ENV.TEST_TYPE === 'average-load-test') {
     return 3;
   } else if (__ENV.TEST_TYPE === 'stress-test') {
-    return 12; 
+    return 5; 
   }
 }
 
