49166c8c0971e8ca5d46540d9e8e57336dc447fa
Christopher Ronning <cronning90@gmail.com>
2025-04-28 14:51:02 -0400

    fix NPI Generator

--- dpc-load-testing/__tests__/npi-generator.test.js
+++ dpc-load-testing/__tests__/npi-generator.test.js
@@ -1,25 +1,6 @@
 import { generateNPI } from "../utils/npi-generator";
 import { isArrayUnique } from "../utils/test-utils";
-
-function luhnCheck(npi) {
-  const appendedNpi = "80840" + npi;
-  const reversed = appendedNpi.split('').reverse();
-  let total = 0;
-  for (const [index, value] of reversed.entries()) {
-    let x = parseInt(value);
-    if (index % 2 === 1) {
-      x = x * 2;
-      // if the result has two digits, add the digits together to get a single digit number
-      if (x > 9) {
-        x.toString().split('').map(Number).reduce((a, b) => a + b);
-      }
-    }
-
-    total += x;
-  }
-
-  return total % 10 === 0;
-}
+import luhn from "../lib/luhn.js";
 
 describe('generateNPI', () => {
   const npis = []
@@ -37,7 +18,7 @@ describe('generateNPI', () => {
 
   test('generated NPIs pass the luhnCheck', () => {
     npis.every(npi => {
-      expect(luhnCheck(npi)).toBeTruthy();
+      expect(luhn.isValid("80840" + npi)).toBeTruthy();
     })
   });
 
