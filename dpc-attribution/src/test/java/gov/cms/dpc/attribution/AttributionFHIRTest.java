6c4de022432ebe0155a58661b8f789e142668f01
Ashley Weaver <ashleyweaver@navapbc.com>
2025-04-28 11:11:06 -0400

    Update AttributionFHIRTest.java

--- dpc-attribution/src/test/java/gov/cms/dpc/attribution/AttributionFHIRTest.java
+++ dpc-attribution/src/test/java/gov/cms/dpc/attribution/AttributionFHIRTest.java
@@ -106,7 +106,6 @@ class AttributionFHIRTest extends AbstractAttributionTest {
         // Remove meta so we can do equality between the two resources
         fetchedGroup.setMeta(null);
 
-
         // TODO: is it that the members aren't necessarily in the same order in each group? -acw
         // update: YES (:
         assertAll(
