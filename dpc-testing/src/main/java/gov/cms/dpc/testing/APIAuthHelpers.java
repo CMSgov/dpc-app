a3e93079c939bcb700edf6cae2e9216f5474f870
Ashley Weaver <ashleyweaver@navapbc.com>
2025-05-16 14:30:39 -0400

    Update APIAuthHelpers.java

--- dpc-testing/src/main/java/gov/cms/dpc/testing/APIAuthHelpers.java
+++ dpc-testing/src/main/java/gov/cms/dpc/testing/APIAuthHelpers.java
@@ -131,7 +131,7 @@ public class APIAuthHelpers {
 
             System.out.println("POST: " + post);
             System.out.println("ENTITY: " + post.getEntity());
-            System.out.println("AUDIENCE: " + audience);
+            System.out.println("AUDIENCE: " + post.getURI());
             try (CloseableHttpResponse response = client.execute(post)) {
                 System.out.println("RESPONSE: " + response);
                 assertEquals(HttpStatus.OK_200, response.getStatusLine().getStatusCode(), "Token validation should have succeeded");
