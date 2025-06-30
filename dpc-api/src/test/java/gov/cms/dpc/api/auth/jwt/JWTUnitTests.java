4ea66f30c5ea555e3ac1fa52761f530466f38e44
Ashley Weaver <ashleyweaver@navapbc.com>
2025-06-30 13:38:29 -0400

    Revert "Update JWTUnitTests.java"
    This reverts commit 0e639ff4c807b8e295b8c4d06499713b7c591fd8.

--- dpc-api/src/test/java/gov/cms/dpc/api/auth/jwt/JWTUnitTests.java
+++ dpc-api/src/test/java/gov/cms/dpc/api/auth/jwt/JWTUnitTests.java
@@ -357,8 +357,7 @@ class JWTUnitTests {
                     .request()
                     .post(Entity.entity("", MediaType.APPLICATION_FORM_URLENCODED));
 
-//            TODO: debug -acw
-//            assertEquals(HttpStatus.OK_200, response.getStatus(), "Should get a good status on first request");
+            assertEquals(HttpStatus.OK_200, response.getStatus(), "Should get a good status on first request");
 
             // Try to submit again
             Response r2 = RESOURCE.target("/v1/Token/auth")
