64756b3555bc065be7a9aede1314eba57ee533de
Luke Short <lukeshortemail@gmail.com>
2025-07-09 09:51:21 -0700

    use _count for limiting page size, per FHIR spec

--- dpc-common/src/main/java/gov/cms/dpc/common/utils/PagingUtils.java
+++ dpc-common/src/main/java/gov/cms/dpc/common/utils/PagingUtils.java
@@ -4,7 +4,11 @@ import ca.uhn.fhir.rest.gclient.IQuery;
 import org.hl7.fhir.dstu3.model.Bundle;
 
 public class PagingUtils {
-    public static final int defaultLimit = 1;
+    private PagingUtils() {
+        throw new UnsupportedOperationException("PagingUtils is a utility class and should not be instantiated");
+    }
+
+    public static final int DEFAULT_LIMIT = 1;
 
     private static String formatURL(String url, int page) {
         return url + "?page=" + page;
@@ -14,16 +18,16 @@ public class PagingUtils {
         bundle.addLink().setRelation(name).setUrl(formatURL(path, page));
     }
 
-    public static Bundle handlePaging(IQuery<Bundle> request, int limit, int page, String requestPath) {
-        request.offset(limit*(page-1));
-        request.count(limit);
+    public static Bundle handlePaging(IQuery<Bundle> request, int count, int page, String requestPath) {
+        request.offset(count*(page-1));
+        request.count(count);
         Bundle resultBundle = request.execute();
 
         addRelationLink(resultBundle, "self", requestPath, page);
         addRelationLink(resultBundle, "first", requestPath, 1);
         if (page > 1) addRelationLink(resultBundle, "previous", requestPath, page-1);
 
-        int lastPage = (int) Math.ceil((float) resultBundle.getTotal() / limit);
+        int lastPage = (int) Math.ceil((float) resultBundle.getTotal() / count);
         if (page + 1 <= lastPage) addRelationLink(resultBundle, "next", requestPath, page+1);
         addRelationLink(resultBundle, "last", requestPath, lastPage);
 
