a4af3c002fc654b2473733d449dc180b6d5ad668
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2025-07-16 13:18:51 -0400

    Added error logging to stderr so the tree runner prints it

--- dpc-aggregation/src/test/java/gov/cms/dpc/aggregation/AggregationServiceTest.java
+++ dpc-aggregation/src/test/java/gov/cms/dpc/aggregation/AggregationServiceTest.java
@@ -105,8 +105,7 @@ public class AggregationServiceTest {
         } catch(ConditionTimeoutException e) {
             JobQueueBatch batch = queue.getJobBatches(jobID).get(0);
             System.err.println("Batch failed to reach queued status, current state of batch: " + batch.getStatus());
-            fail("Failing test due to batch status not set to queued, current status: " + batch.getStatus());
+            fail();
         }
-        
     }
 }
