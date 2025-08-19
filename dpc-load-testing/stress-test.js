6c5d6615d7233ce9c45d21fede78e7da617f81f5
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2025-08-19 16:46:48 -0400

    Comment about provider cap

--- dpc-load-testing/stress-test.js
+++ dpc-load-testing/stress-test.js
@@ -3,6 +3,10 @@ import { constants } from "./constants.js";
 
 // See https://grafana.com/docs/k6/latest/using-k6/k6-options/reference for
 // details on this configuration object.
+
+// Before running this, ensure the org provider cap is removed in your environment.  If you don't, the workflows
+// will start failing since they create a new provider at the start of every iteration.  Your response times will
+// look very fast all of the sudden and your results will be very, very skewed.
 export const options = {
   scenarios: {
     workflow: {
