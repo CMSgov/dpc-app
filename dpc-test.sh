9323e3b78a6dd202f1eb4aacb09c5b03de990a6b
Ashley Weaver <ashleyweaver@navapbc.com>
2025-08-22 12:13:10 -0400

    Update dpc-test.sh

--- dpc-test.sh
+++ dpc-test.sh
@@ -62,14 +62,15 @@ USE_BFD_MOCK=true docker compose -p start-v1-app up --exit-code-from tests tests
 
 docker compose -p start-v1-app down
 
-echo "Starting api server for Postman tests"
-# Start the API server
-USE_BFD_MOCK=true AUTH_DISABLED=true docker compose -p start-v1-app up db attribution aggregation api --wait
-
-echo "Starting Postman tests"
-# Run the Postman tests
-npm install
-npm run test
+# TODO: Postman tests disabled to determine if needed
+#echo "Starting api server for Postman tests"
+## Start the API server
+#USE_BFD_MOCK=true AUTH_DISABLED=true docker compose -p start-v1-app up db attribution aggregation api --wait
+#
+#echo "Starting Postman tests"
+## Run the Postman tests
+#npm install
+#npm run test
 
 # Wait for Jacoco to finish writing the output files
 docker compose -p start-v1-app down -t 60
