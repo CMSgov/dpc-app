9275889a829c3c7ad633522597bb7fd6231b5ea2
Luke Short <lukeshortemail@gmail.com>
2025-08-26 09:02:16 -0700

    [DPC-4420] add pagination to GET /v1/Patient (#2723)
    ## 🎫 Ticket
    
    [DPC-4420](https://jira.cms.gov/browse/DPC-4420)
    
    ## 🛠 Changes
    
    <!-- What was added, updated, or removed in this PR? -->
    - This pull request adds pagination support for `GET /v1/Patient` by
    adding 2 new optional query parameters
    1. `_count` - this is the "page size" or number of patients per page.
    This is what determines when to use pagination logic. Note: this does
    not include any default count size, as outlined in the original ticket.
    2. `_offset` - this is essentially `count * pageNum`. This is how many
    records are skipped before displaying `_count` records.
    - returned Bundle contains [link
    elements](https://build.fhir.org/bundle-definitions.html#Bundle.link)
    for `"self"`, `"first"`, `"previous"` and `"next"` page where
    appropriate
    - `_count=0` is treated as `summary=count` - per guidance[ on FHIR
    docs](https://build.fhir.org/search.html#_count)
    - > If _count has the value 0 (zero), this SHALL be treated the same as
    _summary=count...
    - Note: summary parameter added to the attribution paging backend to
    handle `.summaryMode()` call for `count=0`
    
    ~~- added `defaultPageSize` parameter to application.yml files~~
    
    ## ℹ️ Context
    
    <!-- Why were these changes made? Add background context suitable for a
    non-technical audience. -->
    
    <!-- If any of the following security implications apply, this PR must
    not be merged without Stephen Walter's approval. Explain in this section
    and add @SJWalter11 as a reviewer.
      - Adds a new software dependency or dependencies.
      - Modifies or invalidates one or more of our security controls.
      - Stores or transmits data that was not stored or transmitted before.
    - Requires additional review of security implications for other reasons.
    -->
    - These changes were made to address an issue where `GET /v1/Patient`
    requests for very large patient rosters would timeout and cause a
    service degradation.
    - by paginating requests, less load is put on the database for very
    large queries
    - Legacy behavior is still enabled, meaning the full roster will be
    output when `_count` is not provided
    - this is a continuation from PoC that was created in
    [DPC-4175](https://jira.cms.gov/browse/DPC-4175)
    - previous proof of concept was created for the dpc-api side of things,
    but needed dpc-attribution implementation
    
    
    ## 🧪 Validation
    
    <!-- How were the changes verified? Did you fully test the acceptance
    criteria in the ticket? Provide reproducible testing instructions and
    screenshots if applicable. -->
    
    #### Unit tests added or updated
     - `dpc-api/.../PatientResourceUnitTest.java`
     - `dpc-attribution/.../PatientDAOUnitTest.java`
     - `dpc-attribution/.../PatientResourceUnitTest.java`
     - `dpc-attribution/.../GroupResourceUnitTest.java`
    
    #### Integration tests added or updated
     - `dpc-api/.../PatientResourceTest.java`
    
    ---------
    
    Co-authored-by: Ashley Weaver <ashleyweaver@navapbc.com>
    Co-authored-by: MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>

---
+++ dpc-attribution/src/test/java/gov/cms/dpc/attribution/resources/v1/PatientResourceUnitTest.java
@@ -0,0 +1,150 @@
+package gov.cms.dpc.attribution.resources.v1;
+
+import gov.cms.dpc.attribution.jdbi.*;
+import gov.cms.dpc.common.entities.OrganizationEntity;
+import gov.cms.dpc.common.entities.PatientEntity;
+import gov.cms.dpc.common.utils.PagingService;
+import gov.cms.dpc.fhir.converters.FHIREntityConverter;
+import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.instance.model.api.IBaseBundle;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.*;
+
+import static gov.cms.dpc.attribution.AttributionTestHelpers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+
+class PatientResourceUnitTest {
+
+    private PatientResource patientResource;
+
+    @Mock
+    PatientDAO patientDAO;
+
+    private final FHIREntityConverter converter = FHIREntityConverter.initialize();
+
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        patientResource = new PatientResource(converter, patientDAO, 9999, new PagingService());
+    }
+
+    @Test
+    void testSearchPatientsPaginated() {
+        String requestUrl = "/v1/Patient";
+        UUID orgId = UUID.randomUUID();
+        String orgRef = "Organization/" + orgId;
+        int pageOffset = 30;
+        int count = 10;
+
+        // Create 100 fake PatientEntity records
+        OrganizationEntity org = createOrganizationEntity();
+        List<PatientEntity> patientEntities = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            PatientEntity patientEntity = createPatientEntity(org);
+            patientEntity.setID(UUID.randomUUID());
+            patientEntities.add(patientEntity);
+        }
+
+        // Stub DAO to return paged subset of PatientEntity's
+        // if offset=30, count=10 should get patients from index 30-39
+        List<PatientEntity> pagedEntities = patientEntities.subList(pageOffset, pageOffset + count);
+        PageResult mockedPage = new PageResult<>(
+                pagedEntities,
+                true
+        );
+        Mockito.when(patientDAO.patientSearch(argThat(queryMatches(null, orgId, count, pageOffset))))
+                .thenReturn(mockedPage);
+
+        // this is the droid you are looking for
+        Bundle resultBundle = patientResource.searchPatients(
+                null,
+                null,
+                orgRef,
+                10,
+                30,
+                ""
+        );
+        List<Bundle.BundleEntryComponent> results = resultBundle.getEntry();
+
+
+        assertEquals(10, results.size());
+        for (int i = 0; i < results.size(); i++) {
+            String expectedId = pagedEntities.get(i).getID().toString();
+            String actualId = results.get(i).getResource().getId();
+            assertEquals(expectedId, actualId);
+        }
+        String expectedPrevUrl = requestUrl + "?_count=10&_offset=20";
+        String expectedNextUrl = requestUrl + "?_count=10&_offset=40";
+        String expectedFirstUrl = requestUrl + "?_count=10&_offset=0";
+        String expectedSelfUrl = requestUrl + "?_count=10&_offset=30";
+        assertEquals(expectedPrevUrl, resultBundle.getLink(IBaseBundle.LINK_PREV).getUrl());
+        assertEquals(expectedNextUrl, resultBundle.getLink(IBaseBundle.LINK_NEXT).getUrl());
+        assertEquals(expectedFirstUrl, resultBundle.getLink("first").getUrl());
+        assertEquals(expectedSelfUrl, resultBundle.getLink(IBaseBundle.LINK_SELF).getUrl());
+        assertEquals(count, resultBundle.getEntry().size());
+    }
+
+    @Test
+    void testInvalidPageNumber() {
+        int count = 100;
+        int pageOffset = 999900;
+        String requestUrl = "/v1/Patient";
+        UUID orgId = UUID.randomUUID();
+        String orgRef = "Organization/" + orgId;
+
+        List<PatientEntity> patientEntities = new ArrayList<>();
+        PageResult mockedPage = new PageResult<>(
+                patientEntities,
+                false
+        );
+        Mockito.when(patientDAO.patientSearch(argThat(queryMatches(null, orgId, count, pageOffset))))
+                .thenReturn(mockedPage);
+        Bundle resultBundle = patientResource.searchPatients(
+                null,
+                null,
+                orgRef,
+                count,
+                pageOffset,
+                ""
+        );
+
+        assertNull(resultBundle.getLink(IBaseBundle.LINK_PREV));
+        assertNull(resultBundle.getLink(IBaseBundle.LINK_NEXT));
+        assertEquals(resultBundle.getLink("first").getUrl(), requestUrl + "?_count=100&_offset=0");
+        assertEquals(resultBundle.getLink(IBaseBundle.LINK_SELF).getUrl(), requestUrl + "?_count=" + count + "&_offset=" + pageOffset);
+        assertTrue(resultBundle.getEntry().isEmpty(), "Expected no entries for an out-of-bounds page");
+    }
+
+    @Test
+    void testSummaryBundle() {
+        UUID orgId = UUID.randomUUID();
+        String orgRef = "Organization/" + orgId;
+        PatientSearchQuery expectedQuery = new PatientSearchQuery();
+        expectedQuery.setOrganizationID(orgId);
+        expectedQuery.setCount(0); // because summary="count"
+
+        Mockito.when(patientDAO.countMatchingPatients(argThat(query ->
+                query.getOrganizationID().equals(expectedQuery.getOrganizationID()) &&
+                        query.getCount().orElseThrow() == 0
+        ))).thenReturn(99);
+
+        Bundle summaryBundle = patientResource.searchPatients(
+                null,
+                null,
+                orgRef,
+                0,
+                0,
+                "count"
+        );
+
+        assertEquals(99, summaryBundle.getTotal());
+        assertTrue(summaryBundle.getEntry().isEmpty());
+    }
+}
