d9dc05bba87101339b54362fd3645054b06073b3
Luke Short <lukeshortemail@gmail.com>
2025-09-23 16:15:33 -0700

    add -e flags to README.md, check 2 urls for ea app

--- dpc-smoketest/README.md
+++ dpc-smoketest/README.md
@@ -1,5 +1,5 @@
 # DPC Smoke Tests
 A set of smoke tests to cover different system components and ensure new builds run correctly across all environments.
 ```dtd
-docker run --rm -v $(pwd)/dpc-smoketest:/src --env-file $(pwd)/ops/config/decrypted/local.env --add-host host.docker.internal=host-gateway -e ENVIRONMENT=local -i grafana/k6 run /src/ci-app.js
+docker run --rm -v $(pwd)/dpc-smoketest:/src --env-file $(pwd)/ops/config/decrypted/local.env --add-host host.docker.internal=host-gateway -e PORTAL_HOST=http://localhost:3100 -e WEB_ADMIN_HOST=http://localhost:3000 -e WEB_HOST=http://localhost:3900  -i grafana/k6 run /src/ci-app.js
 ```
--- dpc-smoketest/ci-app.js
+++ dpc-smoketest/ci-app.js
@@ -15,8 +15,30 @@ export const options = {
   }
 };
 
+function checkDPCPortal() {
+  const baseUrl = __ENV.PORTAL_HOST;
+  if (!baseUrl) {
+    throw new Error("PORTAL_HOST environment variable is not set");
+  }
 
-export default function () {
+  const portalUrl = `${baseUrl}/portal`;
+  const portalResponse = http.get(portalUrl);
+  check(portalResponse, {
+    "is status 200": (r) => r.status === 200,
+    "verify homepage text": (r) => r.body.includes("Data at the Point of Care"),
+    "verify login text": (r) => r.body.includes("Log in")
+  });
+
+  const portalOrganizationsUrl = `${baseUrl}/portal/organizations`;
+  const portalOrganizationsResponse = http.get(portalOrganizationsUrl);
+  check(portalOrganizationsResponse, {
+    "is status 200": (r) => r.status === 200,
+    "verify homepage text": (r) => r.body.includes("Data at the Point of Care"),
+    "verify login text": (r) => r.body.includes("Log in")
+  });
+}
+
+function checkDPCWeb() {
   const baseUrl = __ENV.WEB_HOST;
   if (!baseUrl) {
     throw new Error("WEB_HOST environment variable is not set");
@@ -26,9 +48,47 @@ export default function () {
   const res = http.get(url);
 
   check(res, {
-    "status is 200": (r) => r.status === 200,
-    "body has 'Data at the Point of Care'": (r) => r.body.includes("Data at the Point of Care"),
-    "body has 'Log in'": (r) => r.body.includes("Log in"),
+    "is status 200": (r) => r.status === 200,
+    "verify homepage text": (r) => r.body.includes("Data at the Point of Care"),
+    "verify login text": (r) => r.body.includes("Log in")
   });
+
+  const rootUrl = `${baseUrl}/`;
+  const rootRes = http.get(rootUrl);
+
+  check(rootRes, {
+    "is status 200": (r) => r.status === 200,
+    "verify homepage text": (r) => r.body.includes("Data at the Point of Care"),
+    "verify login text": (r) => r.body.includes("Log in")
+  });
+}
+
+function checkDPCWebAdmin() {
+  const baseUrl = __ENV.WEB_ADMIN_HOST;
+  if (!baseUrl) {
+    throw new Error("WEB_ADMIN_HOST environment variable is not set");
+  }
+
+  const adminUrl = `${baseUrl}/admin/internal/sign_in`;
+  const adminResponse = http.get(adminUrl);
+  check(adminResponse, {
+    "is status 200": (r) => r.status === 200,
+    "verify homepage text": (r) => r.body.includes("Data at the Point of Care"),
+    "verify login text": (r) => r.body.includes("Log in")
+  });
+
+  const adminOrganizationsUrl = `${baseUrl}/admin/organizations`;
+  const adminOrganizationsResponse = http.get(adminOrganizationsUrl);
+  check(adminOrganizationsResponse, {
+    "is status 200": (r) => r.status === 200,
+    "verify homepage text": (r) => r.body.includes("Data at the Point of Care"),
+    "verify login text": (r) => r.body.includes("Log in")
+  });
+}
+
+export function workflow() {
+  checkDPCPortal();
+  checkDPCWeb();
+  checkDPCWebAdmin();
 }
 
