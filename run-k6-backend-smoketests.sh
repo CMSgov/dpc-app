6737cb43084323bbf8b01c1a9fb20553d709d611
Luke Short <lukeshortemail@gmail.com>
2025-11-10 15:40:55 -0800

    favor "ENVIRONMENT" over "ENV"

--- run-k6-backend-smoketests.sh
+++ run-k6-backend-smoketests.sh
@@ -4,17 +4,19 @@ set -Ee
 if [[ "$*" == *"--k6-env=local"* ]]; then
   API_ADMIN_URL="http://host.docker.internal:9903"
   API_METADATA_URL="http://host.docker.internal:3002/api/v1"
+
   docker run --rm -v $(pwd)/dpc-load-testing:/src --env-file $(pwd)/ops/config/decrypted/local.env \
   --add-host host.docker.internal=host-gateway \
   -e API_ADMIN_URL=${API_ADMIN_URL} \
   -e API_METADATA_URL=${API_METADATA_URL} \
-  -e ENV=local \
+  -e ENVIRONMENT=local \
   -i grafana/k6 run /src/smoketest-backend-ci-app.js
 else
+    echo "running backed smoketests against ${API_ADMIN_URL} and ${API_METADATA_URL}"
   k6 run \
     -e API_ADMIN_URL=${API_ADMIN_URL} \
     -e API_METADATA_URL=${API_METADATA_URL} \
-    -e ENV=${ENV} \
+    -e ENVIRONMENT=${ENVIRONMENT} \
     ./dpc-load-testing/smoketest-backend-ci-app.js
 fi
 
