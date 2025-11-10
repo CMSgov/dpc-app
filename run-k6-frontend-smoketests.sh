9e34a0b8a16101ad809aa45fb457b86ed3191c6d
Luke Short <lukeshortemail@gmail.com>
2025-11-10 11:25:13 -0800

    fix ENV arg

--- run-k6-frontend-smoketests.sh
+++ run-k6-frontend-smoketests.sh
@@ -8,7 +8,7 @@ if [[ "$*" == *"--k6-env=local"* ]]; then
   WEB_ADMIN_HOST="http://localhost:3000"
   ENV="local"
 
-  echo "running frontend smoketests against ${HOST_URL}"
+  echo "running frontend smoketests locally"
   docker run --rm -v $(pwd)/dpc-load-testing:/src --env-file $(pwd)/ops/config/decrypted/local.env \
     --network=host \
     -e PORTAL_HOST=${PORTAL_HOST} \
@@ -16,14 +16,14 @@ if [[ "$*" == *"--k6-env=local"* ]]; then
     -e WEB_ADMIN_HOST=${WEB_ADMIN_HOST} \
     -e ENV=${ENV} \
     -i grafana/k6 run /src/smoketest-frontend-ci-app.js
-  echo "Done!"
 else
-  echo "running frontend smoketests locally"
+  echo "running frontend smoketests against ${HOST_URL}"
   k6 run \
     -e PORTAL_HOST=${PORTAL_HOST} \
     -e WEB_HOST=${WEB_HOST} \
     -e WEB_ADMIN_HOST=${WEB_ADMIN_HOST} \
-    e ${ENV} \
+    -e ${ENV} \
     ./dpc-load-testing/smoketest-frontend-ci-app.js
 fi
 
+echo "Frontend Smoketests done!"
