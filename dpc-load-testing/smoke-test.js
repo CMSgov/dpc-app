4da5d2013861f225e9d2695d58d5711821c445d2
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2025-11-24 09:31:58 -0500

    Refactor auth smoke test structure

---
+++ dpc-load-testing/smoke-test.js
@@ -0,0 +1,128 @@
+/*global console*/
+/* eslint no-console: "off" */
+
+import { check } from 'k6';
+import exec from 'k6/execution'
+import { fetchGoldenMacaroon, generateDPCToken } from './generate-dpc-token.js';
+import {
+  deleteOrganization,
+  getOrganizationById,
+  createSmokeTestOrganization,
+  findOrganizationByNpi,
+} from './dpc-api-client.js';
+
+import { checkAuthWorkflow } from './smoke_test_workflows/auth.js';
+
+import NPIGeneratorCache from './utils/npi-generator.js';
+
+const npiGeneratorCache = new NPIGeneratorCache();
+
+export const options = {
+  thresholds: {
+    checks: ['rate===1'],
+  },
+  scenarios: {
+    checkAuthWorkflow: {
+      executor: 'per-vu-iterations',
+      vus: 3,
+      iterations: 1,
+      exec: "authWorkflow"
+    }
+  }
+};
+
+export function authWorkflow(data) {
+  checkAuthWorkflow(data);
+}
+
+// Sets up two test organizations
+export function setup() {
+  const goldenMacaroon = fetchGoldenMacaroon();
+  const orgIds = [
+    '0ab352f1-2bf1-44c4-aa7a-3004a1ffef12',
+    '69c0d4d4-9c07-4fa8-9053-e10fb1608b48',
+    'c7f5247b-4c41-478c-84eb-a6e801bdb145'
+  ];
+  const tokens = Array();
+  const npiGenerator = npiGeneratorCache.getGenerator(0);
+  // array returned from setup distributes its members starting from the 1 index
+  for (let i = 0; i < orgIds.length; i++) {
+    // delete smoke test org if present
+    const orgId = orgIds[i];
+    const token = generateDPCToken(orgId, goldenMacaroon);
+    tokens[i] = token;
+    const existingOrgResponse = getOrganizationById(token, orgId);
+
+    const checkGetOrgOutput = check(
+      existingOrgResponse,
+      {
+        'find org by id code 200 or 404': res => res.status == 404 || res.status == 200,
+      }
+    );
+
+    if (!checkGetOrgOutput) {
+      console.error(existingOrgResponse.body);
+      exec.test.abort('failed find org by id');
+    }
+
+    if (existingOrgResponse == 200) {
+      deleteOrganization(orgId, goldenMacaroon);
+    }
+    let npi;
+    let count = 0;
+    while(count < 200) {
+      count += 1;
+      const npiToCheck = npiGenerator.iterate();
+      // check if org with npi exists
+      const existingNpiResponse = findOrganizationByNpi(npiToCheck, goldenMacaroon);
+
+      const checkFindOutput = check(
+        existingNpiResponse,
+        {
+          'find org by npi response code was 200': res => res.status === 200,
+        }
+      );
+
+      if (!checkFindOutput) {
+        console.error(existingNpiResponse.body);
+        exec.test.abort('failed find org by npi');
+      }
+
+      const existingOrgs =  existingNpiResponse.json();
+
+      if ( existingOrgs.total == 0) {
+        npi = npiToCheck;
+        break;
+      }
+    }
+    if (! npi) {
+      exec.test.abort('failed to generate unused npi');
+    }
+
+    const org = createSmokeTestOrganization(npi, orgId, goldenMacaroon);
+
+    const checkOutput = check(
+      org,
+      {
+        'create org response code was 200': res => res.status === 200,
+        'create org response has id': res => res.json().id,
+      }
+    );
+
+    if (!checkOutput) {
+      console.error(org.body);
+      exec.test.abort('failed to create organizations on setup')
+    }
+
+  }
+
+  return { orgIds: orgIds, tokens: tokens, goldenMacaroon: goldenMacaroon };
+}
+
+export function teardown(data) {
+  for (const orgId of data.orgIds) {
+    if (orgId) {
+      deleteOrganization(orgId, data.goldenMacaroon);
+    }
+  }
+}
--- dpc-load-testing/smoke_test_workflows/auth.js
+++ dpc-load-testing/smoke_test_workflows/auth.js
@@ -3,126 +3,20 @@
 
 import { check } from 'k6';
 import exec from 'k6/execution'
-import { fetchGoldenMacaroon, generateDPCToken } from './generate-dpc-token.js';
 import {
   createClientToken,
-  deleteOrganization,
-  getOrganizationById,
   getOrganizationByAccessToken,
   createPublicKey,
-  createSmokeTestOrganization,
-  findOrganizationByNpi,
   retrieveAccessToken,
   validateJwt
-} from './dpc-api-client.js';
+} from '../dpc-api-client.js';
 import {
   exportPublicKey,
   generateKey,
   makeJwt,
   signatureSnippet
-} from './generate-jwt.js'
+} from '../generate-jwt.js'
 
-import NPIGeneratorCache from './utils/npi-generator.js';
-
-const npiGeneratorCache = new NPIGeneratorCache();
-
-export const options = {
-  thresholds: {
-    checks: ['rate===1'],
-  },
-  scenarios: {
-    checkAuthWorkflow: {
-      executor: 'per-vu-iterations',
-      vus: 3,
-      iterations: 1,
-      exec: "checkAuthWorkflow"
-    }
-  }
-};
-
-// Sets up two test organizations
-export function setup() {
-  const goldenMacaroon = fetchGoldenMacaroon();
-  const orgIds = [
-    '0ab352f1-2bf1-44c4-aa7a-3004a1ffef12',
-    '69c0d4d4-9c07-4fa8-9053-e10fb1608b48',
-    'c7f5247b-4c41-478c-84eb-a6e801bdb145'
-  ];
-  const tokens = Array();
-  const npiGenerator = npiGeneratorCache.getGenerator(0);
-  // array returned from setup distributes its members starting from the 1 index
-  for (let i = 0; i < orgIds.length; i++) {
-    // delete smoke test org if present
-    const orgId = orgIds[i];
-    const token = generateDPCToken(orgId, goldenMacaroon);
-    tokens[i] = token;
-    const existingOrgResponse = getOrganizationById(token, orgId);
-
-    const checkGetOrgOutput = check(
-      existingOrgResponse,
-      {
-        'find org by id code 200 or 404': res => res.status == 404 || res.status == 200,
-      }
-    );
-    
-    if (!checkGetOrgOutput) {
-      console.error(existingOrgResponse.body);
-      exec.test.abort('failed find org by id');
-    }
-
-    if (existingOrgResponse == 200) {
-      deleteOrganization(orgId, goldenMacaroon);
-    }
-    var npi;
-    var count = 0;
-    while(count < 200) {
-      count++;
-      const npiToCheck = npiGenerator.iterate();
-      // check if org with npi exists
-      const existingNpiResponse = findOrganizationByNpi(npiToCheck, goldenMacaroon);
-
-      const checkFindOutput = check(
-        existingNpiResponse,
-        {
-          'find org by npi response code was 200': res => res.status === 200,
-        }
-      );
-
-      if (!checkFindOutput) {
-        console.error(existingNpiResponse.body);
-        exec.test.abort('failed find org by npi');
-      }
-      
-      const existingOrgs =  existingNpiResponse.json();
-
-      if ( existingOrgs.total == 0) {
-	npi = npiToCheck;
-	break;
-      }
-    }
-    if (! npi) {
-      exec.test.abort('failed to generate unused npi');
-    }
-
-    const org = createSmokeTestOrganization(npi, orgId, goldenMacaroon);
-
-    const checkOutput = check(
-      org,
-      {
-        'create org response code was 200': res => res.status === 200,
-        'create org response has id': res => res.json().id,
-      }
-    );
-
-    if (!checkOutput) {
-      console.error(org.body);
-      exec.test.abort('failed to create organizations on setup')
-    }
-
-  }
-
-  return { orgIds: orgIds, tokens: tokens, goldenMacaroon: goldenMacaroon };
-}
 
 export async function checkAuthWorkflow(data) {
   const idx = exec.vu.idInInstance - 1;
@@ -208,13 +102,5 @@ export async function checkAuthWorkflow(data) {
   )
   if (!checkGetOrgByAccessToken) {
     exec.test.abort('Failed to get organization by access token');
-  }
-}
-
-export function teardown(data) {
-  for (const orgId of data.orgIds) {
-    if (orgId) {
-      deleteOrganization(orgId, data.goldenMacaroon);
-    }
   }
 }
