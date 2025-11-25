97589dcc8a54953a076f9268bf4602b0055009fc
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2025-11-25 16:28:59 -0500

    remove unused import

--- dpc-load-testing/smoke_test_workflows/auth.js
+++ dpc-load-testing/smoke_test_workflows/auth.js
@@ -2,7 +2,6 @@
 /* eslint no-console: "off" */
 
 import { check } from 'k6';
-import exec from 'k6/execution'
 import { generateDPCToken } from '../generate-dpc-token.js';
 import {
   createClientToken,
