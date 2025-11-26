d18e81b83bc03f64a73606d9041cd4fd3ea518b7
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2025-11-26 09:28:02 -0500

    Attribution starts before aggregation in CI

--- dpc-test.sh
+++ dpc-test.sh
@@ -55,7 +55,9 @@ fi
 
 docker compose -p start-v1-app down
 
-USE_BFD_MOCK=true docker compose -p start-v1-app up db attribution aggregation --wait
+# The attribution DB migration has to finish before the aggregation migration can start
+USE_BFD_MOCK=true docker compose -p start-v1-app up db attribution --wait
+USE_BFD_MOCK=true docker compose -p start-v1-app up db aggregation --wait
 
 # Run the integration tests
 USE_BFD_MOCK=true docker compose -p start-v1-app up --exit-code-from tests tests
