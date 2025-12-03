2e8453698fd56a00d38bce1d136c4405c10ef78b
jose-verdance <jose@verdance.co>
2025-12-03 11:24:55 -0500

    dpc-5009 - updating deployment health check script to use ecs service-stable and removed unnecessary loop logic

--- scripts/check-deployment-status.sh
+++ scripts/check-deployment-status.sh
@@ -4,10 +4,28 @@ set -euxo pipefail
 
 ENV="$1" # env
 TARGET_GROUP="dpc-${ENV}-${2}" # target
-TASK_NAME="dpc-${ENV}-${3}" # task_name
 SVC_NAME="dpc-${ENV}-${4}" # service
 CLUSTER_NAME="dpc-${ENV}-${5}" # cluster
 SVC_VERSION="$6" # service version
+
+# 1) Wait for ECS to certify deployment stability
+
+echo "Waiting for ECS service ${SVC_NAME}-${SVC_VERSION} to become stable..."
+
+aws ecs wait services-stable \
+  --cluster "$CLUSTER_NAME" \
+  --services "${SVC_NAME}-${SVC_VERSION}"
+
+if [ $? -ne 0 ]; then
+    echo "ECS deployment failed or timed out. Circuit Breaker may have initiated a rollback."
+    exit 1
+fi
+
+echo "ECS service ${SVC_NAME}-${SVC_VERSION} stable"
+
+# 2) Wait for ELB to verify target group health
+echo "---"
+echo "1. Checking for existing Target Group: $TARGET_GROUP"
 
 set +e
 TARGET_GROUP_ARN=$(aws elbv2 describe-target-groups \
@@ -18,98 +36,20 @@ set -e
 
 if [ -z "$TARGET_GROUP_ARN" ]
 then
-  echo "No target groups defined! Assuming deploying a new ECS cluster and exiting healthy..."
-  exit 0
+  echo "ERROR: Target Group ${TARGET_GROUP} not found or naming mismatch. Skipping ELB health check and exiting as a failure."
+  exit 1
+else
+  echo "Found Target Group ARN: $TARGET_GROUP_ARN"
 fi
 
-# Verify the deployment's health
-#
-# Criteria:
-# - Only "healthy" container instances are present in target group
-# - "Running tasks" count matches "Desired tasks" count for the service
-# - There are no task definitions marked "INACTIVE" currently in-service
+echo "Waiting for ELB target group ${TARGET_GROUP} to become stable..."
 
-# Stash the new task definition
-set +e
-ACTIVE_TASK=$(aws ecs list-task-definitions \
-    --status ACTIVE \
-    --query 'taskDefinitionArns[0]' \
-    --family-prefix "$TASK_NAME" \
-    --sort desc \
-    --output text)
-set -e
+aws elbv2 wait target-in-service \
+  --target-group-arn ${TARGET_GROUP_ARN}
 
-if [ -z "$ACTIVE_TASK" ]
-then
-  echo "No active task defined! Assuming deploying a new ECS cluster and exiting healthy..."
-  exit 0
-fi
-
-# Try polling for 30 minutes max to verify a deployment's health
-TIMEOUT=1800
-ELAPSED=0
-SLEEP_SECONDS=15
-VIABLE=4
-CHECKED=0
-
-while true
-do
-  if [ "$ELAPSED" -ge "$TIMEOUT" ]
-  then
-    echo "Timeout waiting for healthy containers"
+if [ $? -ne 0 ]; then
+    echo "ELB target group health check failed or timed out."
     exit 1
-  fi
+fi
 
-  sleep "${SLEEP_SECONDS}s"
-
-  set +e
-  RESULT=$(aws elbv2 describe-target-health \
-    --target-group-arn "$TARGET_GROUP_ARN" \
-    --query "TargetHealthDescriptions[*].{health: TargetHealth}" \
-    --output text)
-  set -e
-
-  set +e
-  HEALTH_CHECK=$(echo "$RESULT" | grep -E "unhealthy|initial|draining|unused")
-  set -e
-
-  RUNNING_COUNT=$(aws ecs describe-services --cluster "$CLUSTER_NAME" \
-    --services "${SVC_NAME}-${SVC_VERSION}" \
-    --query "services[?serviceName=='${SVC_NAME}-${SVC_VERSION}'].{r: runningCount}" \
-    --output text)
-
-  set +e
-  RUNNING_TASKS=$(aws ecs describe-tasks \
-    --tasks $(aws ecs list-tasks \
-              --service-name "${SVC_NAME}-${SVC_VERSION}" \
-              --cluster "$CLUSTER_NAME" \
-              --query 'taskArns[*]' \
-              --output text) \
-    --cluster "$CLUSTER_NAME" \
-    --query 'tasks[*].{def: taskDefinitionArn}' \
-    --output text)
-  set -e
-
-  set +e
-  ACTIVE_TASK_CHECK=$(echo "$RUNNING_TASKS" | grep -v "$ACTIVE_TASK")
-  set -e
-
-  set +e
-  DESIRED_COUNT=$(aws ecs describe-services --cluster "$CLUSTER_NAME" \
-    --services "${SVC_NAME}-${SVC_VERSION}" \
-    --query "services[?serviceName=='${SVC_NAME}-${SVC_VERSION}'].{d: desiredCount}" \
-    --output text)
-  set -e
-
-  if [ -z "$ACTIVE_TASK_CHECK" ] && [ -z "$HEALTH_CHECK" ] && [ "$RUNNING_COUNT" == "$DESIRED_COUNT" ]
-  then
-    if [ "$CHECKED" == "$VIABLE" ]
-    then
-      exit 0
-    else
-      CHECKED=$(($CHECKED + 1))
-    fi
-  fi
-
-  ELAPSED=$(($ELAPSED + $SLEEP_SECONDS))
-done
+echo "Deployment successful and application layer confirmed healthy."
