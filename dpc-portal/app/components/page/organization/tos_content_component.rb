d78255a5878ef002d8bdd0e106a9675e73f2b104
Luke Short <lukeshortemail@gmail.com>
2025-12-10 11:09:30 -0800

    move ToS content to a new component

---
+++ dpc-portal/app/components/page/organization/tos_content_component.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Page
+  module Organization
+    # Component to display Terms of Service content
+    class TosContentComponent < ViewComponent::Base
+    end
+  end
+end
+
