c5812752724ec967e0865fc54b77334728a26d3a
Luke Short <lukeshortemail@gmail.com>
2025-12-18 11:38:10 -0800

    add PoC for chaining filestream with gzip stream

--- dpc-api/src/main/java/gov/cms/dpc/api/core/FileManager.java
+++ dpc-api/src/main/java/gov/cms/dpc/api/core/FileManager.java
@@ -46,6 +46,23 @@ public class FileManager {
                 new File(path.toString()));
     }
 
+    public FilePointer getCompressedFile(UUID organizationID, String fileID) {
+
+        final JobQueueBatchFile batchFile = this.jobQueue.getJobBatchFile(organizationID, fileID)
+                .orElseThrow(() -> new WebApplicationException("Cannot find file", Response.Status.NOT_FOUND));
+
+        final JobQueueBatch jobQueueBatch = this.jobQueue.getBatch(batchFile.getBatchID())
+                .orElseThrow(() -> new WebApplicationException("Cannot export job for file", Response.Status.NOT_FOUND));
+
+        final java.nio.file.Path path = Paths.get(String.format("%s/%s.ndjson.gz", fileLocation, batchFile.getFileName()));
+        logger.debug("Streaming compressed file {}", path);
+        return new FilePointer(Hex.toHexString(batchFile.getChecksum()),
+                batchFile.getFileLength(),
+                batchFile.getJobID(),
+                jobQueueBatch.getStartTime().orElseThrow(() -> new IllegalStateException("Cannot find start time of completed job")),
+                new File(path.toString()));
+    }
+
     public static class FilePointer {
 
         private final String checksum;
