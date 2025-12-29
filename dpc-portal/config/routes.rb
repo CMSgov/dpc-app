892b051440d089cc2566f07ee66a222d72b62af9
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2025-12-29 15:42:51 -0800

    cleanup

--- dpc-portal/config/routes.rb
+++ dpc-portal/config/routes.rb
@@ -25,7 +25,6 @@ Rails.application.routes.draw do
   # method, which does not work correctly for applications served on a subpath.
   match '/portal', to: 'organizations#index', via: :get
 
-
   resources :organizations, only: [:index, :show, :new, :create] do
     resources :client_tokens, only: [:new, :create, :destroy]
     resources :public_keys, only: [:new, :create, :destroy]
