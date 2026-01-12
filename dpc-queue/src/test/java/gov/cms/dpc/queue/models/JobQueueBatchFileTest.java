888a843b7423f20534c8cf2ce438b233e70313e1
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2026-01-12 13:45:45 -0500

    Me/dpc 5052 get job batch file upgrade (#2877)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-5052
    
    ## 🛠 Changes
    
    - Use info from file name when searching the DB for an extract file.
    - Update Hibernate HQL to use standard entity naming conventions.
    
    ## ℹ️ Context
    
    We've recently been getting a large number of requests to our `/Data`
    endpoint to download extract files, and these are spiking the CPU on our
    DB and setting off alarms. Checking through AWS RDS logs, the majority
    of our DB usage is coming from the query that loads the
    `JobQueueBatchFile` based on filename and org id. That query runs in
    approximately 0.7s and does a full table scan on the
    `job_queue_batch_file` table since it doesn't hit an index. This new
    version of the query runs in ~0.4s and searches using the table's
    primary key.
    
    ## 🧪 Validation
    
    - Ran locally.
    - [Deployed](https://github.com/CMSgov/dpc-app/actions/runs/20829890644)
    to dev.

--- dpc-queue/src/test/java/gov/cms/dpc/queue/models/JobQueueBatchFileTest.java
+++ dpc-queue/src/test/java/gov/cms/dpc/queue/models/JobQueueBatchFileTest.java
@@ -45,4 +45,47 @@ class JobQueueBatchFileTest {
         assertEquals(1, a.getCount());
         assertEquals(new JobQueueBatchFile.JobQueueBatchFileID(batchID, DPCResourceType.Patient, 0), a.getJobQueueBatchFileID());;
     }
+
+    @Test
+    void testParsesFileName() {
+        final UUID batchID = UUID.randomUUID();
+        final int seq = 1;
+        final DPCResourceType resourceType = DPCResourceType.Patient;
+        String fileName = JobQueueBatchFile.formOutputFileName(batchID, resourceType, seq);
+
+        JobQueueBatchFile.JobQueueBatchFileID fileId = JobQueueBatchFile.getFileIdFromName(fileName);
+        assertEquals(batchID, fileId.getBatchID());
+        assertEquals(seq, fileId.getSequence());
+        assertEquals(DPCResourceType.Patient, fileId.getResourceType());
+    }
+
+    @Test
+    void testHandlesBadFileName() {
+        final String fileName = "bad_file_name";
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> JobQueueBatchFile.getFileIdFromName(fileName));
+        assertEquals(String.format("Could not parse file name: %s", fileName), e.getMessage());
+    }
+
+    @Test
+    void testHandlesBadUUIDInFileName() {
+        final String fileName = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx-0.explanationofbenefit";
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> JobQueueBatchFile.getFileIdFromName(fileName));
+        assertEquals(String.format("Could not parse file name: %s", fileName), e.getMessage());
+    }
+
+    @Test
+    void testHandlesBadSeqInFileName() {
+        final UUID batchId = UUID.randomUUID();
+        final String fileName = String.format("%s-X.explanationofbenefit", batchId);
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> JobQueueBatchFile.getFileIdFromName(fileName));
+        assertEquals(String.format("Could not parse file name: %s", fileName), e.getMessage());
+    }
+
+    @Test
+    void testHandlesBadResourceInFileName() {
+        final UUID batchId = UUID.randomUUID();
+        final String fileName = String.format("%s-0.fake_resource", batchId);
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> JobQueueBatchFile.getFileIdFromName(fileName));
+        assertEquals(String.format("Could not parse file name: %s", fileName), e.getMessage());
+    }
 }
