1456c0a8c99386b5c9e9700b28149788750814e2
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2026-01-22 10:35:40 -0500

    prevent bad flash messages

--- dpc-portal/app/components/core/alert/component.rb
+++ dpc-portal/app/components/core/alert/component.rb
@@ -9,6 +9,13 @@ module Core
       def initialize(status: '', heading: '', include_icon: true)
         super
 
+        if %i[info warning error success notice alert].include?(status.to_sym) || status.blank?
+          @valid = true
+        else
+          log_error(status)
+          @valid = false
+        end
+
         @status = case status
                   when '', 'notice', :notice
                     :info
@@ -19,6 +26,12 @@ module Core
                   end
         @include_icon = include_icon
         @heading = heading
+      end
+
+      def log_error(status)
+        Rails.logger.error(["Unexpected Flash Status: #{status}",
+                            { actionContext: LoggingConstants::ActionContext::Rendering,
+                              actionType: LoggingConstants::ActionType::InvalidFlashStatus }])
       end
     end
   end
