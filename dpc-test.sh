9f1edb5c16f61f41644e834777d812581219b655
Ashley Weaver <ashleyweaver@navapbc.com>
2026-01-26 11:07:14 -0500

    Thread tests

--- dpc-test.sh
+++ dpc-test.sh
@@ -53,8 +53,10 @@ fi
 
 # Build the application
 docker compose -p start-v1-app up db --wait
-mvn clean compile -Perror-prone -B -V -ntp
-mvn package -Pci -ntp
+mvn -T 4 clean compile -Perror-prone -B -V -ntp
+mvn -T 4 -pl -dpc-api test # exclude api from threaded tests
+mvn -pl dpc-api test # test api
+mvn -T 4 package -Pci -ntp -DskipTests
 
 # Format the test results
 if [ -n "$REPORT_COVERAGE" ]; then
