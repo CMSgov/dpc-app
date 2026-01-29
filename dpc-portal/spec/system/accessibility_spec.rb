a92a6f1d96a7066e4fe8ddcbed131b17a07c79a0
Luke Short <lukeshortemail@gmail.com>
2026-01-29 06:09:43 -0800

    log URL for failed accessibility tests

--- dpc-portal/spec/system/accessibility_spec.rb
+++ dpc-portal/spec/system/accessibility_spec.rb
@@ -8,6 +8,14 @@ RSpec.describe 'Accessibility', type: :system do
   before do
     driven_by(:selenium_headless)
   end
+
+  after do |test_case|
+    next unless test_case.exception
+
+    url  = page.current_url rescue nil
+    warn "[Failure URL]: #{url}" if url
+  end
+
   let(:dpc_api_organization_id) { 'some-gnarly-guid' }
   let(:axe_standard) { %w[best-practice wcag21aa] }
   context 'login' do
