fb4ed5ba484c3bb2bdd00443feb8312c3ebdbe33
Ashley Weaver <ashleyweaver@navapbc.com>
2026-02-02 09:55:04 -0500

    Update dpc-test.sh

--- dpc-test.sh
+++ dpc-test.sh
@@ -53,8 +53,8 @@ fi
 
 # Build the application
 docker compose -p start-v1-app up db --wait
-mvn -T 1C clean compile -Perror-prone -B -V -ntp
-mvn -T 1C package -Pci -ntp
+mvn -T 1.5C clean compile -Perror-prone -B -V -ntp
+mvn -T 1.5C package -Pci -ntp
 
 # Format the test results
 if [ -n "$REPORT_COVERAGE" ]; then
