e52cfc4bb1a087435e9e1b4802561b529a5ed4c8
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2026-02-02 10:09:20 -0500

    Moved to insights directory in ops folder.  Updated scheduling and removed date from file names.

--- ops/insights/schedule_view_exports.sql
+++ ops/insights/schedule_view_exports.sql
@@ -7,7 +7,7 @@ Run with:
 psql -U {MASTER_DB_USER} \
 -d postgres \
 -h {DB_ENDPOINT} \
--f scripts/schedule_view_exports.sql
+-f schedule_view_exports.sql
 */
 
 -- Create a job in the cron.job table.  Jobs will be overwritten if they have the same job name, so this is idempotent.
@@ -24,7 +24,7 @@ BEGIN
                 $job$ SELECT * from aws_s3.query_export_to_s3('select * from %s',
                   aws_commons.create_s3_uri(
                     'dpc-prod-aurora-export-20251028154132844600000001',
-                    CONCAT('%s_', TO_CHAR(NOW(), 'YYYY-MM-DD_HH24-MI-SS'), '.csv'),
+                    CONCAT('%s_', '.csv'),
                     'us-east-1'
                   )
                 ) $job$,
@@ -40,11 +40,11 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT create_job('benes_requested_last_week', '0 */6 * * *', 'dpc_queue');
-SELECT create_job('bulk_calls_made_last_week', '5 */6 * * *', 'dpc_queue');
-SELECT create_job('patient_everything_requests_last_week', '10 */6 * * *', 'dpc_queue');
-SELECT create_job('since_requests_last_week', '15 */6 * * *', 'dpc_queue');
-SELECT create_job('unique_benes_served_last_week', '20 */6 * * *', 'dpc_queue');
-SELECT create_job('unique_providers_served_all_time', '25 */6 * * *', 'dpc_queue');
-SELECT create_job('hcos_credentialed_all_time', '30 */6 * * *', 'dpc_auth');
-SELECT create_job('hcos_with_active_credentials', '35 */6 * * *', 'dpc_auth');
+SELECT create_job('benes_requested_last_week', '0 0 * * *', 'dpc_queue');
+SELECT create_job('bulk_calls_made_last_week', '5 0 * * *', 'dpc_queue');
+SELECT create_job('patient_everything_requests_last_week', '10 0 * * *', 'dpc_queue');
+SELECT create_job('since_requests_last_week', '15 0 * * *', 'dpc_queue');
+SELECT create_job('unique_benes_served_last_week', '20 0 * * *', 'dpc_queue');
+SELECT create_job('unique_providers_served_all_time', '25 0 * * *', 'dpc_queue');
+SELECT create_job('hcos_credentialed_all_time', '30 0 * * *', 'dpc_auth');
+SELECT create_job('hcos_with_active_credentials', '35 0 * * *', 'dpc_auth');
