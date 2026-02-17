619c26ec8a1f54c367ee135c7869b0f21f550ceb
Jose-verdance <jose@verdance.co>
2026-02-17 16:55:31 -0500

    Ja/dpc -4984 - Adding smoke test for patient everything (#2906)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-4984
    
    ## 🛠 Changes
    
    <!-- What was added, updated, or removed in this PR? -->
    
    This change adds new smoke test for both synchronous and asynchronous
    patient everything endpoints.
    
    ## ℹ️ Context
    
    <!-- Why were these changes made? Add background context suitable for a
    non-technical audience. -->
    
    <!-- If any of the following security implications apply, this PR must
    not be merged without Stephen Walter's approval. Explain in this section
    and add @SJWalter11 as a reviewer.
      - Adds a new software dependency or dependencies.
      - Modifies or invalidates one or more of our security controls.
      - Stores or transmits data that was not stored or transmitted before.
    - Requires additional review of security implications for other reasons.
    -->
    
    This adds smoke test coverage to this high use endpoint.
    
    ## 🧪 Validation
    
    <!-- How were the changes verified? Did you fully test the acceptance
    criteria in the ticket? Provide reproducible testing instructions and
    screenshots if applicable. -->
    
    Confirmed the smoke test succeed locally and in higher environments. 
    
    Dev
    https://github.com/CMSgov/dpc-app/actions/runs/21990516533
    
    Test
    https://github.com/CMSgov/dpc-app/actions/runs/21958462870
    
    Sandbox
    https://github.com/CMSgov/dpc-app/actions/runs/21959205829
    
    Prod
    https://github.com/CMSgov/dpc-app/actions/runs/21960235280

--- dpc-load-testing/smoke_test_workflows/bulk_export.js
+++ dpc-load-testing/smoke_test_workflows/bulk_export.js
@@ -1,23 +1,20 @@
 /*global console*/
 /* eslint no-console: "off" */
 
-import { check, sleep } from 'k6';
+import { check } from 'k6';
 import exec from 'k6/execution'
 import { generateDPCToken } from '../generate-dpc-token.js';
 import {
-  authorizedGet,
   createGroupWithPatients,
   createPatientsRawData,
   createPractitionersRawData,
   exportGroup,
   findGroupByPractitionerNpi,
 } from '../dpc-api-client.js';
+import {
+  monitorJob,
+} from './smoke_test_utils.js';
 
-
-// We allow errors in local because we don't need to test our own connection to BFD
-const JOB_OUPUT_ERROR_LENGTH = __ENV.ENVIRONMENT == 'local' ? 1 : 0;
-// Our WAF rate limits us to 300 requests every 5 minutes, so don't poll too often
-const EXPORT_POLL_INTERVAL_SEC = __ENV.ENVIRONMENT == 'local' ? 1 : 20;
 const practitionerBundle =  __ENV.ENVIRONMENT == 'prod' ? open('./resources/prod_provider_bundle.json') : open('./resources/provider_bundle.json');
 const patientBundle =  __ENV.ENVIRONMENT == 'prod' ? open('./resources/prod_patient_bundle-dpr.json') : open('./resources/patient_bundle-dpr.json');
 const associationsFile =  __ENV.ENVIRONMENT == 'prod' ? open('./resources/prod_test_associations.csv') : open('./resources/test_associations-dpr.csv');
@@ -114,41 +111,6 @@ function exportGroups(token, orgId, practitioners) {
   // Verify the export worked
   for (const jobUrl of jobUrls) {
     monitorJob(token, jobUrl);
-  }
-}
-
-function monitorJob(token, jobUrl){
-  // Loop until it isn't 202
-  // NB: because we are not refreshing the token, it will eventually get a 401
-  let jobResponse = authorizedGet(token, jobUrl);
-  while(jobResponse.status === 202){
-    sleep(EXPORT_POLL_INTERVAL_SEC);
-    jobResponse = authorizedGet(token, jobUrl);
-  }
-
-  // We got a rare exception when testing, so try is to make sure we capture the problem
-  try {
-    const checkJobResponse = check(
-      jobResponse,
-      {
-        'job response code was 200': res => res.status === 200,
-        'no job output errors': res => res.json().error.length <= JOB_OUPUT_ERROR_LENGTH,
-      }
-    );
-
-    if (!checkJobResponse) {
-      if (jobResponse.status == 401) {
-        console.error(`JOB TIMED OUT FOR TEST - MAYBE NOT FAIL: ${jobUrl}`);
-      } else if (jobResponse.json().error) {
-        console.error(`Too many errors in job output ${jobResponse.json().error.length}: ${jobUrl}`);
-      } else {
-        console.error(`Bad response code when checking job output ${jobResponse.status} ${jobUrl}`);
-      }
-    }
-  } catch (error) {
-    console.error(`Error thrown parsing ${jobResponse.body}: ${jobUrl}`)
-    console.error(error);
-    exec.test.fail();
   }
 }
 
---
+++ dpc-load-testing/smoke_test_workflows/patient_export.js
@@ -0,0 +1,92 @@
+/*global console*/
+/* eslint no-console: "off" */
+
+import { check } from 'k6';
+import { generateDPCToken } from '../generate-dpc-token.js';
+import {
+  createPatientsBatch,
+  patientEverything,
+  createPractitioners,
+} from '../dpc-api-client.js';
+import {
+  monitorJob,
+} from './smoke_test_utils.js';
+
+const practitionerNpi = __ENV.ENVIRONMENT == 'prod' ? "1234329724" : "3247281157";
+
+export async function checkPatientEverythingExportWorkflow(data) {
+  const orgId = data.orgId;
+  const token = generateDPCToken(orgId, data.goldenMacaroon);
+
+  // Uploading practitioner
+  const createPractitionerResponse = createPractitioners(token, practitionerNpi);
+  const checkUploadPractitioners = check(
+    createPractitionerResponse,
+    {
+      'upload single practitioner returns 200': res => res.status == 200,
+      'uploaded one practitioner': res => (res.json().entry || '').length == 1,
+    }
+  )
+  if (!checkUploadPractitioners) {
+    console.error(`Failed to upload single practitioner ${createPractitionerResponse.body}`);
+  }
+
+  // Uploading Patient
+  const patientMbi = "1SQ3F00AA00"; // valid BFD patient 
+  const uploadPatientResponse = createPatientsBatch(token, [patientMbi]);
+  const checkUploadPatient = check(
+    uploadPatientResponse,
+    {
+      'upload single patient returns 200': res => res.status == 200,
+      'uploaded one patient': res => (res.json().entry || '').length == 1,
+    }
+  )
+  if (!checkUploadPatient) {
+    console.error(`Failed to upload patients ${uploadPatientResponse.body}`);
+  }
+
+  // Abort if either upload failed
+  if (!checkUploadPractitioners || !checkUploadPatient) {
+    return;
+  }
+
+  // Create Group for practitioner and patient
+  const practitionerId = createPractitionerResponse.json().entry[0].resource.id;
+  const patientId = uploadPatientResponse.json().entry[0].resource.id;
+  
+  // Patient everything export
+  const patientEverythingResponse = patientEverything(token, orgId, practitionerId, patientId);
+  const checkPatientEverything = check(
+    patientEverythingResponse,
+    {
+      'get patient everything returns 200': res => res.status == 200,
+      'response returns bundle with data': (res) => {
+          const resJson = res.json();
+          return resJson.resourceType == "Bundle" && (resJson.total > 0 || (resJson.entry && resJson.entry.length > 0));
+      },
+    }
+  )
+
+  if (!checkPatientEverything){
+    console.error(`Failed to call patient everything for ${patientId}: ${patientEverythingResponse.body}`);
+  }
+
+  // Patient everything export async
+  const patientEverythingAsyncResponse = patientEverything(token, orgId, practitionerId, patientId, "respond-async");
+  const checkPatientEverythingAsync = check(
+    patientEverythingAsyncResponse,
+    {
+      'get patient everything async returns 202': res => res.status == 202,
+      'response location header present': (res) => !!(res?.headers['Content-Location']),
+    }
+  )
+
+  if (!checkPatientEverythingAsync) {
+    console.error(`Failed to call patient everything async for ${patientId}: ${patientEverythingAsyncResponse.body}`);
+  }
+
+  // Verify patient everything job succeeds
+  const jobUrl = patientEverythingAsyncResponse.headers['Content-Location'];
+  monitorJob(token, jobUrl);
+
+}
