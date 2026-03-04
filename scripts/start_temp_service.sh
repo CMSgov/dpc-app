6041ce091d5b0e73a914d2a83c41ddbfaadcfe9d
Ashley Weaver <ashleyweaver@navapbc.com>
2026-03-04 13:52:50 -0500

    Set env vars within scope

--- scripts/start_temp_service.sh
+++ scripts/start_temp_service.sh
@@ -11,12 +11,9 @@ set -euxo pipefail
 CLUSTER_NAME=$1
 SERVICE_NAME=$2
 INITIAL_TAG=$3
-export CLUSTER_NAME
-export SERVICE_NAME
 
 # Add your initials here
 NEW_SERVICE_NAME="${INITIAL_TAG}_${SERVICE_NAME}"
-export NEW_SERVICE_NAME >> "$GITHUB_ENV"
 
 # Check if the temp service is already up from a previous run, and if not start it.
 NEW_SERVICE_COUNT=$(aws ecs describe-services --cluster "$CLUSTER_NAME" --services "$NEW_SERVICE_NAME" | jq -r '.services[].runningCount')
@@ -25,7 +22,6 @@ if [[ -z "$NEW_SERVICE_COUNT" || "$NEW_SERVICE_COUNT" -eq 0 ]]; then
   echo "Getting task definition."
   TASK_DEF_ARN=$(./dpc-app/scripts/get_writeable_task_def_for_service.sh "$CLUSTER_NAME" "$SERVICE_NAME")
   TASK_DEF=${TASK_DEF_ARN#*/}
-  export TASK_DEF >> "$GITHUB_ENV"
 
   echo "Getting network config." # Gets security groups and subnets
   NETWORK_CONFIG=$(aws ecs describe-services --cluster "$CLUSTER_NAME" --services "$SERVICE_NAME" --query "services[0].networkConfiguration")
@@ -59,8 +55,6 @@ NEW_TASK_ARN=$(echo "$NEW_CONTAINER_INFO" | jq -r '.taskArn')
 NEW_TASK_ID=${NEW_TASK_ARN#*/*/}
 echo "New task id: $NEW_TASK_ID"
 echo "New container name: $NEW_CONTAINER_NAME"
-export NEW_TASK_ID >> "$GITHUB_ENV"
-export NEW_CONTAINER_NAME >> "$GITHUB_ENV"
 
 # Last but not least, get a shell into the container
 aws ecs execute-command \
