4b8597ed0c8b915d9ccced7f33b3b004f815b7e5
Jose-verdance <jose@verdance.co>
2026-03-05 13:34:37 -0500

    Ja/dpc 5209 remove ip address label fix (#2931)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-5209
    
    ## 🛠 Changes
    
    <!-- What was added, updated, or removed in this PR? -->
    This change fixes a bug preventing users from submitting new public IP
    addresses. This change also removes the IP address label since it will
    no longer be used.
    
    ## ℹ️ Context
    
    <!-- Why were these changes made? Add background context suitable for a
    non-technical audience. -->
    
    <!-- If any of the following security implications apply, this PR must
    not be merged without Stephen Walter's approval. Explain in this section
    and add @SJWalter11 as a reviewer.
      - Adds a new software dependency or dependencies.
      - Modifies or invalidates one or more of our security controls.
      - Stores or transmits data that was not stored or transmitted before.
    - Requires additional review of security implications for other reasons.
    -->
    
    An error was being thrown for the missing label value.
    
    ## 🧪 Validation
    
    <!-- How were the changes verified? Did you fully test the acceptance
    criteria in the ticket? Provide reproducible testing instructions and
    screenshots if applicable. -->
    
    Confirmed IP addresses can be submitted successfully and tests pass.
    <img width="1177" height="688" alt="Screenshot 2026-03-04 at 3 34 18 PM"
    src="https://github.com/user-attachments/assets/a23b6a05-05d1-4154-b62c-36c4ea7b7d7e"
    />
    <img width="1063" height="340" alt="Screenshot 2026-03-05 at 9 23 42 AM"
    src="https://github.com/user-attachments/assets/d76181e7-05e1-4a5d-b75e-3f57a76b1504"
    />
    
    Successful deploy to test environment
    https://github.com/CMSgov/dpc-app/actions/runs/22691438982

--- engines/api_client/spec/services/dpc_client_spec.rb
+++ engines/api_client/spec/services/dpc_client_spec.rb
@@ -583,19 +583,18 @@ RSpec.describe DpcClient do
       context 'successful API request' do
         it 'sends data to API and sets response instance variables' do
           stub_request(:post, 'http://dpc.example.com/IpAddress').with(
-            body: { ip_address: '136.226.19.87', label: 'Sandbox IP 1' }
+            body: { ip_address: '136.226.19.87' }
           ).to_return(
             status: 200,
-            body: '{"label":"Sandbox IP 1","createdAt":"2019-11-07T19:38:44.205Z",' \
+            body: '{"createdAt":"2019-11-07T19:38:44.205Z",' \
                   '"id":"3fa85f64-5717-4562-b3fc-2c963f66afa6"}'
           )
 
           api_client = DpcClient.new
-          api_client.create_ip_address(reg_org.api_id, params: { label: 'Sandbox IP 1', ip_address: '136.226.19.87' })
+          api_client.create_ip_address(reg_org.api_id, params: { ip_address: '136.226.19.87' })
           expect(api_client.response_status).to eq(200)
           expect(api_client.response_body).to eq(
             {
-              'label' => 'Sandbox IP 1',
               'createdAt' => '2019-11-07T19:38:44.205Z',
               'id' => '3fa85f64-5717-4562-b3fc-2c963f66afa6'
             }
@@ -608,17 +607,17 @@ RSpec.describe DpcClient do
       context 'successful API request' do
         it 'sends data to API and sets response instance variables' do
           stub_request(:post, 'http://dpc.example.com/IpAddress').with(
-            body: { ip_address: '136.226.19.87', label: 'Sandbox IP 1' }
+            body: { ip_address: '136.226.19.87' }
           ).to_return(
             status: 200,
-            body: '{"label":"Sandbox IP 1","createdAt":"2019-11-07T19:38:44.205Z",' \
+            body: '{"createdAt":"2019-11-07T19:38:44.205Z",' \
                   '"id":"3fa85f64-5717-4562-b3fc-2c963f66afa6"}'
           )
 
           api_client = DpcClient.new
           api_client.create_ip_address(
             reg_org.api_id,
-            params: { label: 'Sandbox IP 1', ip_address: '136.226.19.87' }
+            params: { ip_address: '136.226.19.87' }
           )
           stub_request(:delete, 'http://dpc.example.com/IpAddress/3fa85f64-5717-4562-b3fc-2c963f66afa6')
             .with(
@@ -637,7 +636,7 @@ RSpec.describe DpcClient do
       context 'unsuccessful API request' do
         it 'sends data to API and sets response instance variables' do
           stub_request(:post, 'http://dpc.example.com/IpAddress').with(
-            body: { ip_address: '136.226.19.87', label: 'Sandbox IP 1' }
+            body: { ip_address: '136.226.19.87' }
           ).to_return(
             status: 500,
             body: '{}'
@@ -647,7 +646,6 @@ RSpec.describe DpcClient do
           api_client.create_ip_address(
             reg_org.api_id,
             params: {
-              label: 'Sandbox IP 1',
               ip_address: '136.226.19.87'
             }
           )
@@ -664,7 +662,7 @@ RSpec.describe DpcClient do
             headers: { 'Content-Type' => 'application/json' }
           ).to_return(
             status: 200,
-            body: '[{"id":"4r85cfb4-dc36-4cd0-b8f8-400a6dea2d66","label":"Sandbox IP 1",' \
+            body: '[{"id":"4r85cfb4-dc36-4cd0-b8f8-400a6dea2d66",' \
                   '"createdAt":"2019-11-07T17:15:22.781Z"}]'
           )
 
@@ -674,7 +672,6 @@ RSpec.describe DpcClient do
           expect(api_client.response_body).to eq(
             [{
               'id' => '4r85cfb4-dc36-4cd0-b8f8-400a6dea2d66',
-              'label' => 'Sandbox IP 1',
               'createdAt' => '2019-11-07T17:15:22.781Z'
             }]
           )
