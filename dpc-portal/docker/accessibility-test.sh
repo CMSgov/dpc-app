276614668595dc6372d5391572c634474d6d9f27
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2026-04-06 11:23:57 -0400

    Me/dpc 5305 fix invite cd (#2972)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-5305
    
    ## 🛠 Changes
    
    - Fixed bug where clicking the "Send invite" button when inviting a CD
    to an organization wouldn't work.
    - Added a test that should catch this issue in the future.
    
    ## ℹ️ Context
    
    There is a javascript function that listens for a click on the _Send
    invite_ button on the form. That javascript function does some
    validation, then "clicks" on an invisible button that displays the modal
    dialog where you acknowledge that you want to send an invite. The
    invisible button was accidentally deleted in a previous PR and broke
    this functionality, so this PR puts it back.
    
    This wasn't caught because our existing unit tests can't test dynamic
    functionality provided by javascript, so a new system test was added
    that works similar to our accessibility tests and simulates clicking on
    the button and making sure the modal is displayed.
    
    ## Note
    Unfortunately, browser based tests aren't running on our Code Build
    runners, so the test has to be disabled. You can still run it locally
    for now, but until this gets fixed the test won't run.
    
    ## 🧪 Validation
    
    - Ran locally.
    - Tests added.

--- dpc-portal/docker/accessibility-test.sh
+++ dpc-portal/docker/accessibility-test.sh
@@ -3,4 +3,4 @@
 apk update
 apk add --no-cache icu-data-full
 apk add --no-cache firefox
-ACCESSIBILITY=true bundle exec rspec --tag type:system --format documentation
+ACCESSIBILITY=true bundle exec rspec spec/system/accessibility_spec.rb --tag type:system --format documentation
---
+++ dpc-portal/spec/system/new_invitation_spec.rb
@@ -0,0 +1,117 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Page::CredentialDelegate::NewInvitationComponent, type: :system, js: true do
+  include Devise::Test::IntegrationHelpers
+  include DpcClientSupport
+
+  before do
+    driven_by(:selenium_headless)
+  end
+
+  context 'CD invite' do
+    let(:dpc_api_organization_id) { 'some-gnarly-guid' }
+    let!(:user) { create(:user) }
+    let!(:org) { create(:provider_organization, dpc_api_organization_id:, name: 'Health Hut') }
+    let!(:ao_org_link) { create(:ao_org_link, user:, provider_organization: org) }
+
+    before do
+      sign_in user
+      org.update!(terms_of_service_accepted_by: user)
+    end
+
+    context 'Successful invitation' do
+      let(:mock_client_token_manager) { instance_double(ClientTokenManager) }
+      let(:mock_public_key_manager) { instance_double(PublicKeyManager) }
+      let(:mock_ip_address_manager) { instance_double(IpAddressManager) }
+      let(:tokens) { [] }
+      let(:keys) { [] }
+      let(:ip_addresses) { [] }
+
+      before do
+        allow(ClientTokenManager).to receive(:new).and_return(mock_client_token_manager)
+        allow(PublicKeyManager).to receive(:new).and_return(mock_public_key_manager)
+        allow(IpAddressManager).to receive(:new).and_return(mock_ip_address_manager)
+        allow(mock_client_token_manager).to receive(:client_tokens).and_return(tokens)
+        allow(mock_public_key_manager).to receive(:public_keys).and_return(keys)
+        allow(mock_ip_address_manager).to receive(:ip_addresses).and_return(ip_addresses)
+      end
+
+      it 'can invite cd' do
+        visit "/organizations/#{org.id}/credential_delegate_invitations/new"
+        expect(page).to have_selector('#verify-modal', visible: false)
+
+        # Fill in the form and click send invite
+        fill_in 'First or given name', with: 'John'
+        fill_in 'Last or family name', with: 'Smith'
+        fill_in 'Email', with: 'fake@fake.com'
+        fill_in 'Confirm email', with: 'fake@fake.com'
+        within('#cd-form') do
+          click_button('Send invite')
+        end
+        expect(page).to have_selector('#verify-modal', visible: true)
+
+        click_button('Yes, I acknowledge')
+
+        # Make sure the invitation was created with the correct info
+        invitation = Invitation.last
+        expect(invitation.provider_organization_id).to eq(org.id)
+        expect(invitation.invited_given_name).to eq('John')
+        expect(invitation.invited_family_name).to eq('Smith')
+        expect(invitation.invited_email).to eq('fake@fake.com')
+      end
+    end
+
+    it 'shows error on blank given name' do
+      visit "/organizations/#{org.id}/credential_delegate_invitations/new"
+      fill_in 'First or given name', with: ''
+      find_field('First or given name').send_keys(:tab)
+      expect(page).to have_css('p#invited_given_name_error_msg', text: "Can't be blank")
+    end
+
+    it 'shows error on blank family name' do
+      visit "/organizations/#{org.id}/credential_delegate_invitations/new"
+      fill_in 'Last or family name', with: ''
+      find_field('Last or family name').send_keys(:tab)
+      expect(page).to have_css('p#invited_family_name_error_msg', text: "Can't be blank")
+    end
+
+    it 'shows error on blank email' do
+      visit "/organizations/#{org.id}/credential_delegate_invitations/new"
+      fill_in 'Email', with: ''
+      find_field('Email').send_keys(:tab)
+      expect(page).to have_css('p#invited_email_error_msg', text: "Can't be blank")
+    end
+
+    it 'shows error on invalid email' do
+      visit "/organizations/#{org.id}/credential_delegate_invitations/new"
+      fill_in 'Email', with: 'not-an-email'
+      find_field('Email').send_keys(:tab)
+      expect(page).to have_css('p#invited_email_error_msg', text: 'Invalid email format')
+    end
+
+    it 'shows error on blank email confirmation' do
+      visit "/organizations/#{org.id}/credential_delegate_invitations/new"
+      fill_in 'Confirm email', with: ''
+      find_field('Confirm email').send_keys(:tab)
+      expect(page).to have_css('p#invited_email_confirmation_error_msg', text: "Can't be blank")
+    end
+
+    it 'shows error on email confirmation mismatch' do
+      visit "/organizations/#{org.id}/credential_delegate_invitations/new"
+      fill_in 'Email', with: 'fake@fake.com'
+      fill_in 'Confirm email', with: 'notfake@fake.com'
+      find_field('Confirm email').send_keys(:tab)
+      expect(page).to have_css('p#invited_email_confirmation_error_msg', text: "Email doesn't match")
+    end
+
+    it 'shows error on invalid email confirmation' do
+      visit "/organizations/#{org.id}/credential_delegate_invitations/new"
+      fill_in 'Email', with: 'not-an-email'
+      fill_in 'Confirm email', with: 'not-an-email'
+      find_field('Confirm email').send_keys(:tab)
+      expect(page).to have_css('p#invited_email_confirmation_error_msg', text: 'Invalid email format')
+    end
+  end
+end
