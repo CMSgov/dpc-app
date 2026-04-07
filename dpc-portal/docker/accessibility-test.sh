2084818b06c2aede7c2b8bdc0c99b73cfc134a9b
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2026-04-07 08:34:26 -0700

    DPC-5331 re-enable capybara tests (#2975)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-5331
    
    ## 🛠 Changes
    
    - Installed geckodriver manually for capybara tests
    - Updated accessibility test to use arm runner
    - Enabled capybara tests
    
    ## ℹ️ Context
    
    The move to arm runners messed up capybara tests.
    
    ## 🧪 Validation
    
    Tests run and pass.

--- dpc-portal/docker/accessibility-test.sh
+++ dpc-portal/docker/accessibility-test.sh
@@ -3,4 +3,5 @@
 apk update
 apk add --no-cache icu-data-full
 apk add --no-cache firefox
+apk add --no-cache geckodriver
 ACCESSIBILITY=true bundle exec rspec spec/system/accessibility_spec.rb --tag type:system --format documentation
--- dpc-portal/docker/system-tests.sh
+++ dpc-portal/docker/system-tests.sh
@@ -3,4 +3,5 @@
 apk update
 apk add --no-cache icu-data-full
 apk add --no-cache firefox
+apk add --no-cache geckodriver
 SYSTEM_TEST=true bundle exec rspec --exclude-pattern "spec/system/accessibility_spec.rb" --tag type:system
