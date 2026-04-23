90b5e6064b27b316f5943b7b8195f4fab1df2537
Luke Short <lukeshortemail@gmail.com>
2026-04-23 13:32:54 -0700

    specify arm64/linux for go build command

--- lambda/opt-out-import/parsers.go
+++ lambda/opt-out-import/parsers.go
@@ -129,6 +129,7 @@ func ParseSQSEvent(event events.SQSEvent) (*events.S3Event, error) {
 	var snsEntity events.SNSEntity
 	err := json.Unmarshal([]byte(event.Records[0].Body), &snsEntity)
 
+	log.Warn("snsEntity: ", snsEntity)
 	unmarshalTypeErr := new(json.UnmarshalTypeError)
 	if errors.As(err, &unmarshalTypeErr) {
 		log.Warn("Skipping event due to unrecognized format for SNS")
@@ -139,6 +140,7 @@ func ParseSQSEvent(event events.SQSEvent) (*events.S3Event, error) {
 
 	var s3Event events.S3Event
 	err = json.Unmarshal([]byte(snsEntity.Message), &s3Event)
+	log.Warn("s3Event: ", s3Event)
 
 	unmarshalTypeErr = new(json.UnmarshalTypeError)
 	if errors.As(err, &unmarshalTypeErr) {
