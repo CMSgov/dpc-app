9d04695eff022c8e5d09040aa9b0af13344f4f2c
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2026-05-12 10:10:19 -0400

    Update users in migration

--- dpc-portal/db/migrate/20260424193229_create_csp_users.rb
+++ dpc-portal/db/migrate/20260424193229_create_csp_users.rb
@@ -1,5 +1,5 @@
 class CreateCspUsers < ActiveRecord::Migration[8.0]
-  def change
+  def up
     create_table :csp_users do |t|
       t.references :user, null: false, foreign_key: true
       t.references :csp, null: false, foreign_key: true
@@ -11,8 +11,22 @@ class CreateCspUsers < ActiveRecord::Migration[8.0]
     add_index :csp_users, [:user_id, :csp_id], unique: true
 
     # Populate existing users with login_dot_gov
-    User.find_each do |user|
-      CspUser.create!(user: user, uuid: user.uid, csp: Csp.find_by(name: :login_dot_gov))
+    Csp.find_by(name: :login_dot_gov)&.tap do |csp|
+      User.find_each do |user|
+        # Every user initially created with "openid_connect" should be updated to "login_dot_gov"
+        if user.provider == "openid_connect"
+          user.update_columns(provider: "login_dot_gov")
+        end
+
+        CspUser.find_or_create_by!(user: user, csp: csp) do |csp_user|
+          csp_user.uuid = user.uid
+        end
+      end
     end
+  end
+
+  def down
+    User.where(provider: "login_dot_gov").update_all(provider: "openid_connect")
+    drop_table :csp_users
   end
 end
