6b84c28d8e775c35bc79b4116a1a048907adc8f3
Luke Short <lukeshortemail@gmail.com>
2026-05-13 12:37:30 -0700

    separate buttons to call idme or clear auth endpoints

--- dpc-portal/app/components/page/session/login_component.rb
+++ dpc-portal/app/components/page/session/login_component.rb
@@ -4,9 +4,10 @@ module Page
   module Session
     # Renders the log in page
     class LoginComponent < ViewComponent::Base
-      def initialize(login_path)
+      def initialize(idme_login_path, clear_login_path)
         super
-        @login_path = login_path
+        @login_path = idme_login_path
+        @clear_login_path = clear_login_path
       end
     end
   end
