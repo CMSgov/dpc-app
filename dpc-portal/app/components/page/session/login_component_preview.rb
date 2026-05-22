3c1dc64251522c4f4835d4f1fc8dec2538f8b8ef
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2026-05-22 15:43:47 -0400

    CSP cookie saved on successful login.  Login screen reads cookie.  Login screen button wrapper for last used CSP.

--- dpc-portal/app/components/page/session/login_component_preview.rb
+++ dpc-portal/app/components/page/session/login_component_preview.rb
@@ -4,8 +4,12 @@ module Page
   module Session
     # Previews the log in page
     class LoginComponentPreview < ViewComponent::Preview
-      def default
-        render(Page::Session::LoginComponent.new(root_path))
+      # @param last_used_csp select { choices: { "None": "", "CLEAR": "clear", "ID.me": "id_me","Login.gov": "login_dot_gov" } }
+      def default(last_used_csp: nil)
+        # Make sure if the user selects "None" that the value passed is actually nil and not "".
+        csp_value = last_used_csp.presence
+
+        render(Page::Session::LoginComponent.new(root_path, last_used_csp: csp_value&.to_sym))
       end
     end
   end
