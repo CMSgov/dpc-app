1ab1094a8c134e531a6abb87627653a7c537c4b8
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2026-05-27 15:48:23 -0400

    Add primary to user_emails and model changes

--- dpc-portal/db/schema.rb
+++ dpc-portal/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_04_24_194005) do
+ActiveRecord::Schema[8.0].define(version: 2026_05_27_181236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -252,7 +252,9 @@ ActiveRecord::Schema[8.0].define(version: 2026_04_24_194005) do
     t.datetime "reactivated_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "primary", default: false, null: false
     t.index ["csp_user_id", "email"], name: "index_user_emails_on_csp_user_id_and_email", unique: true
+    t.index ["csp_user_id"], name: "index_unique_primary_email_per_csp_user", unique: true, where: "(\"primary\" = true)"
     t.index ["csp_user_id"], name: "index_user_emails_on_csp_user_id"
   end
 
