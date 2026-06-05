3c99799818df58c518963ec03aa88af2cbddb9f3
Julian Scott <julianscott@navapbc.com>
2026-06-05 14:19:35 -0700

    Updating SOPS workflow

--- ops/services/10-config/variables.tf
+++ ops/services/10-config/variables.tf
@@ -3,3 +3,12 @@ variable "create_local_sops_wrapper" {
   description = "When `true`, creates sops wrapper file at `bin/sopsw`."
   type        = bool
 }
+
+variable "env" {
+  description = "The application environment (dev, test, sandbox, prod)"
+  type        = string
+  validation {
+    condition     = contains(["dev", "test", "sandbox", "prod"], var.env)
+    error_message = "Valid value for env is dev, test, sandbox, or prod."
+  }
+}
