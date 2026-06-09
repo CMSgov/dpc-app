fe729e14d6796f7115356a847cdb63a2c4eccf19
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2026-06-09 11:37:52 -0400

    Add DD tmp dir

--- dpc-attribution/docker/entrypoint.sh
+++ dpc-attribution/docker/entrypoint.sh
@@ -46,6 +46,9 @@ if [ -n "$SEED" ]; then
   eval java ${JAVA_CLASSES} seed ${CONF_FILE}
 fi
 
+# Make sure volumes in our persisted environments are writeable by nobody
+if [ -d "/tmp" ]; then chown nobody:nobody /tmp; fi
+
 echo "Running server via entrypoint as nobody!"
 # Note: -E preserves "most" env variables, but not all.  Ones deemed sensitive, like ENV need to be passed explicitly.
 exec sudo -E ENV=$ENV -u nobody ${CMDLINE} "$@" ${CONF_FILE}
