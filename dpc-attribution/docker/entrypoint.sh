cecce811b826a9a1a745ad16730a3a6923da9b29
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2026-06-09 12:14:39 -0400

    Fix DD tmp dir

--- dpc-attribution/docker/entrypoint.sh
+++ dpc-attribution/docker/entrypoint.sh
@@ -47,7 +47,7 @@ if [ -n "$SEED" ]; then
 fi
 
 # Make sure volumes in our persisted environments are writeable by nobody
-if [ -d "/tmp/ddprof_nobody" ]; then chown nobody:nobody /tmp; fi
+if [ -d "/tmp/ddprof_nobody" ]; then chown nobody:nobody /tmp/ddprof_nobody; fi
 
 echo "Running server via entrypoint as nobody!"
 # Note: -E preserves "most" env variables, but not all.  Ones deemed sensitive, like ENV need to be passed explicitly.
