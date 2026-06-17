d13182a684d103a3d0d31ceb10e81388386e7d43
Ashley Weaver <ashleyweaver@navapbc.com>
2026-06-17 11:26:01 -0400

    Move CSP rows to seeds file

--- dpc-portal/docker/entrypoint.sh
+++ dpc-portal/docker/entrypoint.sh
@@ -16,7 +16,7 @@ if [ "$1" == "portal" ]; then
   echo "Starting Rails server..."
 
   echo "Migrating the database..."
-  bundle exec rails db:migrate
+  bundle exec rails db:migrate db:seed
 
   if [[ "$ENV" == "production" ]]; then
     echo "Starting in production"
