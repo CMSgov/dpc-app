c9567ab7a9707d8f80ba0e46d15c8427a8baf81f
Luke Short <lukeshortemail@gmail.com>
2026-07-10 15:49:43 -0700

    rubocop

--- dpc-portal/spec/support/login_support.rb
+++ dpc-portal/spec/support/login_support.rb
@@ -6,7 +6,7 @@ module LoginSupport
   CSP_MAP = {
     login_dot_gov: 'Login.gov',
     id_me: 'ID.me',
-    clear: 'CLEAR',
+    clear: 'CLEAR'
   }.freeze
 
   def create_user_with_csp(csp:, given_name: 'John', family_name: 'Smith', uuid: SecureRandom.uuid, **user_attrs)
