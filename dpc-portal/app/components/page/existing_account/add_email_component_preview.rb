39cff1318021a79b50d3e11b344e19929028b329
Ashley Weaver <ashleyweaver@navapbc.com>
2026-07-30 09:27:24 -0400

    Update previews

--- dpc-portal/app/components/page/existing_account/add_email_component_preview.rb
+++ dpc-portal/app/components/page/existing_account/add_email_component_preview.rb
@@ -5,7 +5,7 @@ module Page
     # Render the screen to add a new email to an existing account.
     class AddEmailComponentPreview < ViewComponent::Preview
       def default
-        render(Page::MergeAccounts::OrganizationMergeComponent.new('bob@example.com', :login_dot_gov))
+        render(Page::ExistingAccount::AddEmailComponent.new('bob@example.com', :login_dot_gov))
       end
     end
   end
