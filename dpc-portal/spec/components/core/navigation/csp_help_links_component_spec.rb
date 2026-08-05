9e82bd62c2bf9d3c7513614e01626e4f635e4851
Becky Andresen <288941429+andresen-nava@users.noreply.github.com>
2026-08-05 11:42:49 -0500

    test that correct support links are rendered

---
+++ dpc-portal/spec/components/core/navigation/csp_help_links_component_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Core::Navigation::CspHelpLinksComponent, type: :component do
+    let(:component) { described_class.new }
+    it 'renders Login.gov help link' do
+      render_inline(component)
+      expect(page).to have_link('Login.gov Help Center', href: 'https://www.login.gov/help')
+    end
+
+    it 'renders CLEAR support link' do
+      render_inline(component)
+      expect(page).to have_link('CLEAR Support', href: 'https://www.clearme.com/support')
+    end
+
+    it 'renders ID.me help link' do
+      render_inline(component)
+      expect(page).to have_link('ID.me Help Center', href: 'https://help.id.me/hc/en-us')
+    end
+end
