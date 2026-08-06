2d2c481470fa80c9d15f80e3314e2eea5ebfcb49
MEspositoE14s <133133846+MEspositoE14s@users.noreply.github.com>
2026-08-06 15:54:41 -0400

    Turn on force_ssl for prod env

--- dpc-portal/config/environments/production.rb
+++ dpc-portal/config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   ## Disables log coloration
   config.colorize_logging = false
