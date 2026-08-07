e7479e168b0ab9bfbe5086a322cc16e7a01e6644
andresen-nava <288941429+andresen-nava@users.noreply.github.com>
2026-08-07 14:02:17 -0500

    DPC-5576 Add links to CSP support pages and simplify text (#3092)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-5576
    
    ## 🛠 Changes
    
    * add links to the CSP support pages
    * make language more generic
    * remove empty `<p></p>` elements from the registration page
    * empty paragraph elements may be announced by screen readers, so it is
    a best practice to remove them
    
    ## ℹ️ Context
    
    * The sign in pages need links to the CSP support pages so that users
    can quickly find support for each CSP.
    * Some of the language only referred to one CSP, so it was update to be
    applicable to all sign-in options.
    
    ## 🧪 Validation
    * Follow the links to each CSP support page
    
    Sign In Page
    <img width="1920" height="1258" alt="signin"
    src="https://github.com/user-attachments/assets/75caec40-18d4-42d9-a45c-9c3c944251b9"
    />
    
    Registration Page
    <img width="1920" height="1772" alt="registration"
    src="https://github.com/user-attachments/assets/ee74d349-ee91-4019-a4d4-d76081eed4e6"
    />

---
+++ dpc-portal/app/components/core/navigation/csp_help_links_component.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Core
+  module Navigation
+    # Render links to the CSP support/help center pages
+    class CspHelpLinksComponent < ViewComponent::Base
+    end
+  end
+end
