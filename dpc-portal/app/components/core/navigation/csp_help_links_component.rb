db8e1af2057242c160a66e9f01a3f4b8ece76375
Becky Andresen <288941429+andresen-nava@users.noreply.github.com>
2026-08-07 09:42:59 -0500

    rubocop updates

--- dpc-portal/app/components/core/navigation/csp_help_links_component.rb
+++ dpc-portal/app/components/core/navigation/csp_help_links_component.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
-class Core::Navigation::CspHelpLinksComponent < ViewComponent::Base
+module Core
+  module Navigation
+    # Render links to the CSP support/help center pages
+    class CspHelpLinksComponent < ViewComponent::Base
+    end
+  end
 end
