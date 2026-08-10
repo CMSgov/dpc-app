a951144e59dbefe118aac61d16605c9422e3ba7f
Luke Short <lukeshortemail@gmail.com>
2026-08-10 13:15:52 -0700

    fix application_controller helper not working in lookbook

--- dpc-portal/app/helpers/application_helper.rb
+++ dpc-portal/app/helpers/application_helper.rb
@@ -2,6 +2,12 @@
 
 # Utility methods for views
 module ApplicationHelper
+  def current_user
+    return controller.current_user if controller.respond_to?(:current_user)
+
+    nil # required for lookbook
+  end
+
   def omniauth_authorize_path(service)
     "/auth/#{service}"
   end
