5e2f64f1c0c5021e07ef6709799f964641b81818
Luke Short <lukeshortemail@gmail.com>
2026-08-11 10:20:56 -0700

    fix preview component broken in UserEmail migration

--- dpc-portal/app/components/page/utility/error_component_preview.rb
+++ dpc-portal/app/components/page/utility/error_component_preview.rb
@@ -20,7 +20,11 @@ module Page
 
       # @param csp select :csp_codes
       def pii_mismatch(csp: DEFAULT_CSP)
-        user = User.new(email: 'bilbo.baggins@cms.hms.gov')
+        user = User.new
+        # workaround to avoid having to save User for preview
+        user.define_singleton_method(:email) do
+          'bilbo.baggins@cms.hms.gov'
+        end
         invitation = Invitation.new(provider_organization: ProviderOrganization.new(name: ORG_NAME),
                                     invited_by: user)
         reason = 'pii_mismatch'
@@ -48,7 +52,11 @@ module Page
       end
 
       def cd_expired
-        user = User.new(email: 'bilbo.baggins@cms.hms.gov')
+        user = User.new
+        # workaround to avoid having to save User for preview
+        user.define_singleton_method(:email) do
+          'bilbo.baggins@cms.hms.gov'
+        end
         invitation = Invitation.new(id: 6, provider_organization: ProviderOrganization.new(id: 1, name: ORG_NAME),
                                     invited_by: user, invitation_type: :credential_delegate, created_at: 49.hours.ago)
         reason = 'cd_expired'
