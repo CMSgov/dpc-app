aa9e5334b547b2e1fe06c4ec25af6b307694b64f
Becky Andresen <288941429+andresen-nava@users.noreply.github.com>
2026-09-08 16:29:46 -0500

    remove fail to proof text since it is not used anymore

--- dpc-portal/app/components/page/utility/error_component_preview.rb
+++ dpc-portal/app/components/page/utility/error_component_preview.rb
@@ -114,7 +114,7 @@ module Page
 
       def error_codes
         { choices: %i[user_not_authorized_official no_approved_enrollment bad_npi org_med_sanctions ao_med_sanctions
-                      missing_info server_error fail_to_proof] }
+                      missing_info server_error] }
       end
     end
   end
