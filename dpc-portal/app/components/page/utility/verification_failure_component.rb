b610f3822af3b81ff8aabeefd30626920c58e569
Becky Andresen <288941429+andresen-nava@users.noreply.github.com>
2026-09-17 11:20:26 -0500

    add link to sign in page

--- dpc-portal/app/components/page/utility/verification_failure_component.rb
+++ dpc-portal/app/components/page/utility/verification_failure_component.rb
@@ -16,6 +16,18 @@ module Page
         @current_csp = csp&.to_sym
         @csp_display_name = DISPLAY_NAMES.fetch(csp&.to_sym, 'CSP')
       end
+
+      def sign_in_destination
+        if @invitation.nil?
+          sign_in_path
+        elsif @invitation.credential_delegate?
+          confirm_cd_organization_invitation_path(@invitation.provider_organization_id,
+                                                  @invitation.id,
+                                                  @invitation.token)
+        else
+          accept_organization_invitation_path(@invitation.provider_organization_id, @invitation.id, @invitation.token)
+        end
+      end
     end
   end
 end
