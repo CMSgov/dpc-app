a6af61d1ebae51c5f6ce0ce95444cfdbc75aecdb
Luke Short <lukeshortemail@gmail.com>
2026-09-18 18:17:42 -0700

    update methods to parse json blob for test data

--- dpc-portal/app/jobs/verify_cpi_api_gw_job.rb
+++ dpc-portal/app/jobs/verify_cpi_api_gw_job.rb
@@ -5,10 +5,12 @@ class VerifyCpiApiGwJob < ApplicationJob
   queue_as :portal
 
   def perform
+    # CPI GW overrides so this job can use IPML and rest of portal can use fake_cpi_gateway.rb
+    cpi_client_config = get_configuration
+    cpi_api_gw_client = CpiApiGatewayClient.new(cpi_client_config["OAUTH_URL"], cpi_client_config["BASE_URL"])
+
     # let service handle API GW credentials and connection
     # this is what's used by both verify_ao_job and invitations already.
-    cpi_client_config = cpi_api_gateway_client_options
-    cpi_api_gw_client = CpiApiGatewayClient.new(cpi_client_config["OAUTH_URL"], cpi_client_config["BASE_URL"])
     service = AoVerificationService.new(cpi_api_gw_client:)
     test_data = get_test_data
 
@@ -21,6 +23,8 @@ class VerifyCpiApiGwJob < ApplicationJob
 
     # add log that cpi_gateway_results.length organizations processed
     trigger_alarm unless cpi_gateway_results.all?
+  rescue JSON::ParserError, KeyError => e
+    log_failure(e)
   rescue StandardError => e
     log_failure(e)  # if we reach this, alarm should be caught by higher level checks (ie. unexpected errors check)
     raise
@@ -114,13 +118,18 @@ class VerifyCpiApiGwJob < ApplicationJob
   # TODO update methods to actually retrieve from ssm
   def get_test_data
     # retrieve from /dpc/test/web-portal/cpi_api_gw_testdata etc
-    return ["data"]
+    # includes 4 specific test scenarios set up by the CPI API GW team
+    testdata_object = JSON.parse(ENV.fetch('CPI_API_GW_TESTDATA'))
+    return testdata_object.fetch("data")
   end
 
   def get_configuration
     # retrieve from /dpc/test/web-portal/cpi_api_gw_testdata etc
     # includes "BASE_URL" and "OAUTH_URL"
     # "TOKEN_ENDPOINT" should be same as what's already used
-    return ["meta"]["configuration"]["test"]
+    testdata_object = JSON.parse(ENV.fetch('CPI_API_GW_TESTDATA'))
+
+    # might need to change "test" to ENV.fetch('ENV')
+    return testdata_object.fetch("meta").fetch("configuration").fetch("test")
   end
 end
