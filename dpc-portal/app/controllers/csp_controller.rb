c04cf9ababf03c8ce121d2b4677984389ea36811
andresen-nava <288941429+andresen-nava@users.noreply.github.com>
2026-09-21 11:23:45 -0500

    DPC-5663 add verification failure page (#3147)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/DPC-5663
    
    ## 🛠 Changes
    
    These changes display a verification error page when a user fails to
    verify their identity during sign in.
    
    ## ℹ️ Context
    
    A custom error page with help links is displayed for users that fail
    identity verification.
    
    ## 🧪 Validation
    * Added automated tests
    * Added lookbook preview
    * Tested manually from the main sign in and invitation flows

--- dpc-portal/app/controllers/csp_controller.rb
+++ dpc-portal/app/controllers/csp_controller.rb
@@ -28,6 +28,7 @@ class CspController < ApplicationController
     invitation_match = session[:user_return_to]&.match(%r{/invitations/([0-9]+)/([a-zA-Z0-9]{24})})
     invitation = invitation_match ? Invitation.find_by(id: invitation_match[1], token: invitation_match[2]) : nil
 
+    return handle_fail_to_proof(invitation) if csp_user_fail_to_proof?
     return handle_csp_auth_error(invitation) if csp_auth_error?
     return handle_signin_cancel(invitation) if csp_user_cancelled?
 
@@ -35,15 +36,21 @@ class CspController < ApplicationController
   end
 
   def logout
-    store_invitation_return_url if params[:invitation_id].present?
+    store_invitation_return_url if params[:invitation_id].present? || params[:invitation_token].present?
 
-    redirect_to url_for_logout(csp_session.current), allow_other_host: true
+    current_csp = csp_session.current || params[:current_csp]
+    redirect_to url_for_logout(current_csp), allow_other_host: true
   end
 
   private
 
   def store_invitation_return_url
-    invitation = Invitation.find(params[:invitation_id])
+    invitation = if params[:invitation_id].present?
+                   Invitation.find(params[:invitation_id])
+                 else
+                   Invitation.find_by(token: params[:invitation_token])
+                 end
+
     session[:user_return_to] = organization_invitation_url(invitation.provider_organization.id,
                                                            invitation.id,
                                                            invitation.token)
